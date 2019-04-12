1c39bf6ef53f5ef8e06f831a049af364d97f6bda
James Sharkey <jsharkey13@users.noreply.github.com>
2019-04-12 18:32:28 +0100

    Attempt to use the correct API when running on an origin
    # This might not work!

--- src/app/services/constants.tsx
+++ src/app/services/constants.tsx
@@ -1,5 +1,16 @@
 import Remarkable from "remarkable";
 
-export const API_VERSION: string = process.env.REACT_APP_API_VERSION || 'any';
-export const API_PATH: string = "http://localhost:8080/isaac-api/api";
+export const API_VERSION: string = process.env.REACT_APP_API_VERSION || "any";
+
+/*
+ * Configure the api provider with the server running the API:
+ * No need if we want to use the same server as the static content.
+ */
+let apiPath: string = `${document.location.origin}/api/${API_VERSION}/api`;
+if (document.location.hostname === "localhost") {
+
+    apiPath = "http://localhost:8080/isaac-api/api";
+
+}
+export const API_PATH: string = apiPath;
 export const MARKDOWN_RENDERER: Remarkable = new Remarkable();
