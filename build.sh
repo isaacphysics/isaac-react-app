f4729d78cd94de1fb49ddd3a4d8c76d55b9933c1
Ian Davies <git@iandavies.org>
2019-04-12 13:34:18 +0100

    Tweak build scripts.

--- build.sh
+++ build.sh
@@ -3,14 +3,15 @@
 set -e # Exit on failure
 
 if test -z "$1"; then
-  read -p "isaac-cs-app version to build (e.g. v1.3.0 or 'master'):" VERSION_TO_DEPLOY
+  read -p "isaac-cs-app version to build (e.g. v1.3.0 or ['master']): " VERSION_TO_DEPLOY
+  VERSION_TO_DEPLOY=${VERSION_TO_DEPLOY:-master}
 else
   VERSION_TO_DEPLOY="$1"
 fi
 
 BUILD_DIR=/tmp/isaacCSDeploy
 
-echo Building Isaac CS in $BUILD_DIR
+echo Building Isaac CS in $BUILD_DIR: $VERSION_TO_DEPLOY
 
 rm -rf $BUILD_DIR
 mkdir -p $BUILD_DIR
@@ -26,7 +27,7 @@ if [[ $VERSION_TO_DEPLOY == v* ]]; then
 else
     # Change the app src to request the API from a particular branch
     if test -z "$2"; then
-          read -p "Override API version to target [$VERSION_TO_DEPLOY]" SEGUE_VERSION
+          read -p "Override API version to target [$VERSION_TO_DEPLOY]: " SEGUE_VERSION
     else
           SEGUE_VERSION=$2
     fi
@@ -35,20 +36,18 @@ else
     echo "REACT_APP_API_VERSION=" $SEGUE_VERSION >> .env
 fi
 
-npm install &&
-npm run build &&
-docker build -t "docker.isaacscience.org/isaac-cs-app:${VERSION_TO_DEPLOY,,}" --build-arg API_VERSION=$SEGUE_VERSION . &&
-docker push "docker.isaacscience.org/isaac-cs-app:${VERSION_TO_DEPLOY,,}" ||
-exit 1
+npm install
+npm run build
+docker build -t "docker.isaacscience.org/isaac-cs-app:${VERSION_TO_DEPLOY}" --build-arg API_VERSION=$SEGUE_VERSION .
+docker push "docker.isaacscience.org/isaac-cs-app:${VERSION_TO_DEPLOY}"
 
 cd ..
 rm -rf isaac-cs-app
 
-git clone -b $SEGUE_VERSION --depth 1 https://github.com/isaacphysics/isaac-api.git &&
-cd isaac-api &&
-docker build -t "docker.isaacscience.org/isaac-api:$SEGUE_VERSION" . &&
-docker push "docker.isaacscience.org/isaac-api:$SEGUE_VERSION" ||
-exit 1
+git clone -b $SEGUE_VERSION --depth 1 https://github.com/isaacphysics/isaac-api.git
+cd isaac-api
+docker build -t "docker.isaacscience.org/isaac-api:$SEGUE_VERSION" .
+docker push "docker.isaacscience.org/isaac-api:$SEGUE_VERSION"
 
 cd ..
 rm -rf isaac-api
--- src/app/services/constants.tsx
+++ src/app/services/constants.tsx
@@ -1,4 +1,5 @@
 import Remarkable from "remarkable";
 
+export const API_VERSION: string = process.env.REACT_APP_API_VERSION || 'any';
 export const API_PATH: string = "http://localhost:8080/isaac-api/api";
 export const MARKDOWN_RENDERER: Remarkable = new Remarkable();
