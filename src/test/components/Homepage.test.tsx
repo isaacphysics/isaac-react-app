37c67333f2e693a1d413ec56a6c22f2bda514d9f
Meurig Thomas <mlt47@cam.ac.uk>
2019-04-30 15:53:19 +0100

    Fix tests and tidy up a bit of the homepage

--- src/test/components/Homepage.test.tsx
+++ src/test/components/Homepage.test.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import {shallow} from 'enzyme';
-import {HomePageComponent} from "../../app/components/pages/Homepage";
+import {HomepageComponent} from "../../app/components/pages/Homepage";
 import {RegisteredUserDTO} from "../../IsaacApiTypes";
 
-describe('HomePageComponent', () => {
+describe('HomepageComponent', () => {
 
     it('renders correctly with a null valued user', () => {
-        const component = shallow(<HomePageComponent user={null} />);
+        const component = shallow(<HomepageComponent user={null} />);
         expect(component).toMatchSnapshot();
     });
 
     it("prints out the user's name when it is provided", () => {
         const user: RegisteredUserDTO = {givenName: "Alan"};
-        const component = shallow(<HomePageComponent user={user} />);
+        const component = shallow(<HomepageComponent user={user} />);
         expect(component).toMatchSnapshot();
     })
 
