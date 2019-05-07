8b35d6bec1a9bad62a9da8634c2169b52a24cc71
Ben Hanson <bh412@cam.ac.uk>
2019-05-07 13:35:26 +0100

    Removes unused useEffect

--- src/app/components/pages/LogIn.tsx
+++ src/app/components/pages/LogIn.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import {connect} from "react-redux";
 import {Link} from "react-router-dom";
 import {Button, Card, CardBody, Col, Form, FormGroup, Input, Row, Label} from "reactstrap";
