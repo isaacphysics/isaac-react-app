277ff66eaf145ff385328ae17ea26517796ea8f1
Robin Message <robin@lambdacambridge.com>
2019-05-15 11:57:35 +0100

    Fix router tracking to not re-create HOC inside render function

--- src/app/components/navigation/TrackedRoute.tsx
+++ src/app/components/navigation/TrackedRoute.tsx
@@ -1,11 +1,34 @@
-import React from "react";
-import {Route, RouteProps} from "react-router";
-import {withTracker} from "./withTracker";
+import React, {useEffect} from "react";
+import {Route, RouteComponentProps, RouteProps} from "react-router";
+import ReactGA, {FieldsObject} from "react-ga";
 
-export const TrackedRoute = function(props: RouteProps) {
-    if (props.component) {
-        return <Route {...props} component={withTracker(props.component)} />;
+ReactGA.initialize("UA-137475074-1");
+ReactGA.set({ anonymizeIp: true });
+
+const trackPage = (page: string, options?: FieldsObject) => {
+    ReactGA.set({ page, ...options });
+    ReactGA.pageview(page);
+};
+
+type TrackedRouteProps = RouteProps & {trackingOptions?: FieldsObject};
+type TrackedRouteComponentProps = RouteComponentProps & {
+    component: React.ComponentType<RouteComponentProps<any>> | React.ComponentType<any>;
+    trackingOptions?: FieldsObject;
+};
+
+const WrapperComponent = function({component: Component, trackingOptions, ...props}: TrackedRouteComponentProps) {
+    useEffect(() => {
+        trackPage(props.location.pathname, trackingOptions);
+    }, [props.location.pathname, trackingOptions]);
+    return <Component {...props} />;
+};
+
+export const TrackedRoute = function({component, trackingOptions, ...rest}: TrackedRouteProps) {
+    if (component) {
+        return <Route {...rest} render={props => {
+            return <WrapperComponent component={component} trackingOptions={trackingOptions} {...props} />;
+        }} />;
     } else {
-        throw new Error("TrackedRoute only works on components, got: " + JSON.stringify(props));
+        throw new Error("TrackedRoute only works on components, got: " + JSON.stringify(rest));
     }
 };
