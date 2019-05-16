26fc78b4ba704b39469f280cde2327a22603712b
Ben Hanson <bh412@cam.ac.uk>
2019-05-16 15:04:29 +0100

    registration page functionality

--- src/app/components/pages/Registration.tsx
+++ src/app/components/pages/Registration.tsx
@@ -1,15 +1,81 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {connect} from "react-redux";
 import {Link} from "react-router-dom";
-import {Button, Card, CardBody, CardTitle, Col, CustomInput, Form, FormGroup, Input, Row, Label} from "reactstrap";
+import {
+    Button,
+    Card,
+    CardBody,
+    CardTitle,
+    Col,
+    CustomInput,
+    Form,
+    FormGroup,
+    Input,
+    Row,
+    Label,
+    FormFeedback
+} from "reactstrap";
+import {RegisteredUserDTO} from "../../../IsaacApiTypes";
+import {AppState} from "../../state/reducers";
+import {updateCurrentUser} from "../../state/actions";
 
-const stateToProps = null;
-const dispatchToProps = null;
+const stateToProps = (state: AppState) => ({
+    user: state ? state.user : null,
+    errorMessage: state ? state.error : null
+});
+const dispatchToProps = {
+    updateCurrentUser
+};
 
-interface LogInPageProps {
+interface validationUser extends RegisteredUserDTO {
+    password: string | null
 }
-const RegistrationPageComponent = (props: LogInPageProps) => {
-    const register = () => console.log("Registration attempt"); // TODO MT registration action
+
+interface RegistrationPageProps {
+    user: RegisteredUserDTO | null
+    updateCurrentUser: (
+        params: {registeredUser: validationUser; passwordCurrent: string},
+        currentUser: RegisteredUserDTO
+    ) => void
+    errorMessage: string | null
+}
+
+const RegistrationPageComponent = ({user, updateCurrentUser, errorMessage}:  RegistrationPageProps) => {
+    const register = (event: React.FormEvent<HTMLFontElement>) => {
+        event.preventDefault();
+        console.log("Registration attempt")
+    }; // TODO MT registration action
+
+    const emailPreferences = {"NEWS_AND_UPDATES": true, "ASSIGNMENTS": true, "EVENTS": true};
+
+    const tempUser = {};
+    const myUser1 = Object.assign(tempUser, user);
+    const myUser = Object.assign(myUser1, {password: ""});
+    const [isValidEmail, setValidEmail] = useState(true);
+    const [isValidDob, setValidDob] = useState(true);
+    const [isValidPassword, setValidPassword] = useState(false);
+    const [currentPassword, setCurrentPassword] = useState("");
+
+    let today = new Date();
+    let thirteen_years_ago = Date.UTC(today.getFullYear() - 13, today.getMonth(), today.getDate())/1000;
+
+
+    const validateAndSetEmail = (event: any) => {
+        setValidEmail((event.target.value.length > 0 && event.target.value.includes("@")));
+    };
+
+    const validateAndSetDob = (event: any) => {
+        setValidDob((myUser.dateOfBirth != undefined) &&
+            ((new Date(String(event.target.value)).getTime()/1000) <= thirteen_years_ago))
+    };
+
+    const validateAndSetPassword = (event: any) => {
+        setValidPassword(
+            (event.target.value == (document.getElementById("password") as HTMLInputElement).value) &&
+            ((document.getElementById("password") as HTMLInputElement).value != undefined) &&
+            ((document.getElementById("password") as HTMLInputElement).value.length > 5)
+        )
+    };
 
     return <div id="registration-page">
         <h1>Register</h1>
@@ -18,32 +84,40 @@ const RegistrationPageComponent = (props: LogInPageProps) => {
                 <CardTitle tag="h2">
                     <small className="text-muted">Sign up to {" "} <Link to="/">Isaac</Link></small>
                 </CardTitle>
-                <Form name="register" onSubmit={register}>
+                <Form name="register" onSubmit={register} noValidate>
                     <Row>
                         <Col size={12} md={6}>
                             <FormGroup>
                                 <Label htmlFor="first-name-input">First Name</Label>
-                                <Input id="first-name-input" type="text" name="first-name" required />
+                                <Input id="first-name-input" type="text" name="givenName"
+                                       onBlur={(e: any) => {myUser.givenName = e.target.value}}
+                                       required/>
                             </FormGroup>
                         </Col>
                         <Col size={12} md={6}>
                             <FormGroup>
                                 <Label htmlFor="last-name-input">Last Name</Label>
-                                <Input id="last-name-input" type="text" name="last-name" required />
+                                <Input id="last-name-input" type="text" name="familyName"
+                                       onBlur={(e: any) => {myUser.familyName = e.target.value}}
+                                       required/>
                             </FormGroup>
                         </Col>
                     </Row>
                     <Row>
                         <Col size={12} md={6}>
                             <FormGroup>
-                                <Label htmlFor="password-input">Password</Label>
-                                <Input id="password-input" type="password" name="password" required />
+                                <Label htmlFor="password-input">New Password</Label>
+                                <Input id="password" type="password" name="password" required/>
                             </FormGroup>
                         </Col>
                         <Col size={12} md={6}>
                             <FormGroup>
-                                <Label htmlFor="password-input">Confirm Password</Label>
-                                <Input id="password-input" type="password" name="password" required />
+                                <Label htmlFor="password-confirm">Re-enter New Password</Label>
+                                <Input invalid={!isValidPassword} id="password-confirm" type="password" name="password" onBlur={(e: any) => {
+                                    validateAndSetPassword(e);
+                                    (e.target.value == (document.getElementById("password") as HTMLInputElement).value) ? setCurrentPassword(e.target.value) : null}
+                                } aria-describedby="invalidPassword" required/>
+                                <FormFeedback id="invalidPassword">{(!isValidPassword) ? "Passwords must match and be at least 6 characters long" : null}</FormFeedback>
                             </FormGroup>
                         </Col>
                     </Row>
@@ -51,22 +125,40 @@ const RegistrationPageComponent = (props: LogInPageProps) => {
                         <Col size={12} md={6}>
                             <FormGroup>
                                 <Label htmlFor="email-input">Email</Label>
-                                <Input id="email-input" type="email" name="email" required />
+                                <Input invalid={!isValidEmail} id="email-input" type="email"
+                                       name="email" defaultValue={"user email from login screen"}
+                                       onBlur={(e: any) => {
+                                           validateAndSetEmail(e);
+                                           (isValidEmail) ? myUser.email = e.target.value : null
+                                       }}
+                                       aria-describedby="emailValidationMessage" required/>
+                                <FormFeedback id="emailValidationMessage">
+                                    {(!isValidEmail) ? "Enter a valid email address" : null}
+                                </FormFeedback>
                             </FormGroup>
                         </Col>
                         <Col size={12} md={6}>
                             <FormGroup>
                                 <Label htmlFor="dob-input">Date of Birth</Label>
                                 <Row>
-                                    <Col size={12} lg={5}>
+                                    <Col size={12} lg={6}>
                                         <Input
+                                            invalid={!isValidDob}
                                             id="dob-input"
                                             type="date"
                                             name="date-of-birth"
-                                            placeholder="date placeholder"
+                                            defaultValue={myUser.dateOfBirth}
+                                            onBlur={(e: any) => {
+                                                validateAndSetDob;
+                                                (isValidDob) ? myUser.dateOfBirth = e.target.value : null
+                                            }}
+                                            aria-describedby ="ageValidationMessage"
                                         />
+                                        <FormFeedback id="ageValidationMessage">
+                                            {(!isValidDob) ? "You must be over 13 years old" : null}
+                                        </FormFeedback>
                                     </Col>
-                                    <Col size={12} lg={7}>
+                                    <Col size={12} lg={5}>
                                         <CustomInput
                                             id="age-confirmation-input"
                                             type="checkbox"
