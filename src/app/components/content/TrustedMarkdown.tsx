cf258ec9fdf1aa618495b1fe67daee1617c70f6c
Meurig Thomas <mlt47@cam.ac.uk>
2019-05-20 21:20:40 +0100

    Add RegEx replacements to match Isaac Physics' LaTeX inspired functionality

--- src/app/components/content/TrustedMarkdown.tsx
+++ src/app/components/content/TrustedMarkdown.tsx
@@ -3,40 +3,18 @@ import {MARKDOWN_RENDERER} from "../../services/constants";
 import {TrustedHtml} from "./TrustedHtml";
 
 export const TrustedMarkdown = ({markdown}: {markdown: string}) => {
+    // RegEx replacements to match Latex inspired Isaac Physics functionality
+    const regexRules = {
+        "<span isaac-figure-ref='$2'></span>": /(~D)?\\ref{([^}]*)}(~D)?/g,
+        "[$1]($2)": /\\link{([^}]*)}{([^}]*)}/g,
+        "[**Glossary**](/glossary)": /\*\*Glossary\*\*/g,
+        "[**Concepts**](/concepts)": /\*\*Concepts\*\*/g,
+    };
+    let regexProcessedMarkdown = markdown;
+    Object.entries(regexRules).forEach(([replacement, rule]) =>
+        regexProcessedMarkdown = regexProcessedMarkdown.replace(rule, replacement)
+    );
 
-    // TODO MT add these regex replaces
-    // Showdown.extensions.refs = function(_converter) {
-    //     return [{
-    //         type: "lang",
-    //         regex: '(~D)?\\\\ref{([^}]*)}(~D)?',
-    //         replace: '<span isaac-figure-ref="$2"></span>',
-    //     }];
-    // };
-    //
-    // Showdown.extensions.links = function(_converter) {
-    //     return [{
-    //         type: "lang",
-    //         regex: '\\\\link{([^}]*)}{([^}]*)}',
-    //         replace: '[$1]($2)',
-    //     }]
-    // };
-    //
-    // Showdown.extensions.glossary = function(_converter) {
-    //     return [{
-    //         type: "lang",
-    //         regex: '\\*\\*Glossary\\*\\*',
-    //         replace: '[**Glossary**](/glossary)',
-    //     }];
-    // };
-    //
-    // Showdown.extensions.concepts = function(_converter) {
-    //     return [{
-    //         type: "lang",
-    //         regex: '\\*\\*Concepts\\*\\*',
-    //         replace: '[**Concepts**](/concepts)',
-    //     }];
-    // };
-
-    const html = MARKDOWN_RENDERER.render(markdown);
+    const html = MARKDOWN_RENDERER.render(regexProcessedMarkdown);
     return <TrustedHtml html={html} />;
 };
