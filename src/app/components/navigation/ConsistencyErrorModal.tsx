60bdc400f9e6b0300f9b68c0b67dbd3f601c7eba
Meurig Thomas <mlt47@cam.ac.uk>
2019-05-21 16:27:48 +0100

    Some style changes to user consistency warning modal

--- src/app/components/navigation/ConsistencyErrorModal.tsx
+++ src/app/components/navigation/ConsistencyErrorModal.tsx
@@ -3,14 +3,21 @@ import * as RS from "reactstrap";
 
 export const ConsistencyErrorModal = ({consistencyError}: {consistencyError: boolean}) => {
     return <RS.Modal isOpen={consistencyError}>
-        <RS.ModalHeader>Your Isaac Computer Science session has changed</RS.ModalHeader>
-        <RS.ModalBody>
+        <RS.ModalHeader className="h-title pb-5 mb-4">Your Isaac Computer Science session has changed</RS.ModalHeader>
+        <RS.ModalBody className="px-0 mx-4">
             <h3>This browser window / tab is out of sync.</h3>
             <p className="pb-2">This can happen if you have logged out or logged in as another user via another browser window. Please click continue to avoid any issues.</p>
-            <p><em>Note:</em> If you would like to be logged in with two accounts at one time you will need to use your browser&apos;s <a href="https://en.wikipedia.org/wiki/Privacy_mode">private browsing</a> feature.</p>
+            <p>
+                <em>Note:</em>
+                {" If you would like to be logged in with two accounts at one time you will need to use your browser's "}
+                <a href="https://en.wikipedia.org/wiki/Privacy_mode" target="_blank" rel="noopener noreferrer">
+                    private browsing
+                </a>
+                {" feature."}
+            </p>
         </RS.ModalBody>
-        <RS.ModalFooter>
-            <RS.Button color="primary" href="/login">Continue</RS.Button>
+        <RS.ModalFooter className="mb-4 align-self-center">
+            <RS.Button color="secondary" href="/login">Continue</RS.Button>
         </RS.ModalFooter>
     </RS.Modal>;
 };
