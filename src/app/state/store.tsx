04f9d6d686de043594086550541010d3f5ea96dc
Meurig Thomas <mlt47@cam.ac.uk>
2019-05-21 16:40:00 +0100

    Alter redux logger to ignore user consistency check actions

--- src/app/state/store.tsx
+++ src/app/state/store.tsx
@@ -3,6 +3,7 @@ import thunk from "redux-thunk";
 import {createLogger} from "redux-logger";
 import {rootReducer} from "./reducers";
 import {userConsistencyCheckerMiddleware} from "./userConsistencyChecker";
+import {ACTION_TYPE} from "../services/constants";
 
 // @ts-ignore
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
@@ -10,7 +11,10 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const storeFactory = (initialState: object) => {
     const middleware: Middleware[] = [userConsistencyCheckerMiddleware, thunk];
     if (process.env.NODE_ENV !== 'production') {
-        middleware.push(createLogger())
+        const actionTypesToIgnore = [ACTION_TYPE.USER_CONSISTENCY_CHECK];
+        middleware.push(createLogger({
+            predicate: (getState, action) => !actionTypesToIgnore.includes(action.type)
+        }));
     }
 
     const enhancer = composeEnhancers(
