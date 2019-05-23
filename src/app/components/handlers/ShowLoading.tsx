0c8e0d1d09efbeef39b84260b243664aa4fc48a3
Meurig Thomas <mlt47@cam.ac.uk>
2019-05-24 00:15:47 +0100

    Jazz up the show loading page a little

--- src/app/components/handlers/ShowLoading.tsx
+++ src/app/components/handlers/ShowLoading.tsx
@@ -1,4 +1,5 @@
 import React, {ReactElement} from "react";
+import {Spinner} from "reactstrap";
 
 interface ShowLoadingProps {
     until: any;
@@ -6,6 +7,11 @@ interface ShowLoadingProps {
     placeholder?: ReactElement;
 }
 
-export const ShowLoading = ({until, children, placeholder = <h2>Loading...</h2>}: ShowLoadingProps) => {
+const defaultPlaceholder = <div className="w-100 text-center">
+    <h2 className="pt-5 pb-2">Loading...</h2>
+    <Spinner color="primary" />
+</div>;
+
+export const ShowLoading = ({until, children, placeholder = defaultPlaceholder}: ShowLoadingProps) => {
     return until ? children : placeholder;
 };
