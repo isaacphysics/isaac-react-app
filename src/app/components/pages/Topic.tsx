5283170a079506f5cdd58be367b6330556977d13
Meurig Thomas <mlt47@cam.ac.uk>
2019-05-24 00:17:30 +0100

    Filter topic content on exam board selection

--- src/app/components/pages/Topic.tsx
+++ src/app/components/pages/Topic.tsx
@@ -1,14 +1,15 @@
-import React, {useEffect} from "react"
+import React, {ChangeEvent, useEffect, useState} from "react"
 import {Link, withRouter} from "react-router-dom";
 import {connect} from "react-redux";
 import {AppState} from "../../state/reducers";
 import {fetchTopicDetails} from "../../state/actions";
 import {ShowLoading} from "../handlers/ShowLoading";
 import {IsaacContent} from "../content/IsaacContent";
-import {Button, Col, Container, Row} from "reactstrap";
+import {Button, Col, Container, Row, FormGroup, Label, Input} from "reactstrap";
 import {IsaacTopicSummaryPageDTO} from "../../../IsaacApiTypes";
-import {DOCUMENT_TYPE, TAG_ID} from "../../services/constants";
+import {DOCUMENT_TYPE, EXAM_BOARD, TAG_ID} from "../../services/constants";
 import {LinkToContentSummaryList} from "../elements/ContentSummaryListGroupItem";
+import {BreadcrumbTrail} from "../content/BreadcrumbTrail";
 
 const stateToProps = (state: AppState, {match: {params: {topicName}}}: any) => ({
     topicName: topicName,
@@ -26,25 +27,45 @@ const TopicPageComponent = ({topicName, topicPage, fetchTopicDetails}: TopicPage
         () => {fetchTopicDetails(topicName);},
         [topicName]
     );
-
-    const relatedConcepts = topicPage && topicPage.relatedContent &&
-        topicPage.relatedContent.filter(content => content.type === DOCUMENT_TYPE.CONCEPT);
-    const relatedQuestions = topicPage && topicPage.relatedContent &&
-        topicPage.relatedContent.filter(content => content.type === DOCUMENT_TYPE.QUESTION);
+    const [examBoardFilter, setExamBoardFilter] = useState(EXAM_BOARD.AQA);
+    const examBoardTagMap = {
+        AQA: "examboard_aqa",
+        OCR: "examboard_ocr",
+    };
+    const examBoardFilteredContent = topicPage && topicPage.relatedContent &&
+        topicPage.relatedContent.filter(content => content.tags && content.tags.includes(examBoardTagMap[examBoardFilter]));
+    const relatedConcepts = examBoardFilteredContent &&
+        examBoardFilteredContent.filter(content => content.type === DOCUMENT_TYPE.CONCEPT);
+    const relatedQuestions = examBoardFilteredContent &&
+        examBoardFilteredContent.filter(content => content.type === DOCUMENT_TYPE.QUESTION);
 
     return <ShowLoading until={topicPage}>
         {topicPage && <Container id="topic-page">
             <Row>
                 <Col>
-                    {/* TODO Breadcrumbs */}
+                    <BreadcrumbTrail currentPageTitle={topicPage.title} />
                     <h1 className="h-title">{topicPage.title}</h1>
                 </Col>
             </Row>
-            <Row>
+            <Row className="pb-5">
                 <Col md={{size: 8, offset: 2}} className="py-3">
                     {topicPage.children && topicPage.children.map((child, index) =>
                         <IsaacContent key={index} doc={child}/>)
                     }
+                    <div className="text-center">
+                        <Label className="d-inline-block pr-2" for="examBoardSelect">Exam Board</Label>
+                        <Input
+                            className="w-auto d-inline-block pl-1 pr-0"
+                            type="select"
+                            name="select"
+                            id="examBoardSelect"
+                            value={examBoardFilter}
+                            onChange={(event: ChangeEvent<HTMLInputElement>) => setExamBoardFilter(event.target.value as EXAM_BOARD)}
+                        >
+                            <option value={EXAM_BOARD.AQA}>{EXAM_BOARD.AQA}</option>
+                            <option value={EXAM_BOARD.OCR}>{EXAM_BOARD.OCR}</option>
+                        </Input>
+                    </div>
                     {relatedConcepts && <LinkToContentSummaryList items={relatedConcepts} className="my-4" />}
                     {relatedQuestions && <LinkToContentSummaryList items={relatedQuestions} className="my-4" />}
                     <Button tag={Link} to="/coming_soon" color="secondary" block>More coming soon&hellip;</Button>
