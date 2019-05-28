6cc4444eb4c2688c12513ae14c88e7f929ed96b5
Meurig Thomas <mlt47@cam.ac.uk>
2019-05-28 15:29:49 +0100

    Add breadcrumb trail to all pages and improve some homepage style

--- src/app/components/pages/NotFound.tsx
+++ src/app/components/pages/NotFound.tsx
@@ -1,18 +1,20 @@
 import React from "react";
 import {withRouter} from "react-router-dom";
+import {BreadcrumbTrail} from "../elements/BreadcrumbTrail";
 
 interface PageNotFoundProps {readonly location: {readonly pathname: string}}
 
 const PageNotFoundComponent = ({location: {pathname}}: PageNotFoundProps) => {
     return <React.Fragment>
         <div>
+            <BreadcrumbTrail currentPageTitle="Unknown page" />
             <h1 className="h-title">Page Not Found</h1>
             <h3 className="my-4">
                 <small>
                     {"We're sorry, page not found: "}
-                    <pre className="text-secondary">
+                    <code>
                         {pathname}
-                    </pre>
+                    </code>
                 </small>
             </h3>
         </div>
