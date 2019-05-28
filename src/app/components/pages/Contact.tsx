22292fbb13f8dc64715c04739a19afadb40b8bdd
Meurig Thomas <mlt47@cam.ac.uk>
2019-05-29 00:05:38 +0100

    Use useMemo to reset fields when the user has logged in or whenever the preset changes

--- src/app/components/pages/Contact.tsx
+++ src/app/components/pages/Contact.tsx
@@ -1,50 +1,24 @@
-import React, {useState, useEffect} from 'react';
+import React, {useMemo, useState} from 'react';
 import {connect} from "react-redux";
-import {Link} from "react-router-dom";
-import {
-    Alert,
-    TabContent,
-    TabPane,
-    Nav,
-    NavItem,
-    NavLink,
-    Button,
-    Card,
-    CardBody,
-    CardTitle,
-    CardText,
-    CardFooter,
-    Col,
-    CustomInput,
-    Form,
-    FormGroup,
-    Input,
-    Row,
-    Label,
-    Table,
-    FormFeedback
-} from "reactstrap";
-import {RegisteredUserDTO} from "../../../IsaacApiTypes";
+import {Alert, Button, Card, CardBody, CardFooter, Col, Form, FormGroup, Input, Row, Label, FormFeedback} from "reactstrap";
 import {AppState, ErrorState} from "../../state/reducers";
 import {submitMessage} from "../../state/actions";
-import classnames from 'classnames';
-import {string} from "prop-types";
 import {LoggedInUser} from "../../../IsaacAppTypes";
 import {validateEmail} from "../../services/validation";
+import queryString from "query-string";
+import {BreadcrumbTrail} from "../elements/BreadcrumbTrail";
 
-
-const queryString = require('query-string');
-const urlParams = queryString.parse(location.search);
 
 const stateToProps = (state: AppState) => {
+    const urlQuery = queryString.parse(location.search);
     let presetSubject = "";
     let presetMessage = "";
-    if (urlParams && urlParams.preset == "teacherRequest") {
+    if (urlQuery && urlQuery.preset == "teacherRequest") {
         if (state && state.user && state.user.loggedIn && state.user.role != "TEACHER") {
             presetSubject = "Teacher Account Request";
             presetMessage = "Hello,\n\nPlease could you convert my Isaac account into a teacher account.\n\nMy school is: \nI have changed my account email address to be my school email: [Yes/No]\nA link to my school website with a staff list showing my name and email (or a phone number to contact the school) is: \n\nThanks, \n\n" + state.user.givenName + " " + state.user.familyName;
         }
-    } else if (urlParams && urlParams.preset == 'accountDeletion') {
+    } else if (urlQuery && urlQuery.preset == 'accountDeletion') {
         if (state && state.user && state.user.loggedIn) {
             presetSubject = "Account Deletion Request";
             presetMessage = "Hello,\n\nPlease could you delete my Isaac Computer Science account.\n\nThanks, \n\n" + state.user.givenName + " " + state.user.familyName;
@@ -53,9 +27,9 @@ const stateToProps = (state: AppState) => {
     return {
         user: state ? state.user : null,
         errorMessage: state ? state.error : null,
-        presetSubject: presetSubject,
-        presetMessage: presetMessage
-    }
+        presetSubject: urlQuery.subject as string || presetSubject,
+        presetMessage: urlQuery.message as string || presetMessage,
+    };
 };
 
 const dispatchToProps = {
@@ -63,7 +37,7 @@ const dispatchToProps = {
 };
 
 interface ContactPageProps {
-    user: LoggedInUser | null
+    user: LoggedInUser | null;
     submitMessage: (extra: any, params: {firstName: string; lastName: string; emailAddress: string; subject: string; message: string}) => void;
     errorMessage: ErrorState | null;
     presetSubject: string;
@@ -71,17 +45,21 @@ interface ContactPageProps {
 }
 
 const ContactPageComponent = ({user, submitMessage, errorMessage, presetSubject, presetMessage}: ContactPageProps) => {
-
-    const [firstName, setFirstName] = useState(user && user.loggedIn && user.givenName ? user.givenName : "");
-    const [lastName, setLastName] = useState(user && user.loggedIn && user.familyName ? user.familyName : "");
-    const [email, setEmail] = useState(user && user.loggedIn && user.email ? user.email : "");
-    const [subject, setSubject] = useState(presetSubject || urlParams.subject || "");
-    const [message, setMessage] = useState(presetMessage || "");
+    const [firstName, setFirstName] = useState(user && user.loggedIn && user.givenName || "");
+    const [lastName, setLastName] = useState(user && user.loggedIn && user.familyName || "");
+    const [email, setEmail] = useState(user && user.loggedIn && user.email || "");
+    const [subject, setSubject] = useState(presetSubject);
+    const [message, setMessage] = useState(presetMessage);
     const [messageSendAttempt, setMessageSendAttempt] = useState(false);
     const [messageSent, setMessageSent] = useState(false);
 
-    const isValidEmail = validateEmail(email);
+    // set subject and message if any of user, presetSubject or presetMessage change
+    useMemo(() => {
+        setSubject(presetSubject);
+        setMessage(presetMessage);
+    }, [user, presetSubject, presetMessage]);
 
+    const isValidEmail = validateEmail(email);
 
     const sendForm = () => {
         submitMessage(
@@ -91,16 +69,17 @@ const ContactPageComponent = ({user, submitMessage, errorMessage, presetSubject,
                 lastName: lastName,
                 emailAddress: email,
                 subject: subject,
-                message: message})
-    }
+                message: message
+            })
+    };
 
-    return <div id="contact-page">
-        <h1>Contact Us</h1>
-        <h2 className="h-title mb-4">We'd love to hear from you</h2>
-        {presetMessage}
+    return <div id="contact-page" className="pb-5">
+        <BreadcrumbTrail currentPageTitle="Contact us" />
+        <h1>Contact us</h1>
+        <h2 className="h-title mb-5">We'd love to hear from you</h2>
         <div>
             <Row>
-                <Col size={12} md={{size: 3, order: 1}} xs={{order: 2}}>
+                <Col size={12} md={{size: 3, order: 1}} xs={{order: 2}} className="mt-4 mt-md-0">
                     <h3>Upcoming events</h3>
                     <p>If you'd like to find out more about our upcoming events visit our <a href="/events">Events Page</a></p>
                     <h3>Problems with the site?</h3>
@@ -128,7 +107,7 @@ const ContactPageComponent = ({user, submitMessage, errorMessage, presetSubject,
                                 setMessageSent(true)
                             }}>
                                 <CardBody>
-                                    <h3>Send us a Message</h3>
+                                    <h3>Send us a message</h3>
                                     <p>Please get in touch with the Isaac Computer Science team if you have comments about our resources or events, questions about the site, ideas, or other feedback. We would love to hear from you! <br/><br/>
                                         To contact us, please fill out this form:
                                     </p>
@@ -188,7 +167,9 @@ const ContactPageComponent = ({user, submitMessage, errorMessage, presetSubject,
                                     </div>
                                     <Row>
                                         <Col size={12} md={6}>
-                                            <span className="form-required color=$secondary">Required field</span>
+                                            <span className="d-block pb-3 pb-md-0 text-right text-md-left form-required">
+                                                Required field
+                                            </span>
                                         </Col>
                                         <Col size={12} md={6} className="text-right">
                                             <Button color="secondary" type="submit" onClick={() => {
