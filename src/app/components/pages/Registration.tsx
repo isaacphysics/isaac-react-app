604f1847246b01e51c8489bbf5840caea716fdb2
Ben Hanson <bh412@cam.ac.uk>
2019-05-30 16:10:55 +0100

    Unused import removal

--- src/app/components/pages/Registration.tsx
+++ src/app/components/pages/Registration.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useMemo, useEffect} from 'react';
+import React, {useState, useMemo} from 'react';
 import {connect} from "react-redux";
 import {Link} from "react-router-dom";
 import {
