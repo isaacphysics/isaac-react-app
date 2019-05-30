275eba9ea6df2681931904b5d6abeddb10133d3a
Ben Hanson <bh412@cam.ac.uk>
2019-05-30 16:43:21 +0100

    take user to my account page if logged in

--- src/app/components/handlers/EmailAlterHandler.tsx
+++ src/app/components/handlers/EmailAlterHandler.tsx
@@ -1,6 +1,6 @@
 import React, {useEffect} from 'react';
 import {connect} from "react-redux";
-import {handleEmailAlter} from "../../state/actions";
+import {handleEmailAlter, requestCurrentUser} from "../../state/actions";
 import {Button, Col} from "reactstrap";
 import {AppState, ErrorState} from "../../state/reducers";
 import {history} from "../../services/history";
@@ -10,15 +10,16 @@ const stateToProps = (state: AppState, {location: {search}}: any) => ({
     errorMessage: state ? state.error : null,
     queryParams: queryString.parse(search)
 });
-const dispatchToProps = {handleEmailAlter};
+const dispatchToProps = {handleEmailAlter, requestCurrentUser};
 
 interface EmailAlterHandlerProps {
     queryParams: {userid?: string; token?: string};
     handleEmailAlter: (params: {userid: string | null; token: string | null}) => void;
     errorMessage: ErrorState;
+    requestCurrentUser: () => void;
 }
 
-const EmailAlterHandlerComponent = ({queryParams: {userid, token}, handleEmailAlter, errorMessage}: EmailAlterHandlerProps) => {
+const EmailAlterHandlerComponent = ({queryParams: {userid, token}, handleEmailAlter, errorMessage, requestCurrentUser}: EmailAlterHandlerProps) => {
     useEffect(() => {
         if (userid && token) {
             handleEmailAlter({userid, token});
@@ -30,8 +31,10 @@ const EmailAlterHandlerComponent = ({queryParams: {userid, token}, handleEmailAl
             <div>
                 <h3>Email address verified</h3>
                 <Col>
-                    <Button color="primary" onClick={() => {history.push('/account');}} block >
-                        Log In To My Account
+                    <Button color="primary" onClick={() => {
+                        Promise.resolve(requestCurrentUser()).then(() => history.push('/account'));
+                    }} block >
+                        Log in to go to My Account
                     </Button>
                 </Col>
             </div>
