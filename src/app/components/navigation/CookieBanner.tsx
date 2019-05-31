024005afe892b372c927aa07dcbccc5d21d154a7
Meurig Thomas <mlt47@cam.ac.uk>
2019-05-31 01:25:30 +0100

    Adjust the CSS of banners

--- src/app/components/navigation/CookieBanner.tsx
+++ src/app/components/navigation/CookieBanner.tsx
@@ -22,14 +22,14 @@ export const CookieBanner = () => {
                 <RS.Col xs={12} sm={2} md={1}>
                     <h3 className="text-center" role="presentation">ℹ️<span className="d-inline-block d-sm-none">&nbsp;Cookies</span></h3>
                 </RS.Col>
-                <RS.Col xs={12} sm={10} md={7}>
+                <RS.Col xs={12} sm={10} md={8}>
                     <small>Use of this website and the information entered is being recorded. This data is used to support research
                     into online learning at the University of Cambridge. Cookies are used to support this functionality.
                     Full details are in the <Link to="/privacy">privacy policy</Link> and <Link to="/cookies">cookie policy</Link>.
                     Do you agree to participate in this research?</small>
                 </RS.Col>
-                <RS.Col xs={12} md={4}>
-                    <RS.Button color="primary" outline className="d-block d-md-inline-block banner-button" onClick={clickDismiss}>
+                <RS.Col xs={12} md={3} className="text-center">
+                    <RS.Button color="primary" outline className="mt-3 mb-2 d-block d-md-inline-block banner-button" onClick={clickDismiss}>
                         I Agree
                     </RS.Button>
                 </RS.Col>
