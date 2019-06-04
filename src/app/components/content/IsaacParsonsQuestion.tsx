3eadcdd3ce3b7735c9f1d5d4233f516e933cfb3c
Andrea Franceschini <andrea.franceschini@gmail.com>
2019-06-04 16:44:19 +0100

    Sort-of working but can't figure out how to do indentation

--- src/app/components/content/IsaacParsonsQuestion.tsx
+++ src/app/components/content/IsaacParsonsQuestion.tsx
@@ -5,7 +5,7 @@ import {IsaacContentValueOrChildren} from "./IsaacContentValueOrChildren";
 import {AppState} from "../../state/reducers";
 import {IsaacParsonsQuestionDTO, ParsonsChoiceDTO} from "../../../IsaacApiTypes";
 import {IsaacHints} from "./IsaacHints";
-import {DragDropContext, Draggable, Droppable} from "react-beautiful-dnd";
+import {DragDropContext, Draggable, Droppable, ResponderProvided, DragUpdate, DropResult, DragStart} from "react-beautiful-dnd";
 
 const stateToProps = (state: AppState, {questionId}: {questionId: string}) => {
     // TODO MT move this selector to the reducer - https://egghead.io/lessons/javascript-redux-colocating-selectors-with-reducers
@@ -23,6 +23,8 @@ interface IsaacParsonsQuestionProps {
 const IsaacParsonsQuestionComponent = (props: IsaacParsonsQuestionProps) => {
 
     const {doc, questionId, currentAttempt, setCurrentAttempt} = props;
+    const [draggedElement, setDraggedElement] = useState<HTMLElement | null>(null);
+
     let currentAttemptValue: ParsonsChoiceDTO = {};
     if (currentAttempt && currentAttempt.value) {
         try {
@@ -34,9 +36,7 @@ const IsaacParsonsQuestionComponent = (props: IsaacParsonsQuestionProps) => {
         currentAttemptValue.items = doc.items;
     } // TODO Improve this -^
 
-    const swapItems = (fromIndex: number, toIndex: number) => {
-        console.log(fromIndex, toIndex);
-        console.log(currentAttemptValue.items);
+    function swapItems(fromIndex: number, toIndex: number) {
         if (currentAttemptValue.items) {
             const sourceItem = currentAttemptValue.items.splice(fromIndex, 1)[0];
             currentAttemptValue.items.splice(toIndex, 0, sourceItem);
@@ -48,6 +48,28 @@ const IsaacParsonsQuestionComponent = (props: IsaacParsonsQuestionProps) => {
         setCurrentAttempt(questionId, attempt);
     }
 
+    function onDragStart(initial: DragStart, provided: ResponderProvided) {
+        const element = document.getElementById(`parsons-item-${initial.draggableId}`);
+        if (element) {
+            element.addEventListener("drag", (e: DragEvent) => {
+                console.log(e);
+            });
+            setDraggedElement(element);
+        }
+    }
+
+    function onDragUpdate(initial: DragUpdate, provided: ResponderProvided) {
+        // console.log(arguments);
+    }
+
+    function onDragEnd(result: DropResult, provided: ResponderProvided) {
+        const element = document.getElementById(`parsons-item-${result.draggableId}`)
+        if (result.destination) {
+            swapItems(result.source.index, result.destination.index);
+        }
+        setDraggedElement(null);
+    }
+
     return (
         <div className="parsons-question">
             <div className="question-content">
@@ -57,20 +79,29 @@ const IsaacParsonsQuestionComponent = (props: IsaacParsonsQuestionProps) => {
             </div>
             {/* TODO Accessibility */}
             {currentAttemptValue && <div className="parsons-items">
-                <DragDropContext onDragEnd={(e) => e.destination ? swapItems(e.source.index, e.destination.index) : void 0}>
+                <DragDropContext onDragStart={onDragStart} onDragEnd={onDragEnd} onDragUpdate={onDragUpdate}>
                     <Droppable droppableId="droppable">
-                        {(providedDroppable, snapshot) => (
-                            <div {...providedDroppable.droppableProps} ref={providedDroppable.innerRef}>
+                        {(providedDroppable, snapshot) => {
+                            return <div className="parsons-items" ref={providedDroppable.innerRef} {...providedDroppable.droppableProps}>
                                 {currentAttemptValue.items && currentAttemptValue.items.map((item, index) => (
                                     <Draggable draggableId={item.id as string} key={item.id} index={index}>
-                                        {(providedDraggable, snapshot) => (
-                                            <div ref={providedDraggable.innerRef} {...providedDraggable.draggableProps} {...providedDraggable.dragHandleProps}>
-                                                {item.value} {item.indentation}
+                                        {(providedDraggable, snapshot) => {
+                                            let d: JSX.Element = <div
+                                                    id={`parsons-item-${item.id}`}
+                                                    className={`parsons-item indent-${item.indentation || 0}`}
+                                                    ref={providedDraggable.innerRef}
+                                                    {...providedDraggable.dragHandleProps}
+                                                    {...providedDraggable.draggableProps}
+                                                    draggable={true}
+                                                    onDrag={(event: React.DragEvent) => { console.log(event); }}
+                                                ><pre>{item.value} {item.indentation}</pre>
                                             </div>
-                                        )}
-                                    </Draggable>))}
+                                            debugger;
+                                            return d;
+                                        }}
+                                    </Draggable>))}{providedDroppable.placeholder}
                             </div>
-                        )}
+                        }}
                     </Droppable>
                 </DragDropContext>
             </div>}
