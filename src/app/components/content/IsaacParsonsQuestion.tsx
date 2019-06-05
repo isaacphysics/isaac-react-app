be33a3a2d209acc5551780bfd54ea5224e9b61d5
Andrea Franceschini <andrea.franceschini@gmail.com>
2019-06-05 16:28:36 +0100

    Convert to class component because YOLO

--- src/app/components/content/IsaacParsonsQuestion.tsx
+++ src/app/components/content/IsaacParsonsQuestion.tsx
@@ -7,101 +7,106 @@ import {IsaacParsonsQuestionDTO, ParsonsChoiceDTO, ParsonsItemDTO} from "../../.
 import {IsaacHints} from "./IsaacHints";
 import {SortableContainer, SortableElement, SortStart, SortEvent, SortEnd} from "react-sortable-hoc";
 
-const stateToProps = (state: AppState, {questionId}: {questionId: string}) => {
-    // TODO MT move this selector to the reducer - https://egghead.io/lessons/javascript-redux-colocating-selectors-with-reducers
-    const question = state && state.questions && state.questions.filter((question) => question.id == questionId)[0];
-    return question ? {currentAttempt: question.currentAttempt} : {};
-};
-const dispatchToProps = {setCurrentAttempt};
-
 interface IsaacParsonsQuestionProps {
     doc: IsaacParsonsQuestionDTO;
     questionId: string;
     currentAttempt?: ParsonsChoiceDTO;
     setCurrentAttempt: (questionId: string, attempt: ParsonsChoiceDTO) => void;
 }
-const IsaacParsonsQuestionComponent = (props: IsaacParsonsQuestionProps) => {
-    const {doc, questionId, currentAttempt, setCurrentAttempt} = props;
-    const [draggedElement, setDraggedElement] = useState<Element | null>(null);
-    const [resolve, setResolve] = useState<any | null>(() => {});
 
-    let currentAttemptValue: ParsonsChoiceDTO = {};
-    if (currentAttempt && currentAttempt.value) {
-        try {
-            currentAttemptValue = JSON.parse(currentAttempt.value) as ParsonsChoiceDTO;
-        } catch(e) {
+class IsaacParsonsQuestionComponent extends React.Component<IsaacParsonsQuestionProps> {
+    state: {
+        draggedElement?: Element | null;
+        currentAttemptValue: ParsonsChoiceDTO;
+    };
+
+    constructor(props: IsaacParsonsQuestionProps) {
+        super(props);
+        const {doc, currentAttempt} = props;
+
+        let currentAttemptValue: ParsonsChoiceDTO = {};
+        if (currentAttempt && currentAttempt.value) {
+            try {
+                currentAttemptValue = JSON.parse(currentAttempt.value) as ParsonsChoiceDTO;
+            } catch(e) {
+                currentAttemptValue.items = doc.items;
+            }
+        } else {
             currentAttemptValue.items = doc.items;
+        } // TODO Improve this -^
+        this.state = {
+            draggedElement: null,
+            currentAttemptValue: currentAttemptValue,
         }
-    } else {
-        currentAttemptValue.items = doc.items;
-    } // TODO Improve this -^
-
-    function moveItem(fromIndex: number, toIndex: number) {
-        if (currentAttemptValue.items) {
-            const sourceItem = currentAttemptValue.items.splice(fromIndex, 1)[0];
-            currentAttemptValue.items.splice(toIndex, 0, sourceItem);
-        }
-        const attempt: ParsonsChoiceDTO = {
-            type: "parsonsChoice",
-            items: currentAttemptValue.items,
-        }
-        setCurrentAttempt(questionId, attempt);
     }
 
-    const SortableItem = SortableElement(({value}: {value: ParsonsItemDTO}) => {
+    moveItem = (fromIndex: number, toIndex: number) => {
+        const t = Object.assign({}, this.state.currentAttemptValue);
+        if (t.items) {
+            const sourceItem = t.items.splice(fromIndex, 1)[0];
+            t.items.splice(toIndex, 0, sourceItem);
+        }
+        this.setState({ currentAttemptValue: t});
+        const attempt: ParsonsChoiceDTO = {
+            type: "parsonsChoice",
+            items: this.state.currentAttemptValue.items,
+        }
+        this.props.setCurrentAttempt(this.props.questionId, attempt);
+    }
+
+    private SortableItem = SortableElement(({value}: {value: ParsonsItemDTO}) => {
         return value ? <div id={`parsons-item-${value.id}`} className={`parsons-item indent-${value.indentation}`}><pre>{value.value} [{value.indentation}]</pre></div> : <div>ERROR: Missing item?</div>
     });
     
-    const SortableList = SortableContainer(({items}: {items: any}) => {
+    private SortableList = SortableContainer(({items}: {items: any}) => {
         return <div className="parsons-items">
             {items && items.map((item: any, index: any) => {
-                return <SortableItem key={`item-${index}`} index={index} value={item} />;
+                return <this.SortableItem key={`item-${index}`} index={index} value={item} />;
             })}
         </div>
     });
 
-    function onUpdateBeforeSortStart(sort: SortStart, event: SortEvent) {
-        return new Promise(function(res) {
-            setDraggedElement(sort.node);
-            setResolve(res);
+    onUpdateBeforeSortStart = (sort: SortStart, event: SortEvent) => {
+        return new Promise((res) => {
+            this.setState({ draggedElement: sort.node }, res);
         });
     }
 
-    useMemo(() => {
-        debugger;
-        if (resolve) {
-            resolve();
-        }
-    }, [resolve]);
-
-    function onSortMove(event: SortEvent) {
-        console.log(draggedElement);
+    onSortMove = (event: SortEvent) => {
+        console.log(this.state.draggedElement);
     }
 
-    function onSortEnd(sort: SortEnd, event: SortEvent) {
-        moveItem(sort.oldIndex, sort.newIndex);
-        setDraggedElement(null);
+    onSortEnd = (sort: SortEnd, event: SortEvent) => {
+        this.moveItem(sort.oldIndex, sort.newIndex);
+        this.setState({ draggedElement: null });
     }
 
-    return (
-        <div className="parsons-question">
+    render() {
+        return <div className="parsons-question">
             <div className="question-content">
-                <IsaacContentValueOrChildren value={doc.value} encoding={doc.encoding}>
-                    {doc.children}
+                <IsaacContentValueOrChildren value={this.props.doc.value} encoding={this.props.doc.encoding}>
+                    {this.props.doc.children}
                 </IsaacContentValueOrChildren>
             </div>
             {/* TODO Accessibility */}
-            {currentAttemptValue && <div className="parsons-items">
-                <SortableList
-                items={currentAttemptValue.items}
-                updateBeforeSortStart={onUpdateBeforeSortStart}
-                onSortMove={onSortMove}
-                onSortEnd={onSortEnd}
+            {this.state.currentAttemptValue && <div className="parsons-items">
+                <this.SortableList
+                items={this.state.currentAttemptValue.items}
+                updateBeforeSortStart={this.onUpdateBeforeSortStart}
+                onSortMove={this.onSortMove}
+                onSortEnd={this.onSortEnd}
                 />
             </div>}
-            <IsaacHints hints={doc.hints} />
+            <IsaacHints hints={this.props.doc.hints} />
         </div>
-    );
+    }
+}
+
+const stateToProps = (state: AppState, {questionId}: {questionId: string}) => {
+    // TODO MT move this selector to the reducer - https://egghead.io/lessons/javascript-redux-colocating-selectors-with-reducers
+    const question = state && state.questions && state.questions.filter((question) => question.id == questionId)[0];
+    return question ? {currentAttempt: question.currentAttempt} : {};
 };
+const dispatchToProps = {setCurrentAttempt};
 
 export const IsaacParsonsQuestion = connect(stateToProps, dispatchToProps)(IsaacParsonsQuestionComponent);
