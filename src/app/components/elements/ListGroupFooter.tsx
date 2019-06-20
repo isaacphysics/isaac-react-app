83955ca7a445fb164472d19623426a875bfb530d
James Sharkey <jsharkey13@users.noreply.github.com>
2019-06-20 16:41:04 +0100

    Make footer links more usable on mobile
    # This just adds more padding to the link elements themselves and aligns
    # them with the social logos.

--- src/app/components/elements/ListGroupFooter.tsx
+++ src/app/components/elements/ListGroupFooter.tsx
@@ -5,30 +5,30 @@ import {ListGroup, ListGroupItem} from "reactstrap";
 export const ListGroupFooter = () => (
     <div className="footer-links">
         <h5>Links</h5>
-        <div className="d-flex flex-row">
+        <div className="d-flex flex-row pt-lg-3">
             <ListGroup className="w-50 mb-3 link-list">
 
-                <ListGroupItem className="border-0 px-0 py-0 pb-1 bg-transparent">
-                    <Link className="footerLink" to="/about">About Us</Link>
+                <ListGroupItem className="border-0 px-0 py-0 bg-transparent">
+                    <Link className="footerLink py-2" to="/about">About Us</Link>
                 </ListGroupItem>
-                <ListGroupItem className="border-0 px-0 py-0 pb-1 bg-transparent align-items-stretch">
-                    <Link className="footerLink" to="/contact">Contact us</Link>
+                <ListGroupItem className="border-0 px-0 py-0 bg-transparent align-items-stretch">
+                    <Link className="footerLink py-2" to="/contact">Contact us</Link>
                 </ListGroupItem>
-                <ListGroupItem className="border-0 px-0 py-0 pb-1 bg-transparent align-items-stretch">
-                    <a href="https://isaaccomputerscience.org/events" target="_blank" rel="noopener noreferrer">Events</a>
+                <ListGroupItem className="border-0 px-0 py-0 bg-transparent align-items-stretch">
+                    <a className="py-2" href="https://isaaccomputerscience.org/events" target="_blank" rel="noopener noreferrer">Events</a>
                 </ListGroupItem>
 
             </ListGroup>
             <ListGroup className="w-50 mb-3 link-list">
 
-                <ListGroupItem className="border-0 px-0 py-0 pb-1 bg-transparent">
-                    <Link className="footerLink" to="/privacy">Privacy Policy</Link>
+                <ListGroupItem className="border-0 px-0 py-0 bg-transparent">
+                    <Link className="footerLink py-2" to="/privacy">Privacy Policy</Link>
                 </ListGroupItem>
-                <ListGroupItem className="border-0 px-0 py-0 pb-1 bg-transparent align-items-stretch">
-                    <Link className="footerLink" to="/terms">Terms of Use</Link>
+                <ListGroupItem className="border-0 px-0 py-0 bg-transparent align-items-stretch">
+                    <Link className="footerLink py-2" to="/terms">Terms of Use</Link>
                 </ListGroupItem>
-                <ListGroupItem className="border-0 px-0 py-0 pb-1 bg-transparent">
-                    <Link className="footerLink" to="/cookies">Cookie Policy</Link>
+                <ListGroupItem className="border-0 px-0 py-0 bg-transparent">
+                    <Link className="footerLink py-2" to="/cookies">Cookie Policy</Link>
                 </ListGroupItem>
 
             </ListGroup>
