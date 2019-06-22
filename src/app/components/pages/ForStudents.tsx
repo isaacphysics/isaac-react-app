b1c0f9c5231aecefb0aaccbfd6e69126294f9837
Meurig Thomas <mlt47@cam.ac.uk>
2019-06-22 18:24:07 +0100

    Change content on the Teacher and Student pages depending on sign-in

--- src/app/components/pages/ForStudents.tsx
+++ src/app/components/pages/ForStudents.tsx
@@ -2,20 +2,43 @@ import React from "react";
 import {connect} from "react-redux";
 import * as RS from "reactstrap";
 import {TitleAndBreadcrumb} from "../elements/TitleAndBreadcrumb";
+import {PageFragment} from "../elements/PageFragment";
+import {AppState} from "../../state/reducers";
+import {LoggedInUser} from "../../../IsaacAppTypes";
+import {Link} from "react-router-dom";
 
-const ForStudentsComponent = () => {
+const stateToProps = (state: AppState) => ({
+    user: (state && state.user) || null
+});
+
+interface ForStudentProps {
+    user: LoggedInUser | null;
+}
+
+const ForStudentsComponent = ({user}: ForStudentProps) => {
+    const pageTitle = user && user.loggedIn ? "My Isaac" : "How we help students";
+    const registrationButton = <div className="text-center">
+        <RS.Button size="lg" tag={Link} to={"/register"} color="primary" outline>Sign up</RS.Button>
+    </div>;
+
     return <RS.Container>
         <RS.Row>
             <RS.Col>
-                <TitleAndBreadcrumb currentPageTitle="How we help students" breadcrumbTitleOverride="Students" />
+                <TitleAndBreadcrumb currentPageTitle={pageTitle} breadcrumbTitleOverride="Students" />
             </RS.Col>
         </RS.Row>
         <RS.Row>
-            <RS.Col className="pt-4 pb-5">
-                This page is for students.
+            <RS.Col md={{size: 8, offset: 2}} className="pt-4 pb-5">
+                {user && user.loggedIn ?
+                    <div>Welcome back to Isaac Computer Science.</div> :
+                    <React.Fragment>
+                        <PageFragment fragmentId="for_students_logged_out"/>
+                        {registrationButton}
+                    </React.Fragment>
+                }
             </RS.Col>
         </RS.Row>
     </RS.Container>;
 };
 
-export const ForStudents = connect()(ForStudentsComponent);
+export const ForStudents = connect(stateToProps)(ForStudentsComponent);
--- src/app/components/pages/ForTeachers.tsx
+++ src/app/components/pages/ForTeachers.tsx
@@ -2,20 +2,53 @@ import React from "react";
 import {connect} from "react-redux";
 import * as RS from "reactstrap";
 import {TitleAndBreadcrumb} from "../elements/TitleAndBreadcrumb";
+import {PageFragment} from "../elements/PageFragment";
+import {AppState} from "../../state/reducers";
+import {LoggedInUser} from "../../../IsaacAppTypes";
+import {isTeacher} from "../../services/user";
+import {Link} from "react-router-dom";
 
-const ForTeachersComponent = () => {
+const stateToProps = (state: AppState) => ({
+    user: (state && state.user) || null
+});
+
+interface ForTeachersProps {
+    user: LoggedInUser | null;
+}
+
+const ForTeachersComponent = ({user}: ForTeachersProps) => {
+    const pageTitle = user && isTeacher(user) ? "My Isaac teaching" : "How we help teachers";
+
+    const teacherUpgradeLink = <div className="text-muted mt-2">
+        Are you a teacher? {" "}
+        <a href="/pages/teacher_account_request" target="_blank" rel="noopener noreferrer">
+            <span className='sr-only'> Are you a teacher? </span> Let us know
+        </a> {" "}
+        and we&apos;ll convert your account to a teacher account.
+    </div>;
+    const registrationButton = <div className="text-center">
+        <RS.Button size="lg" tag={Link} to={"/register"} color="primary" outline>Sign up</RS.Button>
+    </div>;
+
     return <RS.Container>
         <RS.Row>
             <RS.Col>
-                <TitleAndBreadcrumb currentPageTitle="How we help teachers" breadcrumbTitleOverride="Teachers" />
+                <TitleAndBreadcrumb currentPageTitle={pageTitle} breadcrumbTitleOverride="Teachers" />
             </RS.Col>
         </RS.Row>
         <RS.Row>
-            <RS.Col className="pt-4 pb-5">
-                This page is for teachers.
+            <RS.Col md={{size: 8, offset: 2}} className="pt-4 pb-5">
+                {user && isTeacher(user) ?
+                    <div>Welcome back to Isaac Computer Science.</div> :
+                    <PageFragment fragmentId="for_teachers_logged_out" />
+                }
+                {(user && user.loggedIn) ?
+                    !isTeacher(user) && teacherUpgradeLink :
+                    registrationButton
+                }
             </RS.Col>
         </RS.Row>
     </RS.Container>;
 };
 
-export const ForTeachers = connect()(ForTeachersComponent);
+export const ForTeachers = connect(stateToProps)(ForTeachersComponent);
