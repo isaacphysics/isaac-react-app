3829a66927294f62cd7f01389551800d976fcc2a
Meurig Thomas <mlt47@cam.ac.uk>
2019-06-25 09:55:18 +0100

    Add a comment for future work

--- src/app/services/firstLogin.ts
+++ src/app/services/firstLogin.ts
@@ -1,6 +1,7 @@
 import * as persistence from "./localStorage";
 import {KEY, LOADING_FAILURE_VALUE} from "./localStorage";
 
+// TODO there'll probably be a way to use this in a way that lets the compiler do even more checking for us
 export enum FIRST_LOGIN_STATE {
     BANNER_NOT_SHOWN = "bannerNotShown",
     BANNER_SHOWN = "bannerShown"
