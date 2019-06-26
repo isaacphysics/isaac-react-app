825eefd76633bf2dd81d45c8c793897a99a65e64
Robin Message <robin@lambdacambridge.com>
2019-06-26 09:29:20 +0100

    Fix ordering requirement in test

--- src/test/state/reducers.test.tsx
+++ src/test/state/reducers.test.tsx
@@ -427,7 +427,7 @@ describe("boards reducer", () => {
         previousStates.map((previousState) => {
             const actualNextState = boards(previousState, action);
             const priorBoards = previousState && previousState.boards && previousState.boards.boards || [];
-            expect(selector.boards(actualNextState)).toEqual({totalResults: 40, boards: union(priorBoards, newBoards)});
+            expect(selector.boards(actualNextState)).toEqual({totalResults: 40, boards: union(newBoards, priorBoards)});
         });
     });
 
