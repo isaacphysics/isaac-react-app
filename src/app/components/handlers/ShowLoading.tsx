290ee31cd7d007b6a9d306162fc3f52d6731ab1a
Robin Message <robin@lambdacambridge.com>
2019-06-27 16:01:52 +0100

    ShowLoading: show blank screen for first 200msec of load

--- src/app/components/handlers/ShowLoading.tsx
+++ src/app/components/handlers/ShowLoading.tsx
@@ -1,4 +1,4 @@
-import React, {ReactElement, ReactNode} from "react";
+import React, {ReactElement, ReactNode, useEffect, useState} from "react";
 import {Spinner} from "reactstrap";
 import {NOT_FOUND} from "../../services/constants";
 import {NotFound} from "../pages/NotFound";
@@ -17,10 +17,24 @@ const defaultPlaceholder = <div className="w-100 text-center">
 </div>;
 
 export const ShowLoading = < T extends {} >({until, children, render, placeholder = defaultPlaceholder}: ShowLoadingProps<T>) => {
+    const [loadingTimeout, setLoadingTimeout] = useState();
+    const [duringLoad, setDuringLoad] = useState(false);
+    useEffect( () => {
+        clearTimeout(loadingTimeout);
+        if (until == null) {
+            setDuringLoad(true);
+            setLoadingTimeout(setTimeout(() => setDuringLoad(false), 200));
+        }
+    }, [until, children, render]);
+
     switch(until) {
         case null:
         case undefined:
-            return placeholder;
+            if (duringLoad) {
+                return <div className="min-vh-100" />;
+            } else {
+                return placeholder;
+            }
         case NOT_FOUND:
             return <NotFound/>;
         default:
