ab2848672b084d76e26e243ec15ed4f96dbf6de2
Meurig Thomas <mlt47@cam.ac.uk>
2019-07-04 10:33:40 +0100

    Improve the wrapping of long titles in navigation

--- src/app/components/elements/NavigationLinks.tsx
+++ src/app/components/elements/NavigationLinks.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import {PageNavigation} from "../../services/navigation";
 import {Link} from "react-router-dom";
+import * as RS from "reactstrap";
 
 export const NavigationLinks = ({navigation}: {navigation: PageNavigation}) => {
-    const backToCollectionLink = navigation.backToCollection && <div className="float-left">
+    const backToCollectionLink = navigation.backToCollection && <div className="w-50 w-md-auto">
         <Link to={navigation.backToCollection.to} className="text-decoration-none">
             {navigation.collectionType}:
         </Link>
@@ -12,17 +13,17 @@ export const NavigationLinks = ({navigation}: {navigation: PageNavigation}) => {
         </Link>
     </div>;
 
-    const nextItemLink = navigation.nextItem && <React.Fragment>
+    const nextItemLink = navigation.nextItem && <div className="w-50 w-md-auto text-right">
         <Link to={{pathname: navigation.nextItem.to, search: navigation.queryParams}} className="a-alt lrg-text float-right font-weight-bold">
             {navigation.nextItem.title}
         </Link>
         <Link to={{pathname: navigation.nextItem.to, search: navigation.queryParams}} className="mb-5 next-link float-right">
             Next
         </Link>
-    </React.Fragment>;
+    </div>;
 
-    return <React.Fragment>
+    return <div className="mt-4 d-flex justify-content-between align-items-stretch">
         {backToCollectionLink}
         {nextItemLink}
-    </React.Fragment>
+    </div>
 };
