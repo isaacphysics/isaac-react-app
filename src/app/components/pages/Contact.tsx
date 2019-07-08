9c7fe65eb13c7b892851ce9c35f7f4852ef6dde3
Meurig Thomas <mlt47@cam.ac.uk>
2019-07-08 14:12:17 +0100

    Add padding to the top of the contact us form

--- src/app/components/pages/Contact.tsx
+++ src/app/components/pages/Contact.tsx
@@ -81,11 +81,11 @@ const ContactPageComponent = ({user, submitMessage, errorMessage, presetSubject,
 
     return <Container id="contact-page" className="pb-5">
         <TitleAndBreadcrumb currentPageTitle="Contact us" />
-        <div>
+        <div className="pt-4">
             <Row>
                 <Col size={12} md={{size: 3, order: 1}} xs={{order: 2}} className="mt-4 mt-md-0">
                     <h3>Upcoming events</h3>
-                    <p>If you'd like to find out more about our upcoming events visit our <a href="https://isaaccomputerscience.org/events">Events Page</a></p>
+                    <p>If you&apos;d like to find out more about our upcoming events visit our <a href="https://isaaccomputerscience.org/events">Events Page</a></p>
                     <h3>Problems with the site?</h3>
                     <p>We always want to improve so please report any issues to <a className="small" href="mailto:webmaster@isaaccomputerscience.org">webmaster@isaaccomputerscience.org</a></p>
                     <h3>Follow us</h3>
