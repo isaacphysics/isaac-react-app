7e1bb71253ebac9d0d34c47f75aba408ef81f9a0
James Sharkey <jsharkey13@users.noreply.github.com>
2019-07-18 13:17:36 +0100

    Make the noscript page look nicer using Bootstrap classes and logo
    # This cannot easily be previewed locally, because the stylesheets are
    # loaded by JavaScript in the development server. To actually see the page
    # locally, you need to build the minified version and then serve it using
    # a static server (e.g. python2 -m SimpeHTTPServer 8003) from the `build`
    # directory.
    # The layout falls back gracefully without CSS though!

--- src/index.tsx
+++ src/index.tsx
@@ -6,11 +6,20 @@ import {Provider} from "react-redux";
 import {store} from "./app/state/store";
 import IsaacApp from './app/components/navigation/IsaacApp';
 
-ReactDOM.render(
-    <React.StrictMode>
-        <Provider store={store}>
-            <IsaacApp />
-        </Provider>
-    </React.StrictMode>,
-    document.getElementById('root')
-);
+let rootElement = document.getElementById('root');
+
+if (rootElement) {
+
+    // Cannot add this class to the root element without JavaScript because
+    // it messes up the page formatting for the <noscript> case!
+    rootElement.classList.add("min-vh-100");
+
+    ReactDOM.render(
+        <React.StrictMode>
+            <Provider store={store}>
+                <IsaacApp />
+            </Provider>
+        </React.StrictMode>,
+        rootElement
+    );
+}
