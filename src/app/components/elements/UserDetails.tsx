1d4b294732f55d0620977a8254f8a283764a558c
Meurig Thomas <mlt47@cam.ac.uk>
2019-07-24 16:19:34 +0100

    Use correct react attribute for auto-complete

--- src/app/components/elements/UserDetails.tsx
+++ src/app/components/elements/UserDetails.tsx
@@ -216,7 +216,7 @@ export const UserDetails = (props: UserDetailsProps) => {
                 <FormGroup className="school">
                     <Label htmlFor="school-input">School</Label>
                     <Input
-                        id="school-input" type="text" name="school" placeholder="Type a UK school name..." autocomplete="isaac-off"
+                        id="school-input" type="text" name="school" placeholder="Type a UK school name..." autoComplete="isaac-off"
                         value={
                             schoolQueryText !== null ?
                                 schoolQueryText :
