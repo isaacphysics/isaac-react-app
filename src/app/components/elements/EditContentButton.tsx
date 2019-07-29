6afa0a85795b02cb468276abdac683f4abacf8de
Andrea Franceschini <andrea.franceschini@gmail.com>
2019-07-29 11:46:30 +0100

    Open editor in new tab

--- src/app/components/elements/EditContentButton.tsx
+++ src/app/components/elements/EditContentButton.tsx
@@ -7,6 +7,6 @@ export interface EditContentButtonProps {
 
 export const EditContentButton = ({canonicalSourceFile, className}: EditContentButtonProps) => {
     return <div>
-        <a href={canonicalSourceFile} className={`btn btn-primary mt-3 ${className ? ` ${className}` : ""}`}>View in the Content Editor</a>
+        <a href={canonicalSourceFile} className={`btn btn-primary mt-3 ${className ? ` ${className}` : ""}`} target="_blank">View in the Content Editor</a>
     </div>
 };
