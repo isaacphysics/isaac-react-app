efe2b7f9d1b5b0bcfaee0d307e7e8281a5f0ca38
Ben Hanson <bh412@cam.ac.uk>
2019-09-09 09:48:32 +0100

    Change dummy video to intro video

--- src/app/components/pages/Homepage.tsx
+++ src/app/components/pages/Homepage.tsx
@@ -57,7 +57,7 @@ export const HomepageComponent = ({user}: HomePageProps) => {
                         </Row>
                     </Col>
                     <Col lg="7" className="p-sm-5 pb-5 pr-5 align-self-center text-center">
-                        <iframe width="614" height="390" className="mw-100" src="https://www.youtube-nocookie.com/embed/ygggcqKmUts?enablejsapi=1&rel=0&fs=1&modestbranding=1&origin=home" frameBorder="0"
+                        <iframe width="614" height="390" className="mw-100" src="https://www.youtube-nocookie.com/embed/nW4J-NVDziw?enablejsapi=1&rel=0&fs=1&modestbranding=1&origin=home" frameBorder="0"
                                 allowFullScreen/>
                     </Col>
                 </Row>
