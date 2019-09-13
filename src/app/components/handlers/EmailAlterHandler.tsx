c3d39c44816870622c1d0e2264c841f91ec9780c
Connor Holloway <connor@isaaccomputerscience.org>
2019-09-13 16:04:06 +0100

    Allow verification without need to be logged in
    Allow the verification link to be used without being logged in.
    
    Success/Error messages will be shown depending on the state of
    the user and if the verification fails.

--- src/app/components/handlers/EmailAlterHandler.tsx
+++ src/app/components/handlers/EmailAlterHandler.tsx
@@ -5,8 +5,7 @@ import {AppState, ErrorState} from "../../state/reducers";
 import {Link} from "react-router-dom";
 import queryString from "query-string";
 import {TitleAndBreadcrumb} from "../elements/TitleAndBreadcrumb";
-import {connect} from "react-redux";
-import {LoggedInUser} from "../../../IsaacAppTypes";
+import {connect, useSelector} from "react-redux";
 const stateToProps = (state: AppState, {location: {search}}: {location: {search?: string}}) => ({
     errorMessage: state ? state.error : null,
     queryParams: search ? queryString.parse(search) : {}
@@ -14,7 +13,6 @@ const stateToProps = (state: AppState, {location: {search}}: {location: {search?
 const dispatchToProps = {handleEmailAlter, requestEmailVerification};
 
 interface EmailAlterHandlerProps {
-    user: LoggedInUser | null;
     queryParams: {userid?: string; token?: string};
     handleEmailAlter: (params: {userid: string | null; token: string | null}) => void;
     errorMessage: ErrorState;
@@ -22,7 +20,8 @@ interface EmailAlterHandlerProps {
 }
 
 const EmailAlterHandlerComponent = (props: EmailAlterHandlerProps) => {
-    const {user, queryParams: {userid, token}, handleEmailAlter, errorMessage, requestEmailVerification} = props;
+    const user = useSelector((state: AppState) => state && state.user);
+    const {queryParams: {userid, token}, handleEmailAlter, errorMessage, requestEmailVerification} = props;
     const [verificationReSent, setVerificationReSent] = useState(false);
     const validParameters = userid && token;
     const idsMatch = user && user.loggedIn && user.id == userid;
@@ -31,7 +30,7 @@ const EmailAlterHandlerComponent = (props: EmailAlterHandlerProps) => {
     const emailVerificationSuccess = validParameters && idsMatch && emailVerified;
 
     useEffect(() => {
-        if (userid && token && idsMatch) {
+        if (userid && token) {
             handleEmailAlter({userid, token});
         }
     }, [userid, token, handleEmailAlter]);
@@ -44,22 +43,21 @@ const EmailAlterHandlerComponent = (props: EmailAlterHandlerProps) => {
                     <CardBody className="m-4">
                         {/* Isaac Physics had a large icon here */}
 
-                        {emailVerificationSuccess ?
+                        {(!errorMessage || emailVerificationSuccess) &&
                             <React.Fragment>
-                                <h3 className="mb-4">Email address verified</h3>
+                                <h3 className="mb-4">{ emailVerificationSuccess ? "Email address verified" : "Email address verification token received. Log in to confirm verification." }</h3>
                                 <Button tag={Link} to="/account" color="secondary" block>
                                     Continue to My Account
                                 </Button>
-                            </React.Fragment>
-                            :
+                            </React.Fragment>}
+                        {!emailVerificationSuccess && errorMessage &&
                             <React.Fragment>
                                 <h3 className="mb-4">Couldn&apos;t verify email address</h3>
                                 <p className="m-0">
                                     {!validParameters && "This page received bad parameters."}
-                                    {validParameters && !idsMatch && "You are not signed in as the user for which you are trying to verify an email."}
                                     {errorMessage && errorMessage.type === "generalError" && errorMessage.generalError}
                                 </p>
-                                {idsMatch && <p className="mt-4">
+                                {idsMatch ? <p className="mt-4">
                                     {!verificationReSent ?
                                         <Button onClick={() => {
                                             requestEmailVerification();
@@ -70,7 +68,9 @@ const EmailAlterHandlerComponent = (props: EmailAlterHandlerProps) => {
                                         :
                                         "Verification email sent to " + (user && user.loggedIn && user.email)
                                     }
-                                </p>}
+                                </p>
+                                :
+                                <p>Please login to resend the verification email.</p>}
                             </React.Fragment>
                         }
                     </CardBody>
