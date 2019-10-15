eb3cbe9932cf4c0676cab31eca71c271ffb05b86
Andrea Franceschini <andrea.franceschini@gmail.com>
2019-10-15 15:57:31 +0100

    Replace [glossary:glossary-page|id] with rendered IsaacGlossaryTerm

--- src/app/components/elements/TrustedMarkdown.tsx
+++ src/app/components/elements/TrustedMarkdown.tsx
@@ -1,12 +1,14 @@
-import React, { useEffect } from "react";
-import {useDispatch, useStore} from "react-redux";
+import React, { useEffect, useState } from "react";
+import {useDispatch, useStore, useSelector, ReactReduxContext, Provider} from "react-redux";
 import {AppState} from "../../state/reducers";
 import {MARKDOWN_RENDERER} from "../../services/constants";
 import {TrustedHtml} from "./TrustedHtml";
 import {IsaacGlossaryTerm} from "../content/IsaacGlossaryTerm";
-import {api} from '../../services/api';
 import {fetchGlossaryTerms} from "../../state/actions";
 import {GlossaryTermDTO} from "../../../IsaacApiTypes";
+
+import ReactDOMServer from "react-dom/server";
+import ReactDOM from "react-dom";
 
 const stateToProps = (state: AppState) => {
     const glossaryTerms = state && state.glossaryTerms;
@@ -16,18 +18,32 @@ const stateToProps = (state: AppState) => {
 export const TrustedMarkdown = ({markdown}: {markdown: string}) => {
     const dispatch = useDispatch();
     const store = useStore();
-    const state = store.getState();
+    const glossaryTerms = useSelector((state: AppState) => {
+        return state && state.glossaryTerms;
+    });
+
+    let terms: any = {};
+    if (glossaryTerms) {
+        terms = Object.assign({}, ...glossaryTerms.map(
+            (t: GlossaryTermDTO) => {
+                if (t.id) {
+                    // let el = <IsaacGlossaryTerm doc={t} />;
+                    // let string = ReactDOMServer.renderToString(el);
+                    return { [t.id]: t };
+                }
+            }
+        ));
+        let r = /^\[glossary:([a-z-|]+?)\]/gm;
+        markdown = markdown.replace(r, (_match, id: string) => {
+            let string = ReactDOMServer.renderToStaticMarkup(
+                <Provider store={store}><IsaacGlossaryTerm doc={terms[id]} /></Provider>
+            );
+            return string;
+        });
+    }
 
     useEffect(() => {
         dispatch(fetchGlossaryTerms());
-        debugger;
-        console.log(state.glossaryTerms);
-        
-        // let r = /^\[glossary:([a-z-|]+?)\]/gm;
-        // let m = null;
-        // while ((m = r.exec(markdown)) !== null) {
-        //     console.log(m);
-        // }
     });
     // RegEx replacements to match Latex inspired Isaac Physics functionality
     const regexRules = {
@@ -35,19 +51,11 @@ export const TrustedMarkdown = ({markdown}: {markdown: string}) => {
         "[$1]($2)": /\\link{([^}]*)}{([^}]*)}/g,
         "[**Glossary**](/glossary)": /\*\*Glossary\*\*/g,
         "[**Concepts**](/concepts)": /\*\*Concepts\*\*/g,
-        // "GLOSSARY TERM: $1": /^\[glossary:([a-z-|]+?)\]/gm,
     };
     let regexProcessedMarkdown = markdown;
     Object.entries(regexRules).forEach(([replacement, rule]) =>
         regexProcessedMarkdown = regexProcessedMarkdown.replace(rule, replacement)
     );
-
-    // let glossaryBlockRegex = /^\[glossary:([a-z-|]+?)\]/gm
-    // let glossaryMatches = null;
-    // let glossaryTerms = [];
-    // while (glossaryMatches = glossaryBlockRegex.exec(regexProcessedMarkdown)) {
-    //     console.log(glossaryMatches);
-    // }
 
     const html = MARKDOWN_RENDERER.render(regexProcessedMarkdown);
     return <TrustedHtml html={html} />;
