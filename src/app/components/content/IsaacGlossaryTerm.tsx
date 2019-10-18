9411aedf704932ebe52aaab92d01c8180b9da632
Andrea Franceschini <andrea.franceschini@gmail.com>
2019-10-18 14:58:24 +0100

    Fix rendering of IsaacGlossaryTerm due to changes in explanation format

--- src/app/components/content/IsaacGlossaryTerm.tsx
+++ src/app/components/content/IsaacGlossaryTerm.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import {Col, Row} from "reactstrap";
 import {GlossaryTermDTO} from "../../../IsaacApiTypes";
-import {IsaacContentValueOrChildren} from "./IsaacContentValueOrChildren";
 import {IsaacContent} from "./IsaacContent";
 
 interface IsaacGlossaryTermProps {
@@ -12,13 +11,10 @@ interface IsaacGlossaryTermProps {
 export const IsaacGlossaryTerm = ({doc}: IsaacGlossaryTermProps) => {
     return <Row className="glossary_term">
         <Col md={3}>
-            <a id={doc.id && doc.id.split('|')[1]}><strong>{doc.value}</strong></a>
+            <p id={doc.id && doc.id.split('|')[1]}><strong>{doc.value}</strong></p>
         </Col>
         <Col>
-            {/* <IsaacContentValueOrChildren encoding={doc.encoding}>
-                { doc.explanation && doc.explanation.children }
-            </IsaacContentValueOrChildren> */}
-            { doc.explanation && <IsaacContent doc={doc.explanation} />}
+            {doc.explanation && <IsaacContent doc={doc.explanation} />}
         </Col>
     </Row>;
 };
