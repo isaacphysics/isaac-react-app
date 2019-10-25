25102532b7659c22a503ea1d2ffcf6588ad82192
Ben Hanson <bh412@cam.ac.uk>
2019-10-25 16:39:34 +0100

    Alter page fragment error message

--- src/app/components/elements/PageFragment.tsx
+++ src/app/components/elements/PageFragment.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect} from "react";
 import {AppState} from "../../state/reducers";
 import {ShowLoading} from "../handlers/ShowLoading";
 import {IsaacContent} from "../content/IsaacContent";
@@ -12,23 +12,18 @@ interface PageFragmentComponentProps {
 
 export const PageFragment = ({fragmentId}: PageFragmentComponentProps) => {
     const dispatch = useDispatch();
-    const [pathname, setPathname] = useState("");
     const fragment = useSelector((state: AppState) => state && state.fragments && state.fragments[fragmentId] || null);
 
     useEffect(() => {
         dispatch(fetchFragment(fragmentId))
     }, [fragmentId]);
 
-    useEffect(() => {
-        setPathname(location.pathname);
-    }, [location.pathname]);
-
     const notFoundComponent = <div>
         <h2>Content not found</h2>
         <h3 className="my-4">
             <small>
-                {"We're sorry, page not found: "}
-                <code>{pathname}</code>
+                {"We're sorry, page fragment not found: "}
+                <code>{fragmentId}</code>
             </small>
         </h3>
     </div>;
