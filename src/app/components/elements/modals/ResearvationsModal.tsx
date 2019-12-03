0057e7fa599410eafbcae32da3c6a2e035fc3f1d
Andrea Franceschini <andrea.franceschini@gmail.com>
2019-12-03 18:48:52 +0000

    WiP Getting group members is hard...

--- src/app/components/elements/modals/ResearvationsModal.tsx
+++ src/app/components/elements/modals/ResearvationsModal.tsx
@@ -1,32 +1,67 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import {connect} from "react-redux";
-import {closeActiveModal, loadGroups} from "../../../state/actions";
+import {closeActiveModal, loadGroups, getGroupMembers} from "../../../state/actions";
 import {store} from "../../../state/store";
-import * as RS from "reactstrap";
+import {
+    Row,
+    Col,
+    Button
+} from "reactstrap";
 import {RegisteredUserDTO, UserGroupDTO} from "../../../../IsaacApiTypes";
-import {AppState} from "../../../state/reducers";
+import {AppState, user} from "../../../state/reducers";
 import {groups} from '../../../state/selectors';
+import { ShowLoading } from "../../handlers/ShowLoading";
 
 const stateToProps = (state: AppState) => ({
     user: (state && state.user) as RegisteredUserDTO,
-    groups: groups.active(state)
+    groups: groups.active(state),
+    groupMembers: [] as Array<RegisteredUserDTO>
 });
-const dispatchToProps = { groups, loadGroups };
+const dispatchToProps = { groups, loadGroups, getGroupMembers };
 
 interface ReservationsModalProps {
     user: RegisteredUserDTO;
     groups: UserGroupDTO[] | null;
+    groupMembers: RegisteredUserDTO[] | null;
     loadGroups: (archivedGroupsOnly: boolean) => void;
+    getGroupMembers: (group: UserGroupDTO) => void;
 }
 
 const ReservationsModalComponent = (props: ReservationsModalProps) => {
-    const { user, groups, loadGroups } = props;
+    const { user, groups, loadGroups, getGroupMembers } = props;
+
+    const [groupMembers, setGroupMembers] = useState();
 
     useEffect(() => {
         loadGroups(false);
     }, []);
 
-    return <div>ReservationsModal {JSON.stringify(groups)}</div>
+    useEffect(() => {
+        // Promise here...
+        console.log(groupMembers);
+    }, [groupMembers]);
+
+    return <React.Fragment>
+        <Row>
+            <Col md={4}>
+                <ShowLoading until={groups}>
+                    {groups && groups.map(g => (
+                        <Row key={g.id}>
+                            <Col>
+                                <div className="group-item p-2">
+                                    <div className="d-flex justify-content-between align-items-center">
+                                        <Button color="link text-left" className="flex-fill" onClick={() => {setGroupMembers(getGroupMembers(g))}}>
+                                            {g.groupName}
+                                        </Button>
+                                    </div>
+                                </div>
+                            </Col>
+                        </Row>
+                    ))}
+                </ShowLoading>
+            </Col>
+        </Row>
+    </React.Fragment>
 }
 
 export const reservationsModal = () => {
