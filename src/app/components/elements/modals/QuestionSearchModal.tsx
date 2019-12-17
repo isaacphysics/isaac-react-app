dc78aabc707a62868032e046aa1612b00b6ab44d
James Sharkey <jsharkey13@users.noreply.github.com>
2019-12-17 13:04:03 +0000

    Remove unused imports

--- src/app/components/elements/modals/QuestionSearchModal.tsx
+++ src/app/components/elements/modals/QuestionSearchModal.tsx
@@ -1,4 +1,4 @@
-import React, {Dispatch, SetStateAction, useCallback, useEffect, useMemo, useState} from "react";
+import React, {useCallback, useEffect, useMemo, useState} from "react";
 import {closeActiveModal, searchQuestions} from "../../../state/actions";
 import * as RS from "reactstrap";
 import {SortableTableHeader} from "../SortableTableHeader";
