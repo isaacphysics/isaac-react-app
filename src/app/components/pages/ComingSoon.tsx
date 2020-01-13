d2228d07ff1b6464d82e901ca28738ed28f0a090
James Sharkey <jsharkey13@users.noreply.github.com>
2020-01-13 13:12:49 +0000

    Use more standard ID naming convention

--- src/app/components/pages/ComingSoon.tsx
+++ src/app/components/pages/ComingSoon.tsx
@@ -6,5 +6,5 @@ import {PageFragment} from "../elements/PageFragment";
 export const ComingSoon = () => <Container>
     <TitleAndBreadcrumb currentPageTitle="Coming soon" />
     <div className="mt-4"/>
-    <PageFragment fragmentId="comingSoon"/>
+    <PageFragment fragmentId="coming_soon"/>
 </Container>;
