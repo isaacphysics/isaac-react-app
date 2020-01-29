fb7fdd0b032cc2d03acf7193765b8913bdda2f3f
James Sharkey <jsharkey13@users.noreply.github.com>
2020-01-29 15:00:43 +0000

    Make friendly dates use single-digit day (e.g. 5 Feb, not 05 Feb)
    # Leave it as e.g. 05/02/2020 for numeric dates.

--- src/app/components/elements/DateString.ts
+++ src/app/components/elements/DateString.ts
@@ -4,11 +4,11 @@ export const TIME_ONLY = new Intl.DateTimeFormat(dateLocale, {hour: "numeric", m
 // 22/01/2020
 export const NUMERIC_DATE = new Intl.DateTimeFormat(dateLocale);
 // Wed, 22 Jan 2020
-export const FRIENDLY_DATE = new Intl.DateTimeFormat(dateLocale, {weekday: "short", day: "2-digit", month: "short", year: "numeric"});
+export const FRIENDLY_DATE = new Intl.DateTimeFormat(dateLocale, {weekday: "short", day: "numeric", month: "short", year: "numeric"});
 // 22/01/2020, 13:00:00
 export const NUMERIC_DATE_AND_TIME = new Intl.DateTimeFormat(dateLocale, {year: "numeric", month: "numeric", day: "numeric", hour: "numeric", minute: "numeric", second: "numeric", hour12: false});
 // Wed, 22 Jan 2020, 1:00 pm
-export const FRIENDLY_DATE_AND_TIME = new Intl.DateTimeFormat(dateLocale, {weekday: "short", day: "2-digit", month: "short", year: "numeric", hour: "numeric", minute: "2-digit", hour12: true});
+export const FRIENDLY_DATE_AND_TIME = new Intl.DateTimeFormat(dateLocale, {weekday: "short", day: "numeric", month: "short", year: "numeric", hour: "numeric", minute: "2-digit", hour12: true});
 
 export function formatDate(date: number | Date | undefined) {
     if (!date) return "Unknown";
