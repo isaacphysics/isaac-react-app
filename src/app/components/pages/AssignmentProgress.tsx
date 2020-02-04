71e0230ddded04fb0bb2a6a025214d7b9a6afac8
Meurig Thomas <mlt47@cam.ac.uk>
2020-02-04 15:28:09 +0000

    Remove link to student progress from the assignment progress page

--- src/app/components/pages/AssignmentProgress.tsx
+++ src/app/components/pages/AssignmentProgress.tsx
@@ -342,10 +342,8 @@ const ProgressDetails = (props: ProgressDetailsProps) => {
                             const fullAccess = studentProgress.user.authorisedFullAccess;
                             return <tr key={studentProgress.user.id} className={`${markClasses(studentProgress, assignmentTotalQuestionParts)}${fullAccess ? "" : " revoked"}`} title={`${studentProgress.user.givenName + " " + studentProgress.user.familyName}`}>
                                 <th className="student-name">
-                                    <Button color="link" tag={Link} to={`/progress/${studentProgress.user.id}`} target="_blank">
-                                        {studentProgress.user.givenName}
-                                        <span className="d-none d-lg-inline"> {studentProgress.user.familyName}</span>
-                                    </Button>
+                                    {studentProgress.user.givenName}
+                                    <span className="d-none d-lg-inline"> {studentProgress.user.familyName}</span>
                                 </th>
                                 {questions.map((q, index) =>
                                     <td key={q.id} className={markQuestionClasses(studentProgress, index)} onClick={() => setSelectedQuestion(index)}>
