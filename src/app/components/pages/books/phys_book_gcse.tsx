5298eb32f831e641fb5759880d86696aa45b2734
Ben Hanson <bh412@cam.ac.uk>
2020-02-07 14:26:04 +0000

    Increase content spacing and align correctly

--- src/app/components/pages/books/phys_book_gcse.tsx
+++ src/app/components/pages/books/phys_book_gcse.tsx
@@ -14,42 +14,43 @@ export const PhysBookGcse = () => {
     return <Container className="physics">
         <Col>
             <div className="book-intro">
-                <TitleAndBreadcrumb currentPageTitle="Mastering Essential GCSE Physics" intermediateCrumbs={[STUDENTS_CRUMB]} help={pageHelp} />
+                <TitleAndBreadcrumb className="mb-5" currentPageTitle="Mastering Essential GCSE Physics" intermediateCrumbs={[STUDENTS_CRUMB]} help={pageHelp} />
                 <img className="book-cover" src="/assets/phy/books/phys_book_gcse.jpg" alt="Cover of the book."/>
                 <PageFragment fragmentId="phys_book_gcse_intro"/>
             </div>
-            <Row/>
             <div className="book-levels">
-                <h4>Chapters:</h4>
-                <Row>
-                    <Col md={6} className="p-2">
-                        <BookChapter chapterId={'gcse_ch1_boards'} chapterTitle={'Skills'} chapterSubHeading={'(Sections 1-7)'} chapterIcon={'1'} chapterSubject={SUBJECTS.PHYSICS}/>
-                    </Col>
-                    <Col md={6} className="p-2">
-                        <BookChapter chapterId={'gcse_ch2_boards'} chapterTitle={'Mechanics'} chapterSubHeading={'(Sections 8-21)'} chapterIcon={'2'} chapterSubject={SUBJECTS.PHYSICS}/>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col md={6} className="p-2">
-                        <BookChapter chapterId={'gcse_ch3_boards'} chapterTitle={'Electricity'} chapterSubHeading={'(Sections 22-29)'}  chapterIcon={'3'} chapterSubject={SUBJECTS.PHYSICS}/>
-                    </Col>
-                    <Col md={6} className="p-2">
-                        <BookChapter chapterId={'gcse_ch4_boards'} chapterTitle={'Energy'} chapterSubHeading={'(Sections 30-37)'}  chapterIcon={'4'} chapterSubject={SUBJECTS.PHYSICS}/>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col md={6} className="p-2">
-                        <BookChapter chapterId={'gcse_ch5_boards'} chapterTitle={'Waves and Optics'} chapterSubHeading={'(Sections 38-50)'}  chapterIcon={'5'} chapterSubject={SUBJECTS.PHYSICS}/>
-                    </Col>
-                    <Col md={6} className="p-2">
-                        <BookChapter chapterId={'gcse_ch6_boards'} chapterTitle={'Nuclear'} chapterSubHeading={'(Sections 51-58)'}  chapterIcon={'6'} chapterSubject={SUBJECTS.PHYSICS}/>
-                    </Col>
-                </Row>
-                <Row className="mb-4">
-                    <Col md={6} className="p-2">
-                        <BookChapter chapterId={'gcse_ch7_boards'} chapterTitle={'Gas'} chapterSubHeading={'(Sections 59-62)'}  chapterIcon={'7'} chapterSubject={SUBJECTS.PHYSICS}/>
-                    </Col>
-                </Row>
+                <Col>
+                    <h4>Chapters:</h4>
+                    <Row>
+                        <Col md={6} className="p-2">
+                            <BookChapter chapterId={'gcse_ch1_boards'} chapterTitle={'Skills'} chapterSubHeading={'(Sections 1-7)'} chapterIcon={'1'} chapterSubject={SUBJECTS.PHYSICS}/>
+                        </Col>
+                        <Col md={6} className="p-2">
+                            <BookChapter chapterId={'gcse_ch2_boards'} chapterTitle={'Mechanics'} chapterSubHeading={'(Sections 8-21)'} chapterIcon={'2'} chapterSubject={SUBJECTS.PHYSICS}/>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col md={6} className="p-2">
+                            <BookChapter chapterId={'gcse_ch3_boards'} chapterTitle={'Electricity'} chapterSubHeading={'(Sections 22-29)'}  chapterIcon={'3'} chapterSubject={SUBJECTS.PHYSICS}/>
+                        </Col>
+                        <Col md={6} className="p-2">
+                            <BookChapter chapterId={'gcse_ch4_boards'} chapterTitle={'Energy'} chapterSubHeading={'(Sections 30-37)'}  chapterIcon={'4'} chapterSubject={SUBJECTS.PHYSICS}/>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col md={6} className="p-2">
+                            <BookChapter chapterId={'gcse_ch5_boards'} chapterTitle={'Waves and Optics'} chapterSubHeading={'(Sections 38-50)'}  chapterIcon={'5'} chapterSubject={SUBJECTS.PHYSICS}/>
+                        </Col>
+                        <Col md={6} className="p-2">
+                            <BookChapter chapterId={'gcse_ch6_boards'} chapterTitle={'Nuclear'} chapterSubHeading={'(Sections 51-58)'}  chapterIcon={'6'} chapterSubject={SUBJECTS.PHYSICS}/>
+                        </Col>
+                    </Row>
+                    <Row className="mb-4">
+                        <Col md={6} className="p-2">
+                            <BookChapter chapterId={'gcse_ch7_boards'} chapterTitle={'Gas'} chapterSubHeading={'(Sections 59-62)'}  chapterIcon={'7'} chapterSubject={SUBJECTS.PHYSICS}/>
+                        </Col>
+                    </Row>
+                </Col>
             </div>
         </Col>
     </Container>
