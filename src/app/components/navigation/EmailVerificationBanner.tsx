isaacphysics/isaac-react-app

commit 1e6327e80d5c98c000bf2fe56f5771a545b21544

Add element IDs to make automated testing easier

src/app/components/navigation/EmailVerificationBanner.tsx
@@ -35,7 +35,7 @@ const EmailVerificationBannerComponent = ({user, requestEmailVerification}: Emai
 
     const show = user != null && user.loggedIn && status != "VERIFIED" && !hidden;
 
-    return show ? <div className="banner d-print-none">
+    return show ? <div className="banner d-print-none" id="email-status-banner">
         <RS.Container className="py-3">
 
             <RS.Row style={{alignItems: "center"}}>
@@ -47,14 +47,20 @@ const EmailVerificationBannerComponent = ({user, requestEmailVerification}: Emai
                 </RS.Col>
                 {(status == null || status == "NOT_VERIFIED") && <React.Fragment>
                     <RS.Col xs={12} sm={10} md={8}>
-                        <small>Your email address is not verified -
-                            please find our email in your inbox and follow the verification link. You can <Link onClick={clickVerify}>request a new
-                                verification email</Link> if necessary. To change
+                        <small>Your email address is not verified - please find our email in your inbox and follow the
+                            verification link. You can
+                        <Link onClick={clickVerify} id="email-verification-request">
+                            request a new verification email
+                        </Link> if necessary. To change
                             your account email, go to <Link to="/account">My account</Link>.
                         </small>
                     </RS.Col>
                     <RS.Col xs={12} md={3} className="text-center">
-                        <RS.Button color="primary" outline className="mt-3 mb-2 d-block d-md-inline-block banner-button" onClick={clickSnooze}>Snooze</RS.Button>
+                        <RS.Button color="primary" outline className="mt-3 mb-2 d-block d-md-inline-block banner-button"
+                            onClick={clickSnooze} id="email-verification-snooze"
+                        >
+                            Snooze
+                        </RS.Button>
                     </RS.Col>
                 </React.Fragment>}
                 {(status == "DELIVERY_FAILED") &&

src/app/components/navigation/CookieBanner.tsx
@@ -20,7 +20,7 @@ const CookieBannerComponent = ({logAction}: {logAction: (eventDetails: object) =
         logAction(eventDetails);
     }
 
-    return show ? <div className="banner d-print-none">
+    return show ? <div className="banner d-print-none" id="cookie-banner">
         <RS.Container className="py-3">
             <RS.Row style={{alignItems: "center"}}>
                 <RS.Col xs={12} sm={2} md={1}>

commit 95926483f4ae6351c155be01442a3a40cfb11e08

Ensure spacing is correct in Email Verification banner messages
# This got broken on mobile in 1e6327e80d5c98c000bf2fe56f5771a545b21544

src/app/components/navigation/EmailVerificationBanner.tsx
@@ -48,11 +48,9 @@ const EmailVerificationBannerComponent = ({user, requestEmailVerification}: Emai
                 {(status == null || status == "NOT_VERIFIED") && <React.Fragment>
                     <RS.Col xs={12} sm={10} md={8}>
                         <small>Your email address is not verified - please find our email in your inbox and follow the
-                            verification link. You can
-                        <Link onClick={clickVerify} id="email-verification-request">
-                            request a new verification email
-                        </Link> if necessary. To change
-                            your account email, go to <Link to="/account">My account</Link>.
+                            verification link. You can <Link onClick={clickVerify} id="email-verification-request">
+                            request a new verification email</Link> if necessary. To change your account email,
+                            go to <Link to="/account">My account</Link>.
                         </small>
                     </RS.Col>
                     <RS.Col xs={12} md={3} className="text-center">
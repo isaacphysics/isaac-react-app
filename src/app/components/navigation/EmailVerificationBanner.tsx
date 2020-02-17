95926483f4ae6351c155be01442a3a40cfb11e08
James Sharkey <jsharkey13@users.noreply.github.com>
2020-02-17 14:11:22 +0000

    Ensure spacing is correct in Email Verification banner messages
    # This got broken on mobile in 1e6327e80d5c98c000bf2fe56f5771a545b21544

--- src/app/components/navigation/EmailVerificationBanner.tsx
+++ src/app/components/navigation/EmailVerificationBanner.tsx
@@ -48,11 +48,9 @@ const EmailVerificationBannerComponent = ({user, requestEmailVerification}: Emai
                 {(status == null || status == "NOT_VERIFIED") && <React.Fragment>
                     <RS.Col xs={12} sm={10} md={8}>
                         <small>Your email address is not verified - please find our email in your inbox and follow the
-                            verification link. You can
-                        <Link onClick={clickVerify} id="email-verification-request">
-                            request a new verification email
-                        </Link> if necessary. To change
-                            your account email, go to <Link to="/account">My account</Link>.
+                            verification link. You can <Link onClick={clickVerify} id="email-verification-request">
+                            request a new verification email</Link> if necessary. To change your account email,
+                            go to <Link to="/account">My account</Link>.
                         </small>
                     </RS.Col>
                     <RS.Col xs={12} md={3} className="text-center">
