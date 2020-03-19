c87af86a31a6fb82ab7d735f8bc4bcb3234aa308
Robin Message <robin@lambdacambridge.com>
2020-03-19 17:23:21 +0000

    Physics: footer with all links

--- src/app/components/elements/list-groups/ListGroupFooterBottom.tsx
+++ src/app/components/elements/list-groups/ListGroupFooterBottom.tsx
@@ -1,36 +1,48 @@
 import React from "react";
 import {ListGroup, ListGroupItem} from "reactstrap";
-
-const ExternalLink = ({href, children, className=""}: {href: string; children: any; className?: string}) => (
-    // eslint-disable-next-line react/jsx-no-target-blank
-    <a href={href} target="_blank" rel="noopener" className={className}>
-        {children}
-    </a>
-);
+import {SITE, SITE_SUBJECT} from "../../../services/siteConstants";
+import {ExternalLink} from "../ExternalLink";
 
 export const ListGroupFooterBottom = () => (
-    <div className='footer-links footer-bottom'>
-        <ListGroup className='d-flex flex-wrap flex-row'>
-            <ListGroupItem className='footer-bottom-info border-0 px-0 py-0 bg-transparent'>
-                <p className='pt-2 mb-lg-0'>
-                    All teaching materials on this site are available under the&nbsp;
-                    <ExternalLink href="https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/" className="d-inline text-dark font-weight-bold">
-                        Open Government Licence v3.0
-                    </ExternalLink>, except where otherwise stated.
-                </p>
-            </ListGroupItem>
+    SITE_SUBJECT === SITE.CS ?
+        <div className='footer-links footer-bottom'>
+            <ListGroup className='d-flex flex-wrap flex-row'>
+                <ListGroupItem className='footer-bottom-info border-0 px-0 py-0 bg-transparent'>
+                    <p className='pt-2 mb-lg-0'>
+                        All teaching materials on this site are available under the&nbsp;
+                        <ExternalLink href="https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/" className="d-inline text-dark font-weight-bold">
+                            Open Government Licence v3.0
+                        </ExternalLink>, except where otherwise stated.
+                    </p>
+                </ListGroupItem>
 
-            <ListGroupItem className='footer-bottom-logos border-0 px-0 py-0 pb-4 pb-md-1 bg-transparent d-flex justify-content-between d-print-none'>
-                <ExternalLink href="https://teachcomputing.org/">
-                    <img src="/assets/logos/ncce.png" alt='National Centre for Computing Education website' className='logo-mr' height="57px" />
-                </ExternalLink>
-                <ExternalLink href="https://www.raspberrypi.org/">
-                    <img src="/assets/logos/raspberry-pi.png" alt='Raspberry Pi website' className='logo-mr' height="57px" />
-                </ExternalLink>
-                <ExternalLink href="https://isaacphysics.org/">
-                    <img src="/assets/logos/isaacphysics.png" alt='Issac Physics website' className='logo-mr' height="57px" />
-                </ExternalLink>
-            </ListGroupItem>
-        </ListGroup>
-    </div>
+                <ListGroupItem className='footer-bottom-logos border-0 px-0 py-0 pb-4 pb-md-1 bg-transparent d-flex justify-content-between d-print-none'>
+                    <ExternalLink href="https://teachcomputing.org/">
+                        <img src="/assets/logos/ncce.png" alt='National Centre for Computing Education website' className='logo-mr' height="57px" />
+                    </ExternalLink>
+                    <ExternalLink href="https://www.raspberrypi.org/">
+                        <img src="/assets/logos/raspberry-pi.png" alt='Raspberry Pi website' className='logo-mr' height="57px" />
+                    </ExternalLink>
+                    <ExternalLink href="https://isaacphysics.org/">
+                        <img src="/assets/logos/isaacphysics.png" alt='Issac Physics website' className='logo-mr' height="57px" />
+                    </ExternalLink>
+                </ListGroupItem>
+            </ListGroup>
+        </div>
+        :
+        <div className="w-100">
+            <div className='float-md-right text-right'>
+                All materials on this site are licensed under the<br />
+                <ExternalLink href="https://creativecommons.org/licenses/by/4.0/">
+                    <strong>Creative Commons license</strong>
+                </ExternalLink>, unless stated otherwise.
+            </div>
+            <div className='left'>
+                A&nbsp;<ExternalLink href="https://www.gov.uk/government/organisations/department-for-education">
+                    <strong>Department for Education</strong></ExternalLink> project <br /> at the{' '}
+                <ExternalLink href="https://www.cam.ac.uk"><strong>University of Cambridge</strong></ExternalLink>,
+                <br />supported by{' '}
+                <ExternalLink href="https://www.ogdentrust.com/"><strong>The Ogden Trust</strong></ExternalLink>.
+            </div>
+        </div>
 );
