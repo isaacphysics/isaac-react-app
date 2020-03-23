5b49b1b0b111555184260c9ac7f80c41c8d19e15
Ben Hanson <bh412@cam.ac.uk>
2020-03-23 11:50:19 +0000

    Move out physics specific routes

--- src/app/components/site/phy/RoutesPhy.tsx
+++ src/app/components/site/phy/RoutesPhy.tsx
@@ -6,6 +6,9 @@ import {PhysBookGcse} from "../../pages/books/phys_book_gcse";
 import {PhysicsSkills14} from "../../pages/books/physics_skills_14";
 import {PreUniMaths} from "../../pages/books/pre_uni_maths";
 import {Chemistry16} from "../../pages/books/chemistry_16";
+import StaticPageRoute from "../../navigation/StaticPageRoute";
+import {Redirect} from "react-router";
+import {isTeacher} from "../../../services/user";
 
 let key = 0;
 export const RoutesPhy = [
@@ -18,4 +21,29 @@ export const RoutesPhy = [
     <TrackedRoute key={key++} exact path="/books/physics_skills_14" component={PhysicsSkills14}/>,
     <TrackedRoute key={key++} exact path="/books/pre_uni_maths" component={PreUniMaths}/>,
     <TrackedRoute key={key++} exact path="/books/chemistry_16" component={Chemistry16}/>,
+
+    <StaticPageRoute key={key++} exact path="/glossary" />,
+    <StaticPageRoute key={key++} exact path="/apply_uni" />,
+    <StaticPageRoute key={key++} exact path="/solving_problems" />,
+    <StaticPageRoute key={key++} exact path="/extraordinary_problems" pageId="extraordinary_problems_index" />,
+    <StaticPageRoute key={key++} exact path="/challenge_problems" pageId="challenge_problems_index" />,
+    <StaticPageRoute key={key++} exact path="/bios" />,
+    <StaticPageRoute key={key++} exact path="/why_physics" />,
+    <StaticPageRoute key={key++} exact path="/fast_track_14" pageId="fast_track_14_index" />,
+    <StaticPageRoute key={key++} exact path="/prize_draws" />,
+    <StaticPageRoute key={key++} exact path="/spc" />,
+    <StaticPageRoute key={key++} exact path="/chemistry" pageId="chemistry_landing_page" />,
+    <StaticPageRoute key={key++} exact path="/survey" />,
+    <StaticPageRoute key={key++} exact path="/book/question" pageId="book_question" />,
+    <StaticPageRoute key={key++} exact path="/exam_uni_help" />,
+    <StaticPageRoute key={key++} exact path="/gcse" />,
+    <StaticPageRoute key={key++} exact path="/alevel" />,
+
+    <Redirect key={key++} exact from="/mission" to="/about" />,
+    <Redirect key={key++} exact from="/boards" to="/my_gameboards" />,
+
+    <Redirect key={key++} exact from="/board/:id" to="/gameboards#:id" />,
+
+    <StaticPageRoute key={key++} exact ifUser={isTeacher} path="/teachermentoring_gcse" pageId="fragments/teacher_mentoring_gcse_page_frag" />,
+    <StaticPageRoute key={key++} exact ifUser={isTeacher} path="/teachermentoring_alevel" pageId="fragments/teacher_mentoring_alevel_page_frag" />,
 ];
