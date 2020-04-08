e52c29ff6e8ec24be95b3aee4062fe3bbab549ce
Ben Hanson <bh412@cam.ac.uk>
2020-04-08 11:55:11 +0100

    Correct spelling

--- src/app/components/elements/inputs/subjectInterestInput.tsx
+++ src/app/components/elements/inputs/subjectInterestInput.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import {CustomInput} from "reactstrap";
 
-interface SujectInterestInputProps<T> {
+interface SubjectInterestInputProps<T> {
     stateObject: T;
     propertyName: string;
     setStateFunction: (stateObject: T) => void;
 }
-export const SubjectInterestInput = (props: SujectInterestInputProps<any>) => {
+export const SubjectInterestInput = (props: SubjectInterestInputProps<any>) => {
     const {stateObject, propertyName, setStateFunction} = props
     return <CustomInput id={`${propertyName}-checkbox`} type="checkbox" className="isaac-checkbox" checked={stateObject[propertyName] === true}
         onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
