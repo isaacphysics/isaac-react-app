c47a887bd4685e09fc9e01734789de24ffb7cc31
Meurig Thomas <mlt47@cam.ac.uk>
2020-04-28 12:37:49 +0100

    Simplify tab code so that useEffect is not necessary

--- src/app/components/content/IsaacTabs.tsx
+++ src/app/components/content/IsaacTabs.tsx
@@ -4,8 +4,8 @@ import {ContentDTO} from "../../../IsaacApiTypes";
 import {IsaacContent} from "./IsaacContent";
 import {useCurrentExamBoard} from "../../services/examBoard";
 import {SITE, SITE_SUBJECT} from "../../services/siteConstants";
-import { useDispatch } from "react-redux";
-import {openActiveModal, closeActiveModal} from '../../state/actions';
+import {useDispatch} from "react-redux";
+import {closeActiveModal, openActiveModal} from '../../state/actions';
 import {store} from '../../state/store';
 
 interface IsaacTabsProps {
@@ -26,7 +26,6 @@ export const IsaacTabs = (props: any) => {
         }))
     }
 
-    let activeTab = 1;
     children.forEach((child, index) => {
         const tabTitle = child.title || `Tab ${index + 1}`;
         tabTitlesToContent[tabTitle] = <IsaacContent doc={child} />;
@@ -44,7 +43,7 @@ export const IsaacTabs = (props: any) => {
     }
 
     // Normal case
-    return <Tabs className="isaac-tab" tabContentClass="pt-4" activeTabOverride={activeTab}>
+    return <Tabs className="isaac-tab" tabContentClass="pt-4">
         {tabTitlesToContent}
     </Tabs>;
 };
