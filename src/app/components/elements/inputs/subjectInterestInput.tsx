42642d40f104f2efabddc6dee4bde11adfd66de4
Ben Hanson <bh412@cam.ac.uk>
2020-05-15 13:47:51 +0100

    Remove unused imports

--- src/app/components/elements/inputs/subjectInterestInput.tsx
+++ src/app/components/elements/inputs/subjectInterestInput.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useReducer, useState} from "react";
+import React, {useEffect, useState} from "react";
 import {CustomInput} from "reactstrap";
 
 interface SubjectInterestInputProps<T> {
