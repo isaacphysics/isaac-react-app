8013bafe7b9cfbc0fc37340c502261ae928be715
Andrea Franceschini <andrea.franceschini@gmail.com>
2020-05-21 18:17:48 +0100

    UI styling, undo, and redo

--- src/app/components/elements/modals/GraphSketcherModal.tsx
+++ src/app/components/elements/modals/GraphSketcherModal.tsx
@@ -19,13 +19,9 @@ const GraphSketcherModalComponent = (props: any) => {
         }
     };
 
-    const undo = () => {
-        // TODO: Implement this
-    };
+    const undo = () => sketch?.undo();
 
-    const redo = () => {
-        // TODO: Implement this
-    };
+    const redo = () => sketch?.redo();
 
     const deleteSelected = () => {
         // TODO: Implement this
@@ -56,12 +52,12 @@ const GraphSketcherModalComponent = (props: any) => {
 
     return <div id='graph-sketcher-modal' style={{border: '5px solid black'}}>
         <div className="graph-sketcher-ui">
-            <div className="button" role="button" onClick={undo} onKeyUp={undo} tabIndex={0} id="graph-sketcher-ui-redo">redo</div>
-            <div className="button" role="button" onClick={redo} onKeyUp={redo} tabIndex={0} id="graph-sketcher-ui-undo">undo</div>
-            <div className="button" role="button" onClick={ () => setLineType(LineType.BEZIER) } onKeyUp={ () => setLineType(LineType.BEZIER) } tabIndex={0} id="graph-sketcher-ui-poly">poly</div>
-            <div className="button" role="button" onClick={ () => setLineType(LineType.LINEAR) } onKeyUp={ () => setLineType(LineType.LINEAR) } tabIndex={0} id="graph-sketcher-ui-straight">straight</div>
+            <div className={ ['button', sketch?.isRedoable() ? 'visible' : 'hidden' ].join(' ')} role="button" onClick={redo} onKeyUp={redo} tabIndex={0} id="graph-sketcher-ui-redo-button">redo</div>
+            <div className={ ['button', sketch?.isUndoable() ? 'visible' : 'hidden' ].join(' ')} role="button" onClick={undo} onKeyUp={undo} tabIndex={0} id="graph-sketcher-ui-undo-button">undo</div>
+            <div className="button" role="button" onClick={ () => setLineType(LineType.BEZIER) } onKeyUp={ () => setLineType(LineType.BEZIER) } tabIndex={0} id="graph-sketcher-ui-bezier-button">poly</div>
+            <div className="button" role="button" onClick={ () => setLineType(LineType.LINEAR) } onKeyUp={ () => setLineType(LineType.LINEAR) } tabIndex={0} id="graph-sketcher-ui-linear-button">straight</div>
             <div className="button" role="button" onClick={deleteSelected} onKeyUp={deleteSelected} tabIndex={0} id="graph-sketcher-ui-trash-button">trash</div>
-            <div className="button" role="button" onClick={close} onKeyUp={close} tabIndex={0} id="graph-sketcher-ui-submit">submit</div>
+            <div className="button" role="button" onClick={close} onKeyUp={close} tabIndex={0} id="graph-sketcher-ui-submit-button">submit</div>
             <select className="dropdown" id="graph-sketcher-ui-color-select" defaultValue="Blue">
                 <option value="Blue">Blue</option>
                 <option value="Orange">Orange</option>
