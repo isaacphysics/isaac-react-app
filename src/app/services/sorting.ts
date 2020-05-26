45fa28608bf7258d5fed93defca4f2bb79373586
Meurig Thomas <mlt47@cam.ac.uk>
2020-05-26 16:40:12 +0100

    Display cs related questions only

--- src/app/services/sorting.ts
+++ src/app/services/sorting.ts
@@ -11,4 +11,28 @@ export const sortOnPredicateAndReverse = (predicate: string, reverse: boolean) =
     if (valueFromObject(a, predicate) < valueFromObject(b, predicate)) {return reverse ? 1 : -1;}
     else if (valueFromObject(a, predicate) > valueFromObject(b, predicate)) {return reverse ? -1 : 1;}
     else {return 0;}
+};
+
+export function sortByNumberStringValue<T>(field: keyof T, undefinedFirst: boolean = false) {
+    return function comparator(a: T, b: T) {
+        const aValue: string | undefined = a[field] as any;
+        const bValue: string | undefined = b[field] as any;
+        if (aValue === bValue) return 0;
+        if (aValue === undefined) return undefinedFirst ? -1 : 1;
+        if (bValue === undefined) return undefinedFirst ? 1 : -1;
+        const aInt = parseInt(aValue);
+        const bInt = parseInt(bValue);
+        return aInt > bInt ? 1 : aInt != bInt ? -1 : 0;
+    };
+}
+
+export function sortByStringValue<T>(field: keyof T, undefinedFirst: boolean = false) {
+    return function comparator(a: T, b: T) {
+        const aValue: string | undefined = a[field] as any;
+        const bValue: string | undefined = b[field] as any;
+        if (aValue === bValue) return 0;
+        if (aValue === undefined) return undefinedFirst ? -1 : 1;
+        if (bValue === undefined) return undefinedFirst ? 1 : -1;
+        return aValue.localeCompare(bValue, undefined, {numeric: true, sensitivity: 'base'});
+    };
 }
