aeef4ef87e8babd3f13282b1db095ce33a8416bf
Ben Hanson <bh412@cam.ac.uk>
2020-07-13 13:07:14 +0100

    Alter warning to be site agnostic

--- src/app/components/navigation/DowntimeWarningBanner.tsx
+++ src/app/components/navigation/DowntimeWarningBanner.tsx
@@ -3,6 +3,7 @@ import * as RS from 'reactstrap';
 import {Alert} from 'reactstrap';
 import Cookies from 'js-cookie';
 import {Link} from "react-router-dom";
+import {SITE, SITE_SUBJECT, SITE_SUBJECT_TITLE} from "../../services/siteConstants";
 
 const DOWNTIME_COOKIE = "downtimeBannerDismissed";
 
@@ -11,6 +12,10 @@ export const DowntimeWarningBanner = () => {
         const currentCookieValue = Cookies.get(DOWNTIME_COOKIE);
         return currentCookieValue != "1";
     });
+
+    const twitterLink = (SITE_SUBJECT === SITE.CS) ?
+        "https://twitter.com/isaaccompsci" :
+        "https://twitter.com/isaacphysics";
 
     function clickDismiss() {
         setShow(false);
@@ -22,7 +27,7 @@ export const DowntimeWarningBanner = () => {
             <RS.Container className="py-3">
                 <RS.Row style={{alignItems: "center"}}>
                     <RS.Col xs={12} sm={10} md={8}>
-                        <p>Please note that Isaac Computer Science will be unavailable during the morning of 1<sup>st</sup> August due to planned maintenance. Check our <Link to="https://twitter.com/isaaccompsci" target="_blank">Twitter feed</Link> for more information.</p>
+                        <p>Please note that Isaac {SITE_SUBJECT_TITLE} will be unavailable during the morning of 1<sup>st</sup> August due to planned maintenance. Check our <a href={twitterLink} target="_blank" rel="noopener noreferrer">Twitter feed</a> for more information.</p>
                     </RS.Col>
                     <RS.Col xs={12} md={3} className="text-center">
                         <RS.Button color="primary" outline className="mt-3 mb-2 d-block d-md-inline-block banner-button" onClick={clickDismiss}>
