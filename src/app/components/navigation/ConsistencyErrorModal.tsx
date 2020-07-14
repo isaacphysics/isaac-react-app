adf2eb2d071836a87fc5f448ac45cd8cbc0dfd7c
Meurig Thomas <mlt47@cam.ac.uk>
2020-07-14 11:49:47 +0100

    Use template literal to avoid possible linebreak problem

--- src/app/components/navigation/ConsistencyErrorModal.tsx
+++ src/app/components/navigation/ConsistencyErrorModal.tsx
@@ -4,7 +4,9 @@ import {SITE_SUBJECT_TITLE} from "../../services/siteConstants";
 
 export const ConsistencyErrorModal = ({consistencyError}: {consistencyError: boolean}) => {
     return <RS.Modal id="user-consistency-error" isOpen={consistencyError} size={"lg"}>
-        <RS.ModalHeader className="h-title pb-5 mb-4">Your Isaac {SITE_SUBJECT_TITLE} session has changed</RS.ModalHeader>
+        <RS.ModalHeader className="h-title pb-5 mb-4">
+            {`Your Isaac ${SITE_SUBJECT_TITLE} session has changed`}
+        </RS.ModalHeader>
         <RS.ModalBody className="px-0 mx-4">
             <h3>This browser window / tab is out of sync.</h3>
             <p className="pb-2">This can happen if you have logged out or logged in as another user via another browser window. Please click continue to avoid any issues.</p>
