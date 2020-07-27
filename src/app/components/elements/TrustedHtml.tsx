0b9069689dae3f800949868a9ae146ce57cd2249
James Sharkey <jsharkey13@users.noreply.github.com>
2020-07-27 09:26:06 +0100

    Add support for the `eqalign` LaTeX macro used on some pages
    # It's built-in to MathJax, but KaTeX doesn't have support by default.

--- src/app/components/elements/TrustedHtml.tsx
+++ src/app/components/elements/TrustedHtml.tsx
@@ -18,6 +18,7 @@ const BaseMacros: {[key: string]: MathJaxMacro} = {
     "quantity": ["{#1}\\,{\\rm{#2}}", 2],
     "valuedef": ["{#1}={\\quantity{#2}{#3}}", 3],
     "vtr": ["{\\underline{\\boldsymbol{#1}}}", 1],
+    "eqalign": ["\\begin{aligned}#1\\end{aligned}", 1],
     "d": "\\mathrm{d}",
     "vari": ["#1", 1],
     "s": ["_{\\sf{#1}}", 1],
