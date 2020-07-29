49942017522373164712252462926300e62b3cff
Meurig Thomas <mlt47@cam.ac.uk>
2020-07-29 15:08:49 +0100

    Improve three link navigation on mobile

--- src/app/components/elements/NavigationLinks.tsx
+++ src/app/components/elements/NavigationLinks.tsx
@@ -17,16 +17,23 @@ export const NavigationLinks = ({navigation}: {navigation: PageNavigation}) => {
         </Link>
     </div>;
 
-    const previousItemLink = navigation.previousItem && <div className="w-50 w-md-auto text-right mb-4">
+    const previousItemLink = navigation.previousItem && <div className="w-50 w-md-auto text-left mb-4">
         <Link to={{pathname: navigation.previousItem.to, search: navigation.queryParams}}>
             <div className="isaac-nav-link float-left a-alt lrg-text font-weight-bold">{navigation.previousItem.title}</div>
             <div className="isaac-nav-link float-left previous-link">Previous</div>
         </Link>
     </div>;
 
-    return <div className="mt-4 d-flex justify-content-between align-items-stretch no-print">
-        {previousItemLink}
-        {backToCollectionLink}
-        {nextItemLink}
-    </div>
+    const threeLinks = [previousItemLink, backToCollectionLink, nextItemLink].filter(l => l).length === 3;
+
+    return <React.Fragment>
+        <div className="mt-4 d-flex justify-content-between align-items-stretch no-print">
+            {previousItemLink}
+            <span className={threeLinks ? "d-none d-xl-block" : ""}>{backToCollectionLink}</span>
+            {nextItemLink}
+        </div>
+        {threeLinks && <div className="d-xl-none">
+            {backToCollectionLink}
+        </div>}
+    </React.Fragment>
 };
