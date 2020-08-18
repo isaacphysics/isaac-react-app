19271d83ca25dda630f5452104b85b4d0845e940
Meurig Thomas <mlt47@cam.ac.uk>
2020-08-18 09:42:12 +0100

    Add debugging information to the Client Error page

--- src/app/components/pages/ClientError.tsx
+++ src/app/components/pages/ClientError.tsx
@@ -1,16 +1,28 @@
 import React from "react";
 import {Link} from "react-router-dom";
-import {Container} from "reactstrap";
+import {Alert, Col, Container, Row} from "reactstrap";
 import {TitleAndBreadcrumb} from "../elements/TitleAndBreadcrumb";
 import ReactGA from "react-ga";
 import {WEBMASTER_EMAIL} from "../../services/siteConstants";
 import {FallbackProps} from "react-error-boundary";
+import {useSelector} from "react-redux";
+import {selectors} from "../../state/selectors";
 
 export const ClientError = ({resetErrorBoundary, error, componentStack}: FallbackProps) => {
+    const user = useSelector(selectors.user.orNull);
     ReactGA.exception({
         description: 'client_error',
         fatal: true
     });
+
+    const usefulInformation = {
+        "User ID": user?.loggedIn && user.id || "Not currently logged in",
+        "Location": window.location.href,
+        "User Agent": window.navigator.userAgent,
+        "Error Details": "\n" + error?.stack || "",
+    }
+    const plainTextUsefulInformation = "\n\n---- Useful Error Information ----\n\n" + Object.entries(usefulInformation)
+        .map(([label, value]) => `${label}: ${value}`).join("\n\n")
 
     return <Container>
         <div>
@@ -34,7 +46,7 @@ export const ClientError = ({resetErrorBoundary, error, componentStack}: Fallbac
                     </Link>
                     {", or "}
                     <Link
-                        to={`/contact?subject=Client Error Recorded: ${error?.message || "Error"}`}
+                        to={`/contact?subject=App Error&message=${encodeURI(plainTextUsefulInformation)}`}
                         onKeyPress={() => resetErrorBoundary()} onClick={() => resetErrorBoundary()}
                     >
                         contact
@@ -43,6 +55,18 @@ export const ClientError = ({resetErrorBoundary, error, componentStack}: Fallbac
                     <a href={`mailto:${WEBMASTER_EMAIL}`}>email</a>
                     {" us if this keeps happening."}
                 </small>
+                <Row className="mt-4 mb-5">
+                    <Col>
+                        <div className="alert alert-info small overflow-auto">
+                            <h4>Useful information to include in your email</h4>
+                            <small>
+                                {Object.entries(usefulInformation).map(([label, value]) => (
+                                    <p><strong>{label}: </strong>{value}</p>
+                                ))}
+                            </small>
+                        </div>
+                    </Col>
+                </Row>
             </h3>
         </div>
     </Container>;
