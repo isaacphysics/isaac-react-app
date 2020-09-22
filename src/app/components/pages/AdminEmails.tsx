1c07d9d6a3ace20b2f051abe2eadf69eb5f26072
Connor Holloway <root_pfad@protonmail.com>
2020-09-22 09:29:48 +0100

    Remove unused import

--- src/app/components/pages/AdminEmails.tsx
+++ src/app/components/pages/AdminEmails.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import {useDispatch, useSelector} from "react-redux";
 import * as RS from "reactstrap";
 import {AppState} from "../../state/reducers";
