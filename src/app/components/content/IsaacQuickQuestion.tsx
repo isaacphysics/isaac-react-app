6374127a1759434c1b48183f37e9df89dcb2ab0d
Meurig Thomas <mlt47@dc.cl.cam.ac.uk>
2020-10-06 22:01:06 +0100

    Add FastTrack related content buttons to Quick Questions

--- src/app/components/content/IsaacQuickQuestion.tsx
+++ src/app/components/content/IsaacQuickQuestion.tsx
@@ -3,12 +3,26 @@ import {Alert, Button, Col, Row} from "reactstrap";
 import * as ApiTypes from "../../../IsaacApiTypes";
 import {ContentDTO} from "../../../IsaacApiTypes";
 import {IsaacContentValueOrChildren} from "./IsaacContentValueOrChildren";
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {logAction} from "../../state/actions";
+import {AppState} from "../../state/reducers";
+import {DOCUMENT_TYPE, NOT_FOUND} from "../../services/constants";
+import {resourceFound} from "../../services/validation";
+import {determineFastTrackPrimaryAction, determineFastTrackSecondaryAction} from "../../services/fastTrack";
+import {RouteComponentProps, withRouter} from "react-router";
+import queryString from "query-string";
+import {useCurrentExamBoard} from "../../services/examBoard";
 
+export const IsaacQuickQuestion = withRouter(({doc, location}: {doc: ApiTypes.IsaacQuickQuestionDTO} & RouteComponentProps) => {
+    const {board, questionHistory: questionHistoryUrl}: {board?: string; questionHistory?: string} = queryString.parse(location.search);
+    const questionHistory = questionHistoryUrl?.split(",") || [];
 
-export const IsaacQuickQuestion = ({doc}: {doc: ApiTypes.IsaacQuickQuestionDTO}) => {
     const dispatch = useDispatch();
+    const page = useSelector((state: AppState) => state?.doc && state.doc !== NOT_FOUND ? state.doc : undefined);
+    const pageCompleted = useSelector((state: AppState) => state?.questions ? state.questions.pageCompleted : false);
+    const examBoard = useCurrentExamBoard();
+    const isFastTrackPage = page?.type === DOCUMENT_TYPE.FAST_TRACK_QUESTION;
+
     const [isVisible, setVisible] = useState(false);
 
     const toggle = () => {
@@ -21,22 +35,39 @@ export const IsaacQuickQuestion = ({doc}: {doc: ApiTypes.IsaacQuickQuestionDTO})
     };
 
     const answer: ContentDTO = doc.answer as ContentDTO;
+    const fastTrackInfo = {doc, correct: false, page, pageCompleted, questionHistory, board, examBoard, canSubmit: true}
+
+    const secondaryAction = determineFastTrackSecondaryAction(fastTrackInfo);
 
     return <form onSubmit={e => e.preventDefault()}>
         <div className="question-component p-md-5">
-            <div className="quick-question">
+            <div className={!isFastTrackPage ? "quick-question" : ""}>
                 <div className="question-content clearfix">
                     <IsaacContentValueOrChildren {...doc} />
                 </div>
-                <Row>
-                    <Col sm="12" md={{size: 10, offset: 1}}>
-                        <Button color="secondary" block className={isVisible ? "active": ""} onClick={toggle}>
-                            {isVisible ? "Hide answer" : "Show answer"}
-                        </Button>
-                    </Col>
-                </Row>
+                {!isFastTrackPage ?
+                    <Row>
+                        <Col sm="12" md={{size: 10, offset: 1}}>
+                            <Button color="secondary" block className={isVisible ? "active" : ""} onClick={toggle}>
+                                {isVisible ? "Hide answer" : "Show answer"}
+                            </Button>
+                        </Col>
+                    </Row>
+                    :
+                    <div className={`d-flex align-items-stretch flex-column-reverse flex-sm-row flex-md-column-reverse flex-lg-row mb-4`}>
+                        {secondaryAction && <div className={`m-auto pt-3 pb-1 w-100 w-sm-50 w-md-100 w-lg-50 pr-sm-2 pr-md-0 pr-lg-3`}>
+                            <input {...secondaryAction} className="h-100 btn btn-outline-primary btn-block" />
+                        </div>}
+                        <div className={`m-auto pt-3 pb-1 w-100 w-sm-50 w-md-100 w-lg-50 "pl-sm-2 pl-md-0 pl-lg-3`}>
+                            <input
+                                onClick={toggle} value={isVisible ? "Hide answer" : "Show answer"}
+                                className={`h-100 btn btn-secondary btn-block {isVisible ? "active" : ""}`}
+                            />
+                        </div>
+                    </div>
+                }
                 {isVisible && <Row>
-                    <Col sm="12" md={{size: 10, offset: 1}}>
+                    <Col sm="12" md={!isFastTrackPage ? {size: 10, offset: 1} : {}}>
                         <Alert color="secondary" className="overflow-auto">
                             <IsaacContentValueOrChildren {...answer} />
                         </Alert>
@@ -44,6 +75,5 @@ export const IsaacQuickQuestion = ({doc}: {doc: ApiTypes.IsaacQuickQuestionDTO})
                 </Row>}
             </div>
         </div>
-    </form>
-    ;
-};
+    </form>;
+});
