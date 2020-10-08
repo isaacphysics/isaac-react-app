397c25112e823d37a8c34ae0871844119bf9e4a9
James Sharkey <jsharkey13@users.noreply.github.com>
2020-10-08 15:54:13 +0100

    Neaten user role checking functions and add explicit types for ESLint
    Perhaps I could have added an "isContentEditor" and reduced the duplication
    even further, but this seemed a useful first step.

--- src/app/services/user.tsx
+++ src/app/services/user.tsx
@@ -1,39 +1,40 @@
 import {LoggedInUser} from "../../IsaacAppTypes";
 
-export function isLoggedIn(user?: LoggedInUser | null) {
+export function isLoggedIn(user?: LoggedInUser | null): boolean {
     return user ? user.loggedIn : false;
 }
 
-export function isStudent(user?: LoggedInUser | null) {
-    return user ? user.loggedIn && user.role == "STUDENT" : false;
+export function isStudent(user?: LoggedInUser | null): boolean {
+    return user ? user.loggedIn && user.role === "STUDENT" : false;
 }
 
-export function isTeacher(user?: LoggedInUser | null) {
-    return user ? user.loggedIn && user.role != "STUDENT" : false;
+export function isTeacher(user?: LoggedInUser | null): boolean {
+    return user ? user.loggedIn && user.role !== "STUDENT" : false;
 }
 
-export function isAdmin(user?: LoggedInUser | null) {
-    return user ? user.loggedIn && user.role == "ADMIN" : false;
+export function isAdmin(user?: LoggedInUser | null): boolean {
+    return user ? user.loggedIn && user.role === "ADMIN" : false;
 }
 
-export function isEventManager(user?: LoggedInUser | null) {
-    return user ? user.loggedIn && (user.role == "EVENT_MANAGER") : false;
+export function isEventManager(user?: LoggedInUser | null): boolean {
+    return user ? user.loggedIn && (user.role === "EVENT_MANAGER") : false;
 }
 
-export function isStaff(user?: LoggedInUser | null) {
-    return user ? user.loggedIn && (user.role == "ADMIN" || user.role == "EVENT_MANAGER" || user.role == "CONTENT_EDITOR") : false;
+export function isStaff(user?: LoggedInUser | null): boolean {
+    return user ? user.loggedIn && (user.role === "ADMIN" || user.role === "EVENT_MANAGER" || user.role === "CONTENT_EDITOR") : false;
 }
 
-export function isEventLeader(user?: LoggedInUser | null) {
-    return user ? user.loggedIn && user.role == "EVENT_LEADER" : false;
+export function isEventLeader(user?: LoggedInUser | null): boolean {
+    return user ? user.loggedIn && user.role === "EVENT_LEADER" : false;
 }
 
-export function isAdminOrEventManager(user?: LoggedInUser | null) {
-    return user ? user.loggedIn && (user.role == "ADMIN" || user.role == "EVENT_MANAGER") : false;
+export function isAdminOrEventManager(user?: LoggedInUser | null): boolean {
+    return isAdmin(user) || isEventManager(user);
 }
 
-export function extractTeacherName(teacher: {givenName?: string; familyName?: string} | null) {
-    if (null == teacher)
+export function extractTeacherName(teacher: {givenName?: string; familyName?: string} | null): string | null {
+    if (null == teacher) {
         return null;
+    }
     return (teacher.givenName ? teacher.givenName.charAt(0) + ". " : "") + teacher.familyName;
 }
