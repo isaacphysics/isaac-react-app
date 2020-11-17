6d4822815c87590583ea3332abe8ebed9770fc43
Ben Hanson <bh412@cam.ac.uk>
2020-11-17 11:16:44 +0000

    Change gcse board link to physics and maths level 1

--- src/app/components/pages/Gcse.tsx
+++ src/app/components/pages/Gcse.tsx
@@ -22,7 +22,7 @@ export const Gcse = () => {
                 clickUrl: "/books/phys_book_gcse", image: {src: "/assets/phy/key_stage_sprite.svg#skills-book-cover"}
             }}/>
             <IsaacCard doc={{title: "Question Finder", subtitle: "Practise your problem solving skills with our level 1 questions.",
-                clickUrl: "/gameboards/new", image: {src: "/assets/phy/key_stage_sprite.svg#triple"}
+                clickUrl: "/gameboards/new?levels=1&subjects=physics,maths", image: {src: "/assets/phy/key_stage_sprite.svg#triple"}
             }}/>
         </Row>
         <Row className="card-deck isaac-cards-body my-3">
