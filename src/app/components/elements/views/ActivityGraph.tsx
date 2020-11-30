b579998d0249b4c4bfb65161f7c666a5e26a6cf3
James Sharkey <jsharkey13@users.noreply.github.com>
2020-11-30 18:11:25 +0000

    Fix My Progress activity graph to show all months including current month
    Also has the added bonus of being much simpler, now correctly includes the
    months that were zero from registration as it used to, and has no timezone
    issues since it's all local time now.

--- src/app/components/elements/views/ActivityGraph.tsx
+++ src/app/components/elements/views/ActivityGraph.tsx
@@ -4,38 +4,17 @@ import {NUMERIC_DATE} from "../DateString";
 import {AnsweredQuestionsByDate} from "../../../../IsaacApiTypes";
 
 export const ActivityGraph = ({answeredQuestionsByDate}: {answeredQuestionsByDate: AnsweredQuestionsByDate}) => {
-    const generateDateArray = (min: Date, max: Date) => {
-        const current = new Date(min);
-        const dates = [];
-        while (current <= max) {
-            dates.push(new Date(current));
-            current.setMonth(current.getMonth() + 1);
+
+    let selectedDates: string[] = [];
+    const foundDates = answeredQuestionsByDate ? Object.keys(answeredQuestionsByDate) : [];
+    if (foundDates && foundDates.length > 0) {
+        const nonZeroDates = foundDates.filter((date) => answeredQuestionsByDate && answeredQuestionsByDate[date] > 0);
+        if (nonZeroDates.length > 0) {
+            selectedDates = foundDates.sort().map((date) => NUMERIC_DATE.format(new Date(date)).split("/").reverse().join("-"));
         }
-        return dates;
-    };
+    }
 
     useEffect(() => {
-        const foundDates = answeredQuestionsByDate ? Object.keys(answeredQuestionsByDate) : [];
-        let selectedDates: string[] = [];
-        let minTime;
-        let maxTime;
-        if (foundDates && foundDates.length > 0) {
-            const nonZeroDates = foundDates.filter((date) => answeredQuestionsByDate && answeredQuestionsByDate[date] > 0);
-            if (nonZeroDates.length > 0) {
-                const minNonZeroDate = new Date(nonZeroDates.reduce((min, date) => date < min ? date : min));
-                const maxDate = new Date(foundDates.reduce((max, date) => date > max ? date : max));
-                let tempMinTime = new Date(minNonZeroDate.getTime());
-                let tempMaxTime = new Date(maxDate.getTime());
-                if (minNonZeroDate.getFullYear() == maxDate.getFullYear() && minNonZeroDate.getMonth() == maxDate.getMonth()) {
-                    tempMinTime.setMonth(minNonZeroDate.getMonth() - 1);
-                    tempMaxTime.setMonth(maxDate.getMonth() + 1);
-                }
-                minTime = Date.parse(tempMinTime.toString());
-                maxTime = Date.parse(tempMaxTime.toString());
-                selectedDates = generateDateArray(minNonZeroDate, maxDate)
-                    .map((date) => NUMERIC_DATE.format(date).split("/").reverse().join("-"));
-            }
-        }
         bb.generate({
             data: {
                 x: "x",
@@ -47,13 +26,13 @@ export const ActivityGraph = ({answeredQuestionsByDate}: {answeredQuestionsByDat
                 colors: {activity: "#ffb53f"},
                 xFormat: "%Y-%m-%d"
             },
-            axis: {x: {type: "timeseries", tick: {fit: false, count: 8}, min: minTime, max: maxTime}},
+            axis: {x: {type: "timeseries", tick: {fit: false, count: 8}}},
             zoom: {enabled: true},
             legend: {show: false},
             spline: {interpolation: {type: "monotone-x"}},
             bindto: "#activityGraph"
         });
-    }, [answeredQuestionsByDate]);
+    }, [answeredQuestionsByDate, selectedDates]);
 
     return <div id="activityGraph"/>
 };
