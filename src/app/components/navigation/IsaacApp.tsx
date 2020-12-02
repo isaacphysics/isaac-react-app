isaacphysics/isaac-react-app

commit 5a8822baf26a4566b7fbe3e2f0dfd8f3b4139979

Add /glossary link to CS Topics menu
Feeling just about right, might move it later :kissing_cat:

src/app/components/site/cs/RoutesCS.tsx
@@ -36,6 +36,7 @@ export const RoutesCS = [
     <StaticPageRoute key={key++} exact path="/about" pageId="about_us" />,
     <StaticPageRoute key={key++} exact path="/teaching_order" pageId="teaching_order" />,
     <StaticPageRoute key={key++} exact path="/student_rewards" pageId="student_rewards_programme" />,
+    <StaticPageRoute key={key++} exact path="/glossary" />,
 
     <StaticPageRoute key={key++} exact ifUser={isEventLeaderOrStaff} path="/events_toolkit" pageId="fragments/event_leader_event_toolkit_fragment" />,
 

src/app/components/site/cs/NavigationBarCS.tsx
@@ -29,6 +29,7 @@ export const NavigationBarCS = () => {
             <LinkItem to="/pages/specification_page_ocr">OCR view</LinkItem>
             <LinkItem to="/teaching_order">Suggested teaching order</LinkItem>
             <LinkItem to="/pages/workbooks_2020">Workbooks</LinkItem>
+            <LinkItem to="/glossary">Glossary</LinkItem>
         </NavigationSection>
 
         <NavigationSection title="Events">

commit d6af2c8aa2f44833089414715272dd095bde192a

Remove accidental addition of unnecessary route for glossary, move to CS
Added in 5a8822baf26a4566b7fbe3e2f0dfd8f3b4139979, broke the page.
The /glossary route already existed in IsaacApp.tsx, which clashed with
the old static glossary page route for physics. Move the new fancy glossary
page to just be for CS for now, since there is no old static page for CS.

src/app/components/navigation/IsaacApp.tsx
@@ -69,7 +69,6 @@ import {notificationModal} from "../elements/modals/NotificationModal";
 import {showNotification} from "../../services/notificationChecker";
 import * as persistence from "../../services/localStorage";
 import {KEY} from "../../services/localStorage";
-import {Glossary} from '../pages/Glossary';
 import {DowntimeWarningBanner} from "./DowntimeWarningBanner";
 import {ErrorBoundary} from "react-error-boundary";
 import {ClientError} from "../pages/ClientError";
@@ -184,8 +183,6 @@ export const IsaacApp = () => {
                     <StaticPageRoute exact path="/cookies" pageId="cookie_policy" />
                     <StaticPageRoute exact path="/accessibility" pageId="accessibility_statement" />
                     <StaticPageRoute exact path="/cyberessentials" />
-
-                    <TrackedRoute exact path="/glossary" component={Glossary} />
 
                     {/*
                     // TODO: schools and other admin stats

src/app/components/site/cs/RoutesCS.tsx
@@ -13,6 +13,7 @@ import {SingleAssignmentProgress} from "../../pages/SingleAssignmentProgress";
 import {Workbook20AQA} from "../../pages/books/Workbook20AQA";
 import {Workbook20OCR} from "../../pages/books/Workbook20OCR";
 import {GroupProgress} from "../../pages/GroupProgress";
+import {Glossary} from "../../pages/Glossary";
 
 let key = 0;
 export const RoutesCS = [
@@ -35,11 +36,14 @@ export const RoutesCS = [
     // Books:
     <TrackedRoute key={key++} exact path="/books/workbook_20_aqa" component={Workbook20AQA}/>,
     <TrackedRoute key={key++} exact path="/books/workbook_20_ocr" component={Workbook20OCR}/>,
+
+    // Glossary:
+    <TrackedRoute key={key++} exact path="/glossary" component={Glossary} />,
+
     // Static pages:
     <StaticPageRoute key={key++} exact path="/about" pageId="about_us" />,
     <StaticPageRoute key={key++} exact path="/teaching_order" pageId="teaching_order" />,
     <StaticPageRoute key={key++} exact path="/student_rewards" pageId="student_rewards_programme" />,
-    <StaticPageRoute key={key++} exact path="/glossary" />,
 
     <StaticPageRoute key={key++} exact ifUser={isEventLeaderOrStaff} path="/events_toolkit" pageId="fragments/event_leader_event_toolkit_fragment" />,
 