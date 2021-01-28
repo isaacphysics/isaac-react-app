98132576dfd62aab07a63387ae5e141401e2c735
Ben Hanson <bh412@cam.ac.uk>
2021-01-28 09:43:44 +0000

    Header tooltip rewording

--- src/app/components/site/phy/HeaderPhy.tsx
+++ src/app/components/site/phy/HeaderPhy.tsx
@@ -36,7 +36,7 @@ export const HeaderPhy = () => {
                                         <HeaderStreakGauge streakRecord={streakRecord}/>
                                     </div>
                                     <UncontrolledTooltip placement="bottom" autohide={false} target="header-progress">
-                                        The daily streak indicates the number of consecutive weeks you have been active on Isaac.
+                                        The weekly streak indicates the number of consecutive weeks you have been active on Isaac.
                                         <br/><br/>Answer at least <b>ten question parts</b> correctly per week to fill up your daily progress bar and increase your streak!
                                     </UncontrolledTooltip>
                                 </React.Fragment>}
