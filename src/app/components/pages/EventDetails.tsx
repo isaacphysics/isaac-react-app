8855abaf0be59a8d4d75596547de627246abfc31
Ben Hanson <bh412@cam.ac.uk>
2021-02-19 11:44:16 +0000

    Show confirm reservation button as well as text link

--- src/app/components/pages/EventDetails.tsx
+++ src/app/components/pages/EventDetails.tsx
@@ -259,9 +259,9 @@ export const EventDetails = ({match: {params: {eventId}}, location: {pathname}}:
 
                                 {/* Options for logged-in users */}
                                 {isLoggedIn(user) && !event.hasExpired && <React.Fragment>
-                                    {(canMakeABooking || canBeAddedToWaitingList) && !bookingFormOpen && !['CONFIRMED', 'RESERVED'].includes(event.userBookingStatus || '') &&
+                                    {(canMakeABooking || canBeAddedToWaitingList) && !bookingFormOpen && !['CONFIRMED'].includes(event.userBookingStatus || '') &&
                                         <RS.Button onClick={() => {setBookingFormOpen(true)}}>
-                                            {event.userBookingStatus === 'RESERVED' ? 'Confirm your booking' : 'Book a place'}
+                                            {event.userBookingStatus === 'RESERVED' ? 'Confirm your reservation' : 'Book a place'}
                                         </RS.Button>
                                     }
                                     {canReserveSpaces &&
