5ada44d916c38f656937370336616aed68f38569
Meurig Thomas <mlt47@cam.ac.uk>
2021-03-02 15:55:07 +0000

    Update homepage video and alter margins & paddings

--- src/app/components/site/phy/HomepagePhy.tsx
+++ src/app/components/site/phy/HomepagePhy.tsx
@@ -6,7 +6,7 @@ import {NewsCarousel} from "../../elements/NewsCarousel";
 import {SITE_SUBJECT_TITLE} from "../../../services/siteConstants";
 import {selectors} from "../../../state/selectors";
 import {WarningBanner} from "../../navigation/WarningBanner";
-import {useDeviceSize} from "../../../services/device";
+import {above, useDeviceSize} from "../../../services/device";
 
 export const HomepagePhy = () => {
     useEffect( () => {document.title = "Isaac " + SITE_SUBJECT_TITLE;}, []);
@@ -17,37 +17,38 @@ export const HomepagePhy = () => {
         <WarningBanner/>
         <section id="call-to-action" className="homepageHero">
             <Container className="pt-4">
-                <Row className="mt-4">
+                <Row className="mt-sm-4">
                     <Col lg={5} className="physics-site-intro">
-                        <h1 className={`physics-strapline ${["lg", "xl"].includes(deviceSize) ? "h2" : ""} mb-lg-3`}>
-                            Master Physics by Solving Problems:<br />
-                            <small>from School to University!</small>
+                        <h1 className={`physics-strapline ${above["lg"](deviceSize) ? "h2" : ""} mb-lg-3`}>
+                            {above["sm"](deviceSize) ?
+                                <>Master Physics by Solving Problems:<br /><small>from School to University!</small></> :
+                                <>Master Physics by Solving Problems</>}
                         </h1>
-                        <p>
-                            Welcome to Isaac Physics, the free platform for teachers and students.
-                        </p>
+                        <p>Welcome to Isaac Physics, the free platform for teachers and students.</p>
                         <ul>
                             <li>Use it in the <strong>classroom</strong></li>
                             <li>Use it for <strong>homework</strong></li>
                             <li>Use it for <strong>revision</strong></li>
                         </ul>
                     </Col>
-                    <Col lg={7} className="align-items-stretch d-flex flex-column">
-                        {!(user && user.loggedIn) && <Row className="align-self-end">
-                            <Col className="text-center col-auto">
-                                <Button size="sm" tag={Link} to="/login" color="primary" outline className="mr-2">
+                    <Col lg={7} className={above["lg"](deviceSize) ? `align-items-stretch d-flex flex-column` : ""}>
+                        {!(user && user.loggedIn) && <Row className="align-self-end mt-2 mt-lg-0 mb-1 mb-lg-0">
+                            <Col className="col-6 col-lg-auto pl-lg-0 pr-1 pr-sm-2">
+                                <Button size={above['lg'](deviceSize) || deviceSize === "xs" ? "sm" : ""} tag={Link} to="/login" color="primary" outline className="btn-block">
                                     Log in
                                 </Button>
-                                <Button size="sm" tag={Link} to="/register" color="secondary">
+                            </Col>
+                            <Col className="col-6 col-lg-auto pl-lg-0 pl-1 pl-sm-2">
+                                <Button size={above['lg'](deviceSize) || deviceSize === "xs" ? "sm" : ""} tag={Link} to="/register" color="secondary" className="btn-block">
                                     Sign up
                                 </Button>
                             </Col>
                         </Row>}
-                        <div className={`h-100 pl-lg-4 pt-4 ${user?.loggedIn ? "pt-lg-5" : ""}`}>
+                        <div className={`h-100 pl-lg-4 ${user?.loggedIn ? "pt-1 pt-sm-2 pt-lg-5" : "pt-4 pt-lg-3"}`}>
                             <div className="yt-video-container">
                                 <iframe
                                     title="Isaac Physics introduction video"
-                                    src="https://www.youtube-nocookie.com/embed/wd9vdc_yYKM?enablejsapi=1&rel=0&fs=1&modestbranding=1&origin=home"
+                                    src="https://www.youtube-nocookie.com/embed/kWA2AISiHXQ?enablejsapi=1&rel=0&fs=1&modestbranding=1&origin=home"
                                     frameBorder="0" allowFullScreen className="mw-100"
                                 />
                             </div>
@@ -55,21 +56,23 @@ export const HomepagePhy = () => {
                     </Col>
                 </Row>
 
-                <div className="physics-site-intro mt-4">
+                <div className="physics-site-intro mt-4 mt-lg-2">
                     <strong>Show me resources for...</strong>
                     <Row className="mt-2">
-                        <Col xs={12} sm={4}>
-                            <Button block tag={Link} to="/gcse" className="h-100 d-inline-flex align-items-center justify-content-center">
+                        <Col xs={4} className="pr-1 pr-sm-2">
+                            <Button size={deviceSize==="xs" ? "sm" : ""} block tag={Link} to="/gcse" className="h-100 d-inline-flex align-items-center justify-content-center">
                                 GCSE
                             </Button>
                         </Col>
-                        <Col xs={12} sm={4} className="pt-2 pt-sm-0">
-                            <Button block tag={Link} to="/alevel" className="h-100 d-inline-flex align-items-center justify-content-center">
-                                A level or equivalent
+                        <Col xs={4} className="px-1 px-sm-2">
+                            <Button size={deviceSize==="xs" ? "sm" : ""} block tag={Link} to="/alevel" className="h-100 d-inline-flex align-items-center justify-content-center">
+                                {above["md"](deviceSize) ?
+                                    "A\u00A0level or\u00A0equivalent" :
+                                    "A\u00A0level"}
                             </Button>
                         </Col>
-                        <Col xs={12} sm={4} className="pt-2 pt-sm-0">
-                            <Button block tag={Link} to="/teacher_features" className="h-100 d-inline-flex align-items-center justify-content-center">
+                        <Col xs={4} className="pl-1 pl-sm-2">
+                            <Button size={deviceSize==="xs" ? "sm" : ""} block tag={Link} to="/teacher_features" className="h-100 d-inline-flex align-items-center justify-content-center">
                                 teachers
                             </Button>
                         </Col>
@@ -80,10 +83,10 @@ export const HomepagePhy = () => {
 
         <section id="news">
             <Container>
-                <h2 className="h-title mt-5 mb-4">News and features</h2>
+                <h2 className="h-title mb-4 mt-4 pt-2 mt-sm-5 pt-sm-0">News and features</h2>
                 <Row className="eventList pt-1 pattern-03-reverse">
                     <Col>
-                        <NewsCarousel showTitle={true} descending={false} subject="physics" className={`mx-md-n4`} />
+                        <NewsCarousel showTitle={true} descending={false} subject="physics" className={`mx-sm-n4`} />
                     </Col>
                 </Row>
             </Container>
