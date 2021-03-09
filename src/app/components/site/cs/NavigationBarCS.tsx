833d3b816ea00af0c2ecbe66c17c9acb23009fff
Robin Message <robin@lambdacambridge.com>
2021-03-09 17:19:50 +0000

    Quiz: put navigation for "My quizzes" behind feature flags

--- src/app/components/site/cs/NavigationBarCS.tsx
+++ src/app/components/site/cs/NavigationBarCS.tsx
@@ -14,7 +14,7 @@ export const NavigationBarCS = () => {
             <LinkItem to="/assignments">My assignments {assignmentBadge}</LinkItem>
             <LinkItem to="/my_gameboards">My gameboards</LinkItem>
             <LinkItem to="/progress">My progress</LinkItem>
-            <LinkItem to="/quizzes">My quizzes</LinkItem>
+            {QUIZ_FEATURE && <LinkItem to="/quizzes">My quizzes</LinkItem>}
             <LinkItem to="/student_rewards">Student rewards</LinkItem>
         </NavigationSection>
 
