4d9df125e8e2429319a509de473a7c90eb67345c
Ben Hanson <bh412@cam.ac.uk>
2021-05-10 15:42:54 +0100

    Correct A Level format for phy assignments

--- src/app/components/pages/SetAssignments.tsx
+++ src/app/components/pages/SetAssignments.tsx
@@ -329,7 +329,7 @@ const SetAssignmentsPageComponent = (props: SetAssignmentsPageProps) => {
             <RS.Col md={6} lg={4} className="pt-1">
                 {SITE_SUBJECT === SITE.PHY ?
                     <RS.Button tag={Link} onClick={() => dispatch(openIsaacBooksModal)} color="secondary" block className="px-3">
-                        our GCSE &amp; A-Level books
+                        our GCSE &amp; A Level books
                     </RS.Button> :
                     <RS.Button tag={Link} to={"/pages/gameboards"} color="secondary" block>
                         Pre-made gameboards
