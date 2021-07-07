1c94620e9b2fc8144a1251fe753dff407f971658
Ben Hanson <bh412@cam.ac.uk>
2021-07-07 15:42:40 +0100

    Ensure Isaac board wildcards only appear on Physics

--- src/app/services/gameboards.tsx
+++ src/app/services/gameboards.tsx
@@ -87,8 +87,8 @@ export const generateGameboardSubjectHexagons = (boardSubjects: string[]) => {
 
 export const showWildcard = (board: GameboardDTO) => {
     const re = new RegExp('(phys_book_gcse_ch.*|pre_uni_maths.*)');
-    const isaacBoard = board?.tags?.includes("ISAAC_BOARD");
-    return board?.id && (re.test(board.id) || isaacBoard)
+    const isaacPhysicsBoard = board?.tags?.includes("ISAAC_BOARD") && SITE_SUBJECT === SITE.PHY;
+    return board?.id && (re.test(board.id) || isaacPhysicsBoard)
 };
 
 export const determineGameboardSubjects = (board: GameboardDTO) => {
