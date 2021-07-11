419c4b9646bda8486199021ee472b6c072db3cd6
Meurig Thomas <mlt47@cam.ac.uk>
2021-07-11 13:32:55 +0100

    Show in audience blocks first for cs

--- src/app/components/content/IsaacAccordion.tsx
+++ src/app/components/content/IsaacAccordion.tsx
@@ -2,20 +2,27 @@ import React from "react";
 import {ContentDTO} from "../../../IsaacApiTypes";
 import {Accordion} from "../elements/Accordion";
 import {IsaacContent} from "./IsaacContent";
+import {isIntendedAudience, useUserContext} from "../../services/userContext";
+import {useSelector} from "react-redux";
+import {selectors} from "../../state/selectors";
+import {SITE, SITE_SUBJECT} from "../../services/siteConstants";
 
-interface IsaacAccordionProps {
-    doc: {
-        children: {id?: string; title?: string; children?: ContentDTO[]}[];
-    };
-}
-
-export const IsaacAccordion = (props: any) => {
-    const {doc: {children}} = props as IsaacAccordionProps;
+export const IsaacAccordion = ({doc: {children}}: {doc: ContentDTO}) => {
+    const user = useSelector(selectors.user.orNull);
+    const userContext = useUserContext();
     return <div className="isaac-accordion">
-        {children.map((child, index) => (
-            <Accordion key={index} trustedTitle={child.title} id={child.id} index={index}>
-                <IsaacContent key={index} doc={child} />
-            </Accordion>
-        ))}
+        {(children as ContentDTO[] | undefined)
+
+            // We take the doc's children's index as a key for each section so that react is not confused between filtering/reordering
+            ?.map((section, index) => ({...section, sectionIndex: index}))
+
+            // For CS we want relevant sections to appear first
+            .sort((section) => !(SITE_SUBJECT === SITE.CS && isIntendedAudience(section.audience, userContext, user)) ? 1 : -1)
+
+            .map((section , index) =>
+                <Accordion key={section.sectionIndex} trustedTitle={section?.title || ""} id={section.id} index={index}>
+                    <IsaacContent doc={section} />
+                </Accordion>
+            )}
     </div>;
 };
--- src/app/services/userContext.ts
+++ src/app/services/userContext.ts
@@ -11,17 +11,19 @@ import {
     STAGES_CS,
     STAGES_PHY
 } from "./constants";
-import {ContentSummaryDTO} from "../../IsaacApiTypes";
+import {ContentBaseDTO, ContentSummaryDTO, Role} from "../../IsaacApiTypes";
 import {useSelector} from "react-redux";
 import {AppState} from "../state/reducers";
 import {SITE, SITE_SUBJECT} from "./siteConstants";
+import {PotentialUser} from "../../IsaacAppTypes";
+import {isAdmin, isEventLeaderOrStaff, isEventManager, isLoggedIn, isStaff, isTeacher} from "./user";
 
-interface UserContextProps {
+interface UserContext {
     examBoard: EXAM_BOARD;
     stage: STAGE;
 }
 
-export function useUserContext(): UserContextProps {
+export function useUserContext(): UserContext {
     const {BETA_FEATURE: betaFeature} = useSelector((state: AppState) => state?.userPreferences) || {};
     const user = useSelector((state: AppState) => state && state.user);
     const transientUserContext = useSelector((state: AppState) => state?.transientUserContext) || {};
@@ -86,3 +88,45 @@ export const filterOnExamBoard = (contents: ContentSummaryDTO[], examBoard: EXAM
             content.tags?.includes(examBoardTagMap[examBoard])
     });
 };
+
+export function isIntendedAudience(intendedAudience: ContentBaseDTO['audience'], userContext: UserContext, user: PotentialUser | null, audienceBetaFeatureEnabled?: boolean): boolean {
+    // If no audience is specified, we default to true
+    if (!intendedAudience) {
+        return true;
+    }
+
+    return intendedAudience.some(audienceClause => {
+        let matchesAllCriteriaSoFar = true;
+
+        // If stages are specified do we have any of them in our context
+        if (audienceClause.stage) {
+            // If beta feature is enabled we should treat users as if they have A Level selected
+            const userStages = audienceBetaFeatureEnabled ? [STAGE.A_LEVEL] : userContext.stage;
+            const satisfiesStageCriteria = audienceClause.stage.some(stage => userStages.includes(stage as STAGE));
+            matchesAllCriteriaSoFar = matchesAllCriteriaSoFar && satisfiesStageCriteria;
+        }
+
+        // If exam boards are specified do we have any of them in our context
+        if (audienceClause.examBoard) {
+            const satisfiesExamBoardCriteria = audienceClause.examBoard.some(examBoard => userContext.examBoard.includes(examBoard));
+            matchesAllCriteriaSoFar = matchesAllCriteriaSoFar && satisfiesExamBoardCriteria;
+        }
+
+        // If a role is specified do we have any of those roles or greater
+        if (audienceClause.roleAtLeast) {
+            const roleRequirements: Record<Role, (u: typeof user) => boolean> = {
+                "STUDENT": isLoggedIn,
+                "TEACHER": isTeacher,
+                "EVENT_LEADER": isEventLeaderOrStaff,
+                "CONTENT_EDITOR": isStaff,
+                "EVENT_MANAGER": isEventManager,
+                "ADMIN": isAdmin
+            };
+
+            const matchesRoleCriteria = audienceClause.roleAtLeast.some(role =>
+                Object.keys(roleRequirements).includes(role) && roleRequirements[role as Role](user));
+            matchesAllCriteriaSoFar = matchesAllCriteriaSoFar && matchesRoleCriteria;
+        }
+        return matchesAllCriteriaSoFar;
+    });
+}
