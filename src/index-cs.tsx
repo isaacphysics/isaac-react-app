363557d61bb4ae9ab06007038224faf667334bce
James Sharkey <jsharkey13@users.noreply.github.com>
2021-07-14 16:42:59 +0100

    Improve documentation and naming of line numbering code

--- src/index-cs.tsx
+++ src/index-cs.tsx
@@ -7,10 +7,10 @@ import {Provider} from "react-redux";
 import {store} from "./app/state/store";
 import {IsaacApp} from './app/components/navigation/IsaacApp';
 import {printAsciiArtLogoToConsole} from "./app/services/easterEggs/csLogoAsciiArt";
-import * as highlightJsConfig from "./app/services/highlightJs";
+import * as highlightJsService from "./app/services/highlightJs";
 
 printAsciiArtLogoToConsole();
-highlightJsConfig.registerLanguages();
+highlightJsService.registerLanguages();
 
 ReactDOM.render(
     <React.StrictMode>
