6b398ed9febbe92c44f86623cd194fef1fda4b5d
Chris Purdy <33040507+chrisjpurdy@users.noreply.github.com>
2021-08-09 17:28:02 +0100

    Change `katexify` function to choose boolean logic macro based on user preference
    Also modifies all places that use `katexify`, to fetch user preferences/use null for the new parameter.

--- src/app/components/elements/LaTeX.tsx
+++ src/app/components/elements/LaTeX.tsx
@@ -3,7 +3,7 @@ import {useSelector} from "react-redux";
 import {selectors} from "../../state/selectors";
 import {AppState} from "../../state/reducers";
 import {useUserContext} from "../../services/userContext";
-import {FigureNumberingContext, FigureNumbersById, PotentialUser} from "../../../IsaacAppTypes";
+import {FigureNumberingContext, FigureNumbersById, PotentialUser, UserPreferencesDTO} from "../../../IsaacAppTypes";
 import {EXAM_BOARD} from "../../services/constants";
 import he from "he";
 import {SITE, SITE_SUBJECT} from "../../services/siteConstants";
@@ -225,7 +225,7 @@ const ENDREF = "==ENDREF==";
 const REF_REGEXP = new RegExp(REF + "(.*?)" + ENDREF, "g");
 const SR_REF_REGEXP = new RegExp("start text, " + REF_REGEXP.source + ", end text,", "g");
 
-export function katexify(html: string, user: PotentialUser | null, examBoard: EXAM_BOARD | null, screenReaderHoverText: boolean, figureNumbers: FigureNumbersById) {
+export function katexify(html: string, user: PotentialUser | null, userPreferences : UserPreferencesDTO | null, examBoard: EXAM_BOARD | null, screenReaderHoverText: boolean, figureNumbers: FigureNumbersById) {
     start.lastIndex = 0;
     let match: RegExpExecArray | null;
     let output = "";
@@ -262,7 +262,11 @@ export function katexify(html: string, user: PotentialUser | null, examBoard: EX
                 const latexMunged = munge(latexUnEntitied);
                 let macrosToUse;
                 if (SITE_SUBJECT == SITE.CS) {
-                    macrosToUse = examBoard == EXAM_BOARD.AQA ? KatexMacrosWithEngineeringBool : KatexMacrosWithMathsBool;
+                    if (user?.loggedIn && !userPreferences?.BOOLEAN_NOTATION?.BOARD_SPECIFIC) {
+                        macrosToUse = userPreferences?.BOOLEAN_NOTATION?.ENG ? KatexMacrosWithEngineeringBool : KatexMacrosWithMathsBool;
+                    } else {
+                        macrosToUse = examBoard == EXAM_BOARD.AQA ? KatexMacrosWithEngineeringBool : KatexMacrosWithMathsBool;
+                    }
                 } else {
                     macrosToUse = KatexBaseMacros;
                 }
@@ -321,13 +325,14 @@ export function katexify(html: string, user: PotentialUser | null, examBoard: EX
 
 export function LaTeX({markup}: {markup: string}) {
     const user = useSelector(selectors.user.orNull);
+    const userPreferences = useSelector(selectors.userPreferences.orNull);
     const screenReaderHoverText = useSelector((state: AppState) => state && state.userPreferences &&
         state.userPreferences.BETA_FEATURE && state.userPreferences.BETA_FEATURE.SCREENREADER_HOVERTEXT || false);
     const {examBoard} = useUserContext();
     const figureNumbers = useContext(FigureNumberingContext);
 
     const escapedMarkup = escapeHtml(markup);
-    const katexHtml = katexify(escapedMarkup, user, examBoard, screenReaderHoverText, figureNumbers);
+    const katexHtml = katexify(escapedMarkup, user, userPreferences, examBoard, screenReaderHoverText, figureNumbers);
 
     return <span dangerouslySetInnerHTML={{__html: katexHtml}} />
 }
