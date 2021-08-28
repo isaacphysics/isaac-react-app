0751d564df6816a88600c92fb0e82458822102da
Meurig Thomas <mlt47@cam.ac.uk>
2021-08-29 00:24:18 +0100

    Improve audience warning banner message

--- src/app/components/navigation/IntendedAudienceWarningBanner.tsx
+++ src/app/components/navigation/IntendedAudienceWarningBanner.tsx
@@ -5,7 +5,7 @@ import {ContentBaseDTO} from "../../../IsaacApiTypes";
 import {isIntendedAudience, useUserContext} from "../../services/userContext";
 import {useSelector} from "react-redux";
 import {selectors} from "../../state/selectors";
-import {examBoardLabelMap, stageLabelMap} from "../../services/constants";
+import {EXAM_BOARD_NULL_OPTIONS, examBoardLabelMap, STAGE_NULL_OPTIONS, stageLabelMap} from "../../services/constants";
 
 export function IntendedAudienceWarningBanner({doc}: {doc: ContentBaseDTO}) {
     const user = useSelector(selectors.user.orNull);
@@ -16,10 +16,18 @@ export function IntendedAudienceWarningBanner({doc}: {doc: ContentBaseDTO}) {
         return <React.Fragment />;
     }
 
+    let message = [];
+    if (!STAGE_NULL_OPTIONS.has(userContext.stage)) {
+        message.push(stageLabelMap[userContext.stage]);
+    }
+    if (SITE.CS === SITE_SUBJECT && !EXAM_BOARD_NULL_OPTIONS.has(userContext.examBoard)) {
+        message.push(examBoardLabelMap[userContext.examBoard]);
+    }
+    if (message.length === 0) { // should never happen...
+        message.push("your account settings." /* "anyone!" */)
+    }
     return <RS.Alert color="warning">
         <strong>Note: </strong>
-        {`This page is not relevant for `}
-        {SITE.PHY === SITE_SUBJECT && `${stageLabelMap[userContext.stage]}.`}
-        {SITE.CS === SITE_SUBJECT && `${stageLabelMap[userContext.stage]} ${examBoardLabelMap[userContext.examBoard]}.`}
+        {`This page is not relevant for ${message.join(" ")}.`}
     </RS.Alert>
 }
