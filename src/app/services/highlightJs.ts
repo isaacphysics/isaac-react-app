2c7632697e2161058ff88e4e93ac23bee499e5ad
James Sharkey <jsharkey13@users.noreply.github.com>
2021-09-06 14:43:09 +0100

    Remove unused import in highlighting service

--- src/app/services/highlightJs.ts
+++ src/app/services/highlightJs.ts
@@ -5,7 +5,7 @@ import php from 'highlight.js/lib/languages/php'
 import csharp from 'highlight.js/lib/languages/csharp'
 import plaintext from 'highlight.js/lib/languages/plaintext'
 import sql from 'highlight.js/lib/languages/sql'
-import {Language, LanguageFn} from "highlight.js";
+import {LanguageFn} from "highlight.js";
 
 export function registerLanguages() {
     hljs.registerLanguage('javascript', javascript);
