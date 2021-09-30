45aaf5a825b7685217f81ec1b551ddd23c69ea51
Ben Hanson <bh412@cam.ac.uk>
2021-09-30 16:30:02 +0100

    Add confidence buttons and logging to quick questions

--- src/app/components/content/IsaacQuickQuestion.tsx
+++ src/app/components/content/IsaacQuickQuestion.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useRef, useState} from "react";
 import {Alert, Button, Col, Row} from "reactstrap";
 import * as ApiTypes from "../../../IsaacApiTypes";
 import {ContentDTO} from "../../../IsaacApiTypes";
@@ -7,6 +7,7 @@ import {useDispatch} from "react-redux";
 import {logAction} from "../../state/actions";
 import {determineFastTrackSecondaryAction, useFastTrackInformation} from "../../services/fastTrack";
 import {RouteComponentProps, withRouter} from "react-router";
+import uuid from "uuid";
 
 export const IsaacQuickQuestion = withRouter(({doc, location}: {doc: ApiTypes.IsaacQuickQuestionDTO} & RouteComponentProps) => {
     const dispatch = useDispatch();
@@ -14,14 +15,19 @@ export const IsaacQuickQuestion = withRouter(({doc, location}: {doc: ApiTypes.Is
     const [isVisible, setVisible] = useState(false);
     const answer: ContentDTO = doc.answer as ContentDTO;
     const secondaryAction = determineFastTrackSecondaryAction(fastTrackInfo);
+    const attemptUuid = useRef(uuid.v4().slice(0, 8));
 
-    const toggle = () => {
-        const isNowVisible = !isVisible;
-        setVisible(isNowVisible);
-        if (isNowVisible) {
-            const eventDetails = {type: "QUICK_QUESTION_SHOW_ANSWER", questionId: doc.id};
+    const toggle = (payload: string) => {
+        if (isVisible) {
+            const eventDetails = {type: "QUICK_QUESTION_CORRECT", questionId: doc.id, attemptUuid: attemptUuid.current, correct: payload};
+            dispatch(logAction(eventDetails));
+            attemptUuid.current = uuid.v4().slice(0, 8);
+        } else {
+            const eventDetails = {type: "QUICK_QUESTION_CONFIDENCE", questionId: doc.id, attemptUuid: attemptUuid.current, confidence: payload};
             dispatch(logAction(eventDetails));
         }
+        const isNowVisible = !isVisible;
+        setVisible(isNowVisible);
     };
 
     return <form onSubmit={e => e.preventDefault()}>
@@ -31,13 +37,35 @@ export const IsaacQuickQuestion = withRouter(({doc, location}: {doc: ApiTypes.Is
                     <IsaacContentValueOrChildren {...doc} />
                 </div>
                 {!fastTrackInfo.isFastTrackPage ?
-                    <Row>
-                        <Col sm="12" md={{size: 10, offset: 1}}>
-                            <Button color="secondary" block className={isVisible ? "active" : ""} onClick={toggle}>
-                                {isVisible ? "Hide answer" : "Show answer"}
-                            </Button>
-                        </Col>
-                    </Row>
+                    <div className="quick-question-options">
+                        <Row>
+                            <Col>
+                                <h4>{isVisible ?  "Click a button to hide the answer" : "Click a button to show the answer"}</h4>
+                            </Col>
+                        </Row>
+                        <Row className="mb-2">
+                            <Col>
+                                {isVisible ? "Was your own answer correct?" : "What is your level of confidence (that your own answer is correct)?"}
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col sm="4" md="4">
+                                <Button color="red" block className={isVisible ? "active" : ""} onClick={() => toggle(isVisible ? "No" : "Low")}>
+                                    {isVisible ? "No" : "Low"}
+                                </Button>
+                            </Col>
+                            <Col sm="4" md="4">
+                                <Button color="yellow" block className={isVisible ? "active" : ""} onClick={() => toggle(isVisible ? "Probably" : "Medium")}>
+                                    {isVisible ? "Probably" : "Medium"}
+                                </Button>
+                            </Col>
+                            <Col sm="4" md="4">
+                                <Button color="green" block className={isVisible ? "active" : ""} onClick={() => toggle(isVisible ? "Yes" : "High")}>
+                                    {isVisible ? "Yes" : "High"}
+                                </Button>
+                            </Col>
+                        </Row>
+                    </div>
                     :
                     <div className={`d-flex align-items-stretch flex-column-reverse flex-sm-row flex-md-column-reverse flex-lg-row mb-4`}>
                         {secondaryAction && <div className={`m-auto pt-3 pb-1 w-100 w-sm-50 w-md-100 w-lg-50 pr-sm-2 pr-md-0 pr-lg-3`}>
@@ -45,7 +73,7 @@ export const IsaacQuickQuestion = withRouter(({doc, location}: {doc: ApiTypes.Is
                         </div>}
                         <div className={`m-auto pt-3 pb-1 w-100 w-sm-50 w-md-100 w-lg-50 "pl-sm-2 pl-md-0 pl-lg-3`}>
                             <input
-                                onClick={toggle} value={isVisible ? "Hide answer" : "Show answer"}
+                                onClick={() => toggle("fasttrack")} value={isVisible ? "Hide answer" : "Show answer"}
                                 className={`h-100 btn btn-secondary btn-block {isVisible ? "active" : ""}`}
                             />
                         </div>
