711178d9204c89207fbd6d9d225d698329de375f
Chris Purdy <33040507+chrisjpurdy@users.noreply.github.com>
2021-10-08 16:05:56 +0100

    Fix looping of history on question finder
    This still needs another look - the question finder should allow you to navigate between previously generated boards with the history buttons in my opinion, but this is difficult to implement quickly.
    Also made the component use the new hook-based approach to `react-router-dom` history.

--- src/app/components/pages/GameboardFilter.tsx
+++ src/app/components/pages/GameboardFilter.tsx
@@ -11,7 +11,7 @@ import {generateTemporaryGameboard, loadGameboard} from '../../state/actions';
 import {ShowLoading} from "../handlers/ShowLoading";
 import {selectors} from "../../state/selectors";
 import queryString from "query-string";
-import {history} from "../../services/history";
+import {useHistory} from "react-router-dom";
 import {HierarchyFilterHexagonal, HierarchyFilterSummary, Tier} from "../elements/svg/HierarchyFilter";
 import {Item, unwrapValue} from "../../services/select";
 import {useDeviceSize} from "../../services/device";
@@ -84,16 +84,21 @@ export const GameboardFilter = withRouter(({location}: {location: Location}) => 
     const dispatch = useDispatch();
     const deviceSize = useDeviceSize();
     const userContext = useUserContext();
+    const history = useHistory();
     const {querySelections, queryStages, queryDifficulties} = processQueryString(location.search);
     const gameboardOrNotFound = useSelector(selectors.board.currentGameboardOrNotFound);
     const gameboard = useSelector(selectors.board.currentGameboard);
     const gameboardIdAnchor = location.hash ? location.hash.slice(1) : null;
-    if (gameboard && gameboard.id !== gameboardIdAnchor) {
-        history.push({search: location.search, hash: gameboard.id});
-    } else if (gameboardIdAnchor && gameboardOrNotFound === NOT_FOUND) {
-        // A request returning "gameboard not found" should clear the gameboard.id from the url hash anchor
-        history.push({search: location.search});
-    }
+
+    useEffect(() => {
+        if (gameboard && gameboard.id !== gameboardIdAnchor) {
+            history.replace({search: location.search, hash: gameboard.id});
+        } else if (gameboardIdAnchor && gameboardOrNotFound === NOT_FOUND) {
+            // A request returning "gameboard not found" should clear the gameboard.id from the url hash anchor
+            history.replace({search: location.search});
+        }
+    }, [gameboard, gameboardIdAnchor, gameboardOrNotFound])
+
     const [filterExpanded, setFilterExpanded] = useState(deviceSize != "xs");
     const gameboardRef = useRef<HTMLDivElement>(null);
 
@@ -151,7 +156,7 @@ export const GameboardFilter = withRouter(({location}: {location: Location}) => 
         });
         dispatch(generateTemporaryGameboard({...params, title: boardName}));
         delete params.questionCategories;
-        history.push({search: queryString.stringify(params, {encode: false})});
+        history.replace({search: queryString.stringify(params, {encode: false})});
     }
 
     useEffect(() => {
