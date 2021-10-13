0afc85d7d633b4347c32b9bccdac2ab553b10f1e
Meurig Thomas <mlt47@cam.ac.uk>
2021-10-13 11:55:59 +0100

    Remove expandable tab section while not in use

--- src/app/components/content/IsaacTabs.tsx
+++ src/app/components/content/IsaacTabs.tsx
@@ -2,8 +2,6 @@ import React, {ReactElement} from "react";
 import {Tabs} from "../elements/Tabs";
 import {ContentDTO} from "../../../IsaacApiTypes";
 import {IsaacContent} from "./IsaacContent";
-import {useDispatch} from "react-redux";
-import {closeActiveModal, openActiveModal} from '../../state/actions';
 
 interface IsaacTabsProps {
     doc: {children: {title?: string; children?: ContentDTO[]}[]};
@@ -12,16 +10,6 @@ interface IsaacTabsProps {
 export const IsaacTabs = (props: any) => {
     const {doc: {children}} = props as IsaacTabsProps;
     const tabTitlesToContent: {[title: string]: ReactElement} = {};
-
-    const dispatch = useDispatch();
-
-    function expandToModal(content: any) {
-        dispatch(openActiveModal({
-            closeAction: () => {dispatch(closeActiveModal())},
-            title: '',
-            body: content
-        }))
-    }
 
     children.forEach((child, index) => {
         const tabTitle = child.title || `Tab ${index + 1}`;
