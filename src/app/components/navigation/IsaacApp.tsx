05f41641abcae5433827064534db21ba12ae57f4
James Sharkey <jsharkey13@users.noreply.github.com>
2021-11-22 13:36:46 +0000

    Alter new email endpoint to still use admin prefix
    This name is a little arbitrary, but whatever!

--- src/app/components/navigation/IsaacApp.tsx
+++ src/app/components/navigation/IsaacApp.tsx
@@ -206,7 +206,7 @@ export const IsaacApp = () => {
                     <TrackedRoute exact path="/admin/stats" ifUser={isStaff} component={AdminStats} />
                     <TrackedRoute exact path="/admin/content_errors" ifUser={user => segueEnvironment === "DEV" || isStaff(user)} component={AdminContentErrors} />
                     <TrackedRoute exact path="/admin/emails" ifUser={isAdminOrEventManager} component={AdminEmails} />
-                    <TrackedRoute exact path="/content/emails" ifUser={isAdminOrEventManager} component={ContentEmails} />
+                    <TrackedRoute exact path="/admin/direct_emails" ifUser={isAdminOrEventManager} component={ContentEmails} />
 
                     {/* Authentication */}
                     <TrackedRoute exact path="/login" component={LogIn} />
