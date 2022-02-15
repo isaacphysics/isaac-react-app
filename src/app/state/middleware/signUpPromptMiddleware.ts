f3063f77dc7b6da098bd6c2407d89284bda5b441
Chris Purdy <33040507+chrisjpurdy@users.noreply.github.com>
2022-02-15 12:40:15 +0000

    Remove unsued file

--- src/app/state/middleware/signUpPromptMiddleware.ts
+++
@@ -1,5 +0,0 @@
-import {Dispatch, Middleware, MiddlewareAPI} from "redux";
-
-export const signUpPromptMiddleware: Middleware = (middlewareApi: MiddlewareAPI) => (dispatch: Dispatch) => async action => {
-    return await dispatch(action);
-};
