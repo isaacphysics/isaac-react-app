4f08ae6612af7beebd3a6b6934af22263b129772
Chris Purdy <33040507+chrisjpurdy@users.noreply.github.com>
2022-02-23 17:03:29 +0000

    Refactor IsaacTabs to be more reacty
    And probably re-render less

--- src/app/components/content/IsaacTabs.tsx
+++ src/app/components/content/IsaacTabs.tsx
@@ -1,22 +1,31 @@
-import React, {ReactElement} from "react";
+import React, {ReactElement, useEffect, useState} from "react";
 import {Tabs} from "../elements/Tabs";
 import {ContentDTO} from "../../../IsaacApiTypes";
 import {IsaacContent} from "./IsaacContent";
 import {SITE, SITE_SUBJECT} from "../../services/siteConstants";
 import classNames from "classnames";
+import {isDefined} from "../../services/miscUtils";
 
 interface IsaacTabsProps {
     doc: {children: {title?: string; children?: ContentDTO[]}[]};
 }
 
-export const IsaacTabs = (props: any) => {
-    const {doc: {children}} = props as IsaacTabsProps;
-    const tabTitlesToContent: {[title: string]: ReactElement} = {};
+type IsaacTabChildren = {[title: string]: ReactElement};
 
-    children.forEach((child, index) => {
-        const tabTitle = child.title || `Tab ${index + 1}`;
-        tabTitlesToContent[tabTitle] = <IsaacContent doc={child} />;
-    });
+export const IsaacTabs = (props: any) => {
+    const { doc: { children: tabs } } = props as IsaacTabsProps;
+    const [ tabTitlesToContent , setTabTitlesToContent ] = useState<IsaacTabChildren>({});
+
+    useEffect(() => {
+        if (!isDefined(tabs)) return;
+
+        const newTabTitlesToContent: IsaacTabChildren = {};
+        tabs.forEach((child, index) => {
+            const tabTitle = child.title || `Tab ${index + 1}`;
+            newTabTitlesToContent[tabTitle] = <IsaacContent doc={child} />;
+        });
+        setTabTitlesToContent(newTabTitlesToContent);
+    }, [tabs]);
 
     return <Tabs className={classNames("isaac-tab", {"parsons-layout": SITE_SUBJECT === SITE.CS})} tabContentClass="pt-4">
         {tabTitlesToContent}
