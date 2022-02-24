58078034cea68ed5e78955c54c90c7ff1f2480d9
Chris Purdy <33040507+chrisjpurdy@users.noreply.github.com>
2022-02-24 10:34:18 +0000

    Better scroll prompt logic
    Probably needs some text explaining what it indicates etc.

--- src/app/components/elements/ScrollPrompt.tsx
+++ src/app/components/elements/ScrollPrompt.tsx
@@ -3,50 +3,33 @@ import {isDefined} from "../../services/miscUtils";
 import classNames from "classnames";
 
 export const ScrollPrompt = <T extends HTMLElement>({scrollRef} : {scrollRef : RefObject<T>}) => {
-    const [ counter , setCounter ] = useState<number>(0);
     const [ direction , setDirection ] = useState<'left' | 'right'>();
+    const [ showAtTop , setShowAtTop ] = useState<boolean>();
 
     useLayoutEffect(() => {
         const listener = () => {
             const element = scrollRef?.current;
-            if (!isDefined(element) || counter > 15) {
+            if (!isDefined(element) || isDefined(direction)) {
                 return;
             }
             const windowHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
-            // TODO support if the user scrolls from beneath
-            if (element.getBoundingClientRect().y < (windowHeight * 0.6) && element.scrollWidth > element.clientWidth) {
-                setCounter(1);
-                window.removeEventListener("scroll", listener);
+            if (element.scrollWidth > element.clientWidth) {
+                if (element.getBoundingClientRect().y < (windowHeight * 0.6) && (windowHeight * 0.1) < element.getBoundingClientRect().y) {
+                    setDirection('right');
+                    setShowAtTop(true);
+                    window.removeEventListener("scroll", listener);
+                } else if (element.getBoundingClientRect().y + element.getBoundingClientRect().height > (windowHeight * 0.4) && (windowHeight * 0.9) >= element.getBoundingClientRect().y + element.getBoundingClientRect().height) {
+                    setDirection('right');
+                    setShowAtTop(false);
+                    window.removeEventListener("scroll", listener);
+                }
             }
         }
         window.addEventListener("scroll", listener);
         return () => window.removeEventListener("scroll", listener);
-    }, []);
+    });
 
-    // Check if in window view, if so display for 15*250 millis.
-    useLayoutEffect(() => {
-        const element = scrollRef?.current;
-        if (!isDefined(element) || counter > 15) {
-            setDirection(undefined);
-            return;
-        }
-        if (element.scrollWidth > element.clientWidth) {
-            const timeoutId = setTimeout(() => {
-                // Work out which side is overflowing
-                if (element.scrollLeft < 10) {
-                    setDirection('right');
-                } else if (element.scrollLeft > element.scrollWidth - (element.clientWidth + 10)) {
-                    setDirection('left');
-                } else {
-                    setDirection(undefined);
-                }
-                setCounter((n) => n + 1);
-            }, 250);
-            return () => clearTimeout(timeoutId);
-        }
-    }, [counter]);
-
-    return <div className={classNames("scroll-arrow", direction)}>
+    return <div className={classNames("scroll-arrow", direction)} style={{top: showAtTop ? "50px" : undefined, bottom: showAtTop ? undefined : "80px"}}>
         <span/>
         <span/>
         <span/>
