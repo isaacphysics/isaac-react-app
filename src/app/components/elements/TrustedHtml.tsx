a9abea6a9370f9f19b47cf8e639aa36e57764038
Chris Purdy <33040507+chrisjpurdy@users.noreply.github.com>
2022-03-02 15:29:18 +0000

    Allow tables in content to be expanded

--- src/app/components/elements/TrustedHtml.tsx
+++ src/app/components/elements/TrustedHtml.tsx
@@ -1,4 +1,4 @@
-import React, {useContext} from "react";
+import React, {RefObject, useContext, useRef, useState} from "react";
 import 'katex/dist/contrib/mhchem.js';
 import {FigureNumberingContext} from "../../../IsaacAppTypes";
 import {AppState} from "../../state/reducers";
@@ -7,32 +7,62 @@ import {selectors} from "../../state/selectors";
 import {katexify} from "./LaTeX";
 import {useClozeDropRegionsInHtml} from "../content/IsaacClozeQuestion";
 import {siteSpecific} from "../../services/miscUtils";
+import ReactDOM from "react-dom";
+import classNames from "classnames";
+import {Button} from "reactstrap";
+import {isMobile} from "../../services/device";
+
+interface TableData {
+    id: number;
+    html: string;
+    classes: string[];
+}
 
 const htmlDom = document.createElement("html");
-function manipulateHtml(html: string) {
+function manipulateHtml(html: string): {manipulatedHtml: string, tableData: TableData[]} {
     // This can't be quick but it is more robust than writing regular expressions...
     htmlDom.innerHTML = html;
 
     // Table manipulation
     const tableElements = htmlDom.getElementsByTagName("table");
+    const tableInnerHTMLs: TableData[] = [];
     for (let i = 0; i < tableElements.length; i++) {
         const table = tableElements[i];
-
-        // Add bootstrap classes
-        const currentTableClasses = (table.getAttribute("class") || "").split(" ");
-        const bootstrapTableClasses = ["table", "table-bordered", "w-100", "text-center", "bg-white", "m-0"];
-        const uniqueTableClasses = Array.from(new Set([...currentTableClasses, ...bootstrapTableClasses]));
-        table.setAttribute("class", uniqueTableClasses.join(" "));
-
         // Insert parent div to handle table overflow
         const parent = table.parentElement as HTMLElement;
         const div = document.createElement("div");
-        div.setAttribute("class", siteSpecific("overflow-auto mb-4", "parsons-layout isaac-expand-bg"));
+        div.setAttribute("id", `table-${i}`);
         parent.insertBefore(div, table);
-        div.appendChild(parent.removeChild(table));
+        tableInnerHTMLs.push({id: i, html: table.innerHTML, classes: (table.getAttribute("class") || "").split(" ")});
+        parent.removeChild(table);
     }
+    return {manipulatedHtml: htmlDom.innerHTML, tableData: tableInnerHTMLs};
+}
 
-    return htmlDom.innerHTML;
+// A portal component to manage table elements from inside the React DOM
+const Table = ({id, html, classes, rootElement}: TableData & {rootElement: RefObject<HTMLElement>}) => {
+    const parentElement = rootElement.current?.querySelector(`#table-${id}`);
+
+    const [ expanded, setExpanded ] = useState(false);
+    const toggleExpanded = () => setExpanded(b => !b);
+
+    const [ hovering, setHovering ] = useState(false);
+
+    if (html && parentElement) {
+        return ReactDOM.createPortal(
+            <div className={classNames("position-relative", {
+                "overflow-auto mb-4": !expanded,
+                "parsons-layout isaac-expand-bg": expanded
+            })} onMouseEnter={() => setHovering(true)} onMouseLeave={() => setHovering(false)}>
+                {hovering && !isMobile() && <button className={"position-absolute bg-transparent border-0"} style={expanded ? {top: 31, right: 27} : {top: 9, right: 6}} onClick={toggleExpanded}>
+                    <img style={{width: "30px", height: "auto"}} src={"/assets/expand-arrow.svg"}/>
+                </button>}
+                <table className={classNames(classes, "table table-bordered w-100 text-center bg-white m-0")} dangerouslySetInnerHTML={{__html: html}}/>
+            </div>,
+            parentElement
+        );
+    }
+    return null;
 }
 
 export const TrustedHtml = ({html, span, className}: {html: string; span?: boolean; className?: string}) => {
@@ -43,9 +73,14 @@ export const TrustedHtml = ({html, span, className}: {html: string; span?: boole
 
     const figureNumbers = useContext(FigureNumberingContext);
 
-    html = manipulateHtml(katexify(html, user, booleanNotation, screenReaderHoverText, figureNumbers));
-    html = useClozeDropRegionsInHtml(html);
+    const htmlRef = useRef<HTMLDivElement>(null);
+
+    const {manipulatedHtml, tableData} = manipulateHtml(katexify(html, user, booleanNotation, screenReaderHoverText, figureNumbers));
+    html = useClozeDropRegionsInHtml(manipulatedHtml);
 
     const ElementType = span ? "span" : "div";
-    return <ElementType className={className} dangerouslySetInnerHTML={{__html: html}} />;
+    return <>
+        <ElementType ref={htmlRef} className={className} dangerouslySetInnerHTML={{__html: html}} />
+        {tableData.map(({id, html, classes}) => <Table key={id} rootElement={htmlRef} id={id} html={html} classes={classes} />)}
+    </>;
 };
