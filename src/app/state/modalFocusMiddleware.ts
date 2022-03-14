f53c869c1607f3b478e6cc5957c66d46a4aeb19f
Chris Purdy <33040507+chrisjpurdy@users.noreply.github.com>
2022-03-14 12:53:09 +0000

    Deal with multiple modals and possible different `mainContentId` values

--- src/app/state/modalFocusMiddleware.ts
+++ src/app/state/modalFocusMiddleware.ts
@@ -1,24 +1,27 @@
 import {Dispatch, Middleware, MiddlewareAPI} from "redux";
 import {ACTION_TYPE} from "../services/constants";
 
-let lastFocused: Element | null = null;
+const lastFocusedStack: Element[] = [];
 
 // When a modal is closed, this tries to focus the last element that was focused before it opened. If no element exists,
 // we try to focus the page title. This is for accessibility, mostly to help when navigating the site with a screen-reader.
 export const modalFocusMiddleware: Middleware = (api: MiddlewareAPI) => (next: Dispatch) => action => {
+    const state = api.getState();
+
     switch (action.type) {
         case ACTION_TYPE.ACTIVE_MODAL_OPEN:
             // Before modal open, record the element that is currently focused
-            lastFocused = document.activeElement;
+            if (document.activeElement) lastFocusedStack.push(document.activeElement);
             break;
-        case ACTION_TYPE.ACTIVE_MODAL_CLOSE:
+        case ACTION_TYPE.ACTIVE_MODAL_CLOSE: {
+            const lastFocusedElement = lastFocusedStack.pop() as HTMLElement;
             // Before modal close, try to focus the last focused element, otherwise focus the main heading (if it exists)
-            if (lastFocused !== null && typeof (lastFocused as HTMLElement)?.focus === "function") {
-                (lastFocused as HTMLElement)?.focus();
-                lastFocused = null;
+            if (lastFocusedElement && typeof lastFocusedElement.focus === "function") {
+                lastFocusedElement.focus();
             } else {
-                document.getElementById("main")?.focus();
+                document.getElementById(state?.mainContentId || "main")?.focus();
             }
+        }
     }
 
     return next(action);
