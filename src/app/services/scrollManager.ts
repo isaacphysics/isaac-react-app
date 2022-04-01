751aacd87fce49086e49b7c394301a164497a783
Meurig Thomas <mlt47@cam.ac.uk>
2022-04-01 16:09:30 +0100

    Reintroduce scrollManager as types now work

--- src/app/services/scrollManager.ts
+++ src/app/services/scrollManager.ts
@@ -1,18 +1,18 @@
 import {history} from "./history";
 import { isDefined } from './miscUtils';
 
-// WARNING: the listener here doesn't seem to have an action parameter anywhere to be seen so I'm commenting this block out.
-history.listen(_listener => {
-    // if (["PUSH", "REPLACE"].includes(action) && previousPathname !== listener.pathname) {
-    //     previousPathname = listener.pathname;
-    //     (window as any).followedAtLeastOneSoftLink = true;
-    //     try {
-    //         window.scrollTo({top: 0, left: 0, behavior: "auto"});
-    //     } catch (e) {
-    //         // Some older browsers, notably Safari, don't support the new spec used above!
-    //         window.scrollTo(0, 0);
-    //     }
-    // }
+let previousPathname = "";
+history.listen((location, action) => {
+    if (["PUSH", "REPLACE"].includes(action) && previousPathname !== location.pathname) {
+        previousPathname = location.pathname;
+        (window as any).followedAtLeastOneSoftLink = true;
+        try {
+            window.scrollTo({top: 0, left: 0, behavior: "auto"});
+        } catch (e) {
+            // Some older browsers, notably Safari, don't support the new spec used above!
+            window.scrollTo(0, 0);
+        }
+    }
 });
 
 export function scrollVerticallyIntoView(element: Element, offset = 0): void {
