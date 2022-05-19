4f68b22d7c5e7e7cc4eba594b9b6f307b16de50f
Chris Purdy <33040507+chrisjpurdy@users.noreply.github.com>
2022-05-19 11:12:47 +0100

    Handle nested tables nicely, and make sure they get the correct classes
    Essentially don't make them into React components

--- src/app/components/elements/portals/Tables.tsx
+++ src/app/components/elements/portals/Tables.tsx
@@ -77,7 +77,6 @@ interface TableData {
     expandable?: boolean;
 }
 
-// TODO Need to change this so that it only deals with the top layer of nested tables
 // The component that uses this hook should be using the pattern demonstrated in `TrustedHtml`.
 // This pattern is the following:
 // - The html produced by this hook is rendered within an element using the `dangerouslySetInnerHTML` attribute. Call this the root element.
@@ -90,20 +89,41 @@ export const useAccessibleTablesInHtml: PortalInHtmlHook = (html) => {
     // This is more robust than writing regex, and is surprisingly very quick!
     const htmlDom = document.createElement("html");
     htmlDom.innerHTML = html;
-    // Table manipulation
+
     const tableElements = [...htmlDom.getElementsByTagName("table")];
     if (tableElements.length === 0) return [html, () => []];
 
     const tableInnerHTMLs: TableData[] = [];
-    for (let i = 0; i < tableElements.length; i++) {
-        const table = tableElements[i];
-        // Insert parent div to handle table overflow
+    // Loop through tables in reverse, so that changes to a nested table will happen before the `innerHtml` of its parent
+    // table is recorded in `tableInnerHTMLs`
+    for (let i = tableElements.length - 1; i >= 0; i--) {
+        const table: HTMLTableElement = tableElements[i];
+
+        // If table is marked as ignored, then pass - this means that it is nested inside another table (and this was
+        // found in a prior pass)
+        if ("ignore" in table.dataset) continue;
+
+        const tableCurrentClasses = (table.getAttribute("class") || "").split(/\s+/);
         const parent = table.parentElement as HTMLElement;
         const div = document.createElement("div");
-        div.setAttribute("id", `table-${i}`);
-        parent.insertBefore(div, table);
-        tableInnerHTMLs.push({id: i, html: table.innerHTML, classes: (table.getAttribute("class") || "").split(/\s+/)});
-        parent.removeChild(table);
+
+        // Only manage the `table` in React if it doesn't have another `table` as an ancestor - `table`s will always have at least
+        // a `body` element as an ancestor.
+        if (table.parentElement?.closest("table") === null) {
+            // This table has no table ancestors, so set it up to manage it within React (so we can add shadows and expand them etc.)
+            tableInnerHTMLs.push({id: i, html: table.innerHTML, classes: tableCurrentClasses});
+            div.setAttribute("id", `table-${i}`);
+            parent.insertBefore(div, table);
+            parent.removeChild(table);
+        } else {
+            // This `table` is inside another `table`, so don't make it a portal element
+            table.setAttribute("class", classNames(tableCurrentClasses, "table table-bordered w-100 text-center bg-white m-0"));
+            table.dataset.ignore = "true";
+            // Insert parent div to handle table overflow
+            div.setAttribute("class", "overflow-auto");
+            parent.insertBefore(div, table);
+            div.appendChild(parent.removeChild(table));
+        }
     }
     return [
         htmlDom.innerHTML,
