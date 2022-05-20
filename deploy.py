a3a46053700e14fb20bf8c320538309ac42c796b
Meurig Thomas <mlt47@cam.ac.uk>
2022-05-20 10:55:08 +0100

    Improve and fix deploy script

--- deploy.py
+++ deploy.py
@@ -17,7 +17,8 @@ def assert_using_a_tty():
         sys.exit(1)
 
 def check_react_app_is_up_to_date():
-    input("Check isaac-react-app is up-to-date so that you're using the latest deploy script.")
+    print("// Git pull for the latest version of the deploy script:")
+    ask_to_run_command("git pull")
 
 def parse_command_line_arguments():
     parser = argparse.ArgumentParser(description='Deploy the site')
@@ -34,87 +35,96 @@ def validate_args(args):
         sys.exit(1)
     # TODO if app version is not in vX.X.X shape then ask for an api version
 
+def ask_to_run_command(command):
+    return input(f"{command}\n")
+
 def build_docker_image_for_version(app_version, api_version=None):
-    print("# BUILD")
-    input(f"./build-in-docker.sh {app_version}{' ' + api_version if api_version else ''}")
+    print("\n# BUILD THE APP AND API")
+    ask_to_run_command(f"./build-in-docker.sh {app_version}{' ' + api_version if api_version else ''}")
 
 def update_config(env, site):
     # TODO check if there have been any new config values since last release
-    input(f"Update config:\n/local/data/isaac-config/{site}/segue-config.{env}.properties")
+    print("// If necessary, update config:")
+    ask_to_run_command(f"sudo nano /local/data/isaac-config/{site}/segue-config.{env}.properties")
 
 def run_db_migrations(env, site):
     # TODO check if there are any migrations since the last release
-    print("If there are any DB migrations, run them - preferably in a transaction - using:")
-    input(f"docker exec -it {site}-pg-{env} psql -U rutherford")
+    print("// If there are any DB migrations, run them (in a transaction with a BEGIN; ROLLBACK; or COMMIT;):")
+    ask_to_run_command(f"docker exec -it {site}-pg-{env} psql -U rutherford")
 
 def write_changelog():
     # TODO can get this from GitHub, given app and api versions
-    input("Write changelog - https://github.com/isaacphysics/isaac-react-app/releases")
+    input("\nWrite the changelog at https://github.com/isaacphysics/isaac-react-app/releases")
 
 def bring_down_any_existing_containers(site, env):
-    print(f"Bring down any existing {site} {env} containers")
-    input("docker ps --format '{{.Names}}' | " + f"grep {site}-app-{env}- | cut -c{len(site + '-app-' + env + '-') + 1}- | xargs ./compose {site} {env} down -v")
+    app_name_prefix = site + '-app-' + env + '-'
+    print(f"// Find running {site} {env} versions:")
+    ask_to_run_command("docker ps --format '{{.Names}}' | " + f"grep {app_name_prefix} | cut -c{len(app_name_prefix) + 1}-")
+    print(f"// Bring them down using:")
+    ask_to_run_command("docker ps --format '{{.Names}}' | " + f"grep {app_name_prefix} | cut -c{len(app_name_prefix) + 1}- | xargs ./compose {site} {env} down -v")
 
 def bring_up_the_new_containers(site, env, app):
-    print(f"Bring up the new {site} {env} containers:")
-    input(f"./compose {site} {env} {app} up -d")
+    print(f"// Bring up the new {site} {env} containers:")
+    ask_to_run_command(f"./compose {site} {env} {app} up -d")
 
 
 def deploy_test(site, app):
     bring_down_any_existing_containers(site, 'test')
     print("Note: If there is a database schema change, you might need to alter the default data - usually through a migration followed by a snapshot.")
-    print("Reset the test database.")
-    input(f"./clean-test-db.sh {site}")
+    print("// Reset the test database.")
+    ask_to_run_command(f"./clean-test-db.sh {site}")
     bring_up_the_new_containers(site, 'test', app)
 
 def deploy_staging_or_dev(env, site, app):
-    print(f"# DEPLOY {env.upper()}")
+    print(f"\n# DEPLOY {env.upper()}")
     update_config(env, site)
     run_db_migrations(env, site)
     bring_down_any_existing_containers(site, env)
     bring_up_the_new_containers(site, env, app)
 
-def deploy_live(site, app, api):
-    print("# DEPLOY LIVE")
-    front_end_only_release = input("Is this a front-end-only release? [y/n]").lower()
+def deploy_live(site, app):
+    print("\n# DEPLOY LIVE")
+    front_end_only_release = 'y' == input("Is this a front-end-only release? [y/n]").lower()
     if not front_end_only_release:
         # TODO figure out penultimate version
-        print("Bring down the penultimate live api if that is sensible using something like:")
-        input(f"docker stop {site}-api-live-vPEN.ULTI.MATE")
-        print("And then the same adian but with the docker rm subcommand:")
-        input(f"docker rm {site}-api-live-vPEN.ULTI.MATE")
+        print("// Bring down the penultimate live api, if that is sensible, using something like:")
+        ask_to_run_command(f"docker stop {site}-api-live-vPEN.ULTI.MATE")
+        print("// And then the same again but with the docker rm subcommand:")
+        ask_to_run_command(f"docker rm {site}-api-live-vPEN.ULTI.MATE")
 
         update_config('live', site)
         run_db_migrations('live', site)
 
-        print("Bring up the api of the new app")
-        input("./compose-live {site} {app} up -d {site}-api-live-{api}")
+        api_version = input('What is the new api version? [v1.3.4 | master | some-branch]')
 
-        print("Wait until the api is up - i.e. this command stops returning an error page")
-        input(f"curl https://isaac{'computerscience' if site == 'cs' else 'physics'}.org/api/{api}/api/info/segue_environment")
+        print("// Bring up the new api ready for the new app:")
+        ask_to_run_command(f"./compose-live {site} {app} up -d {site}-api-live-{api_version}")
 
-        print("Let the monitoring service know there is a new api to query")
-        input("cd /local/src/isaac-monitor && ./monitor_services.py --generate --no-prompt && ./monitor_services.py --reload --no-prompt && cd -")
+        print("// Wait until the api is up (i.e. this command stops returning an error page):")
+        ask_to_run_command(f"curl https://isaac{'computerscience' if site == 'cs' else 'physics'}.org/api/{api_version}/api/info/segue_environment")
 
-    print("Bring up the new app and take down the old one")
+        print("// Let the monitoring service know there is a new api service to track:")
+        ask_to_run_command("cd /local/src/isaac-monitor && ./monitor_services.py --generate --no-prompt && ./monitor_services.py --reload --no-prompt && cd -")
+
+    app_name_prefix = f"{site}-app-live-"
     previous_app_version = ""
-    while previous_app_version != "":
-        previous_app_version = input(
-            "What is the previous app version? (i.e. v1.2.3)"
-            "docker ps --format '{{.Names}}' | " + f"grep {site}-app-live- | cut -c{len(site + '-app-live-') + 1}-")
-    input(f"./compose-live {site} {app} up -d {site}-app-live-{app} && "
+    while previous_app_version == "":
+        print("What is the previous app version? (i.e. v1.2.3)")
+        previous_app_version = ask_to_run_command("docker ps --format '{{.Names}}' | " + f"grep {app_name_prefix} | cut -c{len(app_name_prefix) + 1}-")
+    print("// Bring up the new app and take down the old one:")
+    ask_to_run_command(f"./compose-live {site} {app} up -d {site}-app-live-{app} && "
           f"docker stop {site}-app-live-{previous_app_version} && "
           "../isaac-router/reload-router-config")
 
 def deploy_etl(site, app):
-    print("# DEPLOY ETL")
-    continue_anyway = 'y' == input("If there are changes to the content you might want to delay deploying ETL until any old APIs are down. Continue? [y/n]").lower()
+    print("\n# DEPLOY ETL")
+    continue_anyway = 'y' == input("If there are changes to the content model you might want to delay deploying ETL until any old APIs are down.\nDeploy now? [y/n]").lower()
     if continue_anyway:
-        print("Bring down the old API")
+        print("// Bring down the old ETL service")
         previous_app_version = input("What was the previous app version? [v1.2.3]")
-        input(f"./compose-etl {site} {previous_app_version} down -v")
-        print("Bring up the new API")
-        input(f"./compose-etl {site} {app} up -d")
+        ask_to_run_command(f"./compose-etl {site} {previous_app_version} down -v")
+        print("// Bring up the new ETL service")
+        ask_to_run_command(f"./compose-etl {site} {app} up -d")
 
 
 if __name__ == '__main__':
@@ -122,8 +132,9 @@ if __name__ == '__main__':
     context = vars(parse_command_line_arguments())
     validate_args(context)
 
-    print("THIS SCRIPT IS STILL EXPERIMENTAL SO BE CAUTIOUS")
-    print("Go to isaac-react-app on the live machine.\ncd /local/src/isaac-react-app")
+    print("\n! THIS SCRIPT IS STILL EXPERIMENTAL SO CHECK EACH COMMAND BEFORE EXECUTING IT !\n")
+    print("// Go to isaac-react-app on the live machine:")
+    ask_to_run_command("cd /local/src/isaac-react-app")
     check_react_app_is_up_to_date()
 
     build_docker_image_for_version(context['app'], context['api'])
@@ -136,12 +147,11 @@ if __name__ == '__main__':
             deploy_staging_or_dev(context['env'], context['site'], context['app'])
         if context['env'] == 'live':
             deploy_staging_or_dev('staging', context['site'], context['app'])
-            deploy_live(context['site'], context['app'], context['api'])
+            deploy_live(context['site'], context['app'])
             deploy_etl(context['site'], context['app'])
         if context['env'] == 'etl':
             deploy_etl(context['site'], context['app'])
+    print('\nDone!')
 
     if context['env'] == 'live':
         write_changelog()
-
-    print('Done!')
