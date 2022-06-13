7f7b47c8fcce38f46ed0f564b88f4ef27c622784
Chris Purdy <33040507+chrisjpurdy@users.noreply.github.com>
2022-06-13 12:33:39 +0100

    Add negative number examples to numeric question tooltip

--- src/app/components/content/IsaacNumericQuestion.tsx
+++ src/app/components/content/IsaacNumericQuestion.tsx
@@ -155,8 +155,8 @@ const IsaacNumericQuestion = ({doc, questionId, validationResponse, readonly}: I
                                     <UncontrolledTooltip placement="bottom" autohide={false} target={helpTooltipId}>
                                         Here are some examples of numbers you can write:<br /><br />
                                         3.7<br />
-                                        3x10^14<br />
-                                        2.8e12<br /><br />
+                                        -3x10^14<br />
+                                        2.8e-12<br /><br />
                                         Do not use commas or spaces.
                                     </UncontrolledTooltip>
                                 </InputGroupAddon>}
