501ccc0ee0d96cf7552e466280a23363a573919e
Matthew Trew <matthew.trew@raspberrypi.org>
2022-06-29 11:30:25 +0100

    Render optional IsacCardDeck titles

--- src/app/components/content/IsaacCardDeck.tsx
+++ src/app/components/content/IsaacCardDeck.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {IsaacCard} from "./IsaacCard";
 import {IsaacCardDeckDTO} from "../../../IsaacApiTypes";
+import {Col, Container, Row} from "reactstrap";
 
 interface IsaacCardDeckProps {
     doc: IsaacCardDeckDTO,
@@ -9,7 +10,15 @@ interface IsaacCardDeckProps {
 
 export const IsaacCardDeck = ({doc, className}: IsaacCardDeckProps) => {
     const classNameString: string = className !== undefined ? className : "";
-    return <div className={"card-deck isaac-cards-body " + classNameString}>
-        {doc?.cards?.map((props, i) => <IsaacCard key={i} doc={props}/>)}
-    </div>
+    return <Container>
+        {doc.title && <Row className="my-4">
+            <Col>
+                <h3 className="h-title text-center">{doc.title}</h3>
+            </Col>
+        </Row>}
+        <Row className={"card-deck isaac-cards-body my-3" + classNameString}>
+            {doc?.cards?.map((props, i) => <IsaacCard key={i} doc={props}/>)}
+        </Row>
+    </Container>
+
 };
