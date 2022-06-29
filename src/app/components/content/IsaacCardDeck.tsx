238005476adbfb1941ad8daa9cec0b24a9c11b51
Matthew Trew <matthew.trew@raspberrypi.org>
2022-06-29 14:16:49 +0100

    Use CardDeck for conistent alignment on small screens

--- src/app/components/content/IsaacCardDeck.tsx
+++ src/app/components/content/IsaacCardDeck.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import {IsaacCard} from "./IsaacCard";
 import {IsaacCardDeckDTO} from "../../../IsaacApiTypes";
-import {Col, Container, Row} from "reactstrap";
+import {CardDeck, Col, Container, Row} from "reactstrap";
 import classNames from "classnames";
 
 interface IsaacCardDeckProps {
@@ -10,15 +10,15 @@ interface IsaacCardDeckProps {
 }
 
 export const IsaacCardDeck = ({doc, className}: IsaacCardDeckProps) => {
-    return <Container>
+    return <Container className={"px-0"}>
         {doc.title && <Row className="my-4">
             <Col>
                 <h3 className="h-title text-center">{doc.title}</h3>
             </Col>
         </Row>}
-        <Row className={classNames("card-deck isaac-cards-body my-3", className)}>
+        <CardDeck className={classNames("card-deck isaac-cards-body my-3", className)}>
             {doc?.cards?.map((props, i) => <IsaacCard key={i} doc={props}/>)}
-        </Row>
+        </CardDeck>
     </Container>
 
 };
