92471c38f63c37a3dc3ada68455783ad37dc1ba7
Chris Purdy <33040507+chrisjpurdy@users.noreply.github.com>
2022-07-15 14:13:10 +0100

    User context reconfirmation modal with prelim. text
    Will be shown past 24th of August

---
+++ src/app/components/elements/modals/UserContextReconfirmationModal.tsx
@@ -0,0 +1,98 @@
+import React, {useCallback, useMemo, useState} from "react";
+import {isMobile} from "../../../services/device";
+import {Button, Col, Form, Input, Row} from "reactstrap";
+import {validateUserContexts, validateUserSchool} from "../../../services/validation";
+import {UserContextAccountInput} from "../inputs/UserContextAccountInput";
+import {SchoolInput} from "../inputs/SchoolInput";
+import {isDefined} from "../../../services/miscUtils";
+import {BooleanNotation, DisplaySettings} from "../../../../IsaacAppTypes";
+import {useDispatch, useSelector} from "react-redux";
+import {selectors} from "../../../state/selectors";
+import {AppState} from "../../../state/reducers";
+import {isLoggedIn} from "../../../services/user";
+import {closeActiveModal, updateCurrentUser} from "../../../state/actions";
+import {isCS, SITE_SUBJECT_TITLE, siteSpecific} from "../../../services/siteConstants";
+
+const UserContextReconfimationModalBody = () => {
+    const dispatch = useDispatch();
+    const user = useSelector(selectors.user.orNull);
+    const userPreferences = useSelector((state: AppState) => state?.userPreferences);
+
+    const [submissionAttempted, setSubmissionAttempted] = useState(false);
+
+    const initialUserValue = useMemo(() => ({...user, password: null}), [user]);
+    const [userToUpdate, setUserToUpdate] = useState(initialUserValue);
+
+    const initialUserContexts = useMemo(() =>
+        user?.loggedIn && isDefined(user.registeredContexts) ? [...user.registeredContexts] : []
+    , [user]);
+    const [userContexts, setUserContexts] = useState(initialUserContexts.length ? initialUserContexts : [{}]);
+
+    const [booleanNotation, setBooleanNotation] = useState<BooleanNotation | undefined>();
+    const [displaySettings, setDisplaySettings] = useState<DisplaySettings>({...userPreferences?.DISPLAY_SETTING});
+
+    const allFieldsAreValid = useMemo(() =>
+        validateUserContexts(userContexts) && validateUserSchool(userToUpdate)
+    , [userContexts, userToUpdate]);
+
+    const userPreferencesToUpdate = useMemo(() => ({
+        BOOLEAN_NOTATION: booleanNotation, DISPLAY_SETTING: displaySettings
+    }), [booleanNotation, displaySettings]);
+
+    // Form submission
+    const formSubmission = useCallback((event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        setSubmissionAttempted(true);
+        if (user && isLoggedIn(user) && allFieldsAreValid) {
+            dispatch(updateCurrentUser(userToUpdate, userPreferencesToUpdate, userContexts, null, user, false));
+            dispatch(closeActiveModal());
+        }
+    }, [userToUpdate, userContexts, userPreferencesToUpdate, user]);
+
+    return <Form onSubmit={formSubmission} className={"mb-2"}>
+        <div>
+            So that Isaac {SITE_SUBJECT_TITLE} can continue to show you relevant content, we ask that you review your
+            stage{isCS && ", exam board"} and school account details at the beginning of each academic year.
+        </div>
+        <div className="text-right text-muted required-before">
+            Required
+        </div>
+        <Row className="my-2">
+            <Col xs={12} md={siteSpecific(6, 12)} lg={6}>
+                 <UserContextAccountInput
+                    user={userToUpdate} userContexts={userContexts} setUserContexts={setUserContexts}
+                    displaySettings={displaySettings} setDisplaySettings={setDisplaySettings}
+                    setBooleanNotation={setBooleanNotation} submissionAttempted={submissionAttempted}
+                 />
+            </Col>
+            <Col xs={12} md={6}>
+                <SchoolInput
+                    userToUpdate={userToUpdate} setUserToUpdate={setUserToUpdate}
+                    submissionAttempted={submissionAttempted} idPrefix="modal"
+                    required
+                />
+            </Col>
+        </Row>
+        <div className="text-muted small pb-2">
+            Updating this information helps us continue to show you relevant content throughout your educational journey.
+            Full details on how we use your personal information can be found in our <a target="_blank" href="/privacy">Privacy Policy</a>.
+        </div>
+
+        {submissionAttempted && !allFieldsAreValid && <div>
+            <h4 role="alert" className="text-danger text-center mb-4">
+                Not all required fields have been correctly filled.
+            </h4>
+        </div>}
+
+        <div className="text-center pb-3 pt-1">
+            <Button type={"submit"} color={"secondary"} className={"border-0 my-1 w-lg-50 w-100"}>
+                Update details
+            </Button>
+        </div>
+    </Form>;
+}
+
+export const userContextReconfimationModal = {
+    title: "Please review your details",
+    body: <UserContextReconfimationModalBody />,
+}
--- src/app/state/middleware/notificationManager.ts
+++ src/app/state/middleware/notificationManager.ts
@@ -1,25 +1,55 @@
 import {Dispatch, Middleware, MiddlewareAPI} from "redux";
 import {ACTION_TYPE} from "../../services/constants";
-import {Action} from "../../../IsaacAppTypes";
+import {Action, LoggedInUser} from "../../../IsaacAppTypes";
 import {logAction, openActiveModal} from "../actions";
-import {allRequiredInformationIsPresent, withinLast50Minutes, withinLast2Hours} from "../../services/validation";
+import {allRequiredInformationIsPresent, withinLast2Hours, withinLast50Minutes} from "../../services/validation";
 import {isLoggedIn} from "../../services/user";
 import * as persistence from "../../services/localStorage";
 import {KEY} from "../../services/localStorage";
 import {requiredAccountInformationModal} from "../../components/elements/modals/RequiredAccountInformationModal";
 import {loginOrSignUpModal} from "../../components/elements/modals/LoginOrSignUpModal";
+import {userContextReconfimationModal} from "../../components/elements/modals/UserContextReconfirmationModal";
+
+// Returns true if the user hasn't updated their stage, exam board and school since *last August*,
+// and it's at least the 24th of August
+const needToUpdateUserContextDetails = (user: LoggedInUser) => {
+    // First calculate when the last August before today was...
+    let date = new Date();
+    if (date.getMonth() < 8) {
+        date.setFullYear(date.getFullYear() - 1);
+    }
+    date.setMonth(8, 1);
+    date.setHours(0, 0, 0, 0);
+    // Date is now the 1st day of last August
+    if (user.registeredContextsLastConfirmed && user.registeredContextsLastConfirmed < date) {
+        date.setDate(24);
+        // Date is now the 24th of last August (the last week of August, give or take)
+        if (Date.now() > date.valueOf()) {
+            return true;
+        }
+    }
+    return false;
+}
 
 export const notificationCheckerMiddleware: Middleware = (middlewareApi: MiddlewareAPI) => (dispatch: Dispatch) => async (action: Action) => {
 
     const state = middlewareApi.getState();
     if([ACTION_TYPE.CURRENT_USER_RESPONSE_SUCCESS, ACTION_TYPE.ROUTER_PAGE_CHANGE].includes(action.type)) {
-        if (
-            state && isLoggedIn(state.user) &&
-            !allRequiredInformationIsPresent(state.user, state.userPreferences, state.user.registeredContexts) &&
-            !withinLast50Minutes(persistence.load(KEY.REQUIRED_MODAL_SHOWN_TIME))
-        ) {
-            persistence.save(KEY.REQUIRED_MODAL_SHOWN_TIME, new Date().toString());
-            await dispatch(openActiveModal(requiredAccountInformationModal) as any);
+        if (state && isLoggedIn(state.user)) {
+            // Required account info model - takes precedence over stage/exam board re-confirmation modal, only
+            // shown once every 50 minutes (using a key in clients browser storage)
+            if (!allRequiredInformationIsPresent(state.user, state.userPreferences, state.user.registeredContexts) &&
+                !withinLast50Minutes(persistence.load(KEY.REQUIRED_MODAL_SHOWN_TIME))) {
+                persistence.save(KEY.REQUIRED_MODAL_SHOWN_TIME, new Date().toString());
+                await dispatch(openActiveModal(requiredAccountInformationModal));
+            }
+            // User context re-confirmation modal - used to request a user to update their stage and/or exam board
+            // once every academic year.
+            else if (needToUpdateUserContextDetails(state.user) &&
+                     !withinLast50Minutes(persistence.load(KEY.RECONFIRM_USER_CONTEXT_SHOWN_TIME))) {
+                persistence.save(KEY.RECONFIRM_USER_CONTEXT_SHOWN_TIME, new Date().toString());
+                await dispatch(openActiveModal(userContextReconfimationModal));
+            }
         }
     }
 
@@ -28,8 +58,7 @@ export const notificationCheckerMiddleware: Middleware = (middlewareApi: Middlew
 
         if (lastQuestionId === null) {
             persistence.session.save(KEY.FIRST_ANON_QUESTION, action.questionId);
-        } else {
-            if (
+        } else if (
                 state && !isLoggedIn(state.user) &&
                 lastQuestionId !== action.questionId &&
                 !withinLast2Hours(persistence.load(KEY.LOGIN_OR_SIGN_UP_MODAL_SHOWN_TIME))
@@ -40,7 +69,6 @@ export const notificationCheckerMiddleware: Middleware = (middlewareApi: Middlew
                 persistence.session.remove(KEY.FIRST_ANON_QUESTION);
                 persistence.save(KEY.LOGIN_OR_SIGN_UP_MODAL_SHOWN_TIME, new Date().toString());
                 await dispatch(openActiveModal(loginOrSignUpModal) as any);
-            }
         }
     }
 
