2b0b2c672a02e3d95002758c9ece76afe7fab052
Chris Purdy <33040507+chrisjpurdy@users.noreply.github.com>
2022-08-02 15:31:32 +0100

    Add a comment to explain strange type choices

--- src/IsaacApiTypes.tsx
+++ src/IsaacApiTypes.tsx
@@ -458,6 +458,11 @@ export interface QuantityDTO extends ChoiceDTO {
     units?: string;
 }
 
+// TODO move hiding past bast attempts into the backend for more flexibility
+// We use `null` to mean "best attempt hidden" so that all checks for whether `bestAttempt` is defined automatically
+// return false if it's hidden, without having to wrap all of these checks in an `isDefinedOrHidden()` predicate.
+// This latter approach would be more principled however. As it is, we make it clear at the type level that BestAttemptHidden
+// is "null with meaning" which should be fine for now, especially if we move best-attempt-hiding to the backend.
 export type BestAttemptHidden = null;
 export const BEST_ATTEMPT_HIDDEN: BestAttemptHidden = null;
 export interface QuestionDTO extends ContentDTO {
