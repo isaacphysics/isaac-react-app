dd09885222c206525aa458b9cb8ca703c896d93e
Chris Purdy <33040507+chrisjpurdy@users.noreply.github.com>
2022-08-10 12:28:00 +0100

    Change modal show date logic
    Show modal to anyone who hasn't updated their account context since the most recent August.

--- src/app/state/middleware/utils.ts
+++ src/app/state/middleware/utils.ts
@@ -1,23 +1,10 @@
-// Returns true if lastConfirmedDate is before *last August*, and it's at least the 24th of August. False otherwise
-export const needToUpdateUserContextDetails = (lastConfirmedDate: Date | undefined) => {
-    // First calculate when the beginning of last August before today was...
+export const needToUpdateUserContextDetails = (lastConfirmedDate: Date | undefined): boolean => {
     let date = new Date();
-    if (date.getMonth() <= 7) { // Month is 0-indexed for some stupid
+    if (date.getMonth() < 7) { // Month is 0-indexed for some stupid reason
         date.setFullYear(date.getFullYear() - 1);
     }
     date.setMonth(7, 1);
     date.setHours(0, 0, 0, 0);
-    // Date is now the 1st day of last August...
-
-    // If the user last confirmed their registered contexts before last August...
-    if (lastConfirmedDate && lastConfirmedDate < date) {
-        // Check if we're past the 24th of the August after
-        date.setDate(24);
-        date.setFullYear(date.getFullYear() + 1);
-        // Date is now the 24th of the August *after the last one* (the last week, give or take)
-        if (Date.now() > date.valueOf()) {
-            return true;
-        }
-    }
-    return false;
+    // Date is now the 1st day of most recent August
+    return !!lastConfirmedDate && lastConfirmedDate <= date;
 }
--- src/test/services/notifications.test.ts
+++ src/test/services/notifications.test.ts
@@ -5,23 +5,12 @@ jest.useFakeTimers();
 describe("Account detail reconfirmation modal is shown at the correct times", () => {
     it("Doesn't show the modal if the user has updated their details since last August", async () => {
         jest.useFakeTimers().setSystemTime(new Date("2000-09-01")); //
-        expect(needToUpdateUserContextDetails(new Date("2000-04-01"))).toBe(false);
+        expect(needToUpdateUserContextDetails(new Date("2000-08-02"))).toBe(false);
     })
-    it("Doesn't show the modal if the user hasn't updated their details since last August, but it isn't 24th August yet", async () => {
-        jest.useFakeTimers().setSystemTime(new Date("2000-08-01"));
-        expect(needToUpdateUserContextDetails(new Date("1999-04-01"))).toBe(false);
-    })
-    it("Shows the modal if the user hasn't updated their details since last August, and it's past the 24th August", async () => {
+    it("Shows the modal if the user hasn't updated their details since most recent August", async () => {
         jest.useFakeTimers().setSystemTime(new Date("2000-08-25"));
         expect(needToUpdateUserContextDetails(new Date("1999-04-01"))).toBe(true);
-    })
-    it("Doesn't show the modal if the date is between August 1st and 24th of the current year, and current date is 25th August", async () => {
-        jest.useFakeTimers().setSystemTime(new Date("2000-08-25"));
-        expect(needToUpdateUserContextDetails(new Date("2000-08-10"))).toBe(false);
-    })
-    it("Doesn't show the modal if the date is between August 24th and 31st of the current year, and current date is 25th August", async () => {
-        jest.useFakeTimers().setSystemTime(new Date("2000-08-25"));
-        expect(needToUpdateUserContextDetails(new Date("2000-08-28"))).toBe(false);
+        expect(needToUpdateUserContextDetails(new Date("2000-07-25"))).toBe(true);
     })
     it("Doesn't show the modal if somehow the given date is far into the future", async () => {
         jest.useFakeTimers().setSystemTime(new Date("2000-08-25"));
