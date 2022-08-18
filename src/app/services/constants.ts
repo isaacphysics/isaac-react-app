8751223bf72ad449aaf57fc4585ac956fd20b3b8
Meurig Thomas <mlt47@cam.ac.uk>
2022-08-18 09:47:46 +0100

    Remove unused year_7 and year_8 stages

--- src/app/services/constants.ts
+++ src/app/services/constants.ts
@@ -612,8 +612,6 @@ export const examBoardBooleanNotationMap: {[examBoard in ExamBoard]: BOOLEAN_NOT
 // STAGES
 export enum STAGE {
     YEAR_7_AND_8 = "year_7_and_8",
-    YEAR_7 = "year_7",
-    YEAR_8 = "year_8",
     YEAR_9 = "year_9",
     GCSE = "gcse",
     A_LEVEL = "a_level",
@@ -624,11 +622,9 @@ export enum STAGE {
 export const STAGE_NULL_OPTIONS = new Set([STAGE.ALL]);
 export const STAGES_PHY = new Set([STAGE.ALL, STAGE.YEAR_7_AND_8, STAGE.YEAR_9, STAGE.GCSE, STAGE.A_LEVEL, STAGE.FURTHER_A, STAGE.UNIVERSITY]);
 export const STAGES_CS = new Set([STAGE.ALL, STAGE.GCSE, STAGE.A_LEVEL]);
-export const stagesOrdered: Stage[] = ["year_7_and_8", "year_7", "year_8", "year_9", "gcse", "a_level", "further_a", "university", "all"];
+export const stagesOrdered: Stage[] = ["year_7_and_8", "year_9", "gcse", "a_level", "further_a", "university", "all"];
 export const stageLabelMap: {[stage in Stage]: string} = {
     year_7_and_8: "Year\u00A07&8",
-    year_7: "Year\u00A07",
-    year_8: "Year\u00A08",
     year_9: "Year\u00A09",
     gcse: "GCSE",
     a_level: "A\u00A0Level",
