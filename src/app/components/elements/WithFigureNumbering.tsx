4bac86634eafd7a68e06ad81c12f75317f3b98fb
Chris Purdy <33040507+chrisjpurdy@users.noreply.github.com>
2022-09-12 15:59:54 +0100

    Add figure numbering to figures in question choice explanations

--- src/app/components/elements/WithFigureNumbering.tsx
+++ src/app/components/elements/WithFigureNumbering.tsx
@@ -11,31 +11,44 @@ interface WithFigureNumberingProps {
 export const WithFigureNumbering = ({doc, children}: WithFigureNumberingProps) => {
     const figureNumbers = useContext(FigureNumberingContext);
 
+    const figuresOutOfNormalFlow = new Set<string>();
     let n = Object.keys(figureNumbers).length + 1;
-    function walk(d: any) {
+    function walk(d: any, outOfNormalFlow: boolean) {
         if (!d) {
             // Nothing to see here. Move along.
             return;
         } else if (d.type == "figure" && d.id) {
-            const figureId = extractFigureId(d.id)
-            if (!Object.keys(figureNumbers).includes(figureId)) {
+            const figureId = extractFigureId(d.id);
+            if (outOfNormalFlow) {
+                figuresOutOfNormalFlow.add(figureId);
+            } else if (!Object.keys(figureNumbers).includes(figureId)) {
                 figureNumbers[figureId] = n++;
             }
         } else {
             // Walk all the things that might possibly contain figures. Doesn't blow up if they don't exist.
             for (let c of d.children || []) {
-                walk(c);
+                walk(c, outOfNormalFlow);
             }
             for (let h of d.hints || []) {
-                walk(h);
+                walk(h, outOfNormalFlow);
             }
-            walk(d.answer);
-
+            walk(d.answer, outOfNormalFlow);
+            // Walk figures in question choices, marking them as being out of the usual document flow
+            for (let c of d.choices || []) {
+                walk(c.explanation, true);
+            }
             // If we find that some figures aren't getting numbers, add additional walks here to find them.
         }
     }
 
-    walk(doc);
+    walk(doc, false);
+
+    // Add all figures that exist out of the normal flow of the document (figures in choices for example)
+    for (const figureId of figuresOutOfNormalFlow.values()) {
+        if (!Object.keys(figureNumbers).includes(figureId)) {
+            figureNumbers[figureId] = n++;
+        }
+    }
 
     return children;
 };
