2eb932c070b1b82023d99898926a1d5127379b32
James Sharkey <jsharkey13@users.noreply.github.com>
2022-09-13 11:57:29 +0100

    Add further pseudocode builtins and keywords

--- src/app/services/highlightJs.ts
+++ src/app/services/highlightJs.ts
@@ -75,6 +75,8 @@ const isaacPseudocodeHighlightDefinition: LanguageFn = function(hljsLib) {
         "DEFAULT",
         "ENDSWITCH",
         "FOR",
+        "FOREACH",
+        "IN",
         "TO",
         "NEXT",
         "WHILE",
@@ -87,6 +89,10 @@ const isaacPseudocodeHighlightDefinition: LanguageFn = function(hljsLib) {
         "ENDFUNCTION",
         "PROCEDURE",
         "ENDPROCEDURE",
+        "TRY",
+        "CATCH",
+        "FINALLY",
+        "ENDTRY",
         "BYREF",
         "BYVAL",
         "RECORD",
@@ -98,6 +104,10 @@ const isaacPseudocodeHighlightDefinition: LanguageFn = function(hljsLib) {
         "PUBLIC",
         "PROTECTED",
         "NEW",
+        "EXTENDS",
+        "SUPER",
+        "DICTIONARY",
+        "LIST",
     ];
 
     const LITERALS = [
@@ -113,6 +123,19 @@ const isaacPseudocodeHighlightDefinition: LanguageFn = function(hljsLib) {
         "INPUT",
         "PRINT",
         "LEN",
+        "UPPER",
+        "LOWER",
+        "SPLIT",
+        "MID",
+        "LEFT",
+        "RIGHT",
+        "INDEXOF",
+        "STRIP",
+        "CONTAINS",
+        "DELETE",
+        "APPEND",
+        "POP",
+        "CREATE_FILE",
         "OPEN_READ",
         "OPEN_WRITE",
         "OPEN_APPEND",
@@ -124,7 +147,9 @@ const isaacPseudocodeHighlightDefinition: LanguageFn = function(hljsLib) {
         "END_OF_FILE",
         "ASC",
         "CHR",
-        "RANDOM_INT"
+        "RANDOM_INT",
+        "STR_TO_DATE",
+        "STR_TO_TIME",
     ]
 
     const FUNCTION_DEFINITION = {
