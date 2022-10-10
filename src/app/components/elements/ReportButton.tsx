17d65b03dbd53bde3d14ba5fda3fd00d3a9d0b7a
Matthew Trew <matthew.trew@raspberrypi.org>
2022-10-10 17:56:44 +0100

    Add report buttons to concepts, questions and accordions

---
+++ src/app/components/elements/ReportButton.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import {logAction, useAppDispatch} from "../../state";
+
+export const ReportButton = ({pageId} : {pageId?: string}) => {
+    const dispatch = useAppDispatch();
+
+    function logPageReport() {
+        let eventDetails = {
+            type: "REPORT_CONTENT_PAGE",
+            pageId: pageId
+        };
+        dispatch(logAction(eventDetails))
+    }
+
+    return <button
+        className="report-icon btn-action"
+        aria-label="Report a problem (opens in new tab)"
+        title="Report a problem (opens in new tab)"
+        onClick={(event) => {
+            logPageReport()
+            window.open(pageId ? `/contact?preset=contentProblem&page=${pageId}` : "/contact?preset=contentProblem",
+                "_blank")
+            }
+        }
+    />
+}
