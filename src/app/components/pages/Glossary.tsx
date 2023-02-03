60df8064155fc1dda42c9afc924bfce1eb810dba
Chris Purdy <33040507+chrisjpurdy@users.noreply.github.com>
2023-02-03 13:51:53 +0000

    Fix some scrolling issues in CS glossary
    Previously, if you navigate to a letter before the one you are currently on, it would jump to the bottom of the glossary terms for the letter instead of the top. There was also weird behaviour if you, for example, navigate to R, then navigate to J repeatedly.

--- src/app/components/pages/Glossary.tsx
+++ src/app/components/pages/Glossary.tsx
@@ -249,9 +249,9 @@ export const Glossary = () => {
                         {alphabetList}
                     </div>
                 </div>
-                {Object.entries(glossaryTerms).map(([letter, terms]) => <Row key={letter} className="pb-5">
+                {Object.entries(glossaryTerms).map(([letter, terms]) => <div key={letter} className="row pb-5" ref={(el: HTMLDivElement) => alphabetHeaderRefs.current.set(letter, el)}>
                     <Col md={{size: 1, offset: 1}}>
-                        <h2 style={{position: 'sticky', top: '1em'}} ref={(el: HTMLHeadingElement) => alphabetHeaderRefs.current.set(letter, el)}>
+                        <h2 style={{position: 'sticky', top: `calc(0px - ${ALPHABET_HEADER_OFFSET}px)`}}>
                             {letter}
                         </h2>
                     </Col>
@@ -265,7 +265,7 @@ export const Glossary = () => {
                             />
                         )}
                     </Col>
-                </Row>)}
+                </div>)}
             </Col>}
         </Container>
     </div>;
