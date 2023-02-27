6eed15a74f3927a51512555a1a2e553936a7733e
Chris Purdy <33040507+chrisjpurdy@users.noreply.github.com>
2023-02-27 14:40:23 +0000

    Remove test code for multiple audience tags

--- src/app/components/elements/StageAndDifficultySummaryIcons.tsx
+++ src/app/components/elements/StageAndDifficultySummaryIcons.tsx
@@ -6,7 +6,7 @@ import {ViewingContext} from "../../../IsaacAppTypes";
 
 export function StageAndDifficultySummaryIcons({audienceViews}: {audienceViews: ViewingContext[]}) {
     return <div className={classNames("mt-1", {"d-flex": isAda, "d-sm-flex mt-md-0": isPhy})}>
-        {audienceViews.concat(audienceViews).map((view, i) =>
+        {audienceViews.map((view, i) =>
             <div key={`${view.stage} ${view.difficulty} ${view.examBoard}`} className={classNames("d-flex align-self-center", {[siteSpecific("ml-sm-3 ml-md-2", "ml-3")]: i > 0, "d-md-block": isPhy})}>
                 {view.stage && view.stage !== STAGE.ALL && <div className="hierarchy-tags text-center">
                     {stageLabelMap[view.stage]}
