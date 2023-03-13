efa8e31062e6b2fcb8d3ad83691ddc6e0eea1758
Meurig Thomas <mlt47@cam.ac.uk>
2023-03-13 08:57:58 +0000

    Add temporary social links

--- src/app/services/constants.ts
+++ src/app/services/constants.ts
@@ -64,10 +64,10 @@ export const SOCIAL_LINKS = siteSpecific(
     },
     {
         // FIXME ADA: update these - also we don't have an instagram logo
-        youtube: {name: "YouTube", href: "https://www.youtube.com/channel/UC-qoIYj8kgR8RZtQphrRBYQ"},
-        twitter: {name: "Twitter", href: "https://twitter.com/isaaccompsci"},
-        facebook: {name: "Facebook", href: "https://www.facebook.com/IsaacComputerScience"},
-        instagram: {name: "Instagram", href: "https://www.instagram.com/isaaccompsci"}
+        youtube: {name: "YouTube", href: "https://www.youtube.com/c/RaspberryPiFoundation"},
+        twitter: {name: "Twitter", href: "https://twitter.com/RaspberryPi_org"},
+        facebook: {name: "Facebook", href: "https://www.facebook.com/RaspberryPiFoundation"},
+        instagram: {name: "Instagram", href: "https://www.instagram.com/raspberrypifoundation"}
     }
 );
 
