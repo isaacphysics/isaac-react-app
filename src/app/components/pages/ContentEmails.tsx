51cdc41c5ab808d9bc8091092f58d3febae50c45
Matthew Trew <matthew.trew@raspberrypi.org>
2023-03-22 12:19:44 +0000

    Remove resolved content email MailGun FIXME

--- src/app/components/pages/ContentEmails.tsx
+++ src/app/components/pages/ContentEmails.tsx
@@ -46,7 +46,6 @@ const ContentEmails = (props: ContentEmailsProps) => {
         })
     }, [emailSubject, plaintextTemplate, htmlTemplate, overrideEnvelopeFrom]);
 
-    // FIXME ADA need to discuss the change to this
     const mailgunAddress = siteSpecific("no-reply@mail.isaacphysics.org", "no-reply@mail.adacomputerscience.org");
 
     return <RS.Container id="admin-emails-page">
