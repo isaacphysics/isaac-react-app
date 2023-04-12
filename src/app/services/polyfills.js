befeaa04ef9a7bbc75f12029ae63af624c91bad0
James Sharkey <jsharkey13@users.noreply.github.com>
2023-04-12 13:05:37 +0100

    Restore core-js but only for used features
    Apparently some users are using browsers over 3 years out of date, but
    still "modern" enough that we cannot use the Nginx User-Agent detection
    to block them. The polyfills are hence just for the modern features we
    use that are causing problems, and is much smaller than including all of
    core-js.
    This replaces the additional string.prototype.matchall library with the
    same feature already in core-js, and removes Element#closest polyfills
    which are not necessary outside of IE.

--- src/app/services/polyfills.js
+++ src/app/services/polyfills.js
@@ -1,22 +1,4 @@
-import React from "react";
-import matchAll from 'string.prototype.matchall';
-
-React;  // lgtm [js/useless-expression]
-
-matchAll.shim();
-
-if (!Element.prototype.matches) {
-    Element.prototype.matches = Element.prototype.msMatchesSelector || Element.prototype.webkitMatchesSelector;
-}
-
-if (!Element.prototype.closest) {
-    Element.prototype.closest = function(s) {
-        var el = this;
-
-        do {
-            if (el.matches(s)) return el;
-            el = el.parentElement || el.parentNode;
-        } while (el !== null && el.nodeType === 1);
-        return null;
-    };
-}
+import 'core-js/full/string/match-all';
+import 'core-js/full/string/replace-all';
+import 'core-js/full/array/at';
+import 'core-js/full/array/flat-map';
