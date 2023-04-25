566c93e173d925b77724bf1db78549741063c572
Matthew Trew <matthew.trew@raspberrypi.org>
2023-04-25 13:30:40 +0100

    Get groups in SetQuizzes component so augmenting assignments succeeds

--- src/app/components/pages/quizzes/SetQuizzes.tsx
+++ src/app/components/pages/quizzes/SetQuizzes.tsx
@@ -1,5 +1,6 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useMemo, useState} from "react";
 import {
+    isaacApi,
     loadQuizAssignments,
     markQuizAsCancelled,
     selectors,
@@ -85,6 +86,11 @@ const SetQuizzesPageComponent = ({user, location}: SetQuizzesPageProps) => {
     const hashAnchor = location.hash?.slice(1) ?? null;
     const [activeTab, setActiveTab] = useState(MANAGE_QUIZ_TAB.set);
     const [pageTitle, setPageTitle] = useState(siteSpecific((activeTab !== MANAGE_QUIZ_TAB.manage ? "Set" : "Manage") + " Tests", "Manage tests"));
+
+    // todo: This is so when the quizAssignments selector tries to augment quizzes with group names, it works.
+    //  Revisit when refactoring the rest of this component to use RTK Query.
+    const { data: groups } = isaacApi.endpoints.getGroups.useQuery(false);
+
     const quizAssignments = useAppSelector(selectors.quizzes.assignments);
 
     // Set active tab using hash anchor
