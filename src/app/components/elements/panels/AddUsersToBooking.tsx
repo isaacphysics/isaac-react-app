4eaf9972c0631e655cce2d96e35787639f472344
Chris Purdy <33040507+chrisjpurdy@users.noreply.github.com>
2023-06-06 16:49:53 +0100

    Remove unused import

--- src/app/components/elements/panels/AddUsersToBooking.tsx
+++ src/app/components/elements/panels/AddUsersToBooking.tsx
@@ -1,4 +1,4 @@
-import React, {useCallback, useEffect, useRef, useState} from "react";
+import React, {useCallback, useRef, useState} from "react";
 import * as RS from "reactstrap";
 import {Accordion} from "../Accordion";
 import {
