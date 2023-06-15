0051cfd7509b1653f089eb88476ed990f464256a
hannah-whelan <hannah.whelan@and.digital>
2023-06-15 14:24:20 +0100

    changed back to startsWith but checks for www. in hostname

--- src/app/services/constants.ts
+++ src/app/services/constants.ts
@@ -31,8 +31,8 @@ if (document.location.hostname === "localhost") {
 } else if (document.location.hostname.endsWith(".eu.ngrok.io")) {
     apiPath = "https://isaacscience.eu.ngrok.io/isaac-api/api";
 }
-export const isTest = document.location.hostname.includes("test.");
-export const isStaging = document.location.hostname.includes("staging.");
+export const isTest = document.location.hostname.startsWith("test.") || document.location.hostname.startsWith("wwww.test.");
+export const isStaging = document.location.hostname.startsWith("staging.") || document.location.hostname.startsWith("www.staging.");
 
 export const envSpecific = <L, S, D>(live: L, staging: S, dev: D) => isStaging ? staging : process.env.NODE_ENV === 'production' ? live : dev;
 
