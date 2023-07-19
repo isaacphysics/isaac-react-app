9d99bb236595a1377a300ecd5deddec9547948ec
Chris Purdy <33040507+chrisjpurdy@users.noreply.github.com>
2023-07-19 16:48:08 +0100

    Add dependancy list to logout handler useEffect
    So that it only sends one logout request

--- src/app/components/handlers/LogOutHandler.tsx
+++ src/app/components/handlers/LogOutHandler.tsx
@@ -4,13 +4,15 @@ import {IsaacSpinner} from "./IsaacSpinner";
 
 export const LogOutHandler = () => {
     const dispatch = useAppDispatch();
-    useEffect(() => {dispatch(logOutUser())});
-    return <React.Fragment>
+    useEffect(() => {
+        dispatch(logOutUser());
+    }, []);
+    return <>
         <div className="w-100 text-center">
             <h2 className="pt-5 pb-2">
                 Logging out...
             </h2>
             <IsaacSpinner />
         </div>
-    </React.Fragment>
+    </>;
 };
