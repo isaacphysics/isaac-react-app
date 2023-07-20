66f4df8ac490820fb03f6bbfa68c01c91170db88
Chris Purdy <33040507+chrisjpurdy@users.noreply.github.com>
2023-07-20 14:12:51 +0100

    Make Google sign up button black on Ada
    Just to follow brand guidelines

--- src/app/components/elements/GoogleSignInButton.tsx
+++ src/app/components/elements/GoogleSignInButton.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {Button} from "reactstrap";
 import {handleProviderLoginRedirect, useAppDispatch} from "../../state";
+import {siteSpecific} from "../../services";
 
 // Button prompting the user to sign in via Google
 export const GoogleSignInButton = () => {
@@ -10,7 +11,7 @@ export const GoogleSignInButton = () => {
         dispatch(handleProviderLoginRedirect("GOOGLE"));
     };
 
-    return <Button className={"position-relative"} block outline color="primary" onClick={logInWithGoogle}>
+    return <Button className={"position-relative"} block outline color={siteSpecific("primary", undefined)} style={siteSpecific({}, {borderColor: "black"})} onClick={logInWithGoogle}>
         <img className="google-button-logo" src={"/assets/google-logo.svg"} alt={"Google logo"}/>Google
-    </Button>
+    </Button>;
 }
