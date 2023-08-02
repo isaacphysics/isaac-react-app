89fb23e624d5a4ff263b3dca5a9873f5f176c1ec
Chris Purdy <33040507+chrisjpurdy@users.noreply.github.com>
2023-08-02 14:49:50 +0100

    Fix font colour inconsistency on Ada Google sign in button

--- src/app/components/elements/GoogleSignInButton.tsx
+++ src/app/components/elements/GoogleSignInButton.tsx
@@ -11,7 +11,7 @@ export const GoogleSignInButton = () => {
         dispatch(handleProviderLoginRedirect("GOOGLE"));
     };
 
-    return <Button className={"position-relative"} block outline color={siteSpecific("primary", undefined)} style={siteSpecific({}, {borderColor: "black"})} onClick={logInWithGoogle}>
+    return <Button className={"position-relative"} block outline color={siteSpecific("primary", undefined)} style={siteSpecific({}, {borderColor: "black", color: "black"})} onClick={logInWithGoogle}>
         <img className="google-button-logo" src={"/assets/google-logo.svg"} alt={"Google logo"}/>Google
     </Button>;
 }
