45de86405017508b0a720dac1f3852a6dfddeaab
Meurig Thomas <mlt47@cam.ac.uk>
2023-08-08 17:45:20 +0100

    Fix anon log in modal

--- src/app/state/middleware/notificationManager.ts
+++ src/app/state/middleware/notificationManager.ts
@@ -55,9 +55,7 @@ export const notificationCheckerMiddleware: Middleware = (middlewareApi: Middlew
                 lastQuestionId !== action.questionId &&
                 !withinLast2Hours(persistence.load(KEY.LOGIN_OR_SIGN_UP_MODAL_SHOWN_TIME))
             ) {
-                dispatch(logAction({
-                    type: "LOGIN_MODAL_SHOWN"
-                }));
+                logAction({ type: "LOGIN_MODAL_SHOWN" });
                 persistence.session.remove(KEY.FIRST_ANON_QUESTION);
                 persistence.save(KEY.LOGIN_OR_SIGN_UP_MODAL_SHOWN_TIME, new Date().toString());
                 await dispatch(openActiveModal(loginOrSignUpModal) as any);
