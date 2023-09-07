20d682bcc10219d7c65bbae5532292777d71efb3
Meurig Thomas <mlt47@cam.ac.uk>
2023-09-07 14:52:42 +0100

    Draft new my assignments layout for Phy

--- src/app/components/elements/Assignments.tsx
+++ src/app/components/elements/Assignments.tsx
@@ -1,8 +1,9 @@
 import {AssignmentDTO} from "../../../IsaacApiTypes";
-import React, {MouseEvent, useMemo} from "react";
-import {Button, Col, Row} from "reactstrap";
+import React, {MouseEvent, useMemo, useState} from "react";
+import {Button, Col, Collapse, Label, Row} from "reactstrap";
 import {Link} from "react-router-dom";
 import {
+    determineGameboardPercentageAttempted,
     determineGameboardStagesAndDifficulties,
     determineGameboardSubjects,
     difficultyShortLabelMap,
@@ -35,8 +36,11 @@ const CSCircle = ({assignment}: {assignment: AssignmentDTO}) => {
 };
 
 const PhyAssignmentCard = ({assignment}: {assignment: AssignmentDTO}) => {
+    const [showMore, setShowMore] = useState(false);
     const now = new Date();
     const boardStagesAndDifficulties = useMemo(() => determineGameboardStagesAndDifficulties(assignment.gameboard), [assignment.gameboard]);
+    const boardPercentageAttempted = useMemo(() => determineGameboardPercentageAttempted(assignment.gameboard), [assignment.gameboard]);
+
     const topics = tags.getTopicTags(Array.from((assignment.gameboard?.contents || []).reduce((a, c) => {
         if (isDefined(c.tags) && c.tags.length > 0) {
             return new Set([...Array.from(a), ...c.tags.map(id => id as TAG_ID)]);
@@ -44,21 +48,11 @@ const PhyAssignmentCard = ({assignment}: {assignment: AssignmentDTO}) => {
         return a;
     }, new Set<TAG_ID>())).filter(tag => isDefined(tag))).map(tag => tag.title).sort();
     const assignmentStartDate = assignment.scheduledStartDate ?? assignment.creationDate;
+
     return <>
         <hr />
         <Row className="board-card" data-testid={"my-assignment"}>
-            <Col xs={4} md={2} lg={1}>
-                <div className="board-subject-hexagon-container">
-                    {isDefined(assignment.gameboard) && ((assignment.gameboard.percentageCompleted === 100) ?
-                            <span className="board-subject-hexagon subject-complete"/> :
-                            <>
-                                {generateGameboardSubjectHexagons(determineGameboardSubjects(assignment.gameboard))}
-                                <div className="board-percent-completed">{assignment.gameboard.percentageCompleted}</div>
-                            </>
-                    )}
-                </div>
-            </Col>
-            <Col xs={8} md={3} lg={4} className="pl-lg-5">
+            <Col xs={9} md={8} className="pl-lg-5">
                 <Link to={`${PATHS.GAMEBOARD}#${assignment.gameboardId}`}>
                     <h4>{isDefined(assignment.gameboard) && assignment.gameboard.title}</h4>
                 </Link>
@@ -75,38 +69,46 @@ const PhyAssignmentCard = ({assignment}: {assignment: AssignmentDTO}) => {
                 <p><strong>By:</strong> {extractTeacherName(assignment.assignerSummary)}</p>
                 }
             </Col>
-            <Col xs={7} md={5} className="mt-sm-2">
-                <p className="mb-0"><strong>Questions:</strong> {assignment.gameboard?.contents?.length || "0"}</p>
-                {isDefined(topics) && topics.length > 0 && <p className="mb-0">
-                    <strong>{topics.length === 1 ? "Topic" : "Topics"}:</strong>{" "}
-                    {topics.join(", ")}
-                </p>}
-                {boardStagesAndDifficulties.length > 0 && <p className="mb-0">
-                    <table className="w-100">
-                        <thead>
-                        <tr>
-                            <th className="w-50">
-                                {`Stage${boardStagesAndDifficulties.length > 1 ? "s" : ""}:`}
-                            </th>
-                            <th className="w-50">
-                                {`Difficult${boardStagesAndDifficulties.some(([, ds]) => ds.length > 1) ? "ies" : "y"}`}
-                            </th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        {boardStagesAndDifficulties.map(([stage, difficulties]) => <tr key={stage}>
-                            <td className="w-50 align-baseline">
-                                {stageLabelMap[stage]}:
-                            </td>
-                            <td className="w-50 pl-1">
-                                {difficulties.map((d) => difficultyShortLabelMap[d]).join(", ")}
-                            </td>
-                        </tr>)}
-                        </tbody>
-                    </table>
-                </p>}
-                {isDefined(assignment.notes) && <p><strong>Notes:</strong> {assignment.notes}</p>}
+
+            <Col xs={3} md={2}>
+                <Label className="text-center">
+                    %&nbsp;Attempted
+                    <div className="d-flex justify-content-center">
+                        <div className="board-subject-hexagon-container">
+                            {isDefined(assignment.gameboard) && ((boardPercentageAttempted === 100) ?
+                                <span className="board-subject-hexagon subject-complete"/> :
+                                <>
+                                    {generateGameboardSubjectHexagons(determineGameboardSubjects(assignment.gameboard))}
+                                    <div className="board-percent-completed">{boardPercentageAttempted}</div>
+                                </>
+                            )}
+                        </div>
+                    </div>
+                </Label>
             </Col>
+
+            <Col className="d-none d-md-block" md={2}>
+                <Label className="text-center">
+                    %&nbsp;Correct
+                    <div className="d-flex justify-content-center">
+                        <div className="board-subject-hexagon-container">
+                            {isDefined(assignment.gameboard) && ((assignment.gameboard.percentageCompleted === 100) ?
+                                <span className="board-subject-hexagon subject-complete"/> :
+                                <>
+                                    {generateGameboardSubjectHexagons(determineGameboardSubjects(assignment.gameboard))}
+                                    <div className="board-percent-completed">{assignment.gameboard.percentageCompleted}</div>
+                                </>
+                            )}
+                        </div>
+                    </div>
+                </Label>
+            </Col>
+        </Row>
+        <Row>
+            <Button className="ml-3" color="link" onClick={() => setShowMore(!showMore)}>
+                {showMore ? "Show less" : "Show more"}
+            </Button>
+
             <Col xs={5} md={2} className="mt-sm-2 text-right">
                 <Link to={`${PATHS.GAMEBOARD}#${assignment.gameboardId}`}>
                     View Assignment
@@ -114,6 +116,41 @@ const PhyAssignmentCard = ({assignment}: {assignment: AssignmentDTO}) => {
                 {isDefined(assignment.dueDate) && isDefined(assignment.gameboard) && now > midnightOf(assignment.dueDate) && assignment.gameboard.percentageCompleted !== 100 &&
                 <p><strong className="overdue">Overdue:</strong> {formatDate(assignment.dueDate)}</p>}
             </Col>
+
+            <Collapse isOpen={showMore} className="w-100">
+                <Col xs={7} md={5} className="mt-sm-2">
+                    <p className="mb-0"><strong>Questions:</strong> {assignment.gameboard?.contents?.length || "0"}</p>
+                    {isDefined(topics) && topics.length > 0 && <p className="mb-0">
+                        <strong>{topics.length === 1 ? "Topic" : "Topics"}:</strong>{" "}
+                        {topics.join(", ")}
+                    </p>}
+                    {boardStagesAndDifficulties.length > 0 && <p className="mb-0">
+                        <table className="w-100">
+                            <thead>
+                            <tr>
+                                <th className="w-50">
+                                    {`Stage${boardStagesAndDifficulties.length > 1 ? "s" : ""}:`}
+                                </th>
+                                <th className="w-50">
+                                    {`Difficult${boardStagesAndDifficulties.some(([, ds]) => ds.length > 1) ? "ies" : "y"}`}
+                                </th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            {boardStagesAndDifficulties.map(([stage, difficulties]) => <tr key={stage}>
+                                <td className="w-50 align-baseline">
+                                    {stageLabelMap[stage]}:
+                                </td>
+                                <td className="w-50 pl-1">
+                                    {difficulties.map((d) => difficultyShortLabelMap[d]).join(", ")}
+                                </td>
+                            </tr>)}
+                            </tbody>
+                        </table>
+                    </p>}
+                    {isDefined(assignment.notes) && <p><strong>Notes:</strong> {assignment.notes}</p>}
+                </Col>
+            </Collapse>
         </Row>
     </>
 };
