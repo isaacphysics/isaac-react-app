084518b3cf15bb24ae7988969129829a428324c6
Meurig Thomas <mlt47@cam.ac.uk>
2024-01-10 15:24:32 +0000

    Use state for chat messages

--- src/app/components/navigation/ChatWindow.tsx
+++ src/app/components/navigation/ChatWindow.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 
+interface Message {
+    message: string;
+    sender: "user" | "ada";
+}
 
-function ChatMessage({message, sender}: {message: string, sender: "user" | "ada"}) {
+function ChatMessage({message, sender}: Message) {
     const senderStatus = sender === "ada" ? "received" : "sent";
     const senderImage = sender === "ada" ? "/assets/logos/ada_logo_stamp_aqua.svg" : "/assets/card02.png";
     return <div className={`message ${senderStatus}`}>
@@ -12,23 +16,25 @@ function ChatMessage({message, sender}: {message: string, sender: "user" | "ada"
 
 
 export function ChatWindow() {
+    const [messages, setMessages] = useState<Message[]>([]);
+    const [currentMessage, setCurrentMessage] = useState<string>("");
 
     return <div className="chat-window">
         <div className="chat-header">Talk to Ada</div>
         <div className="chat-body">
             <ul>
-                <li className="chat-message">
-                    <ChatMessage sender="ada" message="Hi, I'm Ada. How can I help you?" />
-                </li>
-                <li className="chat-message">
-                    <ChatMessage sender="user" message="I would you like some Computer Science help, pls!" />
-                </li>
+                {messages.map((message, index) => <li key={index} className="chat-message">
+                    <ChatMessage {...message} />
+                </li>)}
             </ul>
         </div>
-        <form>
-            <input type="text" placeholder="Type your message here" />
+        <form onSubmit={e => {
+            e.preventDefault();
+            setMessages([...messages, {message: currentMessage, sender: "user"}]);
+            setCurrentMessage("");
+        }}>
+            <input type="text" placeholder="Type your message here" value={currentMessage} onChange={e => setCurrentMessage(e.target.value)}/>
             <button>Send</button>
         </form>
-        
     </div>;
 }
