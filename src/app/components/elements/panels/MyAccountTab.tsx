57d3327595b0ff2a2e17a13f6beba151e17cee94
Jacob Brown <jeab2@cam.ac.uk>
2024-01-16 16:59:14 +0000

    Improve padding styling

--- src/app/components/elements/panels/MyAccountTab.tsx
+++ src/app/components/elements/panels/MyAccountTab.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { CardBody, Col, Container, Row } from "reactstrap";
-import { isPhy } from "../../../services";
+import { isAda, isPhy } from "../../../services";
+import classNames from "classnames";
 
 export interface MyAccountTabProps {
     leftColumn: React.ReactNode;
@@ -8,18 +9,18 @@ export interface MyAccountTabProps {
 }
 
 export const MyAccountTab = ({leftColumn, rightColumn} : MyAccountTabProps) => {
-    return <CardBody>
-        <Container className="px-0 px-lg-2">
+    return <CardBody className={classNames("px-4", {"px-sm-5": isAda})}>
+        <Container>
             {isPhy ? <Col className="px-0 px-lg-2">
                 {leftColumn}
                 <Row className="pt-4"/>
                 {rightColumn}
             </Col> :
             <Row>
-                <Col lg={6} className="pr-lg-4 px-0 px-lg-2">
+                <Col lg={6} className="pr-lg-4 px-0 pl-lg-2">
                     {leftColumn}
                 </Col>
-                <Col lg={6} className="pl-lg-4 px-0 px-lg-2 pt-4 pt-lg-0">
+                <Col lg={6} className="pl-lg-4 px-0 pl-lg-2 pt-4 pt-lg-0">
                     {rightColumn}
                 </Col>
             </Row>}
