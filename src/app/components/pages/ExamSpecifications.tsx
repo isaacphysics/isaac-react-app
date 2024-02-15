19997c701126fdc13dc4dd988c8b69f3f15b9378
Jacob Brown <jeab2@cam.ac.uk>
2024-02-15 16:41:53 +0000

    Maintain tab if possible when switching stages

--- src/app/components/pages/ExamSpecifications.tsx
+++ src/app/components/pages/ExamSpecifications.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useMemo, useState} from "react";
+import React, {useEffect, useState} from "react";
 import {Col, Container, Row} from "reactstrap";
 import {Tabs} from "../elements/Tabs";
 import {PageFragment} from "../elements/PageFragment";
@@ -6,12 +6,26 @@ import {CS_EXAM_BOARDS_BY_STAGE, EXAM_BOARD, STAGE, STAGES_CS, stageLabelMap} fr
 import {TitleAndBreadcrumb} from "../elements/TitleAndBreadcrumb";
 import {MetaDescription} from "../elements/MetaDescription";
 
+const getStageFromURL = () => {
+    const urlStage = window.location.hash.split("/")[0].slice(1);
+    return STAGES_CS.includes(urlStage as typeof STAGES_CS[number])
+        ? urlStage as typeof STAGES_CS[number] 
+        : STAGE.A_LEVEL;
+};
+
+const getExamBoardFromURL = () => {
+    const urlExamBoard = window.location.hash.split("/")[1];
+    return Object.values(EXAM_BOARD).includes(urlExamBoard as EXAM_BOARD)
+        ? urlExamBoard as EXAM_BOARD
+        : EXAM_BOARD.AQA;
+};
+
 export const ExamSpecifications = () => {
-    const STAGES_WITH_EXAM_SPECIFICATIONS = useMemo(() => [STAGE.A_LEVEL, STAGE.GCSE], []);
-    const [stageTab, setStageTab] = useState<typeof STAGES_CS[number]>(STAGES_CS.includes(window.location.hash.split("/")[0].slice(1) as typeof STAGES_CS[number]) ? window.location.hash.split("/")[0].slice(1) as typeof STAGES_CS[number] : STAGE.A_LEVEL);
-    const [examBoardTab, setExamBoardTab] = useState<EXAM_BOARD>(Object.values(EXAM_BOARD).includes(window.location.hash.split("/")[1] as EXAM_BOARD) ? window.location.hash.split("/")[1] as EXAM_BOARD : EXAM_BOARD.AQA);
-    const [stageTabOverride, _setStageTabOverride] = useState<number | undefined>(STAGES_WITH_EXAM_SPECIFICATIONS.includes(stageTab) ? STAGES_WITH_EXAM_SPECIFICATIONS.indexOf(stageTab) + 1 : undefined);
-    const [examBoardTabOverride, setExamBoardTabOverride] = useState<number | undefined>(CS_EXAM_BOARDS_BY_STAGE[stageTab].includes(examBoardTab) ? CS_EXAM_BOARDS_BY_STAGE[stageTab].indexOf(examBoardTab) + 1 : undefined);
+    const STAGES_WITH_EXAM_SPECIFICATIONS = [STAGE.A_LEVEL, STAGE.GCSE];
+    const [stageTab, setStageTab] = useState<typeof STAGES_CS[number]>(getStageFromURL());
+    const [examBoardTab, setExamBoardTab] = useState<EXAM_BOARD>(getExamBoardFromURL());
+    const [stageTabOverride, _setStageTabOverride] = useState<number | undefined>(STAGES_WITH_EXAM_SPECIFICATIONS.indexOf(stageTab) + 1 || undefined);
+    const [examBoardTabOverride, setExamBoardTabOverride] = useState<number | undefined>(CS_EXAM_BOARDS_BY_STAGE[stageTab].indexOf(examBoardTab) + 1 || undefined);
     const [fragmentId, setFragmentId] = useState<string>("");
 
     const stageExamBoards = CS_EXAM_BOARDS_BY_STAGE[stageTab];
@@ -42,20 +56,17 @@ export const ExamSpecifications = () => {
     const stageTabs = <Tabs style={"buttons"} className={"mt-3"} tabContentClass={"mt-3"}
         activeTabOverride={stageTabOverride}
         onActiveTabChange={(n) => {
-            setStageTab(STAGES_WITH_EXAM_SPECIFICATIONS[n - 1] as typeof STAGES_CS[number]);
-            setExamBoardTab(CS_EXAM_BOARDS_BY_STAGE[stageTab][0] as EXAM_BOARD);
-            setExamBoardTabOverride(1);
+            const newStage = STAGES_WITH_EXAM_SPECIFICATIONS[n - 1] as typeof STAGES_CS[number];
+            const newExamBoard = CS_EXAM_BOARDS_BY_STAGE[newStage].includes(examBoardTab) ? examBoardTab : CS_EXAM_BOARDS_BY_STAGE[newStage][0];
+            setStageTab(newStage);
+            setExamBoardTab(newExamBoard as EXAM_BOARD);
+            setExamBoardTabOverride(CS_EXAM_BOARDS_BY_STAGE[newStage].indexOf(newExamBoard) + 1);
         }}
     >
         {examBoardTabs}
     </Tabs>;
 
     useEffect(() => {
-        // Since examBoardTab is last in the dependency chain, we would ideally only have this useEffect run when examBoardTab changes.
-        // However, since exam boards can be shared among stages, switching from one stage to another can result in the same exam board being selected.
-        // In this case, the useEffect wouldn't run at all if we only had examBoardTab as a dependency.
-        // This unfortunately means we have to have all tabs in the dependency, meaning we can generate an invalid request between when
-        //  the stage has changed but the exam board is yet to update.
         setFragmentId(`${stageTab}_specification_${examBoardTab}`);
         location.replace(`#${stageTab}/${examBoardTab}`);
     }, [examBoardTab, stageTab]);
