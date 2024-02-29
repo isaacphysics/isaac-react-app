46342dd68f3a8e9fd412596662a29b39376c3e00
James Sharkey <jsharkey13@users.noreply.github.com>
2024-02-29 15:41:57 +0000

    Mark ML and AI topics as new
    This should have been done in the previous commit :facepalm:

--- src/app/services/tagsCS.ts
+++ src/app/services/tagsCS.ts
@@ -27,8 +27,8 @@ export class CsTagService extends AbstractBaseTagService {
         {id: TAG_ID.theoryOfComputation, title: "Models of computation", parent: TAG_ID.computerScience, stageOverride: GCSE_HIDDEN},
 
         // AI and machine learning topics
-        {id: TAG_ID.artificialIntelligence, title: "Artificial intelligence", parent: TAG_ID.aiAndMachineLearning},
-        {id: TAG_ID.machineLearning, title: "Machine learning", parent: TAG_ID.aiAndMachineLearning},
+        {id: TAG_ID.artificialIntelligence, title: "Artificial intelligence", parent: TAG_ID.aiAndMachineLearning, new: true},
+        {id: TAG_ID.machineLearning, title: "Machine learning", parent: TAG_ID.aiAndMachineLearning, new: true},
 
         // Algorithms and data structures topics
         {id: TAG_ID.dataStructures, title: "Data structures", parent: TAG_ID.dataStructuresAndAlgorithms},
