ea5174635174c57d26a72433858c33d143354239
Matthew Trew <matthew.trew@raspberrypi.org>
2024-03-11 19:13:55 +0000

    Update benefits, resources and stories slice styling

--- src/app/components/site/cs/HomepageCS.tsx
+++ src/app/components/site/cs/HomepageCS.tsx
@@ -3,9 +3,9 @@ import {selectors, useAppSelector, useGetNewsPodListQuery} from "../../../state"
 import {Link} from "react-router-dom";
 import {Button, CardDeck, Col, Container, Row} from "reactstrap";
 import {isLoggedIn, SITE_TITLE} from "../../../services";
-import {WhySignUpTabs} from "../../elements/WhySignUpTabs";
 import {AdaHero2x1} from "../../elements/svg/AdaHero";
 import {FeaturedNewsItem} from "../../elements/FeaturedNewsItem";
+import {WarningBanner} from "../../navigation/WarningBanner";
 import {IsaacCardDeck} from "../../content/IsaacCardDeck";
 import {NewsCard} from "../../elements/cards/NewsCard";
 import {AdaHomepageSearch} from "../../elements/SearchInputs";
@@ -61,55 +61,60 @@ export const HomepageCS = () => {
             </section>
 
             <section id="benefits-for-teachers-and-students">
-                <Container className={"py-lg-6 py-5"}>
-                    <Row>
-                        <Col lg={6} className={"px-5 my-auto my-lg-0"}>
-                            <picture>
-                                <source srcSet="/assets/cs/decor/benefits-for-homepage-3x4.webp" type="image/webp"/>
-                                <img className={"d-none d-lg-block w-100"} src={"/assets/cs/decor/benefits-for-homepage-3x4.png"} alt="" />
-                            </picture>
-                            <picture>
-                                <source srcSet="/assets/cs/decor/benefits-for-homepage-4x3.webp" type="image/webp"/>
-                                <img className={"d-lg-none d-block w-100"} src={"/assets/cs/decor/benefits-for-homepage-4x3.png"} alt="" />
-                            </picture>
+                <Container className={"py-5 px-md-4 px-xxl-5 mw-1600"}>
+                    <Row className={"align-items-center"}>
+                        <Col xs={12} lg={6}>
+                            <h2 className={"font-size-1-75 mb-4"}>Ada Computer Science is:</h2>
+                            <ul className={"font-size-1-25"}>
+                                <li>Made by the University of Cambridge and the Raspberry Pi Foundation</li>
+                                <li>Mapped to computer science exam specifications, including GCSE and A level</li>
+                                <li>A great way to save time when planning lessons and homework</li>
+                                <li>Perfect for learning a topic after class or preparing for exams</li>
+                                <li>Free forever — no hidden costs</li>
+                            </ul>
                         </Col>
-                        <Col lg={6} className={"order-first order-lg-last pb-5 pb-md-0"}>
-                            <WhySignUpTabs user={user}/>
+                        <Col xs={12} lg={6} className={"mt-4 mt-lg-0"}>
+                            <picture>
+                                <source srcSet="/assets/cs/decor/benefits-for-homepage.webp" type="image/webp"/>
+                                <img className={"d-block w-100"} src={"/assets/cs/decor/benefits-for-homepage.png"} alt="" />
+                            </picture>
                         </Col>
                     </Row>
                 </Container>
             </section>
 
             <section id="what-resources">
-                <Container className={"py-lg-6 py-5"}>
-                    <h2 className={"mb-5 mb-lg-6"}>What are you looking for?</h2>
-                    <IsaacCardDeck doc={{
-                        cards: [{
-                            title: "GCSE computer science",
-                            subtitle: "Our GCSE computer science topics cover the secondary school phase of learning for students aged 14 to 16.",
-                            clickUrl: "/topics#gcse",
-                            buttonText: "View GCSE resources",
-                            imageClassName: "backslash-1"
-                        }, {
-                            title: "A level computer science",
-                            subtitle: "Our A level computer science topics cover the advanced secondary school phase of learning for students aged 16 to 19.",
-                            clickUrl: "/topics#a_level",
-                            buttonText: "View A level resources",
-                            imageClassName: "backslash-2"
-                        }]
-                    }} className={"homepage-cards"} />
+                <Container className={"py-5 px-md-4 px-xxl-5 mw-1600"}>
+                    <h2 className={"font-size-1-75 mb-5"}>What resources are you looking for?</h2>
+                    <IsaacCardDeck
+                        doc={{
+                            cards: [{
+                                title: "Foundational",
+                                subtitle: "Aimed at students aged 14-16, these resources cover core concepts in computer science for students working towards qualifications like GCSEs and National 5s.",
+                                clickUrl: "/topics#gcse",
+                                buttonText: "See more",
+                                imageClassName: "backslash-1"
+                            }, {
+                                title: "Advanced",
+                                subtitle: "Aimed at students aged 16-19, these resources cover advanced concepts. They are useful for students studying for A levels and Advanced Placement courses, and those heading towards further education.",
+                                clickUrl: "/topics#a_level",
+                                buttonText: "See more",
+                                imageClassName: "backslash-2"
+                            }]
+                        }}
+                        className={"homepage-cards"}
+                        containerClassName={"mw-1600"}
+                    />
                 </Container>
             </section>
 
             <section id="computer-science-stories">
-                <Container className={"py-lg-6 py-5"}>
+                <Container className={"py-5 px-md-4 px-xxl-5 mw-1600"}>
                     <Row>
                         <Col xs={12} md={6} id={"cs-stories-text"}>
-                            <h2 className={"mb-4"}>Computer Science Stories</h2>
+                            <h2 className={"font-size-1-75 mb-5"}>Computer science stories</h2>
                             <p className={"mb-4"}>
-                                Ada Lovelace was a true pioneer who is a celebrated figure in the history of computer science.
-                                Inspiring professionals, passionate educators, and young graduates are shaping the field of computer science today.
-                                We share some of their stories.
+                                Discover our monthly interview series and learn from passionate educators within the Isaac community, and recently-graduated computer scientists who are doing AMAZING things in a huge range of computing-related fields!
                             </p>
                             <Button tag={Link} to="/pages/computer_science_stories" color="primary">Discover stories</Button>
                         </Col>
@@ -118,8 +123,8 @@ export const HomepageCS = () => {
             </section>
 
             {news && news.length > 0 && <section id="news">
-                <Container className={"py-lg-6 py-5"}>
-                    <h2 className={"mb-4 mb-lg-5"}>News</h2>
+                <Container className={"py-5 px-md-4 px-xxl-5 mw-1600"}>
+                    <h2 className={"font-size-1-75 mb-5"}>News</h2>
                     <CardDeck data-testid={"news-pod-deck"} className={"justify-content-center"}>
                         {news.slice(0, 4).map(n => <NewsCard newsItem={n} showTitle />)}
                     </CardDeck>
