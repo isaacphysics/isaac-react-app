ae2b3ac17d9b428b6171387c4cd21b7a32107471
Jacob Brown <jeab2@cam.ac.uk>
2024-03-18 14:44:29 +0000

    Add numeric entry zones

---
+++ src/app/components/elements/inputs/InlineNumericEntryZone.tsx
@@ -0,0 +1,88 @@
+import React, { FormEvent, useEffect, useState } from "react";
+import { Dropdown, DropdownItem, DropdownMenu, DropdownToggle, Input, InputProps } from "reactstrap";
+import classNames from "classnames";
+import { selectUnits, wrapUnitForSelect } from "../../content/IsaacNumericQuestion";
+import { Markup } from "../markup";
+import { IsaacNumericQuestionDTO, QuantityDTO, QuantityValidationResponseDTO } from "../../../../IsaacApiTypes";
+import { selectors, useAppSelector, useGetConstantUnitsQuery } from "../../../state";
+import { isLoggedIn, useCurrentQuestionAttempt } from "../../../services";
+import { AppQuestionDTO } from "../../../../IsaacAppTypes";
+
+interface InlineNumericEntryZoneProps extends InputProps {
+    width: number, 
+    height: number, 
+    questionDTO: IsaacNumericQuestionDTO & AppQuestionDTO;
+    setModified: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export const InlineNumericEntryZone = ({width, height, questionDTO, setModified, ...props} : InlineNumericEntryZoneProps) => {
+
+    const questionId = questionDTO?.id ?? "";
+    const { currentAttempt, dispatchSetCurrentAttempt } = useCurrentQuestionAttempt<QuantityDTO>(questionId as string);
+
+    const [value, setValue] = useState<string | undefined>(questionDTO?.bestAttempt?.answer?.value);
+    const [unit, setUnit] = useState<string | undefined>(questionDTO.displayUnit ?? (questionDTO?.bestAttempt?.answer as QuantityDTO)?.units);
+
+    const user = useAppSelector(selectors.user.orNull);
+    const {data: units} = useGetConstantUnitsQuery();
+    const selectedUnits = selectUnits(questionDTO, questionId, units, isLoggedIn(user) ? user.id : undefined);
+    const [isOpen, setIsOpen] = useState(false);
+    const noDisplayUnit = questionDTO.displayUnit == null || questionDTO.displayUnit === "";
+    const readonly = false;
+
+    const currentAttemptUnitsWrong = questionDTO?.validationResponse && (questionDTO?.validationResponse as QuantityValidationResponseDTO).correctUnits === false;
+    const feedbackShowing = false;
+
+    useEffect(function updateCurrentAttempt() {
+        const attempt = {
+            type: "quantity",
+            value: value,
+            units: unit,
+        };
+        dispatchSetCurrentAttempt(attempt);
+        setModified(true);
+    }, [value, unit, setModified]);
+
+    return <div className={`d-flex align-items-center inline-numeric-container ${props.className}`}>
+        <Input 
+            valid={props.valid}
+            invalid={props.invalid}
+            className={classNames({"units-shown" : questionDTO.requireUnits || !noDisplayUnit})}
+            style={{width: `${width}px`, height: `${height}px`}}
+            value={value}
+            onChange={(e) => {
+                setValue(e.target.value);
+            }}
+        />
+        {(questionDTO.requireUnits || !noDisplayUnit) && <Dropdown disabled={readonly} isOpen={isOpen && noDisplayUnit} toggle={() => {setIsOpen(!isOpen);}} className="inline-unit-dropdown d-flex justify-content-center">
+            <DropdownToggle
+                disabled={readonly || !noDisplayUnit}
+                className={classNames("feedback-zone pl-2 pr-0 py-0", {"pr-4": currentAttemptUnitsWrong, "border-dark display-unit": !noDisplayUnit, "feedback-showing": feedbackShowing})}
+                color={noDisplayUnit ? undefined : "white"}
+            >
+                <div className={currentAttemptUnitsWrong ? "pr-3" : "pr-2"}>
+                    <Markup encoding={"latex"}>
+                        {wrapUnitForSelect(noDisplayUnit ? unit : questionDTO.displayUnit)}
+                    </Markup>
+                    {currentAttemptUnitsWrong && noDisplayUnit && <div className="feedback-box pl-2">
+                        <span className={"feedback incorrect"}>✘</span>
+                    </div>}
+                </div>
+            </DropdownToggle>
+            <DropdownMenu right>
+                {selectedUnits.map((unit) =>
+                    <DropdownItem key={wrapUnitForSelect(unit)}
+                        data-unit={unit || 'None'}
+                        className={unit && unit === currentAttempt?.units ? "btn bg-grey selected" : ""}
+                        onClick={(e: FormEvent) => {setUnit(unit); e.preventDefault(); console.log("selected", unit);}}
+                    >
+                        <Markup encoding={"latex"}>
+                            {wrapUnitForSelect(unit)}
+                        </Markup>
+                    </DropdownItem>
+                )}
+            </DropdownMenu>
+        </Dropdown>}
+    </div>;
+
+};
