1fcd40fc34e70c50466fadf734c58b16e414d343
James Sharkey <jsharkey13@users.noreply.github.com>
2024-03-21 16:32:24 +0000

    Restore prominent upgrade role link to My Account
    Annoyingly the Ada designers want bolded links, which means a bunch of
    siteSpecifics. Also remove the existing text next to the context picker
    since it was only shown to students but only helpful to teachers.
    Role names are now lowercase, but these are only used by the
    AccountTypeMessage component anyhow.

--- src/app/components/elements/AccountTypeMessage.tsx
+++ src/app/components/elements/AccountTypeMessage.tsx
@@ -4,25 +4,17 @@ import React from "react";
 import {UserRole} from "../../../IsaacApiTypes";
 
 
-export const AccountTypeMessage = ({role}: { role?: UserRole }) => {
+export const AccountTypeMessage = ({role, hideUpgradeMessage}: { role?: UserRole, hideUpgradeMessage?: boolean }) => {
     return <div>
-        Account type: <b>{role && UserFacingRole[role]}</b> {role == "STUDENT" &&
-        siteSpecific(
-            <span>
-                    <small>(Are you a teacher or tutor? {" "}
-                        <Link to={TEACHER_REQUEST_ROUTE} target="_blank">
-                            Upgrade your account
-                        </Link>{".)"}
-                    </small>
-            </span>,
-            <span>
-                    <p className="font-style-italic">Are you a teacher or tutor? {" "}
-                        <Link to={TEACHER_REQUEST_ROUTE} target="_blank">
-                            Upgrade your account
-                        </Link>{"."}
-                    </p>
+        You have a <strong>{role ? UserFacingRole[role] : ""}</strong> account.
+        {role === "STUDENT" && !hideUpgradeMessage &&
+            <span> If you are a teacher{siteSpecific(" or tutor", "")}, you can {" "}
+                {siteSpecific(
+                    <Link to={TEACHER_REQUEST_ROUTE} target="_blank">upgrade your account</Link>,
+                    <strong><Link to={TEACHER_REQUEST_ROUTE} target="_blank">upgrade your account</Link></strong>
+                )}
+                {"."}
             </span>
-        )
     }
-    </div>
-}
+    </div>;
+};
