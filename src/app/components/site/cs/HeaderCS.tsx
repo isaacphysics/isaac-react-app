07a846a531a7822a6def056ad411759e929a7387
James Sharkey <jsharkey13@users.noreply.github.com>
2024-05-03 11:26:53 +0100

    Add teacher mentoring to Ada events menu

--- src/app/components/site/cs/HeaderCS.tsx
+++ src/app/components/site/cs/HeaderCS.tsx
@@ -79,7 +79,8 @@ export const HeaderCS = () => {
                         </NavigationSection>}
 
                         <NavigationSection title={"Events"}>
-                            <LinkItem to="/pages/student_challenges">Student Challenges</LinkItem>
+                            <LinkItem to="/pages/teacher_mentoring_2024">Teacher events</LinkItem>
+                            <LinkItem to="/pages/student_challenges">Student challenges</LinkItem>
                         </NavigationSection>
 
                         <NavigationSection title={"Help"}>
