44e8ed49ac7dfea7e4ac6c87fd6a4da627f1c963
Skye Purchase <atp45@cam.ac.uk>
2024-05-16 16:43:11 +0100

    Apply styling to figures alongside text styling

--- src/app/components/content/IsaacFigure.tsx
+++ src/app/components/content/IsaacFigure.tsx
@@ -18,7 +18,7 @@ export const IsaacFigure = ({doc}: IsaacFigureProps) => {
 
     const figId = doc.id && extractFigureId(doc.id);
 
-    return <div className="figure_panel px-1">
+    return <div className="figure_panel">
         <FigureNumberingContext.Consumer>
             {figureNumbers => {
                 const figureString = figId && Object.keys(figureNumbers).includes(figId) ?
--- src/app/components/content/IsaacImage.tsx
+++ src/app/components/content/IsaacImage.tsx
@@ -11,7 +11,7 @@ interface IsaacImageProps {
 export const IsaacImage = ({doc}: IsaacImageProps) => {
     const path = doc.src && apiHelper.determineImageUrl(doc.src);
 
-    return <div className="figure_panel px-1">
+    return <div className="figure_panel">
         <figure>
             <div className="text-center">
                 {!doc.clickUrl && <img src={path} alt={doc.altText} />}
