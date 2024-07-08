936a814afb36654b69c0d0cc8d623d4e427d6fcf
Meurig Thomas <mlt47@cam.ac.uk>
2024-07-08 16:04:07 +0100

    Fix type case

--- src/app/services/userConsent.ts
+++ src/app/services/userConsent.ts
@@ -1,11 +1,11 @@
 import { InterstitialCookieState } from '../state';
 import { AppState, useAppSelector } from '../state';
 
-interface UseUserConsentreturnType {
+interface UseUserConsentReturnType {
     cookieConsent: InterstitialCookieState;
 }
 
-export function useUserConsent(): UseUserConsentreturnType {
+export function useUserConsent(): UseUserConsentReturnType {
     const cookieConsent = useAppSelector((state: AppState) => state?.cookieConsent ?? null);
 
     return { cookieConsent };
