72160037be47dd9b73a5e4bbbd487c4be537ceda
James Sharkey <jsharkey13@users.noreply.github.com>
2024-07-16 16:38:34 +0100

    Remove Ada select dropdown icon on account page
    This icon was added recently, but does not now replace the Bootstrap
    default icon which is used in all other select elements across the Ada
    site. There is little difference between them, especially at ~10px
    across!

--- src/app/components/elements/inputs/DropdownInput.tsx
+++ src/app/components/elements/inputs/DropdownInput.tsx
@@ -8,7 +8,7 @@ export interface StyledDropdownProps extends InputProps {
 }
 
 export const StyledDropdown = (props: StyledDropdownProps) => {
-    return <div className="position-relative w-100 styled-dropdown-caret">
+    return <div className="position-relative w-100">
         <Input type="select" {...props} className={classNames("form-control d-inline-block ps-3 pe-4 mt-1 mt-sm-0 " + props.className)}>
             {props.children}
         </Input>
