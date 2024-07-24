07350f1b9ad8c7f923f01ef29a27cad242c46f7c
Meurig Thomas <mlt47@cam.ac.uk>
2024-07-24 12:39:56 +0100

    Show 3 columns in CardDeck on xl devices

--- src/app/components/content/IsaacCardDeck.tsx
+++ src/app/components/content/IsaacCardDeck.tsx
@@ -18,7 +18,7 @@ export const IsaacCardDeck = ({doc, className, containerClassName}: IsaacCardDec
                 <h3 className={classNames("h-title", {"text-center": isPhy})}>{doc.title}</h3>
             </Col>
         </Row>}
-        <Row xs={12} className={classNames("d-flex flex-row card-deck row-cols-1 row-cols-sm-2 justify-content-between", {"my-3" : isAda}, {"isaac-cards-body" : isPhy}, className)}>
+        <Row xs={12} className={classNames("d-flex flex-row card-deck row-cols-1 row-cols-sm-2 row-cols-xl-3 justify-content-between", {"my-3" : isAda}, {"isaac-cards-body" : isPhy}, className)}>
             {doc?.cards?.map((props, i) => <Container key={i} className="card-container p-3"> 
                 <IsaacCard doc={props} imageClassName={props.imageClassName}/>
             </Container>)}
