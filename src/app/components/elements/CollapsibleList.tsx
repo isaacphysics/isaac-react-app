835cf4ce0b2b48c34b7d2018f07b006bab79e2f2
Meurig Thomas <mlt47@cam.ac.uk>
2024-07-30 14:26:14 +0100

    Refactor QF list state to use reducer

--- src/app/components/elements/CollapsibleList.tsx
+++ src/app/components/elements/CollapsibleList.tsx
@@ -1,24 +1,20 @@
-import React, { useEffect, useLayoutEffect, useRef, useState } from "react";
+import React, { useLayoutEffect, useRef, useState } from "react";
 import { Col, Row } from "reactstrap";
 import { Spacer } from "./Spacer";
 import { FilterCount } from "./svg/FilterCount";
 import classNames from "classnames";
-import { isDefined } from "../../services";
 
 export interface CollapsibleListProps {
     title?: string;
-    expanded?: boolean; // initial expanded state only
-    subList?: boolean;
+    asSubList?: boolean;
+    expanded: boolean;
+    toggle: () => void;
     numberSelected?: number;
-    allExpanded?: boolean;
-    onExpand?: (expanded: boolean) => void;
     children?: React.ReactNode;
 }
 
 export const CollapsibleList = (props: CollapsibleListProps) => {
-
-    const firstUpdate = useRef(true);
-    const [expanded, setExpanded] = useState(props.expanded || false);
+    const {expanded, toggle} = props;
     const [expandedHeight, setExpandedHeight] = useState(0);
     const listRef = useRef<HTMLDivElement>(null);
 
@@ -28,36 +24,12 @@ export const CollapsibleList = (props: CollapsibleListProps) => {
         }
     }, [expanded]);
 
-    useEffect(function callExpansionEventHook() {
-        if (firstUpdate.current) {
-            // Notify that this component started expanded
-            if (props.onExpand && expanded) {
-                props.onExpand(expanded);
-            }
-
-            firstUpdate.current = false;
-            return;
-        }
-        if (props.onExpand) {
-            props.onExpand(expanded);
-        }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [expanded]);
-
-    useEffect(() => {
-        if (isDefined(props.allExpanded)) {
-            // sub-lists should not expand on opening all lists
-            setExpanded(!props.subList && props.allExpanded);
-        }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [props.allExpanded]);
-
-    const title = props.title && props.subList ? props.title : <b>{props.title}</b>;
+    const title = props.title && props.asSubList ? props.title : <b>{props.title}</b>;
     const children = <div className="w-100" ref={listRef}>{props.children}</div>;
 
     return <Col>
         <Row className="collapsible-head">
-            <button className={classNames("w-100 d-flex align-items-center p-3 bg-white text-start", {"ps-4": props.subList})} onClick={() => setExpanded(e => !e)}>
+            <button className={classNames("w-100 d-flex align-items-center p-3 bg-white text-start", {"ps-4": props.asSubList})} onClick={toggle}>
                 {title && <span>{title}</span>}
                 <Spacer/>
                 {(props.numberSelected ?? 0) > 0
@@ -68,7 +40,7 @@ export const CollapsibleList = (props: CollapsibleListProps) => {
         {/* TODO: <hr className="mb-3 p-0"/> */}
         <Row className="collapsible-body overflow-hidden" style={{maxHeight: expanded ? expandedHeight : 0}}>
             {/* TODO: this feels wrong find a better way */}
-            {props.subList ? <div><div className="ps-2">{children}</div></div> : children}
+            {props.asSubList ? <div><div className="ps-2">{children}</div></div> : children}
         </Row>
     </Col>;
 };
