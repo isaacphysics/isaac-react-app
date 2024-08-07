cac46f9cf80ecedf1e66a8d5f4195a1526da3e60
James Sharkey <jsharkey13@users.noreply.github.com>
2024-08-07 13:51:31 +0100

    Add missing space to homepage text

--- src/app/components/site/cs/HomepageCS.tsx
+++ src/app/components/site/cs/HomepageCS.tsx
@@ -66,7 +66,7 @@ export const HomepageCS = () => {
                                 <li><b>Interactive questions:</b> Over 1000 questions with instant marking and feedback</li>
                                 <li><b>Teacher tools:</b> Set quizzes and assignments effortlessly</li>
                                 <li><b>AI and machine learning resources:</b> Stay ahead of the AI curve</li>
-                                <li><b>Complete curriculums:</b>For
+                                <li><b>Complete curriculums:</b> For
                                     {" "}<a href={"/exam_specifications_england#gcse/aqa"}>GCSE</a>,
                                     {" "}<a href={"/exam_specifications_england#a_level/aqa"}>A&nbsp;Level</a>,
                                     {" "}<a href={"/concepts/sqa_computing_science?examBoard=sqa&stage=scotland_national_5"}>National&nbsp;5</a>,
