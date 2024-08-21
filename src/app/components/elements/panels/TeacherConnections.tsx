dbfd6454f2f96d4be926f4ee412877905af2a68d
Meurig Thomas <mlt47@cam.ac.uk>
2024-08-21 16:20:39 +0100

    Re-align phy connection inputs

--- src/app/components/elements/panels/TeacherConnections.tsx
+++ src/app/components/elements/panels/TeacherConnections.tsx
@@ -182,7 +182,7 @@ export const TeacherConnections = ({user, authToken, editingOtherUser, userToEdi
                             e.preventDefault(); 
                         }}}
                     />
-                    <RS.Button onClick={processToken} className={classNames("py-2", {"px-0 border-dark h-100": isPhy})} color="secondary" outline disabled={editingOtherUser}>
+                    <RS.Button onClick={processToken} className={classNames("py-2", {"px-0 border-dark": isPhy})} color="secondary" outline disabled={editingOtherUser}>
                         Connect
                     </RS.Button>
                 </RS.InputGroup>
