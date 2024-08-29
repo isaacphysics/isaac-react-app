e678bdee84a6a8a58b1d6d65069bcc9362dcf0ab
Alex Lewin <sl2281@cam.ac.uk>
2024-08-29 14:44:18 +0100

    Fix null error
    Actually fix the error this time - verify if the input is null before setting the search parameter.

--- src/app/components/pages/AdminUserManager.tsx
+++ src/app/components/pages/AdminUserManager.tsx
@@ -175,10 +175,9 @@ export const AdminUserManager = () => {
                             <RS.FormGroup>
                                 <RS.Label htmlFor="verification-status-search">Find by email verification status:</RS.Label>
                                 <RS.Input
-                                    id="verification-status-search" type="select" defaultValue={searchQuery.emailVerificationStatus || undefined}
+                                    id="verification-status-search" type="select" defaultValue={String(searchQuery.emailVerificationStatus)}
                                     onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                                        const verificationStatus = e.target.value as EmailVerificationStatus;
-                                        setParamIfNotDefault("emailVerificationStatus", verificationStatus, "");
+                                        setParamIfNotDefault("emailVerificationStatus", e.target.value === "null" ? "" : e.target.value, "");
                                     }}
                                 >
                                     <option value="null">Any verification status</option>
