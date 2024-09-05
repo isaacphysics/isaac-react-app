249e6520dbcaa49c388b94ba0a4d1f410010a1df
Skye Purchase <atp45@cam.ac.uk>
2024-09-05 17:02:51 +0100

    An[Bother visual change

--- src/app/components/pages/SetAssignments.tsx
+++ src/app/components/pages/SetAssignments.tsx
@@ -418,7 +418,7 @@ export const PhyAddGameboardButtons = ({className, redirectBackTo}: {className: 
     const dispatch = useAppDispatch();
     return <>
         <h4 className="mt-4 mb-3">
-            Add a {siteSpecific("gameboard", "quiz")} from ...
+            Add a worksheet from ...
         </h4>
         <Row className={className}>
             <Col md={6} lg={4} className="pt-1">
