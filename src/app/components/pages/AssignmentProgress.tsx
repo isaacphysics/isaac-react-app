7c68d33d489d821974e3947187e9056ca3f54736
Jacob Brown <jeab2@cam.ac.uk>
2024-09-17 15:28:44 +0100

    Condense downloads into dropdown menu on <=md

--- src/app/components/pages/AssignmentProgress.tsx
+++ src/app/components/pages/AssignmentProgress.tsx
@@ -414,17 +414,35 @@ export const GroupAssignmentProgress = ({group, user}: {group: AppGroup, user: R
         <div  onClick={() => setExpanded(!isExpanded)} className={isExpanded ? "assignment-progress-group active align-items-center" : "assignment-progress-group align-items-center"}>
             <div className="group-name"><span className="icon-group"/><span data-testid={"group-name"}>{group.groupName}</span></div>
             <div className="flex-grow-1" />
-            <div className="py-2"><strong>{assignmentCount}</strong> assignment{assignmentCount != 1 && "s"} or test{assignmentCount != 1 && "s"}<span className="d-none d-md-inline"> set</span></div>
-            <div className="d-none d-md-inline-block"><a className={"download-csv-link"} href={getGroupProgressCSVDownloadLink(group.id as number)} target="_blank" rel="noopener" onClick={openDownloadLink}>
-                (Download group assignments CSV)
-            </a></div>
-            {pageSettings.isTeacher && <div className="d-none d-md-inline-block"><a className={"download-csv-link"} href={getGroupQuizProgressCSVDownloadLink(group.id as number)} target="_blank" rel="noopener" onClick={openDownloadLink}>
-                (Download group tests CSV)
-            </a></div>}
-            <Button color="link" className="px-2" tabIndex={0} onClick={() => setExpanded(!isExpanded)}>
-                <img src={siteSpecific("/assets/common/icons/icon-expand-arrow.png", "/assets/common/icons/chevron-up.svg")} alt="" className="accordion-arrow" />
-                <span className="visually-hidden">{isExpanded ? "Hide" : "Show"}{` ${group.groupName} assignments`}</span>
-            </Button>
+
+            <div className="d-none d-sm-block py-2"><strong>{assignmentCount}</strong> assignment{assignmentCount != 1 && "s"} or test{assignmentCount != 1 && "s"}<span className="d-none d-md-inline"> set</span></div>
+
+            <div className="d-none d-lg-inline-block">
+                <a className={"download-csv-link"} href={getGroupProgressCSVDownloadLink(group.id as number)} target="_blank" rel="noreferrer noopener" onClick={openDownloadLink}>
+                    (Download group assignments CSV)
+                </a>
+                {pageSettings.isTeacher && <a className={"download-csv-link ms-2"} href={getGroupQuizProgressCSVDownloadLink(group.id as number)} target="_blank" rel="noreferrer noopener" onClick={openDownloadLink}>
+                    (Download group tests CSV)
+                </a>}
+                <Button color="link" className="px-2" tabIndex={0} onClick={() => setExpanded(!isExpanded)}>
+                    <img src={siteSpecific("/assets/common/icons/icon-expand-arrow.png", "/assets/common/icons/chevron-up.svg")} alt="" className="accordion-arrow" />
+                    <span className="visually-hidden">{isExpanded ? "Hide" : "Show"}{` ${group.groupName} assignments`}</span>
+                </Button>
+            </div>
+            <UncontrolledButtonDropdown className="d-flex d-lg-none p-0">
+                <DropdownToggle caret className="text-nowrap" size="sm" color="link" onClick={e => e.stopPropagation()}>
+                    Downloads
+                </DropdownToggle>
+                <DropdownMenu>
+                    <a className={"download-csv-link mx-3 w-max-content"} href={getGroupProgressCSVDownloadLink(group.id as number)} target="_blank" rel="noreferrer noopener" onClick={openDownloadLink}>
+                        Group assignments CSV
+                    </a>
+                    <DropdownItem divider />
+                    {pageSettings.isTeacher && <a className={"download-csv-link mx-3 w-max-content"} href={getGroupQuizProgressCSVDownloadLink(group.id as number)} target="_blank" rel="noreferrer noopener" onClick={openDownloadLink}>
+                        Group tests CSV
+                    </a>}
+                </DropdownMenu>
+            </UncontrolledButtonDropdown>
         </div>
         {isExpanded && <GroupDetails group={group} user={user} />}
     </>;
