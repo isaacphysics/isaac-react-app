9e776b873aef8312a31ccec61e8006d66d779e6d
Matthew Trew <matthew.trew@raspberrypi.org>
2024-09-19 17:10:31 +0100

    Add tests for session expiry

---
+++ src/test/pages/SessionExpired.test.tsx
@@ -0,0 +1,56 @@
+import {navigateToMyAccount, renderTestEnvironment} from "../testUtils";
+import {NOW, SOME_FIXED_FUTURE_DATE} from "../dateUtils";
+import {redirectTo} from "../../app/state";
+import * as Actions from "../../app/state/actions";
+
+
+describe("SessionExpired", () => {
+    it('should redirect to session expired login page when session has expired', async () => {
+        // Arrange
+        //  Annoyingly, the hard-redirect we use is not implemented in JSDOM/RTL, so we need to mock it out.
+        jest.spyOn(Actions, "redirectTo");
+        // @ts-ignore
+        redirectTo.mockImplementation(() => true);
+
+        //  Set the session expiry to be now
+        renderTestEnvironment({role: "STUDENT", sessionExpires: new Date(NOW).toUTCString()});
+
+        //  Navigate to My Account (any page will do)
+        await navigateToMyAccount();
+
+        // Act
+        //  Wait for the user consistency check to notice
+        await new Promise((r) => setTimeout(r, 1500));
+
+        // Assert
+        //  Check we were redirected to the login page. Ideally we'd check the page itself, but this is the best we can
+        //  do for the reasons described above.
+        expect(redirectTo).toHaveBeenLastCalledWith("/login_expired");
+
+        // Teardown
+        // @ts-ignore
+        redirectTo.mockRestore();
+    });
+
+    it('should not redirect when session expiry has not passed', async () => {
+        // Arrange
+        jest.spyOn(Actions, "redirectTo");
+        // @ts-ignore
+        redirectTo.mockImplementation(() => true);
+
+        //  Set the session expiry to in the future
+        renderTestEnvironment({role: "STUDENT", sessionExpires: new Date(SOME_FIXED_FUTURE_DATE).toUTCString()});
+
+        //  Navigate to My Account (any page will do)
+        await navigateToMyAccount();
+
+        // Act
+        //  Wait for the user consistency check to notice, if it's going to
+        await new Promise((r) => setTimeout(r, 1500));
+
+        // Assert
+        //  We should still be where we were.
+        expect(window.location.pathname).toEqual("/account");
+        expect(redirectTo).not.toHaveBeenLastCalledWith("/login_expired");
+    });
+});
