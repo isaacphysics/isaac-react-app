5e76703a92a39988b630b158c8dd2a4eb848bbd6
Sol Dubock <94075844+sjd210@users.noreply.github.com>
2024-09-25 12:33:02 +0100

    Revert "Change about us medium page size layout"
    This reverts commit f791dd6a1ae3a61e2478f92f6e7d01b99e6982e3.

--- src/app/components/content/IsaacHorizontal.tsx
+++ src/app/components/content/IsaacHorizontal.tsx
@@ -2,16 +2,12 @@ import React from "react";
 import {ContentDTO} from "../../../IsaacApiTypes";
 import {IsaacContent} from "./IsaacContent";
 import {Col, Row} from "reactstrap";
-import { usePathname } from "../../services";
 
 export const IsaacHorizontal = ({doc: {children}}: {doc: ContentDTO}) => {
-    const path = usePathname();
-    const pageSplit = children ? Math.floor(12/(children.length)) : 12;
-
     return children ? <div className={"content-value"}>
         <Row className="p-0 m-0 dynamic-x-padding">
             {children.map((child, index) => (
-                <Col key={index} md={path == "/about" ? pageSplit : 12} lg={pageSplit} className="pad">
+                <Col key={index} lg={Math.floor(12/(children.length))}  className="pad">
                     <IsaacContent key={index} doc={child} />
                 </Col>
             ))}
