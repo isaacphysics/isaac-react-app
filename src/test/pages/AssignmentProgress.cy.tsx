7a0055bef3a539aef102212aaa0033550a64bd97
Jacob Brown <jeab2@cam.ac.uk>
2024-10-02 16:52:08 +0100

    Add test for Assignment Progress

---
+++ src/test/pages/AssignmentProgress.cy.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {mockUser} from "../../mocks/data";
+import { AssignmentProgress } from "../../app/components/pages/AssignmentProgress";
+
+
+it('Groups should have no visual regressions', () => {
+    // @ts-ignore
+    cy.mountWithStoreAndRouter(<AssignmentProgress user={mockUser}/>);
+    cy.get('[data-testid="loading"]').should('not.exist');
+    cy.contains("Test Group 1").click();
+    cy.matchImage();
+});
