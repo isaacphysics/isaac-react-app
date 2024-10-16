985105dbde36afc16a040e07b2efc9e5f09378b5
Jacob Brown <jeab2@cam.ac.uk>
2024-10-16 17:35:57 +0100

    Apply homepage changes

--- src/app/components/elements/layout/TextBlock.tsx
+++ src/app/components/elements/layout/TextBlock.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { Col, ColProps } from "reactstrap";
 
 export const TextBlock = ({...props} : ColProps) => {
-    return <Col {...props} className={classNames("p-4 d-flex align-items-center", props.className)}>
+    return <Col {...props} className={classNames("d-flex align-items-center", props.className)}>
         <div className="position-relative w-100 text-block">
             {props.children}
         </div>
