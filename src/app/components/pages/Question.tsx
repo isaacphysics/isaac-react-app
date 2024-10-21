70274659f4c42573c672efeebd5da5f32077ec1d
Alex Lewin <sl2281@cam.ac.uk>
2024-10-21 15:15:20 +0100

    Use classNames helper
    Co-authored-by: James Sharkey <jsharkey13@users.noreply.github.com>

--- src/app/components/pages/Question.tsx
+++ src/app/components/pages/Question.tsx
@@ -76,7 +76,7 @@ export const Question = withRouter(({questionIdOverride, match, location, previe
         const isFastTrack = doc && doc.type === DOCUMENT_TYPE.FAST_TRACK_QUESTION;
 
         return <GameboardContext.Provider value={navigation.currentGameboard}>
-            <Container className={classNames(`${doc.subjectId || ""}`, "no-shadow")}>
+            <Container className={classNames(doc.subjectId, "no-shadow")}>
                 {/*High contrast option*/}
                 <TitleAndBreadcrumb
                       currentPageTitle={generateQuestionTitle(doc)}
