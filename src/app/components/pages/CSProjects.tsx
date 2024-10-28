2aa11b412971bc99dc3b0831fd619cd8a9bfdf21
Jacob Brown <jeab2@cam.ac.uk>
2024-10-28 16:07:38 +0000

    Rename variables and reword meta description

--- src/app/components/pages/CSProjects.tsx
+++ src/app/components/pages/CSProjects.tsx
@@ -11,7 +11,7 @@ import {PageFragment} from "../elements/PageFragment";
 
 export const CSProjects = () => {
     const [page, setPage] = useState(0);
-    const [allCourses, setAllCourses] = useState([] as IsaacPodDTO[]); // each query fetches a new page; this acts as a cache for all the news fetched so far
+    const [allProjects, setAllProjects] = useState([] as IsaacPodDTO[]); // each query fetches a new page; this acts as a cache for all the news fetched so far
     const [disableLoadMore, setDisableLoadMore] = useState(false);
 
     const projectsQuery = useGetNewsPodListQuery({subject: "projects_pods", startIndex: page * NEWS_PODS_PER_PAGE});
@@ -21,7 +21,7 @@ export const CSProjects = () => {
 
         projectsQuery.refetch().then((value) => {
             if (!cancelled && value.status === "fulfilled" && value.data !== undefined) {
-                setAllCourses(n => n.concat((value.data as IsaacPodDTO[]).slice(0, NEWS_PODS_PER_PAGE)));
+                setAllProjects(n => n.concat((value.data as IsaacPodDTO[]).slice(0, NEWS_PODS_PER_PAGE)));
                 if (value.data.length < NEWS_PODS_PER_PAGE) {
                     setDisableLoadMore(true);
                 }
@@ -33,13 +33,13 @@ export const CSProjects = () => {
         };
     }, [page]);
 
-    const metaDescription = "Browse the Raspberry Pi Foundation’s free online courses for educators and choose from a range of computing topics.";
+    const metaDescription = "Browse the list of projects available on Ada Computer Science.";
 
     return <Container>
         <TitleAndBreadcrumb currentPageTitle={"Projects"} />
         <MetaDescription description={metaDescription} />
         <PageFragment fragmentId={"projects_help"} />
-        {allCourses.length === 0 ?
+        {allProjects.length === 0 ?
             <ShowLoadingQuery
                 query={projectsQuery}
                 thenRender={() => <div className={"w-100 text-start"}><h4>No projects to display...</h4></div>}
@@ -47,7 +47,7 @@ export const CSProjects = () => {
             /> :
             <>
                 <Row className="d-flex flex-row card-deck row-cols-1 row-cols-md-2 row-cols-lg-3 justify-content-between my-3">
-                    {allCourses.map(n => <NewsCard key={n.id} newsItem={n} showTitle />)}
+                    {allProjects.map(n => <NewsCard key={n.id} newsItem={n} showTitle />)}
                 </Row>
                 <div className="w-100 d-flex justify-content-center mb-5">
                     <Button className={"mt-3"} color={"primary"} disabled={disableLoadMore} onClick={() => setPage(p => p + 1)}>Load more projects</Button>
