7697654c5f921ae5a682addb3884c3bcea40f9ca
James Sharkey <jsharkey13@users.noreply.github.com>
2024-11-06 10:44:24 +0000

    Increase gameboard filter removal banner severity
    Just for physics, since Ada did not have a "warning" class colour.

--- src/app/components/navigation/QuestionFinderBanner.tsx
+++ src/app/components/navigation/QuestionFinderBanner.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import {Link} from "react-router-dom";
 import * as RS from 'reactstrap';
+import {siteSpecific} from "../../services";
 
 export const QuestionFinderBanner = () => {
-    return <RS.Alert color="info" className={"no-print mt-3"}>
+    return <RS.Alert color={siteSpecific("warning", "info")} className={"no-print mt-3"}>
         This page will be removed by the end of 2024; you can <Link to="/questions">find our new and improved question finder here</Link>.
         <br/><Link to="/contact?subject=Old%20question%20finder">Let us know any feedback about this change</Link>.
     </RS.Alert>;
