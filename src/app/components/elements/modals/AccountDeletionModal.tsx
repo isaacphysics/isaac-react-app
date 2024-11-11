9f9aa07c2c817c7e9bf59e37e879c42a26427240
Jacob Brown <jeab2@cam.ac.uk>
2024-11-11 15:51:42 +0000

    Improve styling on Ada

--- src/app/components/elements/modals/AccountDeletionModal.tsx
+++ src/app/components/elements/modals/AccountDeletionModal.tsx
@@ -2,10 +2,11 @@ import React from "react";
 import { PageFragment } from "../PageFragment";
 import { Button } from "reactstrap";
 import { closeActiveModal, store } from "../../../state";
+import { siteSpecific } from "../../../services";
 
 export const ConfirmAccountDeletionRequestModal = (confirmAccountDeletionRequest: () => void) => {
     return {
-        title: "Delete Account",
+        title: siteSpecific("Delete Account", "Delete account"),
         body: <PageFragment fragmentId="account_deletion_email_confirmation_notice" />,
         buttons: [
             <Button key={0} block color="primary" onClick={() => {confirmAccountDeletionRequest();}}>
--- src/app/components/pages/AccountDeletion.tsx
+++ src/app/components/pages/AccountDeletion.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Button, Container, Form, FormGroup, FormProps, Input, Label } from "reactstrap";
 import { TitleAndBreadcrumb } from "../elements/TitleAndBreadcrumb";
-import { isTutorOrAbove, useQueryParams } from "../../services";
+import { isTutorOrAbove, siteSpecific, useQueryParams } from "../../services";
 import { selectors, useAppSelector, useDeleteAccountMutation } from "../../state";
 import { Link } from "react-router-dom";
 import { StyledCheckbox } from "../elements/inputs/StyledCheckbox";
@@ -46,7 +46,7 @@ export const AccountDeletion = () => {
     const [deleteAccount, {isLoading: isDeletingAccount}] = useDeleteAccountMutation();
 
     return <Container className="pb-5">
-        <TitleAndBreadcrumb currentPageTitle={"Account Deletion"} className="mb-4" />
+        <TitleAndBreadcrumb currentPageTitle={siteSpecific("Account Deletion", "Account deletion")} className="mb-4" />
         {!user ? <p>You must be logged in to delete your account.</p> :
             isTutorOrAbove(user) ? <p>Only student accounts can be deleted automatically. Please <Link to="/contact?preset=accountDeletion">contact us</Link> to request account deletion.</p> : 
                 !token ? <p>You must have a valid token to delete your account. Visit your <Link to="/account">account page</Link> to generate a new email with a token.</p> : 
