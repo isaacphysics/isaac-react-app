d56fbe8a1cf6b8b31e7480f17762f87312b31af1
Alex Lewin <sl2281@cam.ac.uk>
2024-11-14 12:16:28 +0000

    Fix postcode regex

--- src/app/components/pages/AdminUserManager.tsx
+++ src/app/components/pages/AdminUserManager.tsx
@@ -111,7 +111,7 @@ export const AdminUserManager = () => {
 
     const search = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-        if (searchQuery.postcode && !/^[A-Z]{1,2}[0-9]{1,2}[A-Z]{0,1} ?([0-9][A-Z]{2})?$/i.test(searchQuery.postcode)) {
+        if (searchQuery.postcode && !/^[A-Z]{1,2}[0-9][A-Z|0-9]? ?([0-9][A-Z]{2})?$/i.test(searchQuery.postcode)) {
             alert("Postcode input invalid");
         }
         else {
