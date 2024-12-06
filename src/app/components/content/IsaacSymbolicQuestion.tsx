82819352c4d8fce8fb3531104c8f090338ff2382
Jacob Brown <jeab2@cam.ac.uk>
2024-12-06 12:39:00 +0000

    Quietly cancel inequality setup on known erroneous start

--- src/app/components/content/IsaacSymbolicQuestion.tsx
+++ src/app/components/content/IsaacSymbolicQuestion.tsx
@@ -166,10 +166,12 @@ const IsaacSymbolicQuestion = ({doc, questionId, readonly}: IsaacQuestionProps<I
     const sketchRef = useRef<Inequality | null | undefined>();
 
     useLayoutEffect(() => {
+        if (readonly) return; // as the ref won't be defined
+
         if (!isDefined(hiddenEditorRef.current)) {
             throw new Error("Unable to initialise inequality; target element not found.");
         }
-        
+
         const {sketch, p} = makeInequality(
             hiddenEditorRef.current,
             100,
