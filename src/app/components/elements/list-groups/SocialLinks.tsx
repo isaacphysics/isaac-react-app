7efbceef6593324268012b15cf4268ed676052bf
Jacob Brown <jeab2@cam.ac.uk>
2024-12-09 11:26:02 +0000

    Remove Nomensa SCSS; restructure footer

--- src/app/components/elements/list-groups/SocialLinks.tsx
+++ src/app/components/elements/list-groups/SocialLinks.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {SITE_TITLE, siteSpecific, SOCIAL_LINKS} from "../../../services";
 import {ExternalLink} from "../ExternalLink";
+import classNames from "classnames";
 
 const getSocialIcon = (name: string) => {
     if (name === "x (twitter)") {
@@ -11,11 +12,11 @@ const getSocialIcon = (name: string) => {
 
 export const SocialLinksRow = () => {
     return (
-        <div className='footer-links footer-links-social'>
-            {siteSpecific(<p className="pb-2 pt-2 footer-link-header">Follow us</p>, <h2>Get social</h2>)}
+        <div className={siteSpecific('', 'footer-links footer-links-social')}>
+            {siteSpecific(<p className="pb-1 footer-link-header">Follow us</p>, <h2>Get social</h2>)}
             <div className='mt-1 text-nowrap'>
-                {Object.entries(SOCIAL_LINKS).map(([_, {name, href}]) =>
-                    <div className={"me-3 d-inline-block"} key={name}>
+                {Object.entries(SOCIAL_LINKS).map(([_, {name, href}], i, a) =>
+                    <div className={classNames(siteSpecific(i !== a.length - 1 ? "me-2" : "", "me-3"), "d-inline-block")} key={name}>
                         <ExternalLink href={href}>
                             <img src={getSocialIcon(name.toLowerCase())} alt={`${SITE_TITLE} on ${name}`}
                                 className='img-fluid footer-social-logo'/>
--- src/app/components/site/phy/FooterPhy.tsx
+++ src/app/components/site/phy/FooterPhy.tsx
@@ -6,56 +6,45 @@ import {Link} from "react-router-dom";
 import {ExternalLink} from "../../elements/ExternalLink";
 
 export const FooterPhy = () => (
-    <footer>
-        <div className="footer-top d-print-none px-5">           
-            <Row className="pt-5">
-                <Col xl={{size: 3, offset: 0}}>
-                    <a href="https://www.cam.ac.uk/" target="_blank" rel="noopener" className="mt-2 mb-1">
-                        <img src="/assets/common/logos/university_of_cambridge.svg" alt='University of Cambridge website' width="160px" className='footer-org-logo' />
-                    </a>
-                    <div className="footer-links logo-text">
-                            Funded by {' '} <ExternalLink href="https://www.cam.ac.uk/" className="d-inline">
-                            <u>University of Cambridge</u>.
-                        </ExternalLink>        
-                        <br />Supported by {' '} <ExternalLink href="https://www.gov.uk/government/organisations/department-for-education" className="d-inline">
-                            <u>Department for Education</u>
-                        </ExternalLink> and {' '} 
-                        <ExternalLink href="https://www.ogdentrust.com/" className="d-inline">
-                            <u>The&nbsp;Ogden&nbsp;Trust</u>.
-                        </ExternalLink>
-                    </div>
-                </Col>
-                <Col xl={{size: 6, offset: 0}} lg={{size: 8, offset: 0}} md={{size: 8, offset: 0}} className="mt-4">
-                    <ListGroupFooter />
-                </Col>
-                <Col lg={{size: 3, offset: 0}} md={{size: 4, offset: 0}} className="mt-4">
-                    <SocialLinksRow />
-                </Col>
-            </Row>
-            <Row className="footer-links logo-text">
-                <Col lg={{size: 5, offset: 0}}>
-                    All materials on this site are licensed under the {' '}
-                    <ExternalLink href="https://creativecommons.org/licenses/by/4.0/" className="d-inline">
-                        <u>Creative&nbsp;Commons&nbsp;license</u>
-                    </ExternalLink> unless stated otherwise.
-                </Col>
-                <Col lg={{size: 6, offset: 1}} className="pt-3 pb-4 footer-link footer-links-aligned">
-                    <Row>
-                        <Col>
-                            <Link to="/accessibility">Accessibility statement</Link>
-                        </Col>
-                        <Col>
-                            <Link to="/privacy">Privacy policy</Link>
-                        </Col>
-                        <Col>
-                            <Link to="/cookies">Cookie policy</Link>
-                        </Col>
-                        <Col>
-                            <Link to="/terms">Terms of use</Link>
-                        </Col>
-                    </Row>                   
-                </Col>    
-            </Row>           
-        </div>
+    <footer className="d-print-none px-5">
+        <Row className="pt-5">
+            <Col xl={{size: 3, offset: 0}}>
+                <a href="https://www.cam.ac.uk/" target="_blank" rel="noopener" className="mt-2 mb-1">
+                    <img src="/assets/common/logos/university_of_cambridge.svg" alt='University of Cambridge website' width="160px" className='footer-org-logo' />
+                </a>
+                <div className="logo-text">
+                    Funded by {' '} <ExternalLink href="https://www.cam.ac.uk/" className="d-inline">
+                        <u>University of Cambridge</u>
+                    </ExternalLink>.
+                    <br />
+                    Supported by {' '} <ExternalLink href="https://www.gov.uk/government/organisations/department-for-education" className="d-inline">
+                        <u>Department for Education</u>
+                    </ExternalLink> and {' '} 
+                    <ExternalLink href="https://www.ogdentrust.com/" className="d-inline">
+                        <u>The&nbsp;Ogden&nbsp;Trust</u>
+                    </ExternalLink>.
+                </div>
+            </Col>
+            <Col md={{size: 8, offset: 0}} lg={{size: 8, offset: 0}} xl={{size: 6, offset: 0}} className="d-flex flex-column flex-sm-row mt-4">
+                <ListGroupFooter />
+            </Col>
+            <Col md={{size: 4, offset: 0}} lg={{size: 3, offset: 1}} xl={{size: 3, offset: 3}} className="mt-sm-4">
+                <SocialLinksRow />
+            </Col>
+        </Row>
+        <Row className="logo-text pe-4">
+            <Col lg={5}>
+                All materials on this site are licensed under the {' '}
+                <ExternalLink href="https://creativecommons.org/licenses/by/4.0/" className="d-inline">
+                    <u>Creative&nbsp;Commons&nbsp;license</u>
+                </ExternalLink> unless stated otherwise.
+            </Col>
+            <Col lg={7} className="d-flex pt-3 pb-4 pe-lg-3 column-gap-4 flex-wrap justify-content-start justify-content-lg-end">
+                <Link to="/accessibility">Accessibility statement</Link>
+                <Link to="/privacy">Privacy policy</Link>
+                <Link to="/cookies">Cookie policy</Link>
+                <Link to="/terms">Terms of use</Link>
+            </Col>    
+        </Row>           
     </footer>
 );
