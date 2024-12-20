f64b084515da2c3d46c6436281ad9e1f77e223b9
Alex Lewin <sl2281@cam.ac.uk>
2024-12-20 10:56:38 +0000

    Use News API request from handlers.ts

--- src/test/pages/News.cy.tsx
+++ src/test/pages/News.cy.tsx
@@ -1,15 +1,7 @@
 import React from "react";
-import { API_PATH, siteSpecific } from "../../app/services";
 import { News } from "../../app/components/pages/News";
 
 it('News page should have no visual regressions', () => {
-    cy.intercept(
-        {
-            method: "GET",
-            url: API_PATH + `/pages/pods/${siteSpecific("physics", "news")}/0`,
-        }, 
-        {}
-    ).as("getNews");
 
     cy.mountWithStoreAndRouter(<News/>, ["/news"]);
 
