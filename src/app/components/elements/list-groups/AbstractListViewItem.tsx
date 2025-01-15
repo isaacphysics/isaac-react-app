11f8ab450834e535159495692692516dec05615c
Sol Dubock <94075844+sjd210@users.noreply.github.com>
2025-01-15 17:02:59 +0000

    Correctly format test buttons

--- src/app/components/elements/list-groups/AbstractListViewItem.tsx
+++ src/app/components/elements/list-groups/AbstractListViewItem.tsx
@@ -29,9 +29,11 @@ import {StageAndDifficultySummaryIcons} from "../StageAndDifficultySummaryIcons"
 import {ShortcutResponse, ViewingContext} from "../../../../IsaacAppTypes";
 import {Markup} from "../markup";
 import classNames from "classnames";
-import {Col, ListGroup, ListGroupItem, Row, UncontrolledTooltip} from "reactstrap";
+import {Button, Col, ListGroup, ListGroupItem, Row, UncontrolledTooltip} from "reactstrap";
 import { CSSModule } from "reactstrap/types/lib/utils";
 import { LLMFreeTextQuestionIndicator } from "../LLMFreeTextQuestionIndicator";
+import { AffixButton } from "../AffixButton";
+import { Spacer } from "../Spacer";
 
 export enum ContentTypeVisibility {
     SHOWN, // default if not specified
@@ -98,12 +100,12 @@ export const AbstractListViewItem = ({icon, title, subtitle, breadcrumb, status,
         <Col xs={2} md={1}>
             {icon}
         </Col>
-        <Col xs={10} md={6} lg={7}>
+        <Col xs={10} md={7} lg={8} xl={7}>
             {/* Title TestTag Subtitle Tags Breadcrumb */}
             <div className="justify-content-between content-summary-link-title">
                 <div className="d-flex">
                     <span className="question-link-title text-secondary">{title}</span>
-                    {testTag && <span className="superseded-tag mx-1 ms-sm-3">{testTag}</span>}
+                    {testTag && <span className="quiz-level-1-tag mx-1 ms-sm-3">{testTag}</span>}
                 </div>
             
                 {subtitle && <div className="small text-muted d-block">{subtitle}</div>}
@@ -123,7 +125,12 @@ export const AbstractListViewItem = ({icon, title, subtitle, breadcrumb, status,
                         <button type="button" className="d-block d-md-none h-4 p-0 set-quiz-button-md btn btn-secondary" style={{minWidth: "120px", height: "45px"}}>Preview</button>
                     </a>
                     <a href={testUrl}>
-                        <button type="button" className="d-block d-md-none h-4 p-0 set-quiz-button-md btn btn-primary" style={{minWidth: "120px", height: "45px"}}>Take the test →</button>
+                        <button type="button" className="d-block d-md-none h-4 p-0 set-quiz-button-md btn btn-primary" style={{minWidth: "120px", height: "45px"}}>
+                            <div className="mx-3">
+                                Take the test
+                                <img className="ps-1 mb-1" src={`/assets/phy/icons/redesign/arrow-right.svg`} alt=""/>
+                            </div>
+                        </button>
                     </a>
                 </Col>}
             </div>
@@ -134,18 +141,23 @@ export const AbstractListViewItem = ({icon, title, subtitle, breadcrumb, status,
                 <StatusDisplay status={status}/>
             </div>}
         </Col>}
-        {audienceViews && <Col md={4} xl={2} className="d-none d-md-flex" style={{borderLeft: "1px solid #a5a5a4"}}>
+        {audienceViews && <Col md={4} lg={3} xl={2} className="d-none d-md-flex" style={{borderLeft: "1px solid #a5a5a4"}}>
             {/* Stage (but only on bigger screens) */}
             <StageAndDifficultySummaryIcons audienceViews={audienceViews} stack={true}/> 
         </Col>}
         {previewUrl && testUrl && <Col xs={12} md={4} className="d-flex align-items-center justify-content-end">
             {/* PreviewURL  TestURL */}
-            <a href={previewUrl}>
-                <button type="button" className="d-none d-md-block h-4 p-0 set-quiz-button-md btn btn-secondary" style={{minWidth: "120px", height: "45px"}}>Preview</button>
-            </a>
-            <a href={testUrl}>
-                <button type="button" className="d-none d-md-block h-4 p-0 set-quiz-button-md btn btn-primary" style={{minWidth: "120px", height: "45px"}}>Take the test →</button>
-            </a>
+            <Spacer/>
+            <Button to={previewUrl} color="keyline" className="d-none d-md-block h-4 p-0 set-quiz-button-md" style={{minWidth: "120px", height: "45px"}}>Preview</Button>
+            <Spacer width={5}/>
+            <AffixButton size="md" color="solid" tag={Link} to={testUrl} affix={{
+                affix: "icon-right",
+                position: "suffix",
+                type: "icon"
+            }}>
+                Take the test
+            </AffixButton>
+            
         </Col>}
     </Row>;
 
