a9474111b0dccb4215bd3ba9684dd24315ae05cd
Sol Dubock <94075844+sjd210@users.noreply.github.com>
2025-01-21 11:00:54 +0000

    Create concepts and event list views

--- src/app/components/elements/list-groups/list-view-items/QuestionListView.tsx
+++ src/app/components/elements/list-groups/list-view-items/QuestionListView.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { AbstractListViewItem } from "../AbstractListViewItem";
 import { ShortcutResponse, ViewingContext } from "../../../../../IsaacAppTypes";
 import { determineAudienceViews } from "../../../../services/userViewingContext";
-import { TAG_ID, tags } from "../../../../services";
+import { DOCUMENT_TYPE, SEARCH_RESULT_TYPE, TAG_ID, tags } from "../../../../services";
 import { ListGroup } from "reactstrap";
 
 export const QuestionListViewItem = (item: ShortcutResponse) => {
@@ -10,7 +10,7 @@ export const QuestionListViewItem = (item: ShortcutResponse) => {
     const audienceViews: ViewingContext[] = determineAudienceViews(item.audience);
 
     return <AbstractListViewItem 
-        icon={<i className="icon-question"/> /*TODO Create actual list view icons*/}
+        icon={<i className="icon-question"/> /*TODO Get actual list view icons*/}
         title={item.title ?? ""} 
         subtitle={item.subtitle} 
         breadcrumb={breadcrumb} 
@@ -20,8 +20,54 @@ export const QuestionListViewItem = (item: ShortcutResponse) => {
     />;
 };
 
-export const QuestionListView = ({items}: {items: ShortcutResponse[]}) => {
+export const ConceptListViewItem = (item: ShortcutResponse) => {
+    return <AbstractListViewItem 
+        icon={<i className="icon-lightbulb"/> /*TODO Get actual list view icons*/}
+        title={item.title ?? ""} 
+        subtitle={item.subtitle} 
+        url={item.url} 
+    />;
+};
+
+export const EventListViewItem = (item: ShortcutResponse) => {
+    return <AbstractListViewItem 
+        icon={<i className="icon-lightbulb"/> /*TODO Get actual list view icons*/}
+        title={item.title ?? ""} 
+        subtitle={item.subtitle} 
+        url={item.url} 
+    />;
+};
+
+export const ListView = ({items}: {items: ShortcutResponse[]}) => {
+
+    // Cards (e.g. the subjects on the homepage)
+    // Questions
+    // Question Packs
+    // Quick Quizzes
+    // Concepts
+    // Tests
+
     return <ListGroup className="link-list list-group-links">
-        {items.map((item, index) => <QuestionListViewItem key={index} {...item}/>)}
+        {items.map((item, index) => { 
+            switch (item.type) {
+                case (SEARCH_RESULT_TYPE.SHORTCUT):
+                    return <QuestionListViewItem key={index} {...item}/>;
+                case (DOCUMENT_TYPE.QUESTION):
+                case (DOCUMENT_TYPE.FAST_TRACK_QUESTION):
+                    return <QuestionListViewItem key={index} {...item}/>;
+                case (DOCUMENT_TYPE.CONCEPT):
+                    return <ConceptListViewItem key={index} {...item}/>;
+                case (DOCUMENT_TYPE.EVENT):
+                    return <EventListViewItem key={index} {...item}/>;
+                case (DOCUMENT_TYPE.TOPIC_SUMMARY):
+                    return <QuestionListViewItem key={index} {...item}/>;
+                case (DOCUMENT_TYPE.GENERIC):
+                    return <QuestionListViewItem key={index} {...item}/>;
+                default:
+                    // Do not render this item if there is no matching DOCUMENT_TYPE
+                    console.error("Not able to display item as a ListViewItem: ", item);
+                    return null;
+            } 
+        })}
     </ListGroup>;
-}
+};
