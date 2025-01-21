8ef7b6fb64a311b2e06735f211c2ebff03ec8914
Jacob Brown <jeab2@cam.ac.uk>
2025-01-21 10:24:48 +0000

    Allow extending `PhyHexIcon` attributes

--- src/app/components/elements/svg/PhyHexIcon.tsx
+++ src/app/components/elements/svg/PhyHexIcon.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import { Subject } from "../../../../IsaacAppTypes";
 import classNames from "classnames";
 
-interface PhyHexIconProps {
+interface PhyHexIconProps extends React.HTMLAttributes<HTMLDivElement> {
     icon: string;
     subject?: Subject;
 }
 
 export const PhyHexIcon = (props: PhyHexIconProps) => {
-    const {icon} = props;
-    return <div className="d-flex pe-3" {...(props?.subject && {"data-bs-theme": props.subject})}>
+    const {icon, subject, className, ...rest} = props;
+    return <div {...rest} className={classNames("d-flex pe-3", className)} {...(subject && {"data-bs-theme": subject})}>
         <div className="phy-hex-icon"/>
         <i className={classNames("icon", icon)}/>
     </div>;
