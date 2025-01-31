93ea3cb281716a2d6dca03b97b93fa829b94e1a4
Parth Sharma <32276128+Parth097@users.noreply.github.com>
2025-01-31 14:43:23 +0000

    Changing wording as per request (#310)

--- src/app/components/pages/IsaacCompetition/content.ts
+++ src/app/components/pages/IsaacCompetition/content.ts
@@ -9,8 +9,8 @@ export default {
     note: {
       heading: "Please note:",
       entryDetails:
-        "Competition entries open in January and last two months. Students will be able to submit their entries until 17 March. Follow our",
-      callToAction: "accounts for updates and teachers can sign up to our expression of interest form below.",
+        "Competition entries open in January and last two months. Students will be able to submit their entries until 28 March 16:00. Follow our",
+      callToAction: "accounts for updates.",
       xLink: "https://x.com/isaaccompsci",
       facebookLink: "https://www.facebook.com/IsaacComputerScience",
     },
