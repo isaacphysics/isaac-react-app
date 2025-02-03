59faa0744b1e44252e649deeab9bc2e66111bb6b
Jacob Brown <jeab2@cam.ac.uk>
2025-02-03 10:35:01 +0000

    Remove duplicated spinner when loading

--- src/app/components/pages/MyGameboards.tsx
+++ src/app/components/pages/MyGameboards.tsx
@@ -199,8 +199,10 @@ export const MyGameboards = () => {
                     :
                     <>
                         <div className="mt-4 mb-2">
-                            {boards && <h4>Showing <strong>{inProgress + notStarted}</strong> gameboards, with <strong>{inProgress}</strong> on the go and <strong>{notStarted}</strong> not started</h4>}
-                            {!boards && <IsaacSpinner size="sm" inline />}
+                            {boards 
+                                ? <h4>Showing <strong>{inProgress + notStarted}</strong> gameboards, with <strong>{inProgress}</strong> on the go and <strong>{notStarted}</strong> not started</h4> 
+                                : <h4>Loading...</h4>
+                            }
                         </div>
                         {isAda && <> 
                             {/* this is in the sidebar on phy */}
