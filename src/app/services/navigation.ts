6c09d42d02d11b29e974e34010a4491e19b68ecf
Jacob Brown <jeab2@cam.ac.uk>
2025-02-10 10:34:37 +0000

    Restore original default breadcrumb on Ada

--- src/app/services/navigation.ts
+++ src/app/services/navigation.ts
@@ -14,6 +14,7 @@ import {
     GENERIC_QUESTION_CRUMB,
     HUMAN_STAGES,
     HUMAN_SUBJECTS,
+    isAda,
     isDefined,
     isDefinedContext,
     isFound,
@@ -44,7 +45,7 @@ export interface PageNavigation {
 }
 
 const defaultPageNavigation = (doc: ContentDTO | NOT_FOUND_TYPE | null, pageContext: NonNullable<PageContextState> | undefined, currentGameboard?: GameboardDTO) : PageNavigation => {
-    if (doc === NOT_FOUND || doc === null) {
+    if (isAda || doc === NOT_FOUND || doc === null) {
         return {breadcrumbHistory: [], currentGameboard};
     }
 
