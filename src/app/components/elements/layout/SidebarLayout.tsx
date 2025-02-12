961aa69ee4dd0b66101b91b05da11a2d8c4ca09b
Alex Lewin <sl2281@cam.ac.uk>
2025-02-12 16:01:54 +0000

    Grey out non-clickable signup flow tabs

--- src/app/components/elements/layout/SidebarLayout.tsx
+++ src/app/components/elements/layout/SidebarLayout.tsx
@@ -497,10 +497,10 @@ export const SignupSidebar = ({activeTab} : {activeTab: number}) => {
     return <ContentSidebar buttonTitle="Create an account">
         <div className="section-divider mt-4"/>
         <h5 className="mt-1">Create an account</h5>
-        {/* Tabs are clickable iff their page could be reached with a Back button */}
-        <StyledTabPicker checkboxTitle={"Sign-up method"} checked={activeTab === 0} onClick={activeTab > 0 && activeTab < 3 ? (() => history.push("/register")) : undefined}/>
-        <StyledTabPicker checkboxTitle={"Age verification"} checked={activeTab === 1} onClick={activeTab === 2 ? (() => history.push("age")) : undefined}/>
-        <StyledTabPicker checkboxTitle={"Account details"} checked={activeTab === 2}/>
-        <StyledTabPicker checkboxTitle={"Preferences"} checked={activeTab === 3}/>
+        {/* Tabs are clickable iff their page could be reached with a Back buttons */}
+        <StyledTabPicker checkboxTitle={"Sign-up method"} checked={activeTab === 0} disabled={activeTab > 2} onClick={activeTab > 0 && activeTab < 3 ? (() => history.push("/register")) : undefined}/>
+        <StyledTabPicker checkboxTitle={"Age verification"} checked={activeTab === 1}  disabled={activeTab < 1 || activeTab > 2} onClick={activeTab === 2 ? (() => history.push("age")) : undefined}/>
+        <StyledTabPicker checkboxTitle={"Account details"} checked={activeTab === 2} disabled={activeTab !== 2}/>
+        <StyledTabPicker checkboxTitle={"Preferences"} checked={activeTab === 3} disabled={activeTab !== 3}/>
     </ContentSidebar>;
 };
