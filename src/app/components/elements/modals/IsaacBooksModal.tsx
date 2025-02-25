d4ff435b7ecd61b5ed695ec857ce0e27bcc539df
Jacob Brown <jeab2@cam.ac.uk>
2025-02-25 17:22:14 +0000

    Add rough stages estimate to books listing

--- src/app/components/elements/modals/IsaacBooksModal.tsx
+++ src/app/components/elements/modals/IsaacBooksModal.tsx
@@ -2,64 +2,73 @@ import {closeActiveModal, store, useAppDispatch} from "../../../state";
 import React from "react";
 import {Col, Row} from "reactstrap";
 import {Link} from "react-router-dom";
-import {siteSpecific, Subject} from "../../../services";
+import {LearningStage, siteSpecific, Subject} from "../../../services";
 
 export interface BookInfo {
     title: string;
     image: string;
     path: string;
     subject: Subject;
+    stages: LearningStage[];
 }
 
 export const isaacBooks: BookInfo[] = siteSpecific(
     [
         {
             title: "Step up to GCSE Physics",
-            image: "assets/phy/books/step_up_phys.jpg",
+            image: "/assets/phy/books/step_up_phys.jpg",
             path: "/books/step_up_phys",
-            subject: "physics"
+            subject: "physics",
+            stages: ["11_14"],
         },
         {
             title: "GCSE Physics",
-            image: "assets/phy/books/phys_book_gcse.jpg",
+            image: "/assets/phy/books/phys_book_gcse.jpg",
             path: "/books/phys_book_gcse",
-            subject: "physics"
+            subject: "physics",
+            stages: ["gcse"],
         },
         {
             title: "Pre-University Physics",
-            image: "assets/phy/books/physics_skills_19.jpg",
+            image: "/assets/phy/books/physics_skills_19.jpg",
             path: "/books/physics_skills_19",
-            subject: "physics"
+            subject: "physics",
+            stages: ["a_level"],
         },
         {
             title: "Linking Concepts in Pre-University Physics",
-            image: "assets/phy/books/linking_concepts.png",
+            image: "/assets/phy/books/linking_concepts.png",
             path: "/books/linking_concepts",
-            subject: "physics"
+            subject: "physics",
+            stages: ["a_level"],
         },
         {
             title: "Using Essential GCSE Mathematics",
-            image: "assets/phy/books/2021_maths_book_gcse.jpg",
+            image: "/assets/phy/books/2021_maths_book_gcse.jpg",
             path: "/books/maths_book_gcse",
-            subject: "maths"
+            subject: "maths",
+            stages: ["gcse"],
         },
         {
             title: "Mathematics for Sciences (2nd edition)",
-            image: "assets/phy/books/pre_uni_maths_2e.jpg",
+            image: "/assets/phy/books/pre_uni_maths_2e.jpg",
             path: "/books/pre_uni_maths_2e",
-            subject: "maths"
+            subject: "maths",
+            stages: ["a_level"],
         },
         {
             title: "Mathematics for Sciences (1st edition)",
-            image: "assets/phy/books/pre_uni_maths.jpg",
+            image: "/assets/phy/books/pre_uni_maths.jpg",
             path: "/books/pre_uni_maths",
-            subject: "maths"
+            subject: "maths",
+            stages: ["a_level"],
         },
         {
             title: "Pre-University Physical Chemistry",
-            image: "assets/phy/books/chemistry_16.jpg",
+            image: "/assets/phy/books/chemistry_16.jpg",
             path: "/books/chemistry_16",
-            subject: "chemistry"
+            subject: "chemistry",
+            stages: ["a_level"],
         }
     ],
     []
