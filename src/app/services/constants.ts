62d6b33dccd3da884aa2628e6f3659bfddad25bd
Barna Magyarkuti <bmagyarkuti@gmail.com>
2025-03-10 16:52:35 +0000

    Remove X, Facebook links from physics footer
    This is because we've stopped posting on these platforms.

--- src/app/services/constants.ts
+++ src/app/services/constants.ts
@@ -71,8 +71,6 @@ export const { trackPageview, trackEvent } = Plausible(
 export const SOCIAL_LINKS = siteSpecific(
     {
         youtube: {name: "YouTube", href: "https://www.youtube.com/user/isaacphysics"},
-        twitter: {name: "X (Twitter)", href: "https://twitter.com/isaacphysics"},
-        facebook: {name: "Facebook", href: "https://www.facebook.com/isaacphysicsUK"},
         bluesky: {name: "Bluesky", href: "https://bsky.app/profile/isaacphysics.org"},
     },
     {
