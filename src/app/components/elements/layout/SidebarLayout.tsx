f5cf8baa3267e539dda95f8ebc0fff7435cacbcd
Jacob Brown <jeab2@cam.ac.uk>
2025-03-31 14:29:06 +0100

    Fix inconsistency with concept pages reusing question sidebar

--- src/app/components/elements/layout/SidebarLayout.tsx
+++ src/app/components/elements/layout/SidebarLayout.tsx
@@ -128,14 +128,16 @@ export const KeyItem = (props: React.HTMLAttributes<HTMLSpanElement> & {icon: st
     return <span {...rest} className={classNames(rest.className, "d-flex align-items-center pt-2")}><img className="pe-2" src={`/assets/phy/icons/redesign/${icon}.svg`} alt=""/> {text}</span>;
 };
 
-interface QuestionSidebarProps extends SidebarProps {
+interface RelatedContentSidebarProps extends SidebarProps {
     relatedContent: ContentSummaryDTO[] | undefined;
+    isConcept?: boolean;
 }
 
-export const QuestionSidebar = (props: QuestionSidebarProps) => {
-    // TODO: this implementation is only for standalone questions; if in the context of a gameboard, the sidebar should show gameboard navigation
+const RelatedContentSidebar = (props: RelatedContentSidebarProps) => {
     const relatedConcepts = props.relatedContent?.filter(c => c.type === "isaacConceptPage") as IsaacConceptPageDTO[] | undefined;
     const relatedQuestions = props.relatedContent?.filter(c => c.type === "isaacQuestionPage") as QuestionDTO[] | undefined;
+
+    const pageType = props.isConcept ? "concept" : "question";
 
     const pageContext = useAppSelector(selectors.pageContext.context);
     const pageContextStage = useAppSelector(selectors.pageContext.stage);
@@ -152,7 +154,7 @@ export const QuestionSidebar = (props: QuestionSidebarProps) => {
                 {relatedConcepts.map((concept, i) => <ConceptLink key={i} concept={concept} />)}
             </ul>
             : <>
-                There are no related concepts for this question.
+                There are no related concepts for this {pageType}.
                 {isFullyDefinedContext(pageContext) && <AffixButton color="keyline" className="mt-3 w-100" tag={Link} to={extendUrl(pageContext, "concepts")} affix={{affix: "icon-right", position: "suffix", type: "icon"}}>
                     See all concepts for {getHumanContext(pageContext)}
                 </AffixButton>}
@@ -191,7 +193,7 @@ export const QuestionSidebar = (props: QuestionSidebarProps) => {
 
             </>
             : <>
-                There are no related questions for this question.
+                There are no related questions for this {pageType}.
                 {isFullyDefinedContext(pageContext) && <AffixButton color="keyline" className="mt-3 w-100" tag={Link} to={extendUrl(pageContext, "questions")} affix={{affix: "icon-right", position: "suffix", type: "icon"}}>
                     See all questions for {getHumanContext(pageContext)}
                 </AffixButton>}
@@ -200,11 +202,13 @@ export const QuestionSidebar = (props: QuestionSidebarProps) => {
     </NavigationSidebar>;
 };
 
-export const ConceptSidebar = (props: QuestionSidebarProps) => {
-    return <QuestionSidebar {...props} />;
+export const QuestionSidebar = (props: RelatedContentSidebarProps) => {
+    return <RelatedContentSidebar {...props} />;
 };
 
-
+export const ConceptSidebar = (props: RelatedContentSidebarProps) => {
+    return <RelatedContentSidebar {...props} isConcept />;
+};
 
 interface FilterCheckboxProps extends React.HTMLAttributes<HTMLElement> {
     tag: Tag;
