e07a5d5a0b0fc517a8c1cea84e1185e5df7b25e0
Jacob Brown <jeab2@cam.ac.uk>
2025-04-02 11:04:45 +0100

    Allow custom scroll behaviour for page groups; apply to books

--- src/app/services/scrollManager.ts
+++ src/app/services/scrollManager.ts
@@ -1,15 +1,36 @@
 import {history, isDefined} from "./";
 
+const hasPageGroupSpecificScroll = (prevPathname: string, pathname: string): boolean => {
+    const prevPathnameParts = prevPathname.split("/");
+    const pathnameParts = pathname.split("/");
+
+    // books
+    if (prevPathnameParts[1] === "books" && pathnameParts[1] === "books" && prevPathnameParts[2] === pathnameParts[2]) {
+        const pageTitle = document.querySelector("#page-title");
+        if (pageTitle) scrollVerticallyIntoView(pageTitle);
+        return true;
+    }
+
+    return false;
+};
+
 let previousPathname = "";
 history.listen((location, action) => {
-    if (["PUSH", "REPLACE"].includes(action) && previousPathname !== location.pathname) {
-        previousPathname = location.pathname;
-        (window as any).followedAtLeastOneSoftLink = true;
-        try {
-            window.scrollTo({top: 0, left: 0, behavior: "auto"});
-        } catch (e) {
-            // Some older browsers, notably Safari, don't support the new spec used above!
-            window.scrollTo(0, 0);
+    if (["PUSH", "REPLACE"].includes(action)) {
+        
+        if (hasPageGroupSpecificScroll(previousPathname, location.pathname)) {
+            return;
+        }
+        
+        if (previousPathname !== location.pathname) {
+            previousPathname = location.pathname;
+            (window as any).followedAtLeastOneSoftLink = true;
+            try {
+                window.scrollTo({top: 0, left: 0, behavior: "auto"});
+            } catch (e) {
+                // Some older browsers, notably Safari, don't support the new spec used above!
+                window.scrollTo(0, 0);
+            }
         }
     }
 });
