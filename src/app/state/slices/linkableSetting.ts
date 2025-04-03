36791c1d456d1cc198cea8f197b7e23f66a1d347
Matthew Trew <matthew.trew@raspberrypi.org>
2025-04-03 10:31:56 +0100

    Reorganise linkable setting slice so default state is null

--- src/app/state/slices/linkableSetting.ts
+++ src/app/state/slices/linkableSetting.ts
@@ -1,26 +1,32 @@
 import {createSlice} from "@reduxjs/toolkit";
 import {routerPageChange} from "../actions/routing";
 
-export type LinkableSettingState = {target: string | undefined, seen: boolean}
+export type LinkableSettingState = {target: string, seen: boolean} | null
 
 export const linkableSettingSlice = createSlice(
     {
         name: 'linkableSettingSlice',
-        initialState: {target: undefined, seen: false} as LinkableSettingState,
+        initialState: null as LinkableSettingState,
         reducers: {
             setTarget: (state, action) => {
-                state.target = action.payload;
-                state.seen = false;
+                if (!state) {
+                    return {target: action.payload, seen: false};
+                } else {
+                    state.target = action.payload;
+                    state.seen = false;
+                }
             },
             setSeen: (state) => {
-                state.seen = true;
+                if (state) {
+                    state.seen = true;
+                }
             }
         },
         extraReducers: (builder) => {
             // If the target setting has been seen, this state is no longer needed - clear it on page change.
             builder.addCase(
                 routerPageChange,
-                (state) => {if (state.seen) state.target = undefined;},
+                (state) => {if (state?.seen) return null;},
             );
         }
     }
