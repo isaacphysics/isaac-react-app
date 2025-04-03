182193f93ab3c7300206882bdf0ea991dec6296c
Matthew Trew <matthew.trew@raspberrypi.org>
2025-04-03 09:25:04 +0100

    Show appropriate card to users who already receive event emails

--- src/app/components/pages/StudentChallenges.tsx
+++ src/app/components/pages/StudentChallenges.tsx
@@ -8,7 +8,7 @@ import {selectors, useAppSelector} from "../../state";
 
 export const StudentChallenges = () => {
 
-    const user = useAppSelector(selectors.user.orNull);
+    const userPreferences = useAppSelector(selectors.user.preferences);
     const {setLinkedSetting} = useLinkableSetting();
 
     return <Container>
@@ -16,15 +16,27 @@ export const StudentChallenges = () => {
         <Row>
             <Col lg={{size: 8, offset: 2}}>
                 <PageFragment fragmentId={"student_challenges_fragment"} />
-                <IconCard
-                    card={{
-                        title: "No challenges running",
-                        icon: {src: "/assets/cs/icons/event.svg"},
-                        bodyText: "There are no challenges running at the moment. Update your communication preferences to be the first to know when they're back.",
-                        clickUrl: "/account#notifications",
-                        buttonText: "Notify me",
-                        onButtonClick: () => {setLinkedSetting("events-preference");}
-                    }}/>
+                {userPreferences?.EMAIL_PREFERENCE?.EVENTS ?
+                    <IconCard
+                        card={{
+                            title: "The next challenge",
+                            icon: {src: "/assets/cs/icons/event.svg"},
+                            bodyText: "There are no challenges running at the moment, but you're subscribed to our mailing list and will be the first to know about the next one!",
+                        }}
+                    />
+                    :
+                    <IconCard
+                        card={{
+                            title: "The next challenge",
+                            icon: {src: "/assets/cs/icons/event.svg"},
+                            bodyText: "There are no challenges running at the moment. Update your communication preferences to be the first to know about the next challenge.",
+                            clickUrl: "/account#notifications",
+                            buttonText: "Notify me",
+                            onButtonClick: () => {setLinkedSetting("events-preference");}
+                        }}
+                    />
+                }
+
             </Col>
         </Row>
     </Container>;
