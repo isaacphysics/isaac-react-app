ff19924fdd2088aa716746f60cb68510d064afbc
Jacob Brown <jeab2@cam.ac.uk>
2025-04-03 16:54:51 +0100

    Refactor page-load-scroll-to-top into a component to permit hooks

--- src/app/services/scrollManager.ts
+++ src/app/services/scrollManager.ts
@@ -1,11 +1,16 @@
+import { useCallback, useEffect } from "react";
 import {history, isDefined} from "./";
+import { useReducedMotion } from "./accessibility";
+import { Action, Location } from "history";
 
-const hasPageGroupSpecificScroll = (prevPathname: string, pathname: string): boolean => {
+const hasPageGroupSpecificScroll = (prevPathname: string, pathname: string, reducedMotion: boolean): boolean => {
     const prevPathnameParts = prevPathname.split("/");
     const pathnameParts = pathname.split("/");
 
     // books
     if (prevPathnameParts[1] === "books" && pathnameParts[1] === "books" && prevPathnameParts[2] === pathnameParts[2]) {
+        if (reducedMotion) return true;
+
         const pageTitle = document.querySelector("#page-title");
         if (pageTitle) scrollVerticallyIntoView(pageTitle);
         return true;
@@ -15,25 +20,37 @@ const hasPageGroupSpecificScroll = (prevPathname: string, pathname: string): boo
 };
 
 let previousPathname = "";
-history.listen((location, action) => {
-    if (["PUSH", "REPLACE"].includes(action)) {
-        
-        if (hasPageGroupSpecificScroll(previousPathname, location.pathname)) {
-            return;
-        }
-        
-        if (previousPathname !== location.pathname) {
-            previousPathname = location.pathname;
-            (window as any).followedAtLeastOneSoftLink = true;
-            try {
-                window.scrollTo({top: 0, left: 0, behavior: "auto"});
-            } catch (e) {
-                // Some older browsers, notably Safari, don't support the new spec used above!
-                window.scrollTo(0, 0);
+
+export const ScrollTopOnPageLoad = () => {
+    const reducedMotion = useReducedMotion();
+
+    const onPageTransition = useCallback((location: Location, action: Action) => {
+        if (["PUSH", "REPLACE"].includes(action)) {
+                
+            if (hasPageGroupSpecificScroll(previousPathname, location.pathname, reducedMotion)) {
+                return;
+            }
+            
+            if (previousPathname !== location.pathname) {
+                previousPathname = location.pathname;
+                (window as any).followedAtLeastOneSoftLink = true;
+                try {
+                    window.scrollTo({top: 0, left: 0, behavior: "auto"});
+                } catch (e) {
+                    // Some older browsers, notably Safari, don't support the new spec used above!
+                    window.scrollTo(0, 0);
+                }
             }
         }
-    }
-});
+    }, [reducedMotion]);
+
+    useEffect(() => {
+        const unregisterListener = history.listen(onPageTransition);
+        return () => unregisterListener();
+    }, [onPageTransition]);
+
+    return null;
+};
 
 export function scrollVerticallyIntoView(element: Element, offset = 0): void {
     const yPosition = element.getBoundingClientRect().top + window.scrollY + offset;
