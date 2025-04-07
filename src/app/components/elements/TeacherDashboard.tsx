6024dd8b5a7dbe628fe540ad2ec00aef6ebf79b1
Jacob Brown <jeab2@cam.ac.uk>
2025-04-07 11:58:17 +0100

    Remove unused import

--- src/app/components/elements/TeacherDashboard.tsx
+++ src/app/components/elements/TeacherDashboard.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, useRef, useState } from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import { selectors, useAppSelector } from '../../state';
 import { Button, Card, Col, Row } from 'reactstrap';
 import { Link } from 'react-router-dom';
