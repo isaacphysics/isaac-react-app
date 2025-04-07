2284d19f9950dbb4dc8ddbd50802adcb42b3a674
Matthew Trew <matthew.trew@raspberrypi.org>
2025-04-07 10:22:37 +0100

    Show CTA on challenges page only if no live challenge fragment exists

--- src/app/components/pages/StudentChallenges.tsx
+++ src/app/components/pages/StudentChallenges.tsx
@@ -4,39 +4,48 @@ import {PageFragment} from "../elements/PageFragment";
 import {Col, Container, Row} from "reactstrap";
 import {IconCard} from "../elements/cards/IconCard";
 import {useLinkableSetting} from "../../services/linkableSetting";
-import {selectors, useAppSelector} from "../../state";
+import {selectors, useAppSelector, useGetPageFragmentQuery} from "../../state";
 
 export const StudentChallenges = () => {
 
     const userPreferences = useAppSelector(selectors.user.preferences);
     const {setLinkedSetting} = useLinkableSetting();
+    const liveChallengeId = "live_challenge";
+    const {data} = useGetPageFragmentQuery(liveChallengeId);
 
     return <Container>
         <TitleAndBreadcrumb currentPageTitle={"Student challenges"} />
         <Row>
             <Col lg={{size: 8, offset: 2}}>
-                <PageFragment fragmentId={"student_challenges_fragment"} />
-                {userPreferences?.EMAIL_PREFERENCE?.EVENTS ?
-                    <IconCard
-                        card={{
-                            title: "The next challenge",
-                            icon: {src: "/assets/cs/icons/event.svg"},
-                            bodyText: "There are no challenges running at the moment, but you're subscribed to our mailing list and will be the first to know about the next one!",
-                        }}
-                    />
+                <PageFragment fragmentId={"student_challenges_intro"} />
+                {data ?
+                    <PageFragment fragmentId={liveChallengeId} />
                     :
-                    <IconCard
-                        card={{
-                            title: "The next challenge",
-                            icon: {src: "/assets/cs/icons/event.svg"},
-                            bodyText: "There are no challenges running at the moment. Update your communication preferences to be the first to know about the next challenge.",
-                            clickUrl: "/account#notifications",
-                            buttonText: "Notify me",
-                            onButtonClick: () => {setLinkedSetting("events-preference");}
-                        }}
-                    />
+                    <>
+                        {userPreferences?.EMAIL_PREFERENCE?.EVENTS ?
+                            <IconCard
+                                card={{
+                                    title: "The next challenge",
+                                    icon: {src: "/assets/cs/icons/event.svg"},
+                                    bodyText: "There are no challenges running at the moment, but you're subscribed to " +
+                                        "our mailing list and will be the first to know about the next one!",
+                                }}
+                            />
+                            :
+                            <IconCard
+                                card={{
+                                    title: "The next challenge",
+                                    icon: {src: "/assets/cs/icons/event.svg"},
+                                    bodyText: "There are no challenges running at the moment. Update your communication" +
+                                        " preferences to be the first to know about the next challenge.",
+                                    clickUrl: "/account#notifications",
+                                    buttonText: "Notify me",
+                                    onButtonClick: () => {setLinkedSetting("events-preference");}
+                                }}
+                            />
+                        }
+                    </>
                 }
-
             </Col>
         </Row>
     </Container>;
