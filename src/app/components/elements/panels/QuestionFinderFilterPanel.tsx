0ab708a3aa2b70d14e0fe0c922c003f37d63316e
Jacob Brown <jeab2@cam.ac.uk>
2025-04-23 11:41:11 +0100

    Show only relevant books to stage in QF

--- src/app/components/elements/panels/QuestionFinderFilterPanel.tsx
+++ src/app/components/elements/panels/QuestionFinderFilterPanel.tsx
@@ -158,7 +158,10 @@ export function QuestionFinderFilterPanel(props: QuestionFinderFilterPanelProps)
     const deviceSize = useDeviceSize();
     const dispatch = useAppDispatch();
     const pageContext = useAppSelector(selectors.pageContext.context);
-    const bookOptions = ISAAC_BOOKS.filter(b => !b.hidden).filter(book => !pageContext?.subject || book.subject === pageContext?.subject);
+    const bookOptions = ISAAC_BOOKS.filter(b => !b.hidden).filter(book => 
+        (!pageContext?.subject || book.subject === pageContext?.subject)
+        && (!pageContext?.stage || book.stages.some(x => pageContext?.stage?.includes(x)))
+    );
 
     const [filtersVisible, setFiltersVisible] = useState<boolean>(above["lg"](deviceSize));
 
