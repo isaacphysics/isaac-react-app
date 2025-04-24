38bf21a0c38123af17cab69c6d7202701dc00b7e
Matthew Trew <matthew.trew@raspberrypi.org>
2025-04-24 11:41:27 +0100

    Add wait to VRT
    The right chevron in the breadcrumb trail doesn't always render immediately.

--- src/test/pages/GameboardBuilder.cy.tsx
+++ src/test/pages/GameboardBuilder.cy.tsx
@@ -5,5 +5,6 @@ import GameboardBuilder from "../../app/components/pages/GameboardBuilder";
 it('Gameboard builder should have no visual regressions', () => {
     // @ts-ignore
     cy.mountWithStoreAndRouter(<GameboardBuilder user={mockUser}/>);
+    cy.wait(500);
     cy.matchImage();
 });
