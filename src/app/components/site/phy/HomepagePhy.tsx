3a074c22799728ca3b08ecb66e11bfaa44c9a889
Jacob Brown <jeab2@cam.ac.uk>
2025-04-24 11:17:33 +0100

    Add aria-labels to homepage tags

--- src/app/components/site/phy/HomepagePhy.tsx
+++ src/app/components/site/phy/HomepagePhy.tsx
@@ -105,7 +105,7 @@ const getListViewSubjectCard = (sc: subjectCategory) => {
         item: item,
         icon: {type: "img", icon: `/assets/phy/icons/redesign/subject-${sc.subject}.svg`},
         subject: sc.subject as Subject,
-        linkTags: sc.subcategories.map((subcat) => ({tag: subcat.humanStage, url: subcat.href})),
+        linkTags: sc.subcategories.map((subcat) => ({tag: subcat.humanStage, url: subcat.href, "aria-label": `Explore ${subcat.humanStage} ${sc.humanSubject}`})),
     };
 
     return listViewSubjectCard;
