2bf054d7b0cdb58f46b8aebcebfa6610d6c2dffd
Jacob Brown <jeab2@cam.ac.uk>
2025-05-22 10:14:45 +0100

    Add "coming soon" to quick quiz cards

--- src/app/components/pages/subjectLandingPageComponents.ts
+++ src/app/components/pages/subjectLandingPageComponents.ts
@@ -68,7 +68,8 @@ const CoreSkillsCard = (context: NonNullable<Required<PageContextState>>): ListV
     },
     icon: {type: "hex", icon: "icon-quiz"},
     subject: context.subject,
-    linkTags: [{tag: "Practise a core skill", url: extendUrl(context, 'quick_quizzes')}]
+    linkTags: [{tag: "Practise a core skill", url: extendUrl(context, 'quick_quizzes')}],
+    state: AbstractListViewItemState.COMING_SOON,
 });
 
 const GlossaryCard = (context: NonNullable<Required<PageContextState>>): ListViewCardProps => ({
