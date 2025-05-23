29154e3f6d668d22f1d516e18ccf9a7225ee3151
Alex Lewin <sl2281@cam.ac.uk>
2025-05-23 12:16:54 +0100

    Prevent text wrapping to 3 lines

--- src/app/components/pages/Concepts.tsx
+++ src/app/components/pages/Concepts.tsx
@@ -132,18 +132,16 @@ export const Concepts = withRouter((props: RouteComponentProps) => {
                     : <GenericConceptsSidebar {...sidebarProps} searchStages={searchStages} setSearchStages={setSearchStages} stageCounts={stageCounts}/>
                 }
                 <MainContent>
-                    {pageContext?.subject && <div className="d-flex align-items-center flex-wrap flex-md-nowrap my-3">
-                        <p className="me-3 mb-3">The concepts shown on this page have been filtered to only show those that are relevant to {getHumanContext(pageContext)}.</p>
-                        <div className="ms-auto mb-3">
-                            <AffixButton size="md" color="keyline" tag={Link} to="/concepts" className="text-nowrap"
-                                affix={{
-                                    affix: "icon-right",
-                                    position: "suffix",
-                                    type: "icon"
-                                }}>
+                    {pageContext?.subject && <div className="d-flex align-items-baseline flex-wrap flex-md-nowrap flex-lg-wrap flex-xl-nowrap mt-3">
+                        <p className="me-3">The concepts shown on this page have been filtered to only show those that are relevant to {getHumanContext(pageContext)}.</p>
+                        <AffixButton size="md" color="keyline" tag={Link} to="/concepts" className="ms-auto"
+                            affix={{
+                                affix: "icon-right",
+                                position: "suffix",
+                                type: "icon"
+                            }}>
                             Browse all concepts
-                            </AffixButton>
-                        </div>
+                        </AffixButton>
                     </div>}
                     {isPhy && <div className="list-results-container p-2 my-4">
                         <ShowLoadingQuery
