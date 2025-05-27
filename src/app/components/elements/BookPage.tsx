be42d1beddc5a0bc73fd7187131f402e7af66f12
Jacob Brown <jeab2@cam.ac.uk>
2025-05-27 14:30:18 +0100

    Add book navigation component

--- src/app/components/elements/BookPage.tsx
+++ src/app/components/elements/BookPage.tsx
@@ -5,12 +5,19 @@ import {IsaacBookDetailPageDTO} from "../../../IsaacApiTypes";
 import {TeacherNotes} from "./TeacherNotes";
 import {EditContentButton} from "./EditContentButton";
 
+const BookSectionLink = ({id, title}: { id: string; title: string }) => {
+    return <a className="d-flex align-items-center ms-1 ps-2 invert-underline" href={`#${id}`}>
+        <i className="icon icon-arrow-down me-2"/>
+        {title}
+    </a>;
+};
+
 export const BookPage = ({ page }: { page: IsaacBookDetailPageDTO }) => {
 
     return <div className="book-page">
         <>
             <h3 className="mb-3">
-                {page.subtitle && <span className="me-2 text-theme">{page.subtitle} </span>}
+                {page.subtitle && <span className="me-3 text-theme">{page.subtitle} </span>}
                 {page.title}
             </h3>
 
@@ -18,8 +25,14 @@ export const BookPage = ({ page }: { page: IsaacBookDetailPageDTO }) => {
 
             <TeacherNotes notes={page.teacherNotes} />
 
+            <div className="content-metadata-container d-flex flex-column gap-2">
+                {!!page.gameboards?.length && <BookSectionLink id="questions" title="Questions" />}
+                {!!page.relatedContent?.length && <BookSectionLink id="review" title="Review" />}
+                {!!page.extensionGameboards?.length && <BookSectionLink id="extension" title="Extension work" />}
+            </div>
+
             {!!page.gameboards?.length && <>
-                <h4 className="mb-3" id="resources">Questions</h4>
+                <h4 className="mb-3" id="questions">Questions</h4>
                 <div className="mt-3 mb-5 list-results-container p-2">
                     <ListView
                         items={page.gameboards.map(g => ({...g, type: "gameboard"}))}
@@ -42,7 +55,7 @@ export const BookPage = ({ page }: { page: IsaacBookDetailPageDTO }) => {
             </IsaacContentValueOrChildren>
 
             {!!page.extensionGameboards?.length && <>
-                <h5 className="mt-5 mb-3" id="extension">Extension work</h5>
+                <h4 className="mt-4 mb-3" id="extension">Extension work</h4>
                 <span>Expand your boundaries by having a go at these additional extension questions.</span>
                 <div className="mt-3 mb-5 list-results-container p-2">
                     <ListView
