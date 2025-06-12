6916ba952644b2cd5bbd62e799feba326435175f
Matthew Trew <matthew.trew@raspberrypi.org>
2025-06-12 13:36:20 +0100

    Add wait to Set Assignments test

--- src/test/pages/SetAssignments.cy.tsx
+++ src/test/pages/SetAssignments.cy.tsx
@@ -7,5 +7,6 @@ it('Set Assignments should have no visual regressions', () => {
     // @ts-ignore
     cy.mountWithStoreAndRouter(<SetAssignments user={mockUser}/>, [PATHS.SET_ASSIGNMENTS]);
     cy.get('[data-testid="loading"]').should('not.exist');
+    cy.wait(1000);
     cy.matchImage();
 });
