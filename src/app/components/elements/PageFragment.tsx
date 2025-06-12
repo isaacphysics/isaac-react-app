bfeff031092f3de48ecfdf90434fe899b7bdc42b
Sol Dubock <94075844+sjd210@users.noreply.github.com>
2025-06-12 10:49:51 +0100

    Show clearer errors when site loaded offline

--- src/app/components/elements/PageFragment.tsx
+++ src/app/components/elements/PageFragment.tsx
@@ -4,6 +4,7 @@ import {WithFigureNumbering} from "./WithFigureNumbering";
 import {EditContentButton} from "./EditContentButton";
 import {useGetPageFragmentQuery} from "../../state";
 import {ShowLoadingQuery} from "../handlers/ShowLoadingQuery";
+import { WEBMASTER_EMAIL } from "../../services";
 
 interface PageFragmentComponentProps {
     fragmentId: string;
@@ -16,8 +17,17 @@ export const PageFragment = ({fragmentId, ifNotFound}: PageFragmentComponentProp
         <h2>Content not found</h2>
         <h3 className="my-4">
             <small>
-                {"We're sorry, page fragment not found: "}
-                <code>{fragmentId}</code>
+                {window.navigator.onLine ? 
+                    <>
+                        We&apos;re sorry, page fragment not found: 
+                        <code>{fragmentId}</code>
+                    </> :
+                    <p>
+                        <br />
+                        It looks like you&apos;re offline. You may want to check your internet connection, and then refresh this page to try again.
+                        <br />
+                        If you are still having issues, please <a href={`mailto:${WEBMASTER_EMAIL}`}>let us know</a>. 
+                    </p>}
             </small>
         </h3>
     </div>;
--- src/app/components/pages/NotFound.tsx
+++ src/app/components/pages/NotFound.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import {useLocation} from "react-router-dom";
 import {Container} from "reactstrap";
 import {TitleAndBreadcrumb} from "../elements/TitleAndBreadcrumb";
-import {siteSpecific} from "../../services";
+import {siteSpecific, WEBMASTER_EMAIL} from "../../services";
 
 const buildContactUrl = (state: {overridePathname?: string}, pathname: string) => {
     const page = encodeURIComponent((state && state.overridePathname) || pathname);
@@ -12,10 +12,10 @@ const buildContactUrl = (state: {overridePathname?: string}, pathname: string) =
 export const NotFound = () => {
     const {pathname, state} = useLocation<{overridePathname?: string}>();
     return <Container>
-        <div>
+        <div className="pb-1">
             <TitleAndBreadcrumb
                 breadcrumbTitleOverride={siteSpecific("Unknown page", "404")}
-                currentPageTitle="Page not found"
+                currentPageTitle={window.navigator.onLine ? "Page not found" : "No internet"}
             />
             <p className="my-4">
                 {"We're sorry, page not found: "}
@@ -23,12 +23,20 @@ export const NotFound = () => {
                     {(state && state.overridePathname) || pathname}
                 </code>
             </p>
-            <p>
-                Expecting to find something here?
-                <a className="ps-1" href={buildContactUrl(state, pathname)} >
-                    Let us know<span className="visually-hidden"> about this missing page</span>
-                </a>.
-            </p>
+            
+            {window.navigator.onLine ? 
+                <p>
+                    Expecting to find something here?
+                    <a className="ps-1" href={buildContactUrl(state, pathname)} >
+                        Let us know<span className="visually-hidden"> about this missing page</span>
+                    </a>.
+                </p> : 
+                <p>
+                    It looks like you&apos;re offline. You may want to check your internet connection, and then refresh this page to try again.
+                    <br />
+                    If you are still having issues, please <a href={`mailto:${WEBMASTER_EMAIL}`}>let us know</a>. 
+                </p> // Email link rather than contact form, as the contact form may not work offline
+            }
         </div>
     </Container>;
 };
