c73ac9b17646b07a39808a410ba046d001e42ac7
Jacob Brown <jeab2@cam.ac.uk>
2025-06-20 15:42:17 +0100

    Use RelatedContentSidebar on generic pages if applicable

--- src/app/components/elements/layout/SidebarLayout.tsx
+++ src/app/components/elements/layout/SidebarLayout.tsx
@@ -165,14 +165,11 @@ const CompletionKey = () => {
 
 interface RelatedContentSidebarProps extends SidebarProps {
     relatedContent: ContentSummaryDTO[] | undefined;
-    isConcept?: boolean;
 }
 
-const RelatedContentSidebar = (props: RelatedContentSidebarProps) => {
+const RelatedContentSidebar = (props: RelatedContentSidebarProps & {pageType: "concept" | "question" | "page"}) => {
     const relatedConcepts = props.relatedContent?.filter(c => c.type === "isaacConceptPage") as IsaacConceptPageDTO[] | undefined;
     const relatedQuestions = props.relatedContent?.filter(c => c.type === "isaacQuestionPage") as QuestionDTO[] | undefined;
-
-    const pageType = props.isConcept ? "concept" : "question";
 
     const pageContext = useAppSelector(selectors.pageContext.context);
     const pageContextStage = useAppSelector(selectors.pageContext.stage);
@@ -187,7 +184,7 @@ const RelatedContentSidebar = (props: RelatedContentSidebarProps) => {
                 {relatedConcepts.map((concept, i) => <ConceptLink key={i} concept={concept} />)}
             </ul>
             : <>
-                There are no related concepts for this {pageType}.
+                There are no related concepts for this {props.pageType}.
                 {isFullyDefinedContext(pageContext) && <AffixButton color="keyline" className="mt-3 w-100" tag={Link} to={extendUrl(pageContext, "concepts")} affix={{affix: "icon-arrow-right", position: "suffix", type: "icon"}}>
                     See all concepts for {getHumanContext(pageContext)}
                 </AffixButton>}
@@ -221,7 +218,7 @@ const RelatedContentSidebar = (props: RelatedContentSidebarProps) => {
 
             </>
             : <>
-                There are no related questions for this {pageType}.
+                There are no related questions for this {props.pageType}.
                 {isFullyDefinedContext(pageContext) && <AffixButton color="keyline" className="mt-3 w-100" tag={Link} to={extendUrl(pageContext, "questions")} affix={{affix: "icon-arrow-right", position: "suffix", type: "icon"}}>
                     See all questions for {getHumanContext(pageContext)}
                 </AffixButton>}
@@ -231,11 +228,15 @@ const RelatedContentSidebar = (props: RelatedContentSidebarProps) => {
 };
 
 export const QuestionSidebar = (props: RelatedContentSidebarProps) => {
-    return <RelatedContentSidebar {...props} />;
+    return <RelatedContentSidebar {...props} pageType="question" />;
 };
 
 export const ConceptSidebar = (props: RelatedContentSidebarProps) => {
-    return <RelatedContentSidebar {...props} isConcept />;
+    return <RelatedContentSidebar {...props} pageType="concept" />;
+};
+
+export const GenericSidebarWithRelatedContent = (props: RelatedContentSidebarProps) => {
+    return <RelatedContentSidebar {...props} pageType="page" />;
 };
 
 interface GameboardQuestionSidebarProps extends SidebarProps {
