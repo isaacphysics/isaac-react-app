afe5d7c784e75af73d1ac7adab5dd21b42b93288
Barna Magyarkuti <bm710@cam.ac.uk>
2025-06-26 11:32:19 +0200

    rename css class as it's used in multiple places

--- src/app/components/elements/MicrosoftSignInButton.tsx
+++ src/app/components/elements/MicrosoftSignInButton.tsx
@@ -11,6 +11,6 @@ export const MicrosoftSignInButton = () => {
     };
 
     return <Button className={"position-relative"} block outline color="primary" style={{borderColor: "#545461", borderWidth: "1.5px"}} onClick={logInWithMicrosoft}>
-        <img className="google-button-logo" src={"/assets/common/logos/microsoft-logo.svg"} alt={"Microsoft logo"}/>Microsoft
+        <img className="authenticator-logo" src={"/assets/common/logos/microsoft-logo.svg"} alt={"Microsoft logo"}/>Microsoft
     </Button>;
 };
