2e99d9a845112946c50f156de36369c27c4b29b7
Barna Magyarkuti <bm710@cam.ac.uk>
2025-06-26 11:28:12 +0200

    Microsoft button is phy-only for now

--- src/app/components/elements/MicrosoftSignInButton.tsx
+++ src/app/components/elements/MicrosoftSignInButton.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import {Button} from "reactstrap";
 import {handleProviderLoginRedirect, useAppDispatch} from "../../state";
-import {siteSpecific} from "../../services";
 
 // Button prompting the user to sign in via Microsoft
 export const MicrosoftSignInButton = () => {
@@ -11,7 +10,7 @@ export const MicrosoftSignInButton = () => {
         dispatch(handleProviderLoginRedirect("MICROSOFT"));
     };
 
-    return <Button className={"position-relative"} block outline color={siteSpecific("primary", undefined)} style={{borderColor: "#545461", borderWidth: "1.5px"}} onClick={logInWithMicrosoft}>
+    return <Button className={"position-relative"} block outline color="primary" style={{borderColor: "#545461", borderWidth: "1.5px"}} onClick={logInWithMicrosoft}>
         <img className="google-button-logo" src={"/assets/common/logos/microsoft-logo.svg"} alt={"Microsoft logo"}/>Microsoft
     </Button>;
 };
