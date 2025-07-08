978a7775d4b84ebf0b94c8aa00b228e593ad2df4
Jacob Brown <jeab2@cam.ac.uk>
2025-07-08 12:25:09 +0100

    Add working VRTs for new markbook groups pages

--- src/test/pages/AssignmentProgress.cy.tsx
+++ src/test/pages/AssignmentProgress.cy.tsx
@@ -2,12 +2,32 @@ import React from "react";
 import {mockUser} from "../../mocks/data";
 import { AssignmentProgress } from "../../app/components/pages/AssignmentProgressWrapper";
 
+describe('Assignment progress', () => {
+    it('Groups listing should have no visual regressions', () => {
+        // @ts-ignore
+        cy.mountWithStoreAndRouter(<AssignmentProgress user={mockUser}/>);
+        cy.get('[data-testid="loading"]').should('not.exist');
+        cy.matchImage();
+    });
 
-it('Assignment Progress should have no visual regressions', () => {
-    // @ts-ignore
-    cy.mountWithStoreAndRouter(<AssignmentProgress user={mockUser}/>);
-    cy.get('[data-testid="loading"]').should('not.exist');
-    cy.contains("Test Group 1").click();
-    cy.get('div.assignment-progress-details').should('be.visible');
-    cy.matchImage();
+    it('Group overview (assignment view) should have no visual regressions', () => {
+        // @ts-ignore
+        cy.mountWithStoreAndRouter(<AssignmentProgress user={mockUser}/>);
+        cy.get('[data-testid="loading"]').should('not.exist');
+        cy.get('[data-testid="group-name"]').contains("Test Group 1").click();
+        cy.matchImage();
+    });
+
+    it('Group overview (test view) should have no visual regressions', () => {
+        // @ts-ignore
+        cy.mountWithStoreAndRouter(<AssignmentProgress user={mockUser}/>);
+        cy.get('[data-testid="loading"]').should('not.exist');
+        cy.get('[data-testid="group-name"]').contains("Test Group 1").click();
+        cy.get('button').contains("Tests").click();
+        cy.matchImage();
+    });
+
+    // TODO: add mock data for progress details; add test on <ProgressDetails /> component
 });
+
+
