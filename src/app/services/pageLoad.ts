5328fe7a542dd68d85cea335666357d59458697d
Jacob Brown <jeab2@cam.ac.uk>
2025-07-09 10:27:55 +0100

    Close sidebar on page load

--- src/app/services/pageLoad.ts
+++ src/app/services/pageLoad.ts
@@ -1,16 +1,19 @@
 import { useCallback, useEffect } from "react";
+import { sidebarSlice, useAppDispatch } from "../state";
 import { scrollTopOnPageLoad } from "./scrollManager";
 import { history } from "./";
 import { Action, Location } from "history";
 import { useReducedMotion } from "./accessibility";
 
 export const OnPageLoad = () => {
+    const dispatch = useAppDispatch();
     const reducedMotion = useReducedMotion();
     const scroll = scrollTopOnPageLoad(reducedMotion);
 
     const onPageLoad = useCallback((location: Location, action: Action) => {
+        dispatch(sidebarSlice.actions.setOpen(false));
         scroll(location, action);
-    }, [scroll]);
+    }, [dispatch, scroll]);
 
     useEffect(() => {
         const unregisterListener = history.listen(onPageLoad);
