4804b08dd2c904df378027b079a415116dec55bd
Matthew Trew <matthew.trew@raspberrypi.org>
2025-07-15 16:49:47 +0100

    Add VRT, make route teacher or above

---
+++ src/test/pages/Overview.cy.tsx
@@ -0,0 +1,14 @@
+import {isAda} from "../../app/services";
+import {Overview} from "../../app/components/pages/Overview";
+import React from "react";
+
+it('Homepage should have no visual regressions', () => {
+    if (isAda) {
+        // Arrange
+        // @ts-ignore
+        cy.mountWithStoreAndRouter(<Overview />, ["/overview"]);
+
+        // Assert
+        cy.matchImage();
+    }
+});
