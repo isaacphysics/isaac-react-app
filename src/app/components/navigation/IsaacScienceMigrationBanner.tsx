feac5ed004a9e17a4cbff1f16d9be3b93a45cc96
Jacob Brown <jeab2@cam.ac.uk>
2025-07-28 11:35:25 +0100

    Update wording

--- src/app/components/navigation/IsaacScienceMigrationBanner.tsx
+++ src/app/components/navigation/IsaacScienceMigrationBanner.tsx
@@ -5,10 +5,9 @@ import { isPhy } from "../../services";
 export const IsaacScienceMigrationBanner = () => {
     return isPhy ? <Alert color="danger" className="mb-0 border-radius-0 mx-0 no-print" fade={false}>
         <Container className="text-center">
-            On August 11th 2025, all pages on Isaac Physics will redirect to the new <a href="https://isaacscience.org" target="_blank">Isaac Science</a> platform.
-            You can log in with the same account, and all progress, boards and groups have been maintained.
-            You can <a href="https://isaacphysics.org/pages/isaacscience" target="_blank">
-                learn more about these changes here <span className="visually-hidden">(opens in a new tab)</span>
+            On August 11th 2025, all pages on <a href="https://isaacphysics.org" target="_blank">isaacphysics.org</a> will redirect to <a href="https://isaacscience.org" target="_blank">isaacscience.org</a>.
+            Your account and activity will not be changing. You can <a href="https://isaacphysics.org/pages/isaacscience" target="_blank">
+                read more about the move <span className="visually-hidden">to Isaac Science</span> here
             </a>.
         </Container>
     </Alert> : null;
