07083b78a893ed53cb444e7873b29e71fc743122
James Sharkey <jsharkey13@users.noreply.github.com>
2025-07-30 09:29:11 +0100

    Migration banner wording improvements

--- src/app/components/navigation/IsaacScienceMigrationBanner.tsx
+++ src/app/components/navigation/IsaacScienceMigrationBanner.tsx
@@ -1,12 +1,13 @@
 import React from "react";
-import { Alert, Container } from "reactstrap";
-import { isPhy } from "../../services";
+import {Alert, Container} from "reactstrap";
+import {isPhy} from "../../services";
 
 export const IsaacScienceMigrationBanner = () => {
     return isPhy ? <Alert color="danger" className="mb-0 border-radius-0 mx-0 no-print" fade={false}>
         <Container className="text-center">
-            On August 11th 2025, all pages on <a href="https://isaacphysics.org" target="_blank">isaacphysics.org</a> will redirect to <a href="https://isaacscience.org" target="_blank">isaacscience.org</a>.
-            Your account and activity will not be changing. You can <a href="https://isaacphysics.org/pages/isaacscience" target="_blank">
+            From August 11th 2025, all pages on <a href="https://isaacphysics.org" target="_blank">isaacphysics.org</a> will redirect to <a href="https://isaacscience.org" target="_blank">isaacscience.org</a>.
+            <br />
+            Your login and account data will not change. You can <a href="https://isaacphysics.org/pages/isaacscience" target="_blank">
                 read more about the move <span className="visually-hidden">to Isaac Science</span> here
             </a>.
         </Container>
