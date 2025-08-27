4a26577470b4b551841cdee2f13763b1df43843b
Barna Magyarkuti <bm710@cam.ac.uk>
2025-08-27 14:41:16 +0100

    add ms sso sign-in to login modal

---
+++ src/test/pages/LogInOrSignUpModal.cy.tsx
@@ -0,0 +1,13 @@
+import { ActiveModal } from "../../app/components/elements/modals/ActiveModal";
+import React from "react";
+import { loginOrSignUpModal } from "../../app/components/elements/modals/LoginOrSignUpModal";
+
+it('Log In or Sign Up Modal should have no visual regressions', () => {
+    // Arrange
+    const modal = loginOrSignUpModal;
+    cy.mountWithStoreAndRouter(<ActiveModal activeModal={modal}/>, ["/"]);
+    cy.get('[data-testid="active-modal"]').should('be.visible');
+        
+    // Assert
+    cy.matchImage();
+});
