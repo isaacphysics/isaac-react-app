c5fb3f94f61bf3f7c2124e5f7b3a810bdb4f63ad
Jaycie Brown <jeab2@cam.ac.uk>
2025-09-11 15:09:47 +0100

    Redirect users to `/dashboard` after teacher upgrade

--- src/app/components/pages/TeacherAccountSelfUpgrade.tsx
+++ src/app/components/pages/TeacherAccountSelfUpgrade.tsx
@@ -3,11 +3,15 @@ import {Button, Col, Container, Row} from "reactstrap";
 import {PageFragment} from "../elements/PageFragment";
 import {TitleAndBreadcrumb} from "../elements/TitleAndBreadcrumb";
 import {
+    mutationSucceeded,
+    requestCurrentUser,
     selectors,
+    useAppDispatch,
     useAppSelector,
     useRequestEmailVerificationMutation,
     useUpgradeToTeacherAccountMutation
 } from "../../state";
+import { useHistory } from "react-router";
 
 
 export const TeacherAccountSelfUpgrade = () => {
@@ -15,10 +19,23 @@ export const TeacherAccountSelfUpgrade = () => {
     const user = useAppSelector(selectors.user.orNull);
     const [emailVerified] = useState(user?.loggedIn && (user.emailVerificationStatus === "VERIFIED"));
     const [sendVerificationEmail] = useRequestEmailVerificationMutation();
+
+    const history = useHistory();
+    const dispatch = useAppDispatch();
+    
     const requestVerificationEmail = () => {
         if (user?.loggedIn && user.email) {
-            sendVerificationEmail({email: user.email});
+            void sendVerificationEmail({email: user.email});
         }
+    };
+
+    const upgrade : React.MouseEventHandler<HTMLButtonElement> = async (event) => {
+        await upgradeToTeacherAccount(event).then(async (response) => {
+            if (mutationSucceeded(response)) {
+                await dispatch(requestCurrentUser()); // Refresh user details locally
+                history.push("/dashboard");
+            }
+        });
     };
 
     return <Container>
@@ -30,7 +47,7 @@ export const TeacherAccountSelfUpgrade = () => {
         <Row>
             <Col className="pt-4 pb-7">
                 <PageFragment fragmentId={"teacher_account_self_upgrade"}></PageFragment>
-                <Button className="my-3" disabled={!emailVerified} onClick={upgradeToTeacherAccount}>Get a teacher account</Button>
+                <Button className="my-3" disabled={!emailVerified} onClick={upgrade}>Get a teacher account</Button>
                 {!emailVerified && <small className="text-danger d-block">Your email address is not verified — please click on the link in the verification email to confirm your email address. You can <Button color="link primary-font-link" onClick={requestVerificationEmail}>request a new verification email</Button> if necessary.</small>}
             </Col>
         </Row>
