66568861099e1101e1fe981e99f43d9dac4a1058
Jaycie Brown <jeab2@cam.ac.uk>
2025-09-11 16:19:54 +0100

    Adjust "total fully correct" text on sci

--- src/app/components/elements/quiz/QuizProgressCommon.tsx
+++ src/app/components/elements/quiz/QuizProgressCommon.tsx
@@ -408,7 +408,12 @@ export function ResultsTable<Q extends QuestionType>({
                     </tbody>
                     <tfoot className="sticky-bottom">
                         <tr>
-                            <th className="sticky-left text-start p-3 fw-bold">Total fully {pageSettings?.attemptedOrCorrect === "CORRECT" ? "correct" : "attempted"}</th>
+                            <th className="sticky-left text-start p-3 fw-bold">
+                                {siteSpecific(
+                                    `No. of students fully ${pageSettings?.attemptedOrCorrect === "CORRECT" ? "correct" : "attempted"}`,
+                                    `Total fully ${pageSettings?.attemptedOrCorrect === "CORRECT" ? "correct" : "attempted"}`
+                                )}
+                            </th>
                             <th/>{/* correct column */}
                             {classAverages.map(([numerator, denominator], index) => (
                                 <td key={index} className={classNames({"selected": index === selectedQuestionIndex})}>
@@ -569,7 +574,12 @@ export function ResultsTablePartBreakdown({
                 </tbody>
                 <tfoot className="sticky-bottom">
                     <tr>
-                        <th className="sticky-left text-start p-3 fw-bold">Total fully {pageSettings?.attemptedOrCorrect === "CORRECT" ? "correct" : "attempted"}</th>
+                        <th className="sticky-left text-start p-3 fw-bold">
+                            {siteSpecific(
+                                `No. of students fully ${pageSettings?.attemptedOrCorrect === "CORRECT" ? "correct" : "attempted"}`,
+                                `Total fully ${pageSettings?.attemptedOrCorrect === "CORRECT" ? "correct" : "attempted"}`
+                            )}
+                        </th>
                         {isPhy && <th/>}{/* correct column */}
                         {classAverages.map(([numerator, denominator], index) => (
                             <td key={index}>{formatMark(numerator, denominator, !!pageSettings?.formatAsPercentage)}</td>
