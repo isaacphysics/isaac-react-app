bc1d5750893bec09ce8450bbe78de22dcc5a41b9
Madhura Bhujbal <60821125+madhurabhujbal@users.noreply.github.com>
2025-09-18 15:06:29 +0100

    Fix the button on competition page (#393)

--- src/app/components/pages/IsaacCompetition/EntryForm/EntryFormHandler.tsx
+++ src/app/components/pages/IsaacCompetition/EntryForm/EntryFormHandler.tsx
@@ -5,7 +5,7 @@ import CompetitionButton from "../Buttons/CompetitionButton";
 import { selectors, useAppSelector } from "../../../../state";
 import { isStudent, isTeacher } from "../../../../services";
 import CompetitionWrapper from "../CompetitionWrapper";
-import { BEFORE_COMPETITION_OPEN_MESSAGE, CLOSED_MESSAGE, STUDENT_MESSAGE, TEACHER_MESSAGE } from "../constants";
+import { CLOSED_MESSAGE, STUDENT_MESSAGE, TEACHER_MESSAGE } from "../constants";
 
 const StudentMessage = () => (
   <Container>
@@ -36,6 +36,12 @@ interface EntryFormHandlerProps {
 const EntryFormHandler = ({ buttons, handleTermsClick }: EntryFormHandlerProps) => {
   const user = useAppSelector(selectors.user.orNull);
 
+  // EOI button configuration - same as HomepageHighlight
+  const eoiButton = {
+    to: "https://forms.office.com/e/23bsQuZfjm",
+    label: "Express your interest",
+  };
+
   const renderEntryForm = () => {
     if (isTeacher(user)) {
       return (
@@ -55,7 +61,7 @@ const EntryFormHandler = ({ buttons, handleTermsClick }: EntryFormHandlerProps) 
       beforeCompetitionOpenContent={
         <Container>
           <Col className="d-flex flex-column align-items-start pb-4 pl-0" xs="auto">
-            <p className="pb-3 body-text">{BEFORE_COMPETITION_OPEN_MESSAGE}</p>
+            <CompetitionButton buttons={[eoiButton]} />
           </Col>
         </Container>
       }
--- src/app/components/pages/IsaacCompetition/dateUtils.ts
+++ src/app/components/pages/IsaacCompetition/dateUtils.ts
@@ -1,12 +1,12 @@
 //Expression of interest banner will be shown till midnight on 2 Nov 2025. Entries open banner will be displayed after this till COMPETITION_END_DATE
 //The competition entry form will be displayed on the Events -> National Competition page after this date.
-export const COMPETITION_OPEN_DATE = new Date("2025-11-02T23:59:59");
+export const COMPETITION_OPEN_DATE = new Date("2025-11-02T23:59:59Z"); // UTC timezone
 
 //The Entries open banner will be displayed till midnight on 31 Jan 2026. Entries closed banner will be displayed after this till ENTRIES_CLOSED_BANNER_END_DATE
-export const COMPETITION_END_DATE = new Date("2026-01-31T23:59:59");
+export const COMPETITION_END_DATE = new Date("2026-01-31T23:59:59Z"); // UTC timezone
 
 //The Entries closed banner will be displayed till midnight on 13 Mar 2026.
-export const ENTRIES_CLOSED_BANNER_END_DATE = new Date("2026-03-13T23:59:59");
+export const ENTRIES_CLOSED_BANNER_END_DATE = new Date("2026-03-13T23:59:59Z"); // UTC timezone
 
 export const FOUR_WEEKS_AFTER_END_DATE = new Date(COMPETITION_END_DATE.getTime() + 4 * 7 * 24 * 60 * 60 * 1000); // 4 weeks after end date
 
