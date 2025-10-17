39b6d03832036bd9c261e10ec9d23768c90b5252
Jaycie Brown <jeab2@cam.ac.uk>
2025-10-17 15:51:34 +0100

    Make Programmes VRT Sci only

--- src/test/pages/Programmes.cy.tsx
+++ src/test/pages/Programmes.cy.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { Programmes } from "../../app/components/pages/Programmes";
+import { isPhy } from "../../app/services";
 
 describe("Programmes page", () => {
     it('should have no visual regressions', () => {
-        cy.mountWithStoreAndRouter(<Programmes />, ['/programmes']);
-        cy.get('[data-testid="loading"]').should('not.exist');
-        cy.matchImage();
+        if (isPhy) {
+            cy.mountWithStoreAndRouter(<Programmes />, ['/programmes']);
+            cy.get('[data-testid="loading"]').should('not.exist');
+            cy.matchImage();
+        }
     });
 });
 
