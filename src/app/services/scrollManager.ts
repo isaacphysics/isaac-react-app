8e0d78fd3eaa31cf37cceb3736a9fa8e252935f0
Jaycie Brown <jeab2@cam.ac.uk>
2025-10-30 14:46:14 +0000

    Adjust scroll on load behaviour to work around iOS bug

--- src/app/services/scrollManager.ts
+++ src/app/services/scrollManager.ts
@@ -28,18 +28,30 @@ export const scrollTopOnPageLoad = (reducedMotion: boolean) => (previousPathname
         }
         
         (window as any).followedAtLeastOneSoftLink = true;
-        try {
-            window.scrollTo({top: 0, left: 0, behavior: reducedMotion ? "instant" : "auto"});
-        } catch {
-            // Some older browsers, notably Safari, don't support the new spec used above!
-            window.scrollTo(0, 0);
-        }
+
+        safeScrollTo({top: 0, left: 0, behavior: reducedMotion ? "instant" : "auto"});
     }
 };
 
 export function scrollVerticallyIntoView(element: Element, offset = 0): void {
     const yPosition = element.getBoundingClientRect().top + window.scrollY + offset;
     if (isDefined(yPosition)) {
-        window.scrollTo(0, yPosition);
+        safeScrollTo(0, yPosition);
     }
+}
+
+// unfortunately window.scrollTo is an overloaded function and inferring the type via Parameters<typeof window.scrollTo> only gives one of the signatures
+// https://stackoverflow.com/questions/74208041/infer-a-parameter-of-an-overload-function
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const safeScrollTo = (...props: any) => {
+    // this function exists to wrap window.scrollTo in both a timeout and a try/catch to fix various browser issues.
+    // timeout: https://stackoverflow.com/questions/24616322/mobile-safari-why-is-window-scrollto0-0-not-scrolling-to-0-0
+    // try-catch: https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollTo compatibility table for Safari options.behaviour
+    setTimeout(() => {
+        try {
+            window.scrollTo(...props);
+        } catch {
+            window.scrollTo(0, 0);
+        }
+    }, 10);
 }
