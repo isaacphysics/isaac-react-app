70cbdd0a6ccd99b2ffe3802476535cbd099ebbec
Barna Magyarkuti <bm710@cam.ac.uk>
2025-10-31 11:07:33 +0000

    extract decorate helper for type-safe overrides

--- src/app/services/scrollManager.ts
+++ src/app/services/scrollManager.ts
@@ -1,4 +1,4 @@
-import {isDefined} from "./";
+import {decorate, isDefined} from "./";
 import { Action } from "history";
 
 const hasPageGroupSpecificScroll = (prevPathname: string | undefined, pathname: string, reducedMotion: boolean): boolean => {
@@ -40,18 +40,15 @@ export function scrollVerticallyIntoView(element: Element, offset = 0): void {
     }
 }
 
-// unfortunately window.scrollTo is an overloaded function and inferring the type via Parameters<typeof window.scrollTo> only gives one of the signatures
-// https://stackoverflow.com/questions/74208041/infer-a-parameter-of-an-overload-function
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const safeScrollTo = (...props: any) => {
-    // this function exists to wrap window.scrollTo in both a timeout and a try/catch to fix various browser issues.
-    // timeout: https://stackoverflow.com/questions/24616322/mobile-safari-why-is-window-scrollto0-0-not-scrolling-to-0-0
-    // try-catch: https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollTo compatibility table for Safari options.behaviour
+// this function exists to wrap window.scrollTo in both a timeout and a try/catch to fix various browser issues.
+// timeout: https://stackoverflow.com/questions/24616322/mobile-safari-why-is-window-scrollto0-0-not-scrolling-to-0-0
+// try-catch: https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollTo compatibility table for Safari options.behaviour
+const safeScrollTo = decorate(window.scrollTo, original => {
     setTimeout(() => {
         try {
-            window.scrollTo(...props);
+            original();
         } catch {
             window.scrollTo(0, 0);
         }
     }, 10);
-};
+});
