12d5fcdfb70f52cd174895874c011ae7cb25ee27
Jaycie Brown <jeab2@cam.ac.uk>
2025-11-03 10:36:36 +0000

    Fix onboarding modal test

--- src/test/pages/TeacherOnboardingModal.cy.tsx
+++ src/test/pages/TeacherOnboardingModal.cy.tsx
@@ -1,11 +1,13 @@
-import { AdaTeacherOnboardingModal } from "../../app/components/elements/modals/AdaTeacherOnboardingModal";
+import { scheduleTeacherOnboardingModalForNextOverviewVisit } from "../../app/components/elements/modals/AdaTeacherOnboardingModal";
+import { Overview } from "../../app/components/pages/Overview";
 import { isAda } from "../../app/services";
 import React from "react";
 
 it('Teacher Onboarding Modal should have no visual regressions', () => {
     if (isAda) {
         // Arrange
-        cy.mountWithStoreAndRouter(<AdaTeacherOnboardingModal/>, ["/dashboard"]);
+        scheduleTeacherOnboardingModalForNextOverviewVisit();
+        cy.mountWithStoreAndRouter(<Overview />, ["/dashboard"]);
         cy.get('[data-testid="active-modal"]').should('be.visible');
         
         // Assert
