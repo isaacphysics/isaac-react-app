18096d5e45e0fda0dd00e599582c0817fa24a32b
Sol Dubock <94075844+sjd210@users.noreply.github.com>
2025-11-06 16:16:45 +0000

    Replace Ada ListView "img" icons with "icon" ones

--- src/app/components/elements/list-groups/ListView.tsx
+++ src/app/components/elements/list-groups/ListView.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { AbstractListViewItem, AbstractListViewItemProps } from "./AbstractListViewItem";
 import { ShortcutResponse, ViewingContext } from "../../../../IsaacAppTypes";
 import { determineAudienceViews } from "../../../services/userViewingContext";
-import { BOOK_DETAIL_ID_SEPARATOR, DOCUMENT_TYPE, documentTypePathPrefix, getThemeFromContextAndTags, HUMAN_STATUS, ISAAC_BOOKS, isAda, isPhy, PATHS, QUESTION_STATUS_TO_ICON, SEARCH_RESULT_TYPE, Subject, TAG_ID, TAG_LEVEL, tags } from "../../../services";
+import { BOOK_DETAIL_ID_SEPARATOR, DOCUMENT_TYPE, documentTypePathPrefix, getThemeFromContextAndTags, HUMAN_STATUS, ISAAC_BOOKS, isAda, isPhy, PATHS, QUESTION_STATUS_TO_ICON, SEARCH_RESULT_TYPE, siteSpecific, Subject, TAG_ID, TAG_LEVEL, tags } from "../../../services";
 import { ListGroup, ListGroupItem, ListGroupProps } from "reactstrap";
 import { AffixButton } from "../AffixButton";
 import { CompletionState, ContentSummaryDTO, GameboardDTO, IsaacWildcard, QuizSummaryDTO } from "../../../../IsaacApiTypes";
@@ -44,10 +44,7 @@ export const QuestionListViewItem = (props : QuestionListViewItemProps) => {
     const state = item.state ?? CompletionState.NOT_ATTEMPTED;
     const icon: TitleIconProps = isPhy
         ? {type: "icon", icon: "icon-question", size: "lg"}
-        : {type: "icon", icon: QUESTION_STATUS_TO_ICON[state], size: "md"};
-
-    if (isAda) icon.alt = classNames(HUMAN_STATUS[state], "question icon");
-    if (isAda && !props.hideIconLabel) icon.label = linkedBoardId ? HUMAN_STATUS[state] : "Question";
+        : {type: "icon", icon: QUESTION_STATUS_TO_ICON[state], size: "md", alt: classNames(HUMAN_STATUS[state], "question icon"), label: linkedBoardId ? HUMAN_STATUS[state] : "Question"};
 
     return <AbstractListViewItem
         {...rest}
@@ -73,10 +70,12 @@ export const ConceptListViewItem = ({item, ...rest}: ConceptListViewItemProps) =
     const itemSubject = getThemeFromContextAndTags(pageSubject, tags.getSubjectTags((item.tags || []) as TAG_ID[]).map(t => t.id));
     const breadcrumb = rest.hasCaret ? getBreadcrumb(item.tags as TAG_ID[]) : undefined;
     const url = `/${documentTypePathPrefix[DOCUMENT_TYPE.CONCEPT]}/${item.id}`;
-
-    const icon: TitleIconProps = isPhy
-        ? {type: "icon", icon: "icon-concept", size: "lg"}
-        : {type: "img", icon: iconPath("concept"), width: "24px", height: "24px", alt: "Concept page icon", label: "Concept"};
+    const icon: TitleIconProps = {type: "icon", icon: "icon-concept", size: siteSpecific("lg", "md")};
+    
+    if (isAda) {
+        icon.label = "Concept";
+        icon.alt = "Concept page icon";
+    }
 
     return <AbstractListViewItem
         icon={icon}
@@ -98,7 +97,7 @@ export const TopicListViewItem = ({item, ...rest}: TopicListViewItemProps) => {
     const itemSubject = getThemeFromContextAndTags(pageSubject, tags.getSubjectTags((item.tags || []) as TAG_ID[]).map(t => t.id));
     const breadcrumb = rest.hasCaret ? getBreadcrumb(item.tags as TAG_ID[]) : undefined;
     const url = `/${documentTypePathPrefix[DOCUMENT_TYPE.TOPIC_SUMMARY]}/${item.id?.slice("topic_summary_".length)}`;
-    const icon: TitleIconProps = {type: "img", icon: iconPath("topic"), width: "24px", height: "24px", alt: "Topic summary page icon", label: "Topic"};
+    const icon: TitleIconProps = {type: "icon", icon: "icon-topic", alt: "Topic summary page icon", label: "Topic"};
 
     return <AbstractListViewItem
         icon={icon}
@@ -225,10 +224,12 @@ export const GenericListViewItem = ({item, ...rest}: GenericListViewItemProps) =
     const audienceViews: ViewingContext[] = determineAudienceViews(item.audience);
     const itemSubject = tags.getSpecifiedTag(TAG_LEVEL.subject, item.tags as TAG_ID[])?.id as Subject;
     const url = `/${documentTypePathPrefix[DOCUMENT_TYPE.GENERIC]}/${item.id}`;
+    const icon: TitleIconProps = {type: "icon", icon: "icon-info", size: siteSpecific("lg", "md")};
 
-    const icon: TitleIconProps = isPhy
-        ? {type: "icon", icon: "icon-info", size: "lg"}
-        : {type: "img", icon: iconPath("info-filled"), width: "24px", height: "24px", alt: "Generic page icon", label: "Info"};
+    if (isAda) {
+        icon.label = "Info";
+        icon.alt = "IGeneric page icon";
+    }
 
     return <AbstractListViewItem
         icon={icon}
--- src/app/components/elements/svg/HexIcon.tsx
+++ src/app/components/elements/svg/HexIcon.tsx
@@ -13,6 +13,6 @@ export const HexIcon = (props: HexIconProps) => {
     const {icon, subject, size, className, ...rest} = props;
     return <div {...rest} className={classNames("d-flex pe-3", className)} {...(subject && {"data-bs-theme": subject})}>
         {isPhy && <div className={`phy-hex-icon icon-${size ?? "xl"}`}/>}
-        <i className={classNames(`icon icon-${size ?? "xl"}`, icon)} alt={props.alt}/>
+        <i className={classNames(`icon icon-${size ?? "xl"}`, icon)} aria-label={props.alt}/>
     </div>;
 };
