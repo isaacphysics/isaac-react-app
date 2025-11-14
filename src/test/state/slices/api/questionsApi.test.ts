6a1e5a6389873c4921fc67c5ad0cfba01dfe9077
Barna Magyarkuti <bm710@cam.ac.uk>
2025-11-14 15:55:33 +0000

    clean up questionsApi tests

--- src/test/state/slices/api/questionsApi.test.ts
+++ src/test/state/slices/api/questionsApi.test.ts
@@ -4,89 +4,98 @@ import { useSearchQuestionsQuery } from "../../../../app/state";
 import { act, waitFor } from "@testing-library/react";
 import { buildMockQuestionFinderResults, buildMockQuestions } from "../../../../mocks/utils";
 import { buildFunctionHandler } from "../../../../mocks/handlers";
-import { HttpHandler } from "msw";
 import { mockQuestionFinderResults } from "../../../../mocks/data";
 import { useState } from "react";
+import { SEARCH_RESULTS_PER_PAGE } from "../../../../app/services";
 
 describe('questions API', () => {
     it('calls the /pages/questions endpoint', async () => {
-        const q0 = nQuestions(0);
-        await renderQuestionsQueryHook(createSearchQuery(), q0.handler);
-        expect(q0.handlerMock).toHaveBeenCalledTimes(1);
+        const { handler } = await renderQuestionsQueryHook(createSearchQuery());
+        expect(handler).toHaveBeenCalledTimes(1);
     });
 
     it('forwards any query parameters', async () => {
-        const q0 = nQuestions(0);
-        await renderQuestionsQueryHook(createSearchQuery({ searchString: "horse" }), q0.handler);
-        expect(q0.handlerMock).toHaveBeenCalledTimes(1);
-        expect(q0.handlerMock).toHaveBeenCalledWith(expect.objectContaining({ searchString: "horse" }));
+        const { handler } = await renderQuestionsQueryHook(createSearchQuery({ searchString: "horse" }));
+        expect(handler).toHaveBeenCalledTimes(1);
+        expect(handler).toHaveBeenCalledWith(expect.objectContaining({ searchString: "horse" }));
     });
 
     it('requests an extra question to determine whether there are more', async () => {
-        const q6 = nQuestions(6);
-        await renderQuestionsQueryHook(createSearchQuery({ limit: 5 }), q6.handler);
-        expect(q6.handlerMock).toHaveBeenCalledTimes(1);
-        expect(q6.handlerMock).toHaveBeenCalledWith(expect.objectContaining({ limit: "6" }));
+        const { handler } = await renderQuestionsQueryHook(createSearchQuery({ limit: 5 }));
+        expect(handler).toHaveBeenCalledTimes(1);
+        expect(handler).toHaveBeenCalledWith(expect.objectContaining({ limit: "6" }));
     });
 
     it('returns one less questions to comply with original request', async () => {
-        const q6 = nQuestions(6);
-        const result = await renderQuestionsQueryHook(createSearchQuery({ limit: 5 }), q6.handler);
-        expect(result.data?.results).toEqual(q6.questions.slice(0, 5));
+        const { response, questions } = await renderQuestionsQueryHook(createSearchQuery({ limit: 5 }));
+        expect(response().data?.results).toEqual(questions.slice(0, 5));
     });
 
-    it('indicates whether there are more results', async () => {
-        const q6 = nQuestions(6);
-        const result = await renderQuestionsQueryHook(createSearchQuery({ limit: 5 }), q6.handler);
-        expect(result.data?.moreResultsAvailable).toBe(true);
+    it('indicates when there are more results', async () => {
+        const { response } = await renderQuestionsQueryHook(createSearchQuery({ limit: 5 }), { totalQuestions: 10});
+        expect(response().data?.moreResultsAvailable).toBe(true);
     });
 
-    it ('uses a default limit of SEARCH_RESULTS_PER_PAGE', async () => {
-        const q35 = nQuestions(35);
-        const result = await renderQuestionsQueryHook(createSearchQuery(), q35.handler);
-        expect(q35.handlerMock).toHaveBeenCalledTimes(1);
-        expect(q35.handlerMock).toHaveBeenCalledWith(expect.objectContaining({ limit: "31" }));
-        expect(result.data?.results).toEqual(q35.questions.slice(0, 30));
-        expect(result.data?.moreResultsAvailable).toBe(true);
+    it('indicates when there are no more results', async () => {
+        const { response } = await renderQuestionsQueryHook(createSearchQuery({ limit: 5 }), { totalQuestions: 5});
+        expect(response().data?.moreResultsAvailable).toBe(false);
+    });
+
+    it('uses a default limit of SEARCH_RESULTS_PER_PAGE', async () => {
+        const { response, handler, questions } = await renderQuestionsQueryHook(
+            createSearchQuery(),
+            { totalQuestions: SEARCH_RESULTS_PER_PAGE + 1}
+        );
+        expect(handler).toHaveBeenCalledTimes(1);
+        expect(handler).toHaveBeenCalledWith(expect.objectContaining({ limit: "31" }));
+        expect(response().data?.results).toEqual(questions.slice(0, SEARCH_RESULTS_PER_PAGE));
+        expect(response().data?.moreResultsAvailable).toBe(true);
     });
 
     describe('when paging through results', () => {
         it('appends the new page to the existing one', async () => {
-            const questions = buildMockQuestions(20, mockQuestionFinderResults);
+            const { questions, response, setQuery } = await renderQuestionsQueryHook(
+                createSearchQuery({ startIndex: 0, limit: 10 }),
+                { totalQuestions: 20 }
+            );
             
-            const handler = buildFunctionHandler('/pages/questions', ['startIndex', 'limit'], ({ startIndex, limit }) => {
-                return buildMockQuestionFinderResults(questions, parseInt(startIndex), parseInt(limit));
-            });
+            expect(response().data?.results).toEqual(questions.slice(0, 10));
+            expect(response().data?.moreResultsAvailable).toBe(true);
 
-            const useTest = () => {
-                const [query, setQuery] = useState<QuestionSearchQuery>(createSearchQuery({ limit: 10 }));
-                const response = useSearchQuestionsQuery(query);
-                return { response, setQuery } as const;
-            };
-            
-            const { result } = renderTestHook(useTest, { extraEndpoints: [handler] });
-            await waitFor(() => expect(result.current.response.isFetching).toBe(false));
-            
-            expect(result.current.response.data?.results).toEqual(questions.slice(0, 10));
-            expect(result.current.response.data?.moreResultsAvailable).toBe(true);
+            await setQuery(createSearchQuery({ startIndex: 10, limit: 10  }));
+            await waitFor(() => expect(response().isFetching).toBe(false));
 
-            await act(async() => result.current.setQuery(createSearchQuery({ limit: 10, startIndex: 10 })));
-            await waitFor(() => expect(result.current.response.isFetching).toBe(false));
-
-            expect(result.current.response.data?.results).toEqual(questions);
-            expect(result.current.response.data?.moreResultsAvailable).toBe(false);
+            expect(response().data?.results?.length).toEqual(20);
+            expect(response().data?.moreResultsAvailable).toBe(false);
         });
     });
 });
 
 
-const renderQuestionsQueryHook = async (query: QuestionSearchQuery, handler: HttpHandler) => {
+const renderQuestionsQueryHook = async (initialQuery: QuestionSearchQuery, { totalQuestions } = { totalQuestions: 10}) => {
+    const questions = buildMockQuestions(totalQuestions, mockQuestionFinderResults);
     const useTest = () => {
-        return useSearchQuestionsQuery(query);
+        const [query, setQuery] = useState<QuestionSearchQuery>(createSearchQuery(initialQuery));
+        const response = useSearchQuestionsQuery(query);
+        return { response, setQuery } as const;
     };
-    const { result } = renderTestHook(useTest, { extraEndpoints: [handler] });
-    await waitFor(() => expect(result.current.isFetching).toBe(false));
-    return result.current;            
+    const handler = jest.fn(({ startIndex, limit } : { startIndex: string, limit: string }) => {
+        return buildMockQuestionFinderResults(questions, parseInt(startIndex), parseInt(limit));
+    });
+            
+    const { result } = renderTestHook(useTest, { extraEndpoints: [
+        buildFunctionHandler('/pages/questions', ['startIndex', 'limit', 'searchString'], handler)
+    ]});
+    await waitFor(() => expect(result.current.response.isFetching).toBe(false));
+    return { 
+        handler, questions,
+        response() {
+            return result.current.response;
+        },
+        async setQuery(q: QuestionSearchQuery) {
+            await act(async() => result.current.setQuery(q));
+        }
+    };            
 };
 
 const createSearchQuery = ({limit, searchString, startIndex = 0} : { limit?: number, searchString?: string, startIndex?: number } = { }): QuestionSearchQuery => {
@@ -95,10 +104,3 @@ const createSearchQuery = ({limit, searchString, startIndex = 0} : { limit?: num
         startIndex, limit, searchString      
     };
 };
-
-const nQuestions = (n: number) => {
-    const questions = buildMockQuestions(n, mockQuestionFinderResults);
-    const handlerMock = jest.fn(() => buildMockQuestionFinderResults(questions, 0));
-    const handler = buildFunctionHandler('/pages/questions', ['limit', 'searchString'], handlerMock );
-    return { handler, handlerMock, questions };  
-}; 
