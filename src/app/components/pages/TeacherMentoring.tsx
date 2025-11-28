ff776314486269c4504490c8af193061183e4421
Alex Lewin <sl2281@cam.ac.uk>
2025-11-28 14:31:11 +0000

    Use multiple page fragments for teacher mentoring page

--- src/app/components/pages/TeacherMentoring.tsx
+++ src/app/components/pages/TeacherMentoring.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { Alert, Col, Container, Row } from "reactstrap";
+import { Col, Container, Row } from "reactstrap";
 import { TitleAndBreadcrumb } from "../elements/TitleAndBreadcrumb";
 import { PageFragment } from "../elements/PageFragment";
 import { Link } from "react-router-dom";
 import { selectors, useAppSelector, useGetGroupMembershipsQuery } from "../../state";
 import { skipToken } from "@reduxjs/toolkit/query";
+import { isTeacherOrAbove } from "../../services";
 
 export const TeacherMentoring = () => {
     const groupId = 3182;
@@ -13,22 +14,25 @@ export const TeacherMentoring = () => {
     const {data: groupMemberships} = useGetGroupMembershipsQuery(userId);
     const userInGroup = groupMemberships?.some(gm => gm.group.id == groupId);
 
-    const groupJoinPrompt = <Alert color={"warning"}> {/* a11y? */}
+    const groupJoinPrompt = <div className="bg-yellow-300 p-4">
+        <span className="icon-help ms-0"/>
         {user?.loggedIn
-            ? <>To participate in the program, join the teacher mentoring group on Ada CS so we can set you questions. You can
+            ? <>To participate in the programme, join the teacher mentoring group on Ada CS so we can set you questions. You can
                 <Link to={"/account?authToken=T33YVV"}> join the group here</Link>.</>
 
-            : <><Link to={"/account?authToken=T33YVV"}>Log in or sign up</Link> to join the teacher mentoring group.</>
+            : <><Link to={"/account?authToken=T33YVV"}>Log in or sign up</Link> to join the teacher mentoring group and participate in the programme.</>
         }
         <p className="mt-2">All of our resources are completely free. You can join (or leave) the programme at any time.</p>
-    </Alert>;
+    </div>;
 
     return <Container>
         <TitleAndBreadcrumb currentPageTitle={"Teacher mentoring programme"} />
         <Row>
             <Col lg={{size: 8, offset: 2}}>
+                <PageFragment fragmentId={"teacher_mentoring_2025_intro"} />
                 {!userInGroup && groupJoinPrompt}
-                <PageFragment fragmentId={"teacher_mentoring_2025_fragment"} />
+                <PageFragment fragmentId={"teacher_mentoring_2025_info"} />
+                {isTeacherOrAbove(user) && <PageFragment fragmentId={"teacher_mentoring_2025_tabs"} />}
             </Col>
         </Row>
     </Container>;
