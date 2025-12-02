b02e4fff2bffc294b5e6cab17d4f109911bcfb94
Jaycie Brown <jeab2@cam.ac.uk>
2025-12-02 11:38:08 +0000

    Use `pathname` over `host` in URL check

--- src/app/services/subject.ts
+++ src/app/services/subject.ts
@@ -5,13 +5,13 @@ if (document.location.host === "localhost:8004" || document.location.host.includ
     id = undefined;
 } else if (document.location.host === "localhost:8003" || document.location.host.includes("adacomputerscience")) {
     id = "computer_science" as TAG_ID;
-} else if (document.location.host.includes("/physics/")) {
+} else if (document.location.pathname.includes("/physics/")) {
     id = TAG_ID.physics;
-} else if (document.location.host.includes("/maths/")) {
+} else if (document.location.pathname.includes("/maths/")) {
     id = TAG_ID.maths;
-} else if (document.location.host.includes("/chemistry/")) {
+} else if (document.location.pathname.includes("/chemistry/")) {
     id = TAG_ID.chemistry;
-} else if (document.location.host.includes("/biology/")) {
+} else if (document.location.pathname.includes("/biology/")) {
     id = TAG_ID.biology;
 } else {
     id = undefined;
