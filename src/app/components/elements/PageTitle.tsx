f796223df183b64b29afb05ce012ec307799bdcb
Sol Dubock <94075844+sjd210@users.noreply.github.com>
2026-01-05 16:56:36 +0000

    Remove unused import

--- src/app/components/elements/PageTitle.tsx
+++ src/app/components/elements/PageTitle.tsx
@@ -1,4 +1,4 @@
-import React, {ReactElement, useEffect, useRef} from "react";
+import React, {ReactElement, useEffect} from "react";
 import {UncontrolledTooltip} from "reactstrap";
 import {
     isAda,
