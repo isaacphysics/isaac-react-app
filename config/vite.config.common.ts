42deb144f643017b98c0f83a0660fbb4d2b96e2a
Jaycie Brown <jeab2@cam.ac.uk>
2026-01-07 13:53:57 +0000

    Update Vite config to build in old format

--- config/vite.config.common.ts
+++ config/vite.config.common.ts
@@ -1,6 +1,6 @@
 import type { Plugin, UserConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import htmlPurge from 'vite-plugin-html-purgecss';
+import { PurgeCSS, UserDefinedSafelist } from 'purgecss';
 import fs from 'fs/promises';
 
 const resolveSiteSpecificIndexPlugin = (site: "sci" | "ada", renderer?: boolean): Plugin => ({
@@ -8,23 +8,58 @@ const resolveSiteSpecificIndexPlugin = (site: "sci" | "ada", renderer?: boolean)
         Vite requires an index.html file at the project root. Since we have two sites and each needs its own index.html,
         we use this plugin to load the appropriate site-specific index.html file to replace the default one.
     */
-    name: 'site-specific-index-html',
+    name: 'site-specific-index-html-plugin',
     enforce: 'pre',
     async transformIndexHtml() {
-        return await fs.readFile(`./public/index-${site}${renderer ? '-renderer' : ''}.html`, 'utf-8');
+        return await fs.readFile(`./index-${site}${renderer ? '-renderer' : ''}.html`, 'utf-8');
     }
 });
 
+// inspired by https://www.npmjs.com/package/vite-plugin-css-sourcemap and https://www.jsdelivr.com/package/npm/vite-plugin-purgecss, 
+const purgeCssPlugin = (safeList?: UserDefinedSafelist): Plugin => {
+    return {
+        name: 'purgecss-plugin',
+        enforce: 'post',
+        async generateBundle(_options, bundle) {
+            const cssFiles = Object.keys(bundle).filter(key => key.endsWith('.css'));
+            if (!cssFiles) return;
+            for (const file of cssFiles) {
+                const css = bundle[file];
+                if (css.type !== "asset") continue;
+                const purged = await new PurgeCSS().purge({
+                    content: ['index*.html', { raw: Object.values(bundle).map(v => { return (v.type === "chunk" ? v.code : ""); }).join("; "), extension: 'js' }],
+                    css: [{raw: css.source.toString()}],
+                    safelist: safeList || [],
+                    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+                });
+                (bundle[file] as any).source = purged[0].css;
+            }
+        }
+    };
+};
+
+const renameIndexPlugin = (site: "sci" | "ada"): Plugin => {
+    return {
+        name: 'rename-index-html-plugin',
+        enforce: 'post',
+        async generateBundle(_options, bundle) {
+            bundle[`index-${site}.html`].fileName = bundle[`index-${site}.html`].fileName.replace(`index-${site}.html`, "index.html");
+        }
+    };
+};
+
 export const generateConfig = (site: "sci" | "ada", renderer?: boolean) => (env: Record<string, any>) => {
     const isRenderer = env['isRenderer'] ?? false;
-    // TODO: rename more phy => sci; bottleneck on nginx config
+    // TODO: rename more phy => sci; bottleneck on router config
     const oldStyleSite = site === "sci" ? "phy" : "ada";
+    const isBuild = env['command'] === 'build';
     
     return {
         plugins: [
-            resolveSiteSpecificIndexPlugin(site, renderer),
+            !isBuild && resolveSiteSpecificIndexPlugin(site, renderer),
             react({}),
-            htmlPurge() as Plugin,
+            purgeCssPlugin(),
+            renameIndexPlugin(site),
         ],
 
         build: {
@@ -33,17 +68,18 @@ export const generateConfig = (site: "sci" | "ada", renderer?: boolean) => (env:
             emptyOutDir: true,
             rollupOptions: {
                 input: {
-                    main: `./public/index-${site}${renderer ? '-renderer' : ''}.html`,
+                    main: `./index-${site}${renderer ? '-renderer' : ''}.html`,
                 },
                 output: {
                     entryFileNames: 'assets/[name].[hash].js',
                     chunkFileNames: 'assets/[name].[hash].js',
                     assetFileNames: 'assets/[name].[hash].[ext]',
-                }
-            }
+                },
+            },
         },
 
         css: {
+            devSourcemap: true,
             preprocessorOptions: {
                 scss: {
                     // https://github.com/twbs/bootstrap/issues/40962 – should be able to remove when Bootstrap 6 is available
