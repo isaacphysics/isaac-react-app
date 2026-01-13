b88db12cb47f3d0f66af6219b8f8b92a0d8c78b3
Sol Dubock <94075844+sjd210@users.noreply.github.com>
2026-01-13 14:34:43 +0000

    Skip assignment progress VRTs

--- src/test/pages/AssignmentProgress.cy.tsx
+++ src/test/pages/AssignmentProgress.cy.tsx
@@ -6,31 +6,31 @@ import { Route, Routes } from "react-router-dom";
 import { RegisteredUserDTO } from "../../IsaacApiTypes";
 
 describe('Assignment progress', () => {
-    it('Groups listing should have no visual regressions', () => {
+    it.skip('Groups listing should have no visual regressions', () => {
         cy.mountWithStoreAndRouter(<AssignmentProgressWithRoute />, [PATHS.ASSIGNMENT_PROGRESS]);
         cy.get('[data-testid="loading"]').should('not.exist');
         cy.matchImage();
     });
 
-    it('Group overview (assignment view) should have no visual regressions', () => {
+    it.skip('Group overview (assignment view) should have no visual regressions', () => {
         cy.mountWithStoreAndRouter(<AssignmentProgressWithRoute />, [`${PATHS.ASSIGNMENT_PROGRESS}/group/2`]);
         cy.get('button').contains("Download assignments CSV").should('exist');
         cy.matchImage();
     });
 
-    it('Group overview (test view) should have no visual regressions', () => {
+    it.skip('Group overview (test view) should have no visual regressions', () => {
         cy.mountWithStoreAndRouter(<AssignmentProgressWithRoute />, [`${PATHS.ASSIGNMENT_PROGRESS}/group/2`]);
         cy.get('button').contains("Tests").click();
         cy.matchImage();
     });
 
-    it('Individual assignment view (Group Overview tab) should have no visual regressions', () => {
+    it.skip('Individual assignment view (Group Overview tab) should have no visual regressions', () => {
         cy.mountWithStoreAndRouter(<AssignmentProgressWithRoute />, [`${PATHS.ASSIGNMENT_PROGRESS}/40`]);
         cy.get('button').contains("Download CSV").should('exist');
         cy.matchImage();
     });
 
-    it('Individual assignment view (Detailed Marks tab) should have no visual regressions', () => {
+    it.skip('Individual assignment view (Detailed Marks tab) should have no visual regressions', () => {
         cy.mountWithStoreAndRouter(<AssignmentProgressWithRoute />, [`${PATHS.ASSIGNMENT_PROGRESS}/40`]);
         cy.get('button').contains("Detailed marks").click();
         cy.get('div.assignment-progress-card > button').first().click('right');
