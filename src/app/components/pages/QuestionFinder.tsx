44ec51b846066590266c6773bb26cd02fadc7303
Sol Dubock <94075844+sjd210@users.noreply.github.com>
2026-01-13 15:28:22 +0000

    Remove some useEffect dependencies

--- src/app/components/pages/QuestionFinder.tsx
+++ src/app/components/pages/QuestionFinder.tsx
@@ -310,7 +310,7 @@ export const QuestionFinder = () => {
         if (randomSeed !== undefined) params.randomSeed = randomSeed.toString();
 
         void navigate({...location, search: queryString.stringify(params, {encode: false})}, {state: location.state, replace: true});
-    }, [searchStages, pageContext?.subject, debouncedSearch, searchQuery, searchTopics, searchExamBoards, searchBooks, searchDifficulties, selections, excludeBooks, searchStatuses, randomSeed, filteringByStatus, navigate, location]);
+    }, [searchStages, pageContext, debouncedSearch, searchQuery, searchTopics, searchExamBoards, searchBooks, searchDifficulties, selections, excludeBooks, searchStatuses, randomSeed, filteringByStatus, navigate, location]);
 
     // Automatically search for content whenever the searchQuery changes, without changing whether filters have been applied or not
     useEffect(() => {
@@ -353,7 +353,8 @@ export const QuestionFinder = () => {
             searchAndUpdateURL();
         }
 
-    }, [searchDifficulties, searchTopics, searchExamBoards, searchStages, searchBooks, excludeBooks, selections, searchStatuses, readingFromUrlParams, searchAndUpdateURL]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [searchDifficulties, searchTopics, searchExamBoards, searchStages, searchBooks, excludeBooks, selections, searchStatuses]);
 
     const clearFilters = useCallback(() => {
         setSearchDifficulties([]);
