5931f04e52b48f71dc0562e49ad4e7c693806579
Jaycie Brown <jeab2@cam.ac.uk>
2026-01-14 12:49:41 +0000

    Undo logic move into 202
    I was misinformed of what responses could be generated 😔

--- src/app/state/actions/index.tsx
+++ src/app/state/actions/index.tsx
@@ -290,12 +290,12 @@ export const logInUser = (provider: AuthenticationProvider, credentials: Credent
                 dispatch(requestCurrentUser() as any);
                 return;
             }
-            
-            // requestCurrentUser gives us extra information like auth settings, preferences and time until session expiry
-            dispatch(requestCurrentUser() as any);
-            dispatch({type: ACTION_TYPE.USER_LOG_IN_RESPONSE_SUCCESS, authResponse: result.data});
-            continueToAfterAuthPath(result.data);
         }
+
+        // requestCurrentUser gives us extra information like auth settings, preferences and time until session expiry
+        dispatch(requestCurrentUser() as any);
+        dispatch({type: ACTION_TYPE.USER_LOG_IN_RESPONSE_SUCCESS, authResponse: result.data});
+        continueToAfterAuthPath(result.data);
     } catch (e: any) {
         dispatch({type: ACTION_TYPE.USER_LOG_IN_RESPONSE_FAILURE, errorMessage: extractMessage(e)});
     }
