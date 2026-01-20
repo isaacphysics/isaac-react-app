7382cfc1c23ffc83c607db1b23d8eee422768aaa
Sol Dubock <94075844+sjd210@users.noreply.github.com>
2026-01-20 11:45:04 +0000

    Warm up every file in the cypress folder

--- config/vite.config.common.ts
+++ config/vite.config.common.ts
@@ -102,7 +102,7 @@ export const generateConfig = (site: "sci" | "ada", renderer?: boolean) => (env:
         
         server: {
             warmup: {
-                clientFiles: ['**/*'],
+                clientFiles: ['cypress/**/*'],
             }
         },
 
