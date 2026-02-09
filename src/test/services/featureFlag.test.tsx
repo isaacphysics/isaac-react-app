6df488d8320f07ddd39b595cd0748c5c8c3e1ac7
Jaycie Brown <jeab2@cam.ac.uk>
2026-02-09 10:58:38 +0000

    Limit scope of provider in test environments

--- src/test/services/featureFlag.test.tsx
+++ src/test/services/featureFlag.test.tsx
@@ -1,47 +1,45 @@
-import { FeatureFlag, FeatureFlagWrapper, useFeatureFlag } from "../../app/services/featureFlag";
-import React from "react";
+import { FeatureFlag, FeatureFlagProvider, FeatureFlagWrapper, useFeatureFlag } from "../../app/services/featureFlag";
+import React, { ReactNode } from "react";
 import { screen, waitFor } from "@testing-library/react";
 import { renderTestEnvironment, renderTestHook, waitForLoaded } from "../testUtils";
 import { http } from "msw/core/http";
 import { API_PATH } from "../../app/services";
-import { HttpResponse } from "msw";
+import { handlerThatReturns } from "../../mocks/handlers";
 
 describe('Feature flag service', () => {
-    const setEnviornment = (segueEnvironment: string) => [
-        http.get(API_PATH + "/info/segue_environment", () => {
-            return HttpResponse.json({segueEnvironment}, { status: 200 });
-        }),
-    ];
+    const environmentHandler = (segueEnv: string) => handlerThatReturns({data: {segueEnvironment: segueEnv}, status: 200 });
 
     describe('FeatureFlagWrapper', () => {
-        const render =  async <P,> (
-            Component: React.FC<P>,
-            { segueEnvironment } : { segueEnvironment: "DEV" | "PROD"}
-        ) => {
+        const renderWithFlagProvider = async (Component: ReactNode, { segueEnvironment }: { segueEnvironment: string }) => {
             await renderTestEnvironment({
-                PageComponent: Component,
-                initalRouteEntries: ['/some_path'],
-                extraEndpoints: setEnviornment(segueEnvironment)
+                PageComponent: () => <FeatureFlagProvider>
+                    {Component}
+                </FeatureFlagProvider>,
+                extraEndpoints: [
+                    http.get(API_PATH + "/info/segue_environment", environmentHandler(segueEnvironment)),
+                ]
             });
             await waitForLoaded();
         };
 
         describe('one-way interface', () => {
-            const WhenTestOn = () => <div> Test ON</div>;
-
             it('shows its child when the flag condition is met', async () => {
-                await render(() => <FeatureFlagWrapper flag={FeatureFlag.TEST_FEATURE}>
-                    <WhenTestOn/>
-                </FeatureFlagWrapper>, { segueEnvironment: "DEV" }
+                await renderWithFlagProvider(
+                    <FeatureFlagWrapper flag={FeatureFlag.TEST_FEATURE}>
+                        <>Test ON</>
+                    </FeatureFlagWrapper>,
+                    { segueEnvironment: "DEV" }
                 );
 
                 expect(screen.queryByText("Test ON")).toBeInTheDocument();
             });
         
             it('does not show the child when the flag condition is not met', async () => {
-                await render(() => <FeatureFlagWrapper flag={FeatureFlag.TEST_FEATURE}>
-                    <WhenTestOn/>
-                </FeatureFlagWrapper>, { segueEnvironment: "PROD" }
+                await renderWithFlagProvider(
+                    <FeatureFlagWrapper flag={FeatureFlag.TEST_FEATURE}>
+                        <>Test ON</>
+                    </FeatureFlagWrapper>,
+                    { segueEnvironment: "PROD" }
                 );
 
                 expect(screen.queryByText("Test ON")).not.toBeInTheDocument();
@@ -50,20 +48,26 @@ describe('Feature flag service', () => {
 
         describe('two-way interface', () => {
             it('shows the onSet component when the flag condition is met', async () => {
-                await render(() => <FeatureFlagWrapper flag={FeatureFlag.TEST_FEATURE}
-                    onSet={<>Test ON</>}
-                    onUnset={<>Test OFF</>}
-                />, { segueEnvironment: "DEV" }
+                await renderWithFlagProvider(
+                    <FeatureFlagWrapper 
+                        flag={FeatureFlag.TEST_FEATURE}
+                        onSet={<>Test ON</>}
+                        onUnset={<>Test OFF</>}
+                    />,
+                    { segueEnvironment: "DEV" }
                 );
 
                 expect(await screen.findByText("Test ON")).toBeInTheDocument();
             });
 
             it('shows the onUnset component when the flag condition is not met', async () => {
-                await render(() => <FeatureFlagWrapper flag={FeatureFlag.TEST_FEATURE}
-                    onSet={<>Test ON</>}
-                    onUnset={<>Test OFF</>}
-                />, { segueEnvironment: "PROD" }
+                await renderWithFlagProvider(
+                    <FeatureFlagWrapper 
+                        flag={FeatureFlag.TEST_FEATURE}
+                        onSet={<>Test ON</>}
+                        onUnset={<>Test OFF</>}
+                    />,
+                    { segueEnvironment: "PROD" }
                 );
 
                 expect(await screen.findByText("Test OFF")).toBeInTheDocument();
@@ -73,16 +77,30 @@ describe('Feature flag service', () => {
 
     describe('useFeatureFlag hook', () => {
         it('flag is enabled when the flag condition is met', async () => {
-            const { result } = renderTestHook(() => useFeatureFlag(FeatureFlag.TEST_FEATURE), {
-                extraEndpoints: setEnviornment("DEV")
-            });
+            const { result } = renderTestHook(
+                () => useFeatureFlag(FeatureFlag.TEST_FEATURE),
+                {
+                    Wrapper: ({ children }: { children: React.ReactNode }) => <FeatureFlagProvider>{children}</FeatureFlagProvider>,
+                    extraEndpoints: [
+                        http.get(API_PATH + "/info/segue_environment", environmentHandler("DEV")),
+                    ]
+                }
+            );
+
             await waitFor(() => expect(result.current).toBe(true));
         });
 
-        it('flag is enabled when at least one of the flags is enabled', async () => {
-            const { result } = renderTestHook(() => useFeatureFlag(["OTHER" as FeatureFlag, FeatureFlag.TEST_FEATURE]), {
-                extraEndpoints: setEnviornment("DEV")
-            });
+        it('flag is enabled when at least one of the flags are enabled', async () => {
+            const { result } = renderTestHook(
+                () => useFeatureFlag(["OTHER" as FeatureFlag, FeatureFlag.TEST_FEATURE]),
+                {
+                    Wrapper: ({ children }: { children: React.ReactNode }) => <FeatureFlagProvider>{children}</FeatureFlagProvider>,
+                    extraEndpoints: [
+                        http.get(API_PATH + "/info/segue_environment", environmentHandler("DEV")),
+                    ]
+                }
+            );
+
             await waitFor(() => expect(result.current).toBe(true));
         });
 
