ab7cafcb2b3c695195a9dd96a21f81a6592db310
Jaycie Brown <jeab2@cam.ac.uk>
2026-02-24 09:56:14 +0000

    Add spacing to "Get a different question" button

--- src/app/components/pages/SubjectLandingPage.tsx
+++ src/app/components/pages/SubjectLandingPage.tsx
@@ -64,7 +64,7 @@ const RandomQuestionBanner = ({context}: {context?: PageContextState}) => {
     return <div  className="d-flex flex-column pb-4 container-override random-question-panel" >
         <div className="d-flex my-3 justify-content-between align-items-center">
             <h4 className="m-0">Try a random question!</h4>
-            <button className="btn btn-link invert-underline d-flex align-items-center gap-2" onClick={handleGetDifferentQuestion}>
+            <button className="btn btn-link invert-underline d-flex align-items-center gap-2 p-1" onClick={handleGetDifferentQuestion}>
                 Get a different question
                 <i className="icon icon-refresh icon-color-black"/>
             </button>
