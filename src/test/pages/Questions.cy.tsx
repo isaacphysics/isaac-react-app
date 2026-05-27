b0e8970d753c7f970428d616b76a37bf336c472a
Jaycie Brown <jeab2@cam.ac.uk>
2026-05-27 12:23:21 +0100

    Avoid using click (which scrolls by default) to blur last accordion

--- src/test/pages/Questions.cy.tsx
+++ src/test/pages/Questions.cy.tsx
@@ -12,14 +12,13 @@ it('Question types\' regression test page should have no visual regressions', ()
     // open all accordions (that weren't already open)
     cy.get('.isaac-accordion > button.accordion-header:not(.active)').each(($el) => {
         cy.wrap($el).scrollIntoView();
-        cy.wrap($el).click();
+        cy.wrap($el).click({scrollBehavior: false});
     });
 
-    cy.get('body').click(0, 0);
-
+    cy.wait(500);
+    cy.focused().blur();
     cy.scrollTo('top');
-
-    cy.wait(1000);
+    cy.wait(500);
 
     cy.matchImage();
 });
