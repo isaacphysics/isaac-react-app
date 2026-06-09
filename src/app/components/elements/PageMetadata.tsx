2c2c4ed77658fdae8f401a34e466b93b4fd85293
Jaycie Brown <jeab2@cam.ac.uk>
2026-06-09 10:50:39 +0100

    Prevent share link's float clipping page width if no toptext

--- src/app/components/elements/PageMetadata.tsx
+++ src/app/components/elements/PageMetadata.tsx
@@ -50,7 +50,7 @@ interface ActionButtonsProps extends React.HTMLAttributes<HTMLDivElement> {
 export const ActionButtons = ({location, isQuestion, helpModalId, doc, ...rest}: ActionButtonsProps) => {
     const deviceSize = useDeviceSize();
 
-    const anyActionButtonShown = isPhy && helpModalId || above['sm'](deviceSize) || doc?.id;
+    const anyActionButtonShown = isPhy && helpModalId || (above['sm'](deviceSize) && (doc || isPhy)) || doc?.id;
 
     return anyActionButtonShown && <div {...rest} className={classNames("d-flex no-print gap-2", rest.className)}>
         {isPhy && isQuestion && <FeatureFlagWrapper flag={FeatureFlag.ENABLE_SCI_BOOKMARKS}>
@@ -59,7 +59,7 @@ export const ActionButtons = ({location, isQuestion, helpModalId, doc, ...rest}:
         }
         {isPhy && helpModalId && <HelpButton modalId={helpModalId} />}
         {above['sm'](deviceSize) && <>
-            <ShareLink linkUrl={location.pathname + location.hash} clickAwayClose />
+            {(doc || isPhy) && <ShareLink linkUrl={location.pathname + location.hash} clickAwayClose />}
             {doc && <PrintButton questionPage={isQuestion} />} {/* don't show print for internal (non content-driven) pages */}
         </>}
         {doc?.id && <ReportButton pageId={doc.id} />}
