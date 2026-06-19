9cdcc20781820876876022121c878abef9e17f36
Alex Lewin <sl2281@cam.ac.uk>
2026-06-19 11:16:06 +0100

    Don't add empty book url params

--- src/app/components/pages/Gameboard.tsx
+++ src/app/components/pages/Gameboard.tsx
@@ -48,7 +48,8 @@ export const Gameboard = () => {
 
     const urlParams = new URLSearchParams(location.search);
     const linkedBookSection = [urlParams.get("book") ?? "", urlParams.get("section") ?? ""];
-    const linkedBookSectionUrlParams = `?book=${encodeURIComponent(linkedBookSection[0])}&section=${encodeURIComponent(linkedBookSection[1])}`;
+    const hasLinkedBookSection = linkedBookSection && linkedBookSection[0] && linkedBookSection[1];
+    const linkedBookSectionUrlParams = hasLinkedBookSection ? `?book=${encodeURIComponent(linkedBookSection[0])}&section=${encodeURIComponent(linkedBookSection[1])}` : "";
 
     const questionThemes = gameboard?.contents?.map(q => getThemeFromTags(q.tags)).filter((v, i, a) => a.indexOf(v) === i);
     const singleSubject = questionThemes?.length === 1 ? questionThemes[0] : undefined;
