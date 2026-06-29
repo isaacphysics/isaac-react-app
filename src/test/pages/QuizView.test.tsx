57c4ff1729aa34270bea476ad47beefa76487186
Jaycie Brown <jeab2@cam.ac.uk>
2026-06-29 11:59:52 +0100

    Fix remaining quiz tests

--- src/test/pages/QuizView.test.tsx
+++ src/test/pages/QuizView.test.tsx
@@ -1,16 +1,15 @@
 import {expectH1, expectH4, expectLink, expectUrl} from "../testUtils";
 import {mockRubrics} from "../../mocks/data";
 import {
-    editButton,
     expectAdaBreadCrumbs,
     expectErrorMessage,
     expectPhyBreadCrumbs,
     expectRubric,
     expectMobileSidebarToggleToHaveText,
-    previewButton,
+    previewTestButtonLocator,
     renderQuizPage,
     quizSidebarCommonTests,
-    testSectionsHeader
+    testSectionsHeaderLocator
 } from "../helpers/quiz";
 import {isPhy, siteSpecific} from "../../app/services";
 import {screen} from "@testing-library/react";
@@ -25,8 +24,8 @@ describe("QuizView", () => {
     it('shows quiz title on the breadcrumbs', async () => {
         await studentViewsQuiz();
         siteSpecific(
-            () => expectPhyBreadCrumbs({href: "/view_tests", text: "Practice tests"}),
-            () => expectAdaBreadCrumbs([{href: '/', text: "Home"}, {href: "/view_tests", text: "Practice tests"}, rubric.title])
+            () => expectPhyBreadCrumbs({href: "/view_tests", text: "View tests"}),
+            () => expectAdaBreadCrumbs([{href: '/', text: "Home"}, {href: "/view_tests", text: "View tests"}, rubric.title])
         )();
     });
 
@@ -47,44 +46,20 @@ describe("QuizView", () => {
 
     it("does not show Test sections", async () => {
         await studentViewsQuiz();
-        expect(testSectionsHeader()).toBe(null);
+        expect(testSectionsHeaderLocator()).toBe(null);
     });
 
-    it('shows "Take Test" button that loads the attempts page and allows navigating back', async () => {
+    it('shows "Take Test" button that loads the attempts page', async () => {
         await studentViewsQuiz();
         await expectLink("Take Test", `/test/attempt/${quizId}`);
     });
 
-    it('does not show "Preview" button', async() => {
+    it('does not show "Preview questions" button', async() => {
         await studentViewsQuiz();
-        expect(previewButton()).toBe(null);
+        expect(previewTestButtonLocator()).toBe(null);
     });
 
-    describe('for teachers', () => {
-        const teacherViewsQuiz = () => renderQuizView({ role: 'TEACHER', quizId });
-
-        it('shows "Preview" button that loads the preview page and allows navigating back', async () => {
-            await teacherViewsQuiz();
-            await expectLink("Preview", `/test/preview/${quizId}`);
-        });
-    });
-
-    describe('for content editors', () => {
-        const editorViewsQuiz = () => renderQuizView({ role: 'TEACHER', quizId });
-
-        // It'd be more consistent with, eg. the `/preview` page to show the edit button.
-        // However, we'd need `canonicalSourceFile` for this, which the `/rubric` endpoint doesn't return.
-        // For now, James suggested this was not worth the effort.
-        it('does not show edit button', async () => {
-            await editorViewsQuiz();
-            expect(editButton()).toBe(null);
-        });
-
-        it('shows "Preview" button that loads the preview page and allows navigating back', async () => {
-            await editorViewsQuiz();
-            await expectLink("Preview", `/test/preview/${quizId}`);
-        });
-    });
+    // Teachers should never be directed to the /view page. If they end up there, they see the same as students.
 
     describe('for unregistered users', () => {
         const anonymousViewsMissingQuiz = () => renderQuizView({ role: 'ANONYMOUS', quizId: 'some_non_existent_test'});
@@ -101,8 +76,8 @@ describe("QuizView", () => {
         it ('shows Unknown Test on breadcrumbs', async () => {
             await studentViewsMissingQuiz();
             siteSpecific(
-                () => expectPhyBreadCrumbs({href: '/view_tests', text: "Practice tests"}),
-                () => expectAdaBreadCrumbs([{href: '/', text: "Home"}, {href: "/view_tests", text: "Practice tests"}, "Unknown Test"])
+                () => expectPhyBreadCrumbs({href: '/view_tests', text: "View tests"}),
+                () => expectAdaBreadCrumbs([{href: '/', text: "Home"}, {href: "/view_tests", text: "View tests"}, "Unknown Test"])
             )();
         });
 
