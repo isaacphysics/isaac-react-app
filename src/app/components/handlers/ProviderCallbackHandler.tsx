5a4d1470f6ffdd27a78015fcd17803282770dc66
Jaycie Brown <jeab2@cam.ac.uk>
2026-07-17 17:11:22 +0100

    Ensure Google login callback fn is only run once

--- src/app/components/handlers/ProviderCallbackHandler.tsx
+++ src/app/components/handlers/ProviderCallbackHandler.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useCallback, useEffect} from 'react';
 import {handleProviderCallback, useAppDispatch} from "../../state";
 import {AuthenticationProvider} from "../../../IsaacApiTypes";
 import {IsaacSpinner} from "./IsaacSpinner";
@@ -9,7 +9,18 @@ export const ProviderCallbackHandler = () => {
     const location = useLocation();
     const {provider} = useParams<{provider: AuthenticationProvider}>();
     const dispatch = useAppDispatch();
-    useEffect(() => provider && void handleProviderCallback(dispatch, navigate, provider, location.search), [dispatch, navigate, provider, location.search]);
+
+    const handleProviderCallbackOnLoad = useCallback(() => {
+        if (provider) {
+            void handleProviderCallback(dispatch, navigate, provider, location.search);
+        }
+    }, [dispatch, navigate, provider, location.search]);
+
+    useEffect(() => {
+        // avoid strict mode issues via event handler; handleProviderCallback *must* be called only once
+        window.addEventListener('load', handleProviderCallbackOnLoad);
+        return () => window.removeEventListener('load', handleProviderCallbackOnLoad);
+    }, [handleProviderCallbackOnLoad]);
 
     return <React.Fragment>
         <div className="w-100 text-center">
