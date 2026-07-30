01e3849507750056b3c65c8d68b74fda1ebef9ff
Sol Dubock <94075844+sjd210@users.noreply.github.com>
2026-07-30 09:26:02 +0100

    Correct banner from merge conflict

--- src/app/components/elements/alerts/InaccessibleContentWarningAlert.tsx
+++ src/app/components/elements/alerts/InaccessibleContentWarningAlert.tsx
@@ -1,11 +1,33 @@
 import React from 'react';
 import { Alert } from 'reactstrap';
-import { ACCESSIBILITY_WARNINGS } from '../../../services/accessibility';
-import { isAda } from '../../../services';
+import { ACCESSIBILITY_WARNINGS, useDragAndDropAccessibility } from '../../../services/accessibility';
+import { Spacer } from '../Spacer';
+import { selectors, useAppSelector } from '../../../state';
+import StyledToggle from '../inputs/StyledToggle';
+
+export const DragAndDropInputModeToggle = () => {
+    const { dragAndDropEnabled, toggleDragAndDropEnabled } = useDragAndDropAccessibility();
+
+    return <div className="input-mode-toggle">
+        <span>Question input mode</span>
+        <Spacer />
+        <StyledToggle
+            checked={dragAndDropEnabled}
+            falseLabel="Dropdown"
+            trueLabel="Drag and drop"
+            onChange={(e) => {toggleDragAndDropEnabled(); e.stopPropagation();}}
+        />
+    </div>
+};
 
 export const InaccessibleContentWarningAlert = ({type}: {type: keyof typeof ACCESSIBILITY_WARNINGS}) => {
-    return <Alert color="warning" className="d-flex no-print my-2 align-items-center">
-        {isAda && <i className={`icon icon-md ${ACCESSIBILITY_WARNINGS[type].icon} icon-access-visual icon-color-black me-2 mb-1`} />}
+    const pageContainsClozeOrDragAndDropQuestion = useAppSelector(selectors.questions.includesClozeOrDragAndDropQuestion);
+
+    return <Alert color="warning" className="no-print d-flex align-items-center my-2">
         {ACCESSIBILITY_WARNINGS[type].description}
+        {pageContainsClozeOrDragAndDropQuestion && <>
+            <Spacer/>
+            <DragAndDropInputModeToggle/>
+        </>}
     </Alert>;
 };
