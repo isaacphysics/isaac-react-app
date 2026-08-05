d0691de780cf558320e2cfafd9866887833f59a7
Jaycie Brown <jeab2@cam.ac.uk>
2026-08-05 15:55:06 +0100

    Use consistent button texts across age verification paths

--- src/app/components/pages/RegistrationAgeCheckFailed.tsx
+++ src/app/components/pages/RegistrationAgeCheckFailed.tsx
@@ -37,7 +37,7 @@ export const RegistrationAgeCheckFailed = () => {
                         <Button className="mt-2 w-100" color="keyline" onClick={() => navigate(-1)}>Back</Button>
                     </Col>}
                     <Col sm={6} lg={4}>
-                        <Button className="mt-2 w-100" color="solid" onClick={returnToHomepage}>{siteSpecific("Take me back", "Back to the site")}</Button>
+                        <Button className="mt-2 w-100" color="solid" onClick={returnToHomepage}>{siteSpecific("Take me back", "Go home")}</Button>
                     </Col>
                 </Row>
             </CardBody>
