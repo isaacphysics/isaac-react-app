48b37c3dd3243b8c3b195e8a8b3322802bc881dc
Yoan-Daniel Malinov <yoan.malinov@raspberrypi.org>
2026-08-11 16:05:41 +0100

    Improve accessibility by adding aria labels

--- src/app/components/elements/SearchInputs.tsx
+++ src/app/components/elements/SearchInputs.tsx
@@ -64,14 +64,14 @@ function withSearch(Component: React.FC<SearchInputProps>) {
 }
 
 const PhysicsSearchButton = () => {
-    return <button aria-label='search' className='d-flex bg-transparent search-button justify-content-center align-items-center'>
+    return <button aria-label='Search' className='d-flex bg-transparent search-button justify-content-center align-items-center'>
         <i className="icon icon-search icon-color-black"/>
         <span className='visually-hidden'>Search</span>
     </button>;
 };
 
 const AdaSearchButton = () => (
-    <Button color="link" aria-label="search" className="search-button">
+    <Button color="link" aria-label="Search" className="search-button">
         <svg xmlns='http://www.w3.org/2000/svg' width='36' height='36' viewBox='0 0 36 36'>
             <g fill='none' fillRule='evenodd'>
                 <path fill='#000' d='M27.5 25.333l-4.43-4.396a6.82 6.82 0 0 0 1.325-4.045c0-3.8-3.116-6.892-6.948-6.892-3.83 0-6.947 3.092-6.947 6.892 0 3.801 3.117 6.892 6.947 6.892a6.95 6.95 0 0 0 3.918-1.203L25.82 27l1.681-1.667zm-14.962-8.44c0-2.687 2.203-4.872 4.91-4.872 2.708 0 4.91 2.185 4.91 4.871 0 2.686-2.202 4.871-4.91 4.871s-4.91-2.185-4.91-4.87z' />
@@ -120,7 +120,7 @@ export const AdaHeaderSearch = withSearch(({inputProps, setSearchText, searchTex
                 id="header-search" {...inputProps}
                 value={searchText} onChange={setSearchTextAsValue}
             />
-            <button className="d-flex align-items-center justify-content-center">
+            <button className="d-flex align-items-center justify-content-center" aria-label="Search" >
                 <i className="icon icon-search icon-color-jet"/>
             </button>
         </InputGroup>
