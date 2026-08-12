89de14ac0bb7c7c95ceb1f330c156f99e34be954
Barna Magyarkuti <bm710@cam.ac.uk>
2026-08-12 14:16:12 +0100

    add VRT for LogInOrSignUpModal

---
+++ src/test/pages/LogInOrSignUpModal.cy.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import { persistence, KEY, ACTION_TYPE } from "../../app/services";
+import { ActiveModals } from "../../app/components/elements/modals/ActiveModals";
+import {store} from "../../app/state";
+
+it('LogInOrSignUpModal should have no visual regressions', () => {
+    // Arrange
+    cy.stub(persistence.session, 'load').withArgs(KEY.FIRST_ANON_QUESTION).returns("some_question_id");
+
+    // Act
+    cy.mountWithStoreAndRouter(<ActiveModals/>, ["/"]);
+    store.dispatch({ type: ACTION_TYPE.QUESTION_ATTEMPT_REQUEST });    
+    cy.get('[data-testid="active-modal"]').should('be.visible');
+            
+    // Assert
+    cy.get('[data-testid="active-modal"]').matchImage();
+});
