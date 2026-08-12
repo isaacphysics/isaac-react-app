2a17560b2cf183284b9d17977b9d4c8cf1baf41a
Barna Magyarkuti <bm710@cam.ac.uk>
2026-08-12 16:03:08 +0100

    test modal on multiple screen sizes

--- src/test/pages/LogInOrSignUpModal.cy.tsx
+++ src/test/pages/LogInOrSignUpModal.cy.tsx
@@ -4,15 +4,18 @@ import { persistence, KEY, ACTION_TYPE } from "../../app/services";
 import { ActiveModals } from "../../app/components/elements/modals/ActiveModals";
 import {store} from "../../app/state";
 
-it('LogInOrSignUpModal should have no visual regressions', () => {
+[500, 900, 1300].forEach(size => 
+    it(`LogInOrSignUpModal should have no visual regressions ${size}`, () => {
     // Arrange
-    cy.stub(persistence.session, 'load').withArgs(KEY.FIRST_ANON_QUESTION).returns("some_question_id");
+        cy.stub(persistence.session, 'load').withArgs(KEY.FIRST_ANON_QUESTION).returns("some_question_id");
 
-    // Act
-    cy.mountWithStoreAndRouter(<div style={{height: "1200px"}}><ActiveModals /></div>, ["/"]);
-    store.dispatch({ type: ACTION_TYPE.QUESTION_ATTEMPT_REQUEST });
+        // Act
+        cy.mountWithStoreAndRouter(<div style={{height: "1200px"}}><ActiveModals /></div>, ["/"]);
+        store.dispatch({ type: ACTION_TYPE.QUESTION_ATTEMPT_REQUEST });
             
-    // Assert
-    cy.get('body').invoke('css', 'transform', 'scale(1)');
-    cy.get('[data-testid="active-modal"]').matchImage();
-});
+        // Assert
+        cy.viewport(size, 750);
+        cy.get('body').invoke('css', 'transform', 'scale(1)');
+        cy.get('[data-testid="active-modal"]').matchImage();
+    })
+);
