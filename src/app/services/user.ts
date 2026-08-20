113e278bd7dc896b51fe3bde14b88eae5943c5e8
Jaycie Brown <jeab2@cam.ac.uk>
2026-08-20 12:08:45 +0100

    Simplify under 13 check

--- src/app/services/user.ts
+++ src/app/services/user.ts
@@ -79,8 +79,7 @@ export const roleRequirements: Record<UserRole, (u: {readonly role?: UserRole, r
 };
 
 export const isUnder13 = (user?: Immutable<PotentialUser> | {emailVerificationStatus?: string} | null) => {
-    if (!user) return false;
-    return user && 'loggedIn' in user && user.loggedIn && user.emailVerificationStatus === "AGE_RESTRICTED";
+    return !!user && 'loggedIn' in user && user.loggedIn && user.emailVerificationStatus === "AGE_RESTRICTED";
 };
 
 export function extractTeacherName(teacher: {readonly givenName?: string; readonly familyName?: string} | null | undefined): string | null {
