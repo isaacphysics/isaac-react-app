3386f8830f97f73503186238c8d3a58adfbc0ace
Sol Dubock <94075844+sjd210@users.noreply.github.com>
2026-08-21 14:29:49 +0100

    Replace .h* where styling rules are overridden

--- src/app/components/site/phy/NavigationMenuPhy.tsx
+++ src/app/components/site/phy/NavigationMenuPhy.tsx
@@ -142,7 +142,7 @@ const ContentNavAccordionWrapper = (props : ContentNavWrapperProps) => {
 
     return <Accordion className="explore-group mb-3" open={open} toggle={toggle} tag={"li"}>
         <AccordionItem>
-            <AccordionHeader targetId="1" tag={"h5"}>
+            <AccordionHeader targetId="1" tag={"div"}>
                 {title}
             </AccordionHeader>
             <AccordionBody accordionId="1" {...rest}>
@@ -247,7 +247,7 @@ const ContentNavSection = (props: NavigationSectionProps) => {
                     <StaticNavigationDropdown title={title} {...rest}>
                         {categories?.map((category, i) => {
                             return <div key={i}>
-                                <div className="px-4 m-0 py-2 h5">{category.title}</div>
+                                <div className="nav-list-title px-4 m-0 py-2">{category.title}</div>
                                 <ul className="plain-list">
                                     {category.subcategories.map((subcategory, j) => {
                                         return <NavigationItem key={j} className="align-items-center" href={subcategory.href} data-bs-theme={subcategory.subject}>
@@ -298,7 +298,7 @@ const ContentNavProfile = ({toggleMenu}: {toggleMenu: () => void}) => {
                     <div className="d-flex flex-column flex-grow-1">
 
                         {deviceSize === "xs" && isTutorOrAbove(user) && <>
-                            <div className="mt-2 h5">ACCOUNT</div>
+                            <div className="nav-list-title mt-2">ACCOUNT</div>
                             <ul className="plain-list">
                                 <NavigationItemClose href="/account">
                                     My account
@@ -307,7 +307,7 @@ const ContentNavProfile = ({toggleMenu}: {toggleMenu: () => void}) => {
                             <div className="section-divider" />
                         </>}
 
-                        {isTutorOrAbove(user) && <div className="pt-2 pt-sm-0 h5">STUDENT</div>}
+                        {isTutorOrAbove(user) && <div className="nav-list-title pt-2 pt-sm-0">STUDENT</div>}
                         <ul className="plain-list flex-grow-1">
                             <NavigationItemClose href={PATHS.MY_GAMEBOARDS}>
                                 My saved decks
@@ -333,7 +333,7 @@ const ContentNavProfile = ({toggleMenu}: {toggleMenu: () => void}) => {
                                 ? <>
                                     <div className="section-divider me-n2" />
                                     <Spacer />
-                                    <div className="mt-2 h5">ACCOUNT</div>
+                                    <div className="nav-list-title mt-2">ACCOUNT</div>
                                 </>
                                 : <div className="section-divider" />
                             }
@@ -348,7 +348,7 @@ const ContentNavProfile = ({toggleMenu}: {toggleMenu: () => void}) => {
                     {isTutorOrAbove(user) && <>
                         <div className={above["sm"](deviceSize) ? "section-divider-y" : "section-divider"}/>
                         <div className="flex-grow-1">
-                            <div className="pt-2 pt-sm-0 h5">{isTutor(user) ? "TUTOR" : "TEACHER"}</div>
+                            <div className="nav-list-title pt-2 pt-sm-0">{isTutor(user) ? "TUTOR" : "TEACHER"}</div>
                             <ul className="plain-list">
                                 {isTutor(user)
                                     ? <NavigationItemClose href="/tutor_features">
