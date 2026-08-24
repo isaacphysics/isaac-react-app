dd0534bb2356d742f8f3d16c215ad7dfca16dc5e
Yoan-Daniel Malinov <yoan.malinov@raspberrypi.org>
2026-08-24 17:16:50 +0100

    Add second ErrorBoundary for main content errors

--- src/app/components/navigation/IsaacApp.tsx
+++ src/app/components/navigation/IsaacApp.tsx
@@ -9,7 +9,7 @@ import {
     useAppDispatch,
     useAppSelector,
 } from "../../state";
-import {createBrowserRouter, createRoutesFromElements, Navigate, Outlet, Route, RouterProvider} from "react-router-dom";
+import {createBrowserRouter, createRoutesFromElements, Navigate, Outlet, Route, RouterProvider, useLocation} from "react-router-dom";
 import {Question} from "../pages/Question";
 import {Concept} from "../pages/Concept";
 import {Contact} from "../pages/Contact";
@@ -85,7 +85,10 @@ const GameboardBuilder = lazy(() => import('../pages/GameboardBuilder'));
 
 const RootLayout = () => {
     const mainContentRef = useRef(null);
+    const location = useLocation();
 
+    // Needs two ErrorBoundaries, since the outer one would trigger on any error, and the footer/header would not be rendered
+    // Inner one also gets reset on route change, as if you had an error, and clicked on a link in the footer, it would not reset the error state
     return <ErrorBoundary FallbackComponent={ChunkOrClientError}>
         <FeatureFlagProvider>
             <FigureNumberingProvider>
@@ -95,9 +98,11 @@ const RootLayout = () => {
                 <SiteBanners />
                 <OnPageLoad />
                 <main ref={mainContentRef} id="main" data-testid="main" role="main" className="flex-fill content-body">
-                    <Suspense fallback={<Loading/>}>
-                        <Outlet />
-                    </Suspense>
+                    <ErrorBoundary FallbackComponent={ChunkOrClientError} resetKeys={[location.pathname, location.search]}>
+                        <Suspense fallback={<Loading/>}>
+                            <Outlet />
+                        </Suspense>
+                    </ErrorBoundary>
                 </main>
                 <ScrollToTop mainContent={mainContentRef}/>
                 <SiteSpecific.Footer />
