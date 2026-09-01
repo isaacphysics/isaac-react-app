a09e921ae9cbfb395d25b8c499c25b95f8024910
Sol Dubock <94075844+sjd210@users.noreply.github.com>
2026-09-01 17:18:55 +0100

    Use more consistent markdown rendering rule for #s

--- src/app/components/elements/markup/markdownRendering.ts
+++ src/app/components/elements/markup/markdownRendering.ts
@@ -14,6 +14,17 @@ MARKDOWN_RENDERER.renderer.rules.link_open = function(tokens: Remarkable.LinkOpe
         return `<a class="a-link" href="${href}" ${title} target="_blank" rel="noopener nofollow">`;
     }
 };
+
+// "#" headings used for styling that do not match the structure of the page should not use <h*> tags.
+// Headings that do follow this structure should instead be written as <h*> tags in the content directly.
+// https://www.w3.org/WAI/tutorials/page-structure/headings
+MARKDOWN_RENDERER.renderer.rules.heading_open = function(tokens: Remarkable.HeadingOpenToken[], idx: number) {
+    return `<div class="h${tokens[idx].hLevel}">`;
+};
+MARKDOWN_RENDERER.renderer.rules.heading_close = function() {
+    return `</div>\n`;
+};
+
 export const renderRemarkableMarkdown = (markdown: string) => MARKDOWN_RENDERER.render(markdown);
 
 // This is used to match and render cloze question drop zones into span elements
@@ -92,15 +103,6 @@ export const regexProcessMarkdown = (pageContext?: PageContextState) => (markdow
     const regexRules = {
         // RegEx replacement to match Latex inspired Isaac Physics functionality
         "[$1]($2)": /\\link{([^}]*)}{([^}]*)}/g,
-        // "#" headings used for styling that do not match the structure of the page should not use <h*> tags
-        // headings that do follow this structure should instead be written as <h*> tags in the content directly
-        // https://www.w3.org/WAI/tutorials/page-structure/headings
-        "<div class=\"h1\">$1</div>": /^(?<!#)#\s+(.+)$/gm,
-        "<div class=\"h2\">$1</div>": /^(?<!#)##\s+(.+)$/gm,
-        "<div class=\"h3\">$1</div>": /^(?<!#)###\s+(.+)$/gm,
-        "<div class=\"h4\">$1</div>": /^(?<!#)####\s+(.+)$/gm,
-        "<div class=\"h5\">$1</div>": /^(?<!#)#####\s+(.+)$/gm,
-        "<div class=\"h6\">$1</div>": /^######\s+(.+)$/gm,
     };
     if (isPhy) {
         if (pageContext?.subject && !isSingleStageContext(pageContext)) {
