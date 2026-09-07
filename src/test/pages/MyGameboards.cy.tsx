dd794baa701569a4616b6069e2f370f1db61bdca
Jaycie Brown <jeab2@cam.ac.uk>
2026-09-07 16:47:43 +0100

    Add step debugging to now-consistently failing GBs VRT

--- src/test/pages/MyGameboards.cy.tsx
+++ src/test/pages/MyGameboards.cy.tsx
@@ -13,12 +13,19 @@ describe("My Gameboards", () => {
         cy.matchImage();
     });
     it('should have no visual regressions in card view', () => {
+        console.log("step 1");
         cy.mountWithStoreAndRouter(<MyGameboards user={mockUser} />, [PATHS.MY_GAMEBOARDS], PATHS.MY_GAMEBOARDS, mockUser);
+        console.log("step 2");
         if (isPhy) cy.openSidebar();
+        console.log("step 3");
         cy.get('[data-testid="display-select"]').select("Card View");
+        console.log("step 4");
         cy.get('[data-testid="limit-select"]').should('be.visible').select("6");
+        console.log("step 5");
         if (isPhy) cy.closeSidebar();
+        console.log("step 6");
         cy.get('[data-testid="loading"]').should('not.exist');
+        console.log("step 7");
         cy.matchImage();
     });
 });
