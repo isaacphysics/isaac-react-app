b2dcdc76cbfeec6b027f1bf4eb70252075f45c8d
Jaycie Brown <jeab2@cam.ac.uk>
2026-09-07 17:26:05 +0100

    Replace non-queued JS with enqueuing `cy` command

--- src/test/pages/MyGameboards.cy.tsx
+++ src/test/pages/MyGameboards.cy.tsx
@@ -6,26 +6,19 @@ import {MyGameboards} from "../../app/components/pages/MyGameboards";
 describe("My Gameboards", () => {
     it('should have no visual regressions in table view', () => {
         cy.mountWithStoreAndRouter(<MyGameboards user={mockUser} />, [PATHS.MY_GAMEBOARDS], PATHS.MY_GAMEBOARDS, mockUser);
-        if (isPhy) cy.openSidebar();
+        cy.then(() => {if (isPhy) cy.openSidebar();});
         cy.get('[data-testid="display-select"]').select("Table View");
-        if (isPhy) cy.closeSidebar();
+        cy.then(() => {if (isPhy) cy.closeSidebar();});
         cy.get('[data-testid="loading"]').should('not.exist');
         cy.matchImage();
     });
     it('should have no visual regressions in card view', () => {
-        cy.log("step 1");
         cy.mountWithStoreAndRouter(<MyGameboards user={mockUser} />, [PATHS.MY_GAMEBOARDS], PATHS.MY_GAMEBOARDS, mockUser);
-        cy.log("step 2");
-        if (isPhy) cy.openSidebar();
-        cy.log("step 3");
+        cy.then(() => {if (isPhy) cy.openSidebar();});
         cy.get('[data-testid="display-select"]').select("Card View");
-        cy.log("step 4");
         cy.get('[data-testid="limit-select"]').should('be.visible').select("6");
-        cy.log("step 5");
-        if (isPhy) cy.closeSidebar();
-        cy.log("step 6");
+        cy.then(() => {if (isPhy) cy.closeSidebar();});
         cy.get('[data-testid="loading"]').should('not.exist');
-        cy.log("step 7");
         cy.matchImage();
     });
 });
