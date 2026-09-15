e3abdac1f34e179ae1e7b235d820fa4392cfcd9c
Barna Magyarkuti <bm710@cam.ac.uk>
2026-09-15 14:45:41 +0100

    remove semi that got displayed by page

--- src/app/components/pages/AllTopics.tsx
+++ src/app/components/pages/AllTopics.tsx
@@ -180,6 +180,6 @@ export const AllTopics = () => {
                 ["11 to 14 years"]: <TopicsListing tagCols={partition(ks4Tags, s => !s.comingSoonDate || ks4Tags.indexOf(s) <= 4)} age="11-14" />,
                 ["14 to 19 years"]: <TopicsListing tagCols={partition(coreAdvancedTags, s => s.title.charAt(0) <= "D")} age="14-19" />
             }}
-        </Tabs>;
+        </Tabs>
     </PageContainer>;
 };
