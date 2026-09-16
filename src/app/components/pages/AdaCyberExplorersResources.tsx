91c30439b0e3d7db34ccc63ec1a1096e64583e50
Jaycie Brown <jeab2@cam.ac.uk>
2026-09-16 09:36:43 +0100

    Hide TechFirst RDPs link temporarily

--- src/app/components/pages/AdaCyberExplorersResources.tsx
+++ src/app/components/pages/AdaCyberExplorersResources.tsx
@@ -165,7 +165,7 @@ export const AdaCyberExplorersResources = () => {
                         <p>
                             Schools can also work towards TechFirst School recognition, with Bronze, Silver and Gold awards available. From September 2026, TechFirst will operate in every region and home nation of the UK.
                         </p>
-                        <Button outline className="bg-white" href="/pages/techfirst_rdps" target="_blank">Find out more</Button>
+                        {/* <Button outline className="bg-white" href="/pages/techfirst_rdps" target="_blank">Find out more</Button> */}
                     </TextBlock>
                     <ImageBlock>
                         <img className="px-0 px-sm-3 px-md-0 px-lg-2 px-xl-4" src="/assets/cs/decor/regional-partners.png" alt="Stylised map of the UK showing example regional partner locations."/>
